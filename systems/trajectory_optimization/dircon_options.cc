3891fdd1ca1920871f82aaad05a9bb1b804c1271
mposa <michael.a.posa@gmail.com>
2018-03-26 18:05:47 -0400

    Added options for force costs and different start/end constraints.

--- systems/trajectory_optimization/dircon_options.cc
+++ systems/trajectory_optimization/dircon_options.cc
@@ -3,17 +3,18 @@
 using std::vector;
 
 namespace drake{
+namespace systems{
+namespace trajectory_optimization{
+
 DirconOptions::DirconOptions(int n_constraints) {
   n_constraints_ = n_constraints;
   is_constraints_relative_ = vector<bool>(n_constraints_);
   for (int i=0; i < n_constraints_; i++) {
     is_constraints_relative_[i] = false;
   }
-  bool constrain_start_ = true;
-  bool constrain_end_ = true;
-  bool constrain_phi_start_ = true;
-  bool constrain_phi_end_ = true;
-  accel_cost_ = 0;
+  start_constraint_type_ = DirconKinConstraintType::kAll;
+  end_constraint_type_ = DirconKinConstraintType::kAll;
+  force_cost_ = 1.0e-6;
 }
 
 void DirconOptions::setAllConstraintsRelative(bool relative) {
@@ -26,24 +27,16 @@ void DirconOptions::setConstraintRelative(int index, bool relative) {
   is_constraints_relative_[index]  = relative;
 }
 
-void DirconOptions::setConstraintStart(bool constrain_start) {
-  constrain_start_ = constrain_start;
+void DirconOptions::setStartType(DirconKinConstraintType type) {
+  start_constraint_type_ = type;
 }
 
-void DirconOptions::setConstraintEnd(bool constrain_end) {
-  constrain_end_ = constrain_end;
+void DirconOptions::setEndType(DirconKinConstraintType type) {
+  end_constraint_type_ = type;
 }
 
-void DirconOptions::setConstraintPhiStart(bool constrain_phi_start) {
-  constrain_phi_start_ = constrain_phi_start;
-}
-
-void DirconOptions::setConstraintPhiEnd(bool constrain_phi_end) {
-  constrain_phi_end_ = constrain_phi_end;
-}
-
-void DirconOptions::setAccelCost(double accel_cost) {
-  accel_cost_ = accel_cost;
+void DirconOptions::setForceCost(double force_cost) {
+  force_cost_ = force_cost;
 }
 
 int DirconOptions::getNumConstraints() {
@@ -54,23 +47,18 @@ bool DirconOptions::getSingleConstraintRelative(int index) {
   return is_constraints_relative_[index];
 }
 
-bool DirconOptions::getConstrainStart() {
-  return constrain_start_;
+DirconKinConstraintType DirconOptions::getStartType() {
+  return start_constraint_type_;
 }
 
-bool DirconOptions::getConstrainEnd() {
-  return constrain_end_;
+DirconKinConstraintType DirconOptions::getEndType() {
+  return end_constraint_type_;
 }
 
-bool DirconOptions::getConstrainPhiStart() {
-  return constrain_phi_start_;
+double DirconOptions::getForceCost() {
+  return force_cost_;
 }
 
-bool DirconOptions::getConstrainPhiEnd() {
-  return constrain_phi_end_;
-
 }
-double DirconOptions::getAccelCost() {
-  return accel_cost_;
 }
 }
--- systems/trajectory_optimization/dircon_options.h
+++ systems/trajectory_optimization/dircon_options.h
@@ -1,35 +1,36 @@
 #pragma once
 
 #include <vector>
+#include "dircon_opt_constraints.h"
 
 namespace drake{
+namespace systems{
+namespace trajectory_optimization{
+
 class DirconOptions {
   private:
     int n_constraints_;
     std::vector<bool> is_constraints_relative_;
-    bool constrain_start_;
-    bool constrain_end_;
-    bool constrain_phi_start_;
-    bool constrain_phi_end_;
-    double accel_cost_;  
+    DirconKinConstraintType start_constraint_type_;
+    DirconKinConstraintType end_constraint_type_;
+    double force_cost_;
 
   public:
     DirconOptions(int n_constraints);
 
     void setAllConstraintsRelative(bool relative);
     void setConstraintRelative(int index, bool relative);
-    void setConstraintStart(bool constrain_start);
-    void setConstraintEnd(bool constrain_end);
-    void setConstraintPhiStart(bool constrain_phi_start);
-    void setConstraintPhiEnd(bool constrain_phi_end);
-    void setAccelCost(double accel_cost);
+    void setStartType(DirconKinConstraintType type);
+    void setEndType(DirconKinConstraintType type);
+    void setForceCost(double force_cost);
 
     int getNumConstraints();
     bool getSingleConstraintRelative(int index);
-    bool getConstrainStart();
-    bool getConstrainEnd();
-    bool getConstrainPhiStart();
-    bool getConstrainPhiEnd();
-    double getAccelCost();
+    DirconKinConstraintType getStartType();
+    DirconKinConstraintType getEndType();
+    double getForceCost();
 };
+
+}
+}
 }

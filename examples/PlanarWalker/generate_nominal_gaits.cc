c0ef49648e6b593115316bf5f6e18bc7a1208ffb
mposa <michael.a.posa@gmail.com>
2018-05-16 22:36:29 -0400

    Added nominal gaits.

---
+++ examples/PlanarWalker/generate_nominal_gaits.cc
@@ -0,0 +1,70 @@
+#include <memory>
+#include <chrono>
+#include <random>
+#include <gflags/gflags.h>
+
+
+#include "drake/solvers/mosek_solver.h"
+#include "drake/solvers/gurobi_solver.h"
+#include "drake/solvers/mathematical_program.h"
+
+#include "sgd_iter.h"
+#include "src/manifold_constraint.h"
+#include "src/file_utils.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::string;
+
+using drake::goldilocks_walking::writeCSV;
+using drake::goldilocks_walking::readCSV;
+using drake::solvers::VectorXDecisionVariable;
+
+namespace drake{
+namespace goldilocks_walking {
+
+void generateNominalGaits() {
+  std::random_device randgen;
+  std::default_random_engine e1(randgen());
+  std::uniform_real_distribution<> dist(0, 1);
+
+
+  int n_batch = 5;
+
+  // int n_weights = 43;
+  int n_weights =  16;
+  VectorXd theta_0 = VectorXd::Zero(n_weights);
+  theta_0(0) = -0.1;
+  theta_0(5) = 1.0;
+  writeCSV("data/0_theta.csv", theta_0);
+
+  double length = 0.5;
+  double duration = 1;
+  int snopt_iter = 200;
+  string directory = "data/";
+  string init_z = "z_save.csv";
+  string weights = "0_theta.csv";
+  string output_prefix;
+  VectorXd lengths(5);
+  lengths << .5, .4, .3, .2, .1;
+
+  double speed = 0.5;
+
+  for (int i = 0; i < lengths.size(); i++) {
+    length = lengths(i);
+    // duration = length/speed;
+    duration = 1;
+    output_prefix = "init_length_" + std::to_string(i) + "_speed_" + std::to_string(0) + "_";
+    sgdIter(length, duration, snopt_iter, directory, init_z, weights, output_prefix);
+    init_z = output_prefix + "z.csv";
+  }
+}
+}
+}
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::srand(time(0));  // Initialize random number generator.
+
+  drake::goldilocks_walking::generateNominalGaits();
+}

6f1ebaf23d6f4c5dd10b85009496d6baae9be836
mposa <michael.a.posa@gmail.com>
2018-06-08 16:28:29 -0400

    Working commit.

---
+++ examples/Cassie/bkp_controller_lcm.h
@@ -0,0 +1,67 @@
+
+namespace drake {
+using dairlib::systems::TimestampedVector;
+using Eigen::VectorXd;
+using systems::LeafSystem;
+using systems::Context;
+
+
+/// Receives the output of a controller controller, and outputs it as an LCM
+/// message with type lcm_cassie_u. Its output port is usually connected to
+/// an LcmPublisherSystem to publish the messages it generates.
+class CassieCommandSender : public systems::LeafSystem<double> {
+ public:
+  CassieCommandSender();
+
+ private:
+  void OutputCommand(const systems::Context<double>& context,
+                     dairlib::lcmt_cassie_input* output) const;
+  const RigidBodyTree<double>* tree_;
+};
+
+/// Implementation of TimestampedVector to store, set, and get PD configuration:
+/// desired position/velocity and gains (kp/kd)
+class CassiePDConfig : public TimestampedVector<double> {
+  public:
+    CassiePDConfig(int num_joints);
+
+    //Getters and setters
+    VectorXd getDesiredPosition() const {return desired_position_;};
+    VectorXd getDesiredVelocity() const {return desired_velocity_;};
+    VectorXd getKp() const {return kp_;};
+    VectorXd getKd() const {return kd_;};
+    double getDesiredPosition(int index) const {return desired_position_(index);};
+    double getDesiredVelocity(int index) const {return desired_velocity_(index);};
+    double getKp(int index) const {return kp_(index);};
+    double getKd(int index) const {return kd_(index);};
+    void setDesiredPosition(VectorXd desired_position);
+    void setDesiredVelocity(VectorXd desired_velocity);
+    void setKp(VectorXd kp);
+    void setKd(VectorXd kd);
+    void setDesiredPosition(int index, double position);
+    void setDesiredVelocity(int index, double velocity);
+    void setKp(int index, double kp);
+    void setKd(int index, double kd);
+  private:
+    CassiePDConfig* DoClone() const override;
+
+    VectorXd desired_position_;
+    VectorXd desired_velocity_;
+    VectorXd kp_;
+    VectorXd kd_;
+    int num_joints_;
+};
+
+/// Receives the output of an LcmSubsriberSystem that subsribes to the
+/// Cassie PD configuration channel with LCM type lcmt_cassie_pd_config, 
+/// and outputs the CassiePDConfig as Context
+class CassiePDConfigReceiver : public systems::LeafSystem<double> {
+ public:
+  CassiePDConfigReceiver();
+
+
+ private:
+  void CopyConfig(const systems::Context<double>& context,
+                    CassiePDConfig* output) const;
+};
+}
--- examples/Cassie/cassie_controller_lcm.cc
+++ examples/Cassie/cassie_controller_lcm.cc
@@ -1,213 +1,144 @@
 #include "datatypes/cassie_names.h"
-#include "multibody/rbt_utils.h"
 #include "cassie_controller_lcm.h"
 
 namespace dairlib{
 
 using std::string;
 using drake::systems::Context;
-using dairlib::systems::TimestampedVector;
+using systems::StateVector;
 using Eigen::VectorXd;
-
-const int CASSIE_NUM_COMMANDS = cassieJointNames.size();
-const int CASSIE_NUM_JOINTS = cassieJointNames.size();
-
-/// Given a string name, finds the index related to that joint
-/// looks into cassieJointNames list
-/// This is a slow implementation and should be fixed. It should also probably
-/// be move to the cassie_names header itself
-int getJointIndex(string joint) {
-  //should probably use std::find
-  for (int i = 0; i < CASSIE_NUM_JOINTS; i++) {
-    if (cassieJointNames[i].compare(joint) == 0) {
-      return i;
-    }
-  }
-  return -1;
-}
+using drake::systems::LeafSystem;
 
 /*--------------------------------------------------------------------------*/
 // methods implementation for CassieStateReceiver.
 CassieStateReceiver::CassieStateReceiver(RigidBodyTree<double>& tree) {
   tree_ = &tree;
-  positionIndexMap_ = multibody::makeNameToPositionsMap(tree);
-  velocityIndexMap_ = multibody::makeNameToVelocitiesMap(tree);
+  positionIndexMap_ = multibody::utils::makeNameToPositionsMap(tree);
+  velocityIndexMap_ = multibody::utils::makeNameToVelocitiesMap(tree);
   this->DeclareAbstractInputPort();
   this->DeclareVectorOutputPort(StateVector<double>(
-    tree.get_num_positions() + tree.get_num_velocities()),
-   &CassieStateReceiver::CopyStateOut);
+    tree.get_num_positions(), tree.get_num_velocities()),
+    &CassieStateReceiver::CopyStateOut);
 }
 
 void CassieStateReceiver::CopyStateOut(
     const Context<double>& context, StateVector<double>* output) const {
-  const systems::AbstractValue* input = this->EvalAbstractInput(context, 0);
+  const drake::systems::AbstractValue* input = this->EvalAbstractInput(context, 0);
   DRAKE_ASSERT(input != nullptr);
-  const auto& state_msg = input->GetValue<dairlib::lcmt_cassie_state>();
+  const auto& state_msg = input->GetValue<dairlib::lcmt_robot_output>();
 
-  VectorXd positions = VectorXd::Zero(tree_.get_num_positions());
+  VectorXd positions = VectorXd::Zero(tree_->get_num_positions());
   for (int i = 0; i < state_msg.num_positions; i++) {
-    state(positionIndexMap_[state_msg.joint_names[i]]) = state_msg.position[i];
+    int j = positionIndexMap_.at(state_msg.position_names[i]);
+    positions(j) = state_msg.position[i];
   }
-  VectorXd velocities = VectorXd::Zero(tree_.get_num_velocities());
+  VectorXd velocities = VectorXd::Zero(tree_->get_num_velocities());
   for (int i = 0; i < state_msg.num_velocities; i++) {
-    state(velocityIndexMap_[state_msg.joint_names[i]]) = state_msg.velocity[i];
+    int j = velocityIndexMap_.at(state_msg.velocity_names[i]);
+    velocities(j) = state_msg.velocity[i];
   }
-  //todo
-  output->SetDataVector(state);
+  output->SetPositions(positions);
+  output->SetVelocities(velocities);
   output->set_timestamp(state_msg.timestamp);
 }
+
+
+/*--------------------------------------------------------------------------*/
+// methods implementation for CassieStateSender.
+
+CassieStateSender::CassieStateSender(RigidBodyTree<double>& tree) {
+  tree_ = &tree;
+  positionIndexMap_ = multibody::utils::makeNameToPositionsMap(tree);
+  velocityIndexMap_ = multibody::utils::makeNameToVelocitiesMap(tree);
+
+  this->DeclareVectorInputPort(StateVector<double>(tree.get_num_positions(),
+                                                   tree.get_num_velocities()));
+  this->DeclareAbstractOutputPort(&CassieStateSender::OutputState);
 }
 
-namespace drake {
+/// Populate a state message with all states
+void CassieStateSender::OutputState(const Context<double>& context,
+                                         dairlib::lcmt_robot_output* state_msg) const {
+  const StateVector<double>* state =
+      (StateVector<double>*) this->EvalVectorInput(context, 0);
+
+  //using the time from the context
+  state_msg->timestamp = context.get_time()*1e6;
+
+  int nq = tree_->get_num_positions();
+  int nv = tree_->get_num_positions();
+
+  state_msg->num_positions = nq;
+  state_msg->num_velocities = nv;
+  state_msg->position_names.resize(nq);
+  state_msg->velocity_names.resize(nv);
+  state_msg->position.resize(nq);
+  state_msg->velocity.resize(nv);
+
+  for (int i = 0; i < nq; i++) {
+    state_msg->position[i] = state->GetPositionAtIndex(i);
+    state_msg->position_names[i] = tree_->get_position_name(i);
+  }
+  for (int i = 0; i < nv; i++) {
+    state_msg->velocity[i] = state->GetVelocityAtIndex(i);
+    state_msg->velocity_names[i] = tree_->get_velocity_name(i);
+  }
+}
 
 /*--------------------------------------------------------------------------*/
 // methods implementation for CassieInputReceiver.
-CassieInputReceiver::CassieInputReceiver() {
+CassieInputReceiver::CassieInputReceiver(RigidBodyTree<double>& tree) {
+  tree_ = &tree;
+  actuatorIndexMap_ = multibody::utils::makeNameToActuatorsMap(tree);
+
   this->DeclareAbstractInputPort();
-  this->DeclareVectorOutputPort(TimestampedVector<double>(CASSIE_NUM_JOINTS),
-   &CassieInputReceiver::CopyInputOut);
+  this->DeclareVectorOutputPort(TimestampedVector<double>(
+      tree.get_num_actuators()), &CassieInputReceiver::CopyInputOut);
 }
 
 void CassieInputReceiver::CopyInputOut(
-    const Context<double>& context, TimestampedVector<double>* output) const {  
-  const systems::AbstractValue* input = this->EvalAbstractInput(context, 0);
+    const Context<double>& context, TimestampedVector<double>* output) const {
+  const drake::systems::AbstractValue* input = this->EvalAbstractInput(context, 0);
   DRAKE_ASSERT(input != nullptr);
-  const auto& input_msg = input->GetValue<dairlib::lcmt_cassie_input>();
+  const auto& input_msg = input->GetValue<dairlib::lcmt_robot_input>();
 
-  VectorXd input_vector = VectorXd::Zero(CASSIE_NUM_JOINTS);
-  for (int i = 0; i < input_msg.num_joints; i++) {
-    int j = getJointIndex(input_msg.joint_names[i]);
+  VectorXd input_vector = VectorXd::Zero(tree_->get_num_actuators());
+
+  for (int i = 0; i < input_msg.num_inputs; i++) {
+    int j = actuatorIndexMap_.at(input_msg.input_names[i]);
     input_vector(j) = input_msg.inputs[i];
   }
   output->SetDataVector(input_vector);
   output->set_timestamp(input_msg.timestamp);
 }
 
+
 /*--------------------------------------------------------------------------*/
 // methods implementation for CassieCommandSender.
 
-CassieCommandSender::CassieCommandSender() {
-  this->DeclareVectorInputPort(TimestampedVector<double>(CASSIE_NUM_JOINTS));
+CassieCommandSender::CassieCommandSender(RigidBodyTree<double>& tree) {
+  tree_ = &tree;
+  actuatorIndexMap_ = multibody::utils::makeNameToActuatorsMap(tree);
+
+  this->DeclareVectorInputPort(TimestampedVector<double>(tree.get_num_actuators()));
   this->DeclareAbstractOutputPort(&CassieCommandSender::OutputCommand);
 }
 
 void CassieCommandSender::OutputCommand(const Context<double>& context,
-                                         dairlib::lcmt_cassie_input* input_msg) const {
+                                         dairlib::lcmt_robot_input* input_msg) const {
   const TimestampedVector<double>* command = (TimestampedVector<double>*)
       this->EvalVectorInput(context, 0);
 
+  int nu = tree_->get_num_actuators();
+
   input_msg->timestamp = command->get_timestamp(); //context.get_time()*1e6;
-  input_msg->num_joints = CASSIE_NUM_JOINTS;
-  input_msg->joint_names.resize(CASSIE_NUM_JOINTS);
-  input_msg->inputs.resize(CASSIE_NUM_JOINTS);
-  for (int i = 0; i < CASSIE_NUM_JOINTS; i++) {
-    input_msg->joint_names[i] = cassieJointNames[i];
+  input_msg->num_inputs = nu;
+  input_msg->input_names.resize(nu);
+  input_msg->inputs.resize(nu);
+  for (int i = 0; i < nu; i++) {
+    input_msg->input_names[i] = tree_->actuators[i].name_;
     input_msg->inputs[i] = command->GetAtIndex(i);
   }
-}
-
-/*--------------------------------------------------------------------------*/
-// methods implementation for CassieStateSender.
-
-CassieStateSender::CassieStateSender() {
-  this->DeclareInputPort(systems::kVectorValued, 2*(CASSIE_NUM_JOINTS));
-  this->DeclareAbstractOutputPort(&CassieStateSender::OutputState);
-}
-
-/// Populate a state message with all states
-/// Assumes: number of positions == number of velocities
-///          state is a vector ordered [positions; velocities]
-void CassieStateSender::OutputState(const Context<double>& context,
-                                         dairlib::lcmt_cassie_state* state_msg) const {
-  auto state =
-      this->EvalVectorInput(context, 0);
-
-  //using the time from the context
-  state_msg->timestamp = context.get_time()*1e6;
-
-  state_msg->num_joints = CASSIE_NUM_JOINTS;
-  state_msg->joint_names.resize(CASSIE_NUM_JOINTS);
-  state_msg->position.resize(CASSIE_NUM_JOINTS);
-  state_msg->velocity.resize(CASSIE_NUM_JOINTS);
-  for (int i = 0; i < CASSIE_NUM_JOINTS; i++) {
-    state_msg->position[i] = state->GetAtIndex(i);
-    state_msg->velocity[i] = state->GetAtIndex(i + CASSIE_NUM_JOINTS);
-    state_msg->joint_names[i] = cassieJointNames[i];
-  }
-}
-
-
-// methods implementation for CassiePDConfigReceiver.
-CassiePDConfigReceiver::CassiePDConfigReceiver() {
-  this->DeclareAbstractInputPort();
-  this->DeclareVectorOutputPort(CassiePDConfig(CASSIE_NUM_JOINTS),
-                                &CassiePDConfigReceiver::CopyConfig);
-}
-
-/// read and parse a configuration LCM message
-void CassiePDConfigReceiver::CopyConfig(
-    const Context<double>& context, CassiePDConfig* output) const {
-  const systems::AbstractValue* input = this->EvalAbstractInput(context, 0);
-  DRAKE_ASSERT(input != nullptr);
-  const auto& config_msg = input->GetValue<dairlib::lcmt_cassie_pd_config>();
-
-  for (int i = 0; i < config_msg.num_joints; i++) {
-    int j = getJointIndex(config_msg.joint_names[i]);
-    output->setDesiredPosition(j, config_msg.desired_position[i]);
-    output->setDesiredVelocity(j, config_msg.desired_velocity[i]);
-    output->setKp(j, config_msg.kp[i]);
-    output->setKd(j, config_msg.kd[i]);
-  }
-}
-
-CassiePDConfig::CassiePDConfig(int num_joints) : TimestampedVector<double>(4*num_joints) {
-  num_joints_ = num_joints;
-  desired_position_ = VectorXd::Zero(num_joints);
-  desired_velocity_ = VectorXd::Zero(num_joints);
-  kp_ = VectorXd::Zero(num_joints);
-  kd_ = VectorXd::Zero(num_joints);
-}
-
-void CassiePDConfig::setDesiredPosition(VectorXd desired_position) {
-  desired_position_ = desired_position;
-}
-
-void CassiePDConfig::setDesiredVelocity(VectorXd desired_velocity) {
-  desired_velocity_ = desired_velocity;
-}
-
-void CassiePDConfig::setKp(VectorXd kp) {
-  kp_ = kp;
-}
-
-void CassiePDConfig::setKd(VectorXd kd) {
-  kd_ = kd;
-}
-
-void CassiePDConfig::setDesiredPosition(int index, double position) {
-  desired_position_(index) = position;
-}
-void CassiePDConfig::setDesiredVelocity(int index, double velocity) {
-  desired_velocity_(index) = velocity;
-}
-void CassiePDConfig::setKp(int index, double kp) {
-  kp_(index) = kp;
-}
-void CassiePDConfig::setKd(int index, double kd) {
-  kd_(index) = kd;
-}
-
-/// Clone a configuration
-/// This function must be implmemented for Drake
-CassiePDConfig* CassiePDConfig::DoClone() const {
-  auto config = new CassiePDConfig(num_joints_);
-
-  config-> setDesiredPosition(getDesiredPosition());
-  config-> setDesiredVelocity(getDesiredVelocity());
-  config-> setKp(getKp());
-  config-> setKd(getKd());
-
-  return config;
 }
 
 

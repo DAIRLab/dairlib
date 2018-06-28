62c16c9b1a6541520820c99f932b20fb7cf93668
specas <shrenik95@gmail.com>
2018-06-28 14:48:28 -0400

    Moved constraint solving to multibody

--- examples/Cassie/cassie_utils.cc
+++ examples/Cassie/cassie_utils.cc
@@ -1,8 +1,11 @@
-#include "examples/Cassie/cassie_utils.h"
-#include "common/find_resource.h"
 #include "drake/solvers/mathematical_program.h"
 
-namespace dairlib {
+#include "examples/Cassie/cassie_utils.h"
+#include "common/find_resource.h"
+
+namespace dairlib
+{
+
 using Eigen::VectorXd;
 using Eigen::Vector3d;
 using drake::solvers::Constraint;
@@ -49,45 +52,6 @@ void buildFixedBaseCassieTree(RigidBodyTree<double>& tree,
                            achilles_length);
 }
 
-VectorXd solvePositionConstraints(const RigidBodyTree<double>& tree,
-                                  VectorXd q_init,
-                                  std::vector<int> fixed_joints) {
-  MathematicalProgram prog;
-  auto q = prog.NewContinuousVariables(tree.get_num_positions(), "q");
-  auto constraint = std::make_shared<TreePositionConstraint>(tree);
-  prog.AddConstraint(constraint, q);
-  for (uint i = 0; i < fixed_joints.size(); i++) {
-    int j = fixed_joints[i];
-    prog.AddConstraint(q(j) == q_init(j));
-  }
-  prog.AddQuadraticCost((q - q_init).dot(q - q_init));
-  prog.SetInitialGuessForAllVariables(q_init);
-  prog.Solve();
-  return prog.GetSolution(q);
-}
 
-TreePositionConstraint::TreePositionConstraint(
-    const RigidBodyTree<double>& tree, const std::string& description) :
-    Constraint(tree.getNumPositionConstraints(),
-               tree.get_num_positions(),
-               VectorXd::Zero(tree.getNumPositionConstraints()),
-               VectorXd::Zero(tree.getNumPositionConstraints()),
-               description) {
-  tree_ = &tree;
-}
-
-void TreePositionConstraint::DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
-                                    Eigen::VectorXd& y) const {
-  AutoDiffVecXd y_t;
-  Eval(drake::math::initializeAutoDiff(x), y_t);
-  y = drake::math::autoDiffToValueMatrix(y_t);
-}
-
-void TreePositionConstraint::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
-                                    AutoDiffVecXd& y) const {
-  const AutoDiffVecXd q = x.head(tree_->get_num_positions());
-  KinematicsCache<drake::AutoDiffXd> cache = tree_->doKinematics(q);
-  y = tree_->positionConstraints(cache);
-}
 
 }

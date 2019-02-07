27583d259f692f42ad7145b1fb00f793db9987e7
shrenikm <shrenik95@gmail.com>
2019-02-06 21:48:39 -0500

    Added position constraint and build file.
    * Added RBT position constraint class.
    * Added the build file for multibody_solvers.

--- multibody/multibody_solvers.cc
+++ multibody/multibody_solvers.cc
@@ -3,8 +3,10 @@
 namespace dairlib {
 namespace multibody {
 
+using std::logic_error;
 using std::string;
 
+using drake::AutoDiffXd;
 using drake::AutoDiffVecXd;
 using drake::math::autoDiffToValueMatrix;
 using drake::math::autoDiffToGradientMatrix;
@@ -16,24 +18,51 @@ using drake::symbolic::Expression;
 using drake::VectorX;
 using Eigen::VectorXd;
 
-FixedPointConstraint::FixedPointConstraint(const RigidBodyTree<double>& tree,
-                                           const int num_forces,
-                                           const string& description)
-    : Constraint(
-          tree.get_num_velocities(),
-          tree.get_num_positions() + tree.get_num_actuators() + num_forces,
-          VectorXd::Zero(tree.get_num_velocities()),
-          VectorXd::Zero(tree.get_num_velocities()), description),
-      tree_(tree),
-      num_forces_(num_forces) {}
+PositionConstraint::PositionConstraint(const RigidBodyTree<double>& tree,
+                                       const string& description)
+    : Constraint(tree.getNumPositionConstraints(), tree.get_num_positions(),
+                 VectorXd::Zero(tree.getNumPositionConstraints()),
+                 VectorXd::Zero(tree.getNumPositionConstraints()), description),
+      tree_(tree) {}
 
-void FixedPointConstraint::DoEval(
-    const Eigen::Ref<const Eigen::VectorXd>& q_u_l,
-    Eigen::VectorXd* y) const {
+void PositionConstraint::DoEval(const Eigen::Ref<const Eigen::VectorXd>& q,
+                                Eigen::VectorXd* y) const {
   AutoDiffVecXd y_t;
-  Eval(initializeAutoDiff(q_u_l), &y_t);
+  Eval(drake::math::initializeAutoDiff(q), &y_t);
   *y = autoDiffToValueMatrix(y_t);
 }
+
+void PositionConstraint::DoEval(const Eigen::Ref<const drake::AutoDiffVecXd>& q,
+                                drake::AutoDiffVecXd* y) const {
+  const AutoDiffVecXd q_t = q.head(tree_.get_num_positions());
+  KinematicsCache<AutoDiffXd> k_cache = tree_.doKinematics(q_t);
+  *y = tree_.positionConstraints(k_cache);
+}
+
+void PositionConstraint::DoEval(
+    const Eigen::Ref<const drake::VectorX<drake::symbolic::Variable>>& x,
+    drake::VectorX<drake::symbolic::Expression>* y) const {
+  throw logic_error("PositionConstraint does not support symbolic evaluation.");
+}
+
+// FixedPointConstraint::FixedPointConstraint(const RigidBodyTree<double>& tree,
+//                                           const int num_forces,
+//                                           const string& description)
+//    : Constraint(
+//          tree.get_num_velocities(),
+//          tree.get_num_positions() + tree.get_num_actuators() + num_forces,
+//          VectorXd::Zero(tree.get_num_velocities()),
+//          VectorXd::Zero(tree.get_num_velocities()), description),
+//      tree_(tree),
+//      num_forces_(num_forces) {}
+//
+// void FixedPointConstraint::DoEval(
+//    const Eigen::Ref<const Eigen::VectorXd>& q_u_l, Eigen::VectorXd* y) const
+//    {
+//  AutoDiffVecXd y_t;
+//  Eval(initializeAutoDiff(q_u_l), &y_t);
+//  *y = autoDiffToValueMatrix(y_t);
+//}
 
 }  // namespace multibody
 }  // namespace dairlib
--- multibody/multibody_solvers.h
+++ multibody/multibody_solvers.h
@@ -8,42 +8,42 @@
 namespace dairlib {
 namespace multibody {
 
-class FixedPointConstraint : public drake::solvers::Constraint {
- public:
-  FixedPointConstraint(const RigidBodyTree<double>& tree, const int num_forces,
-                       const std::string& description = "");
-  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& q_u_l,
-              Eigen::VectorXd* y) const override;
-  void DoEval(const Eigen::Ref<const drake::AutoDiffVecXd>& q_u_l,
-              drake::AutoDiffVecXd* y) const override;
-  void DoEval(
-      const Eigen::Ref<const drake::VectorX<drake::symbolic::Variable>>& q_u_l,
-      drake::VectorX<drake::symbolic::Expression>* y) const override;
-
- private:
-  const RigidBodyTree<double>& tree_;
-  const int num_forces_;
-};
-
 class PositionConstraint : public drake::solvers::Constraint {
  public:
   PositionConstraint(const RigidBodyTree<double>& tree,
                      const std::string& description = "");
-  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& q_u_l,
-              drake::AutoDiffVecXd* y) const override;
-  void DoEval(const Eigen::Ref<const drake::AutoDiffVecXd>& q_u_l,
+  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& q,
+              Eigen::VectorXd* y) const override;
+  void DoEval(const Eigen::Ref<const drake::AutoDiffVecXd>& x,
               drake::AutoDiffVecXd* y) const override;
   void DoEval(
-      const Eigen::Ref<const drake::VectorX<drake::symbolic::Variable>>& q_u_l,
+      const Eigen::Ref<const drake::VectorX<drake::symbolic::Variable>>& x,
       drake::VectorX<drake::symbolic::Expression>* y) const override;
 
  private:
   const RigidBodyTree<double>& tree_;
 };
 
-class FixedPointSolver {};
-
-class PositionSolver {};
+//class FixedPointConstraint : public drake::solvers::Constraint {
+// public:
+//  FixedPointConstraint(const RigidBodyTree<double>& tree, const int num_forces,
+//                       const std::string& description = "");
+//  void DoEval(const Eigen::Ref<const Eigen::VectorXd>& q_u_l,
+//              Eigen::VectorXd* y) const override;
+//  void DoEval(const Eigen::Ref<const drake::AutoDiffVecXd>& q_u_l,
+//              drake::AutoDiffVecXd* y) const override;
+//  void DoEval(
+//      const Eigen::Ref<const drake::VectorX<drake::symbolic::Variable>>& q_u_l,
+//      drake::VectorX<drake::symbolic::Expression>* y) const override;
+//
+// private:
+//  const RigidBodyTree<double>& tree_;
+//  const int num_forces_;
+//};
+//
+//class FixedPointSolver {};
+//
+//class PositionSolver {};
 
 }  // namespace multibody
 }  // namespace dairlib

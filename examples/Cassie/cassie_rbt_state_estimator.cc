9799312d61747380b786bb4750237ca0ff28cb77
posa <posa@seas.upenn.edu>
2019-02-17 19:07:37 -0500

    Added stub state estimator (copying joint/elmo values)

---
+++ examples/Cassie/cassie_rbt_state_estimator.cc
@@ -0,0 +1,133 @@
+#include "examples/Cassie/cassie_rbt_state_estimator.h"
+#include "examples/Cassie/datatypes/cassie_names.h"
+
+namespace dairlib {
+namespace systems {
+
+using std::string;
+using drake::systems::Context;
+using drake::systems::LeafSystem;
+using systems::OutputVector;
+
+CassieRbtStateEstimator::CassieRbtStateEstimator(
+    const RigidBodyTree<double>& tree) :
+    tree_(tree) {
+  actuatorIndexMap_ = multibody::makeNameToActuatorsMap(tree);
+  positionIndexMap_ = multibody::makeNameToPositionsMap(tree);
+  velocityIndexMap_ = multibody::makeNameToVelocitiesMap(tree);
+
+  this->DeclareAbstractInputPort("cassie_out_t",
+      drake::systems::Value<cassie_out_t>{});
+  this->DeclareVectorOutputPort(OutputVector<double>(tree.get_num_positions(),
+      tree.get_num_velocities(), tree.get_num_actuators()),
+      &CassieRbtStateEstimator ::Output);
+}
+
+void CassieRbtStateEstimator::Output(
+    const Context<double>& context, OutputVector<double>* output) const {
+  const auto& cassie_out =
+      this->EvalAbstractInput(context, 0)->GetValue<cassie_out_t>();
+
+  // Stub: copy known values
+  // Copy actuators
+  output->SetEffortAtIndex(actuatorIndexMap_.at("hip_roll_left_motor"),
+      cassie_out.leftLeg.hipRollDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("hip_yaw_left_motor"),
+      cassie_out.leftLeg.hipYawDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("hip_pitch_left_motor"),
+      cassie_out.leftLeg.hipPitchDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("knee_left_motor"),
+      cassie_out.leftLeg.kneeDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("toe_left_motor"),
+      cassie_out.leftLeg.footDrive.torque);
+
+  output->SetEffortAtIndex(actuatorIndexMap_.at("hip_roll_right_motor"),
+      cassie_out.rightLeg.hipRollDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("hip_yaw_right_motor"),
+      cassie_out.rightLeg.hipYawDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("hip_pitch_right_motor"),
+      cassie_out.rightLeg.hipPitchDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("knee_right_motor"),
+      cassie_out.rightLeg.kneeDrive.torque);
+  output->SetEffortAtIndex(actuatorIndexMap_.at("toe_right_motor"),
+      cassie_out.rightLeg.footDrive.torque);
+
+  // Copy positions
+  output->SetPositionAtIndex(actuatorIndexMap_.at("hip_roll_left"),
+      cassie_out.leftLeg.hipRollDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("hip_yaw_left"),
+      cassie_out.leftLeg.hipYawDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("hip_pitch_left"),
+      cassie_out.leftLeg.hipPitchDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("knee_left"),
+      cassie_out.leftLeg.kneeDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("toe_left"),
+      cassie_out.leftLeg.footDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("knee_joint_left"),
+      cassie_out.leftLeg.shinJoint.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("ankle_joint_right"),
+      cassie_out.leftLeg.tarsusJoint.position);
+  // TODO(mposa): double check these joint mappings. Also unclear what
+  // footJoint corresponds to
+  // output->SetPositionAtIndex(actuatorIndexMap_.at("ankle_spring_joint_right"),
+  //     cassie_out.leftLeg.footJoint.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("hip_roll_right"),
+      cassie_out.rightLeg.hipRollDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("hip_yaw_right"),
+      cassie_out.rightLeg.hipYawDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("hip_pitch_right"),
+      cassie_out.rightLeg.hipPitchDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("knee_right"),
+      cassie_out.rightLeg.kneeDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("toe_right"),
+      cassie_out.rightLeg.footDrive.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("knee_joint_right"),
+      cassie_out.rightLeg.shinJoint.position);
+  output->SetPositionAtIndex(actuatorIndexMap_.at("ankle_joint_right"),
+      cassie_out.rightLeg.tarsusJoint.position);
+  // TODO(mposa): double check these joint mappings. Also unclear what
+  // footJoint corresponds to
+  // output->SetPositionAtIndex(actuatorIndexMap_.at("ankle_spring_joint_right"),
+  //     cassie_out.rightLeg.footJoint.position);
+
+  // Copy velocities
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("hip_roll_leftdot"),
+      cassie_out.leftLeg.hipRollDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("hip_yaw_leftdot"),
+      cassie_out.leftLeg.hipYawDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("hip_pitch_leftdot"),
+      cassie_out.leftLeg.hipPitchDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("knee_leftdot"),
+      cassie_out.leftLeg.kneeDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("toe_leftdot"),
+      cassie_out.leftLeg.footDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("knee_joint_leftdot"),
+      cassie_out.leftLeg.shinJoint.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("ankle_joint_rightdot"),
+      cassie_out.leftLeg.tarsusJoint.velocity);
+  // TODO(mposa): double check these joint mappings. Also unclear what
+  // footJoint corresponds to
+  // output->SetVelocityAtIndex(actuatorIndexMap_.at("ankle_spring_joint_rightdot"),
+  //     cassie_out.leftLeg.footJoint.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("hip_roll_rightdot"),
+      cassie_out.rightLeg.hipRollDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("hip_yaw_rightdot"),
+      cassie_out.rightLeg.hipYawDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("hip_pitch_rightdot"),
+      cassie_out.rightLeg.hipPitchDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("knee_rightdot"),
+      cassie_out.rightLeg.kneeDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("toe_rightdot"),
+      cassie_out.rightLeg.footDrive.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("knee_joint_rightdot"),
+      cassie_out.rightLeg.shinJoint.velocity);
+  output->SetVelocityAtIndex(actuatorIndexMap_.at("ankle_joint_rightdot"),
+      cassie_out.rightLeg.tarsusJoint.velocity);
+  // TODO(mposa): double check these joint mappings. Also unclear what
+  // footJoint corresponds to
+  // output->SetVelocityAtIndex(actuatorIndexMap_.at("ankle_spring_joint_rightdot"),
+  //     cassie_out.rightLeg.footJoint.velocity);
+}
+
+}  // namespace systems
+}  // namespace dairlib
--- examples/Cassie/networking/cassie_input_translator.cc
+++ examples/Cassie/networking/cassie_input_translator.cc
@@ -9,6 +9,7 @@ using std::string;
 using Eigen::VectorXd;
 using drake::systems::Context;
 using drake::systems::LeafSystem;
+using drake::systems::BasicVector;
 using drake::multibody::JointActuatorIndex;
 using drake::multibody::JointIndex;
 using drake::multibody::MultibodyPlant;
@@ -27,6 +28,27 @@ CassieInputTranslator::CassieInputTranslator(
   for (auto const& name : cassieEffortNames) {
     userin_to_uvector_index_.push_back(actuatorIndexMap.at(name));
   }
+
+  this->DeclareVectorInputPort("vector_command",
+      TimestampedVector<double>(kNumActuators));
+  this->DeclareAbstractOutputPort(
+      "cassie_user_out_t", &CassieInputTranslator::Output);
+}
+
+CassieInputTranslator::CassieInputTranslator(
+    const RigidBodyTree<double>& tree) {
+  std::map<std::string, int> actuatorIndexMap =
+    multibody::makeNameToActuatorsMap(tree);
+  // build index map
+  DRAKE_ASSERT(kNumActuators == cassieEffortNames.size());
+  for (auto const& name : cassieEffortNames) {
+    userin_to_uvector_index_.push_back(actuatorIndexMap.at(name));
+  }
+
+  this->DeclareVectorInputPort("vector_command",
+      TimestampedVector<double>(kNumActuators));
+  this->DeclareAbstractOutputPort(
+      "cassie_user_out_t", &CassieInputTranslator::Output); 
 }
 
 

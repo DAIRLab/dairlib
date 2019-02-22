18b0ee39f2de140ee31eed6c82000b60ca3783bc
Yu-Ming Chen <yminchen@umich.edu>
2019-02-22 13:51:00 -0500

    Add a simple example for audoDiff

---
+++ examples/simple_examples/autoDiff_example.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "math.h"
+#include "drake/math/autodiff_gradient.h"
+
+using std::cout;
+using std::endl;
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using drake::AutoDiffVecXd;
+using drake::AutoDiffXd;
+using drake::math::DiscardGradient;
+using drake::math::autoDiffToValueMatrix;
+using drake::math::autoDiffToGradientMatrix;
+using drake::math::initializeAutoDiff;
+
+int main() {
+  int x_dim = 2;
+  int y_dim = 3;
+  int z_dim = 1;
+
+  //////////////////////////////////////////////////////////
+  // Declare audoDiff x
+  VectorXd x_val(x_dim);
+  x_val << M_PI/2, 3;
+  AutoDiffVecXd x = initializeAutoDiff(x_val);
+
+  // Compute jacobian of x; Default wrt itself since it's not derived from other
+  // autoDiff
+  MatrixXd dxdSTH  = autoDiffToGradientMatrix(x);
+  cout << "dxdSTH = \n" <<  dxdSTH << "\n\n";
+
+  //////////////////////////////////////////////////////////
+  // Declare audoDiff y = x1 + x2^3
+  AutoDiffVecXd y(y_dim);
+  y << x(0) + x(1)*x(1)*x(1),
+       x(0) * x(1),
+       cos(x(0)) + sqrt(x(1));
+
+  // Get the value of y
+  VectorXd y_val = autoDiffToValueMatrix(y);
+  cout << "y_val = \n" << y_val << "\n\n";
+
+  // Compute jacobian of y wrt x
+  MatrixXd dydx  = autoDiffToGradientMatrix(y);
+  cout << "dydx = \n" << dydx << "\n\n";
+
+  //////////////////////////////////////////////////////////
+  // Declare audoDiff z = y1 + y2
+  AutoDiffVecXd z(z_dim);
+  z << y(0) + y(1);
+
+  // Get the value of z
+  VectorXd z_val = autoDiffToValueMatrix(z);
+  cout << "z_val = \n" << z_val << "\n\n";
+
+  // Compute jacobian of z wrt ?? (x!)
+  MatrixXd dzdSTH  = autoDiffToGradientMatrix(z);
+  cout << "dzdSTH = \n" << dzdSTH << "\n\n";
+
+
+  return 0;
+}
+

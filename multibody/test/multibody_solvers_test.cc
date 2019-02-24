9753b4a1f3024db609936fab32ac643e50f2f365
shrenikm <shrenik95@gmail.com>
2019-02-23 22:17:06 -0500

    Split the tests into smaller test chunks. FP solver for floating base still needs to be fixed.

--- multibody/test/multibody_solvers_test.cc
+++ multibody/test/multibody_solvers_test.cc
@@ -152,7 +152,15 @@ class MultibodySolversTest : public ::testing::Test {
 
     num_contacts_ = contact_info_.num_contacts;
     num_position_constraints_ = tree_floating_.getNumPositionConstraints();
-    num_forces_ = num_contacts_ * 3 + num_position_constraints_;
+    num_forces_ = num_position_constraints_ + num_contacts_ * 3;
+
+    q_fixed_ = x0_fixed_.head(num_positions_fixed_);
+    u_fixed_ = VectorXd::Zero(num_efforts_fixed_);
+    lambda_fixed_ = VectorXd::Zero(num_position_constraints_);
+
+    q_floating_ = x0_floating_.head(num_positions_floating_);
+    u_floating_ = VectorXd::Zero(num_efforts_floating_);
+    lambda_floating_ = VectorXd::Zero(num_forces_);
   }
 
   RigidBodyTree<double> tree_fixed_;
@@ -171,52 +179,65 @@ class MultibodySolversTest : public ::testing::Test {
   ContactInfo contact_info_;
   VectorXd x0_fixed_;
   VectorXd x0_floating_;
+  VectorXd q_fixed_;
+  VectorXd q_floating_;
+  VectorXd u_fixed_;
+  VectorXd u_floating_;
+  VectorXd lambda_fixed_;
+  VectorXd lambda_floating_;
 };
 
-TEST_F(MultibodySolversTest, InitializationTest) {
+TEST_F(MultibodySolversTest, TestPositionConstraintInitialization) {
   // Constraint class initializations
   // Position constraint
 
   PositionConstraint position_constraint_fixed(tree_fixed_);
   PositionConstraint position_constraint_floating(tree_floating_);
+}
 
+TEST_F(MultibodySolversTest, TestContactConstraintInitialization) {
   // Contact constraint
   ContactConstraint contact_constraint(tree_floating_, contact_info_);
+}
 
+TEST_F(MultibodySolversTest, TestFixedPointConstraintInitialization) {
   // Fixed point constraint with and without contact
   FixedPointConstraint fp_constraint1(tree_fixed_);
   FixedPointConstraint fp_constraint2(tree_floating_, contact_info_);
+}
 
-  // Solver class initializations
-
-  // PositionSolver
+TEST_F(MultibodySolversTest, TestPositionSolverBasic) {
   // Testing basic getters and setters
+  // Fixed base
   PositionSolver position_solver_fixed(tree_floating_);
   position_solver_fixed.set_filename("position_log");
   position_solver_fixed.set_major_tolerance(0.001);
   position_solver_fixed.set_minor_tolerance(0.01);
 
+  shared_ptr<MathematicalProgram> prog_position_fixed =
+      position_solver_fixed.get_program();
+
   ASSERT_EQ(position_solver_fixed.get_filename(), "position_log");
   ASSERT_EQ(position_solver_fixed.get_major_tolerance(), 0.001);
   ASSERT_EQ(position_solver_fixed.get_minor_tolerance(), 0.01);
 
+  // Floating base
   PositionSolver position_solver_floating(tree_floating_);
   position_solver_floating.set_filename("position_log");
   position_solver_floating.set_major_tolerance(0.001);
   position_solver_floating.set_minor_tolerance(0.01);
 
-  ASSERT_EQ(position_solver_floating.get_filename(), "position_log");
-  ASSERT_EQ(position_solver_floating.get_major_tolerance(), 0.001);
-  ASSERT_EQ(position_solver_floating.get_minor_tolerance(), 0.01);
-
-  // Testing mathematical program getter
-  shared_ptr<MathematicalProgram> prog_position_fixed =
-      position_solver_fixed.get_program();
   shared_ptr<MathematicalProgram> prog_position_floating =
       position_solver_floating.get_program();
 
-  // ContactSolver
+  ASSERT_EQ(position_solver_floating.get_filename(), "position_log");
+  ASSERT_EQ(position_solver_floating.get_major_tolerance(), 0.001);
+  ASSERT_EQ(position_solver_floating.get_minor_tolerance(), 0.01);
+}
+
+TEST_F(MultibodySolversTest, TestContactSolverBasic) {
   // Testing basic getters and setters
+  // Only the floating base model is used for contact
   ContactSolver contact_solver(tree_floating_, contact_info_);
   contact_solver.set_filename("contact_log");
   contact_solver.set_major_tolerance(0.002);
@@ -228,51 +249,43 @@ TEST_F(MultibodySolversTest, InitializationTest) {
 
   // Testing mathematical program getter
   shared_ptr<MathematicalProgram> prog_contact = contact_solver.get_program();
+}
 
-  // FixedPointSolver
+TEST_F(MultibodySolversTest, TestFixedPointSolverBasic) {
   // Testing basic getters and setters
+  // Fixed base
   FixedPointSolver fp_solver_fixed(tree_fixed_);
   fp_solver_fixed.set_filename("fp_log");
   fp_solver_fixed.set_major_tolerance(0.003);
   fp_solver_fixed.set_minor_tolerance(0.03);
 
+  shared_ptr<MathematicalProgram> prog_fp_fixed = fp_solver_fixed.get_program();
+
   ASSERT_EQ(fp_solver_fixed.get_filename(), "fp_log");
   ASSERT_EQ(fp_solver_fixed.get_major_tolerance(), 0.003);
   ASSERT_EQ(fp_solver_fixed.get_minor_tolerance(), 0.03);
 
+  // Floating base
   FixedPointSolver fp_solver_floating(tree_floating_, contact_info_);
   fp_solver_floating.set_filename("fp_log");
   fp_solver_floating.set_major_tolerance(0.003);
   fp_solver_floating.set_minor_tolerance(0.03);
 
+  shared_ptr<MathematicalProgram> prog_fp_floating =
+      fp_solver_floating.get_program();
+
   ASSERT_EQ(fp_solver_floating.get_filename(), "fp_log");
   ASSERT_EQ(fp_solver_floating.get_major_tolerance(), 0.003);
   ASSERT_EQ(fp_solver_floating.get_minor_tolerance(), 0.03);
-
-  // Testing mathematical program getter
-  shared_ptr<MathematicalProgram> prog_fp_fixed = fp_solver_fixed.get_program();
-  shared_ptr<MathematicalProgram> prog_fp_floating =
-      fp_solver_floating.get_program();
 }
 
-TEST_F(MultibodySolversTest, SolveTest) {
-  // Testing the solvers
-
-  VectorXd q_fixed = x0_fixed_.head(num_positions_fixed_);
-  VectorXd u_fixed = VectorXd::Zero(num_efforts_fixed_);
-  VectorXd lambda_fixed = VectorXd::Zero(num_position_constraints_);
-
-  VectorXd q_floating = x0_floating_.head(num_positions_floating_);
-  VectorXd u_floating = VectorXd::Zero(num_efforts_floating_);
-  VectorXd lambda_floating = VectorXd::Zero(num_position_constraints_);
-
-  // PositionSolver
+TEST_F(MultibodySolversTest, TestPositionSolverSolution) {
   // Fixed base
   PositionSolver position_solver_fixed(tree_fixed_);
-  position_solver_fixed.SetInitialGuessQ(q_fixed);
+  position_solver_fixed.SetInitialGuessQ(q_fixed_);
 
   cout << "Position solver result (Fixed base): "
-       << position_solver_fixed.Solve(q_fixed) << endl;
+       << position_solver_fixed.Solve(q_fixed_) << endl;
 
   VectorXd q_sol_fixed = position_solver_fixed.GetSolutionQ();
 
@@ -283,10 +296,10 @@ TEST_F(MultibodySolversTest, SolveTest) {
 
   // Floating base
   PositionSolver position_solver_floating(tree_floating_);
-  position_solver_floating.SetInitialGuessQ(q_floating);
+  position_solver_floating.SetInitialGuessQ(q_floating_);
 
   cout << "Position solver result (Floating base): "
-       << position_solver_floating.Solve(q_floating) << endl;
+       << position_solver_floating.Solve(q_floating_) << endl;
 
   VectorXd q_sol_floating = position_solver_floating.GetSolutionQ();
 
@@ -294,58 +307,34 @@ TEST_F(MultibodySolversTest, SolveTest) {
   ASSERT_EQ(q_sol_floating.size(), num_positions_floating_);
   // Checking if the solution constraints have been satisfied
   ASSERT_TRUE(position_solver_floating.CheckConstraint(q_sol_floating));
+}
 
-  // ContactSolver
+TEST_F(MultibodySolversTest, TestContactSolverSolution) {
   ContactSolver contact_solver(tree_floating_, contact_info_);
-  contact_solver.SetInitialGuessQ(q_floating);
+  contact_solver.SetInitialGuessQ(q_floating_);
 
   std::cout << "Contact solver result (Floating base): "
-            << contact_solver.Solve(q_floating) << std::endl;
+            << contact_solver.Solve(q_floating_) << std::endl;
 
-  q_sol_floating = contact_solver.GetSolutionQ();
+  VectorXd q_sol_floating = contact_solver.GetSolutionQ();
 
   // Solution dimension check
   ASSERT_EQ(q_sol_floating.size(), num_positions_floating_);
   // Checking if the solution constraints have been satisfied
   ASSERT_TRUE(contact_solver.CheckConstraint(q_sol_floating));
+}
 
-  // FixedPointSolver
+TEST_F(MultibodySolversTest, TestFixedPointSolverSolution) {
   // Fixed base
   FixedPointSolver fp_solver_fixed(tree_fixed_);
-  // fp_solver.SetInitialGuess(q, u, lambda);
-  // fp_solver.AddSpreadNormalForcesCost();
-  // fp_solver.AddFrictionConeConstraint(0.8);
+  fp_solver_fixed.SetInitialGuess(q_fixed_, u_fixed_, lambda_fixed_);
 
-  cout << "Fixed point solver result: "
-       << fp_solver_fixed.Solve(q_fixed, u_fixed) << endl;
+  cout << "Fixed point solver result (Fixed base): "
+       << fp_solver_fixed.Solve(q_fixed_, u_fixed_) << endl;
 
-  q_sol_fixed = fp_solver_fixed.GetSolutionQ();
+  VectorXd q_sol_fixed = fp_solver_fixed.GetSolutionQ();
   VectorXd u_sol_fixed = fp_solver_fixed.GetSolutionU();
   VectorXd lambda_sol_fixed = fp_solver_fixed.GetSolutionLambda();
-
-  VectorXd x_sol_fixed =
-      VectorXd::Zero(num_positions_fixed_ + num_velocities_fixed_);
-  x_sol_fixed.head(num_positions_fixed_) = q_sol_fixed;
-  // ContactToolkit object with empty contacts (Default)
-  ContactToolkit<double> ct_fixed(tree_fixed_, ContactInfo());
-  cout << ct_fixed.CalcMVDot(x_sol_fixed, u_sol_fixed, lambda_sol_fixed)
-              .transpose()
-       << endl;
-  cout << "qdot ----------------------" << endl;
-  cout << ct_fixed
-              .CalcTimeDerivatives(x_sol_fixed, u_sol_fixed, lambda_sol_fixed)
-              .transpose()
-       << endl;
-  cout << endl;
-  cout << "q ----------------------" << endl;
-  cout << q_sol_fixed.transpose() << endl;
-  cout << std::endl;
-  cout << "u ----------------------" << endl;
-  cout << u_sol_fixed.transpose() << endl;
-  cout << endl;
-  cout << "lambda ----------------------" << endl;
-  cout << lambda_sol_fixed.transpose() << endl;
-  cout << endl;
 
   // Solution dimension check
   ASSERT_EQ(q_sol_fixed.size(), num_positions_fixed_);
@@ -354,6 +343,54 @@ TEST_F(MultibodySolversTest, SolveTest) {
   // Solution constraints check
   ASSERT_TRUE(fp_solver_fixed.CheckConstraint(q_sol_fixed, u_sol_fixed,
                                               lambda_sol_fixed));
+
+  // Floating base
+  FixedPointSolver fp_solver_floating(tree_floating_, contact_info_);
+  fp_solver_floating.SetInitialGuess(q_floating_, u_floating_,
+                                     lambda_floating_);
+  // fp_solver.AddSpreadNormalForcesCost();
+  // fp_solver.AddFrictionConeConstraint(0.8);
+
+  cout << "Fixed point solver result (Floating base): "
+       << fp_solver_floating.Solve(q_floating_, u_floating_) << endl;
+
+  VectorXd q_sol_floating = fp_solver_floating.GetSolutionQ();
+  VectorXd u_sol_floating = fp_solver_floating.GetSolutionU();
+  VectorXd lambda_sol_floating = fp_solver_floating.GetSolutionLambda();
+
+  VectorXd x_sol_floating =
+      VectorXd::Zero(num_positions_floating_ + num_velocities_floating_);
+  x_sol_floating.head(num_positions_floating_) = q_sol_floating;
+  // ContactToolkit object for the actual ground contact.
+  ContactToolkit<double> ct_floating(tree_floating_, contact_info_);
+  cout << ct_floating
+              .CalcMVDot(x_sol_floating, u_sol_floating, lambda_sol_floating)
+              .transpose()
+       << endl;
+  cout << "qdot ----------------------" << endl;
+  cout << ct_floating
+              .CalcTimeDerivatives(x_sol_floating, u_sol_floating,
+                                   lambda_sol_floating)
+              .transpose()
+       << endl;
+  cout << endl;
+  cout << "q ----------------------" << endl;
+  cout << q_sol_floating.transpose() << endl;
+  cout << std::endl;
+  cout << "u ----------------------" << endl;
+  cout << u_sol_floating.transpose() << endl;
+  cout << endl;
+  cout << "lambda ----------------------" << endl;
+  cout << lambda_sol_floating.transpose() << endl;
+  cout << endl;
+
+  // Solution dimension check
+  ASSERT_EQ(q_sol_floating.size(), num_positions_floating_);
+  ASSERT_EQ(u_sol_floating.size(), num_efforts_floating_);
+  ASSERT_EQ(lambda_sol_floating.size(), num_forces_);
+  // Solution constraints check
+  ASSERT_TRUE(fp_solver_floating.CheckConstraint(q_sol_floating, u_sol_floating,
+                                                 lambda_sol_floating));
 }
 
 }  // namespace

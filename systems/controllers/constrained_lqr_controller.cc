124c44a0f1a147ccf3a3e496d7cf79a67475bd79
shrenikm <shrenik95@gmail.com>
2019-03-03 13:43:55 -0500

    K and E computation after change of coordinates.

---
+++ systems/controllers/constrained_lqr_controller.cc
@@ -0,0 +1,113 @@
+#include "systems/controllers/constrained_lqr_controller.h"
+
+namespace dairlib {
+namespace systems {
+
+using std::make_unique;
+using std::unique_ptr;
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using Eigen::HouseholderQR;
+using dairlib::multibody::ContactInfo;
+using dairlib::multibody::ContactToolkit;
+using drake::AutoDiffXd;
+using drake::AutoDiffVecXd;
+using drake::math::initializeAutoDiff;
+using drake::math::autoDiffToGradientMatrix;
+using drake::math::autoDiffToValueMatrix;
+using drake::systems::controllers::LinearQuadraticRegulator;
+using drake::systems::controllers::LinearQuadraticRegulatorResult;
+
+ConstrainedLQRController::ConstrainedLQRController(
+    const RigidBodyTree<double>& tree, ContactInfo contact_info)
+    : AffineController(tree.get_num_positions(), tree.get_num_velocities(),
+                       tree.get_num_actuators()),
+      tree_(tree),
+      contact_info_(contact_info),
+      contact_toolkit_(
+          make_unique<ContactToolkit<AutoDiffXd>>(tree, contact_info)),
+      num_positions_(tree.get_num_positions()),
+      num_velocities_(tree.get_num_velocities()),
+      num_states_(num_positions_ + num_velocities_),
+      num_efforts_(tree.get_num_actuators()),
+      num_forces_(tree.getNumPositionConstraints() +
+                  contact_info.num_contacts * 3) {}
+
+void ConstrainedLQRController::SetupController(VectorXd q0, VectorXd u0,
+                                               VectorXd lambda0, MatrixXd Q,
+                                               MatrixXd R) {
+  // checking the validity of the parameters
+  DRAKE_DEMAND(q0.size() == num_positions_);
+  DRAKE_DEMAND(u0.size() == num_efforts_);
+  DRAKE_DEMAND(lambda0.size() == num_forces_);
+
+  // TODO: Drake demand for Q and R sizes.
+
+  // Creating the full state vector (Velocities are zero as it is a fixed point)
+  VectorXd x0(num_states_);
+  x0 << q0, VectorXd::Zero(num_velocities_);
+  VectorX<AutoDiffXd> x0_autodiff = initializeAutoDiff(x0);
+
+  // Computing the full Jacobian (Position and contact)
+  MatrixXd J_tree, J_contact;
+  KinematicsCache<double> k_cache = tree_.doKinematics(q0);
+
+  // Position Jacobian
+  J_tree = tree_.positionConstraintsJacobian(k_cache);
+
+  // Contact Jacobian (If contact information is provided)
+  if (contact_info_.num_contacts > 0) {
+    J_contact = autoDiffToValueMatrix(
+        contact_toolkit_->CalcContactJacobian(x0_autodiff));
+  }
+
+  MatrixXd J(J_tree.rows() + J_contact.rows(), num_positions_);
+  J << J_tree, J_contact;
+
+  // Computing F
+  MatrixXd F = MatrixXd::Zero(2 * J.rows(), 2 * J.cols());
+  F.block(0, 0, J.rows(), J.cols()) = J;
+  F.block(J.rows(), J.cols(), J.rows(), J.cols()) = J;
+
+  // Computing the null space of F
+  HouseholderQR<MatrixXd> qr_decomp(F.transpose());
+  MatrixXd q_decomp = qr_decomp.householderQ();
+
+  MatrixXd P =
+      q_decomp.block(0, F.rows(), q_decomp.rows(), q_decomp.cols() - F.rows());
+  P.transposeInPlace();
+
+  // To compute the linearization, xdot (autodiff) is computed
+  // Creating a combined autodiff vector and then extracting the individual
+  // components to ensure proper gradient initialization.
+  VectorXd xul0(num_states_ + num_efforts_ + num_forces_);
+  xul0 << x0, u0, lambda0;
+  AutoDiffVecXd xul0_autodiff = initializeAutoDiff(xul0);
+  x0_autodiff = xul0_autodiff.head(num_states_);
+  AutoDiffVecXd u0_autodiff = xul0.segment(num_states_, num_efforts_);
+  AutoDiffVecXd lambda0_autodiff = xul0.tail(num_forces_);
+
+  // xdot
+  AutoDiffVecXd xdot0_autodiff = contact_toolkit_->CalcTimeDerivatives(
+      x0_autodiff, u0_autodiff, lambda0_autodiff);
+
+  // Making sure that the derivative is zero
+  DRAKE_DEMAND(autoDiffToValueMatrix(xdot0_autodiff).isZero(1e-6));
+
+  MatrixXd AB = autoDiffToGradientMatrix(xdot0_autodiff);
+  MatrixXd A = AB.leftCols(num_states_);
+  MatrixXd B = AB.block(0, num_states_, AB.rows(), num_efforts_);
+
+  // A and B matrices in the new coordinates
+  A_ = P * A * P.transpose();
+  B_ = P * B;
+
+  lqr_result_ = LinearQuadraticRegulator(A_, B_, Q, R);
+  K_ = lqr_result_.K * P;
+  E_ = u0;
+
+
+}
+
+}  // namespace systems
+}  // namespace dairlib
--- systems/controllers/constrained_lqr_controller.h
+++ systems/controllers/constrained_lqr_controller.h
@@ -1,8 +1,42 @@
 #pragma once
 
+#include "drake/systems/controllers/linear_quadratic_regulator.h"
+#include "attic/multibody/contact_toolkit.h"
 #include "systems/controllers/affine_controller.h"
 
 namespace dairlib {
 namespace systems {
 
+class ConstrainedLQRController : public AffineController {
+ public:
+  ConstrainedLQRController(const RigidBodyTree<double>& tree,
+                           dairlib::multibody::ContactInfo contact_info =
+                               dairlib::multibody::ContactInfo());
+  void SetupController(Eigen::VectorXd q0, Eigen::VectorXd u0,
+                       Eigen::VectorXd lambda0, Eigen::MatrixXd Q,
+                       Eigen::MatrixXd R);
 
+  Eigen::MatrixXd get_A() { return A_; }
+  Eigen::MatrixXd get_B() { return B_; }
+  Eigen::MatrixXd get_K() { return K_; }
+  Eigen::MatrixXd get_E() { return E_; }
+
+ private:
+  const RigidBodyTree<double>& tree_;
+  dairlib::multibody::ContactInfo contact_info_;
+  std::unique_ptr<dairlib::multibody::ContactToolkit<drake::AutoDiffXd>>
+      contact_toolkit_;
+  Eigen::MatrixXd A_;
+  Eigen::MatrixXd B_;
+  Eigen::MatrixXd K_;
+  Eigen::MatrixXd E_;
+  drake::systems::controllers::LinearQuadraticRegulatorResult lqr_result_;
+  const int num_positions_;
+  const int num_velocities_;
+  const int num_states_;
+  const int num_efforts_;
+  const int num_forces_;
+};
+
+}  // namespace systems
+}  // namespace dairlib

5a10d6cc72253e7ef3f8bae09f83bed7bf8034bc
posa <posa@seas.upenn.edu>
2019-06-24 12:01:01 -0400

    Fixed free/delete usage.

--- lcm/log_sequence_rectifier.cc
+++ lcm/log_sequence_rectifier.cc
@@ -85,7 +85,7 @@ int main(int argc, char **argv) {
       // Free and remove message
       free(buffer.front()->channel);
       free(buffer.front()->data);
-      free(buffer.front());
+      delete buffer.front();
       buffer.pop_front();
       N--;
     }
@@ -116,7 +116,7 @@ int main(int argc, char **argv) {
     // Free and remove message
     free(buffer.front()->channel);
     free(buffer.front()->data);
-    free(buffer.front());
+    delete buffer.front();
     buffer.pop_front();
     N--;
   }

6b3eac74b545f4e8e05f640072184b33e8dfa923
Yu-Ming Chen <yminchen@umich.edu>
2019-07-04 12:17:36 -0400

    Change to nullptr so that we can detect if the user set the variables or not

--- systems/controllers/operational_space_control/osc_tracking_data.h
+++ systems/controllers/operational_space_control/osc_tracking_data.h
@@ -29,10 +29,9 @@ class OscTrackingData {
   int n_r_;  // dimension of the traj
 
   // Finite state machine
-  bool use_with_finite_state_machine_;
-  std::vector<int> state_indices_;
+  std::vector<int>* state_indices_ = nullptr;
   // Whether to track the traj or not in each state in finite state machine
-  std::vector<bool> do_track_;
+  std::vector<bool>* do_track_ = nullptr;
 
   // Cost weights
   bool cost_weight_is_matrix_;
@@ -40,21 +39,15 @@ class OscTrackingData {
   Eigen::MatrixXd W_;
 
   // PD control gains
-  bool user_define_p_gain_;
-  bool user_define_d_gain_;
-  bool p_gain_is_matrix_;
-  bool d_gain_is_matrix_;
-  double k_p_;
-  double k_d_;
-  Eigen::MatrixXd K_p_;
-  Eigen::MatrixXd K_d_;
-  //TODO: update the booleans in setter
+  double* k_p_ = nullptr;
+  double* k_d_ = nullptr;
+  Eigen::MatrixXd* K_p_ = nullptr;
+  Eigen::MatrixXd* K_d_ = nullptr;
   //TODO: note that the users don't need to track position only, it could be
   // velocity only or acceleration only.
 
   // Whether or not users created a function themselves
-  bool user_define_traj_function_ = false;
-  OscUserDefinedTrajData user_defined_data_;
+  OscUserDefinedTrajData* user_defined_data_ = nullptr;
 
   // Whether or not the desired traj is from another leafsystem's output
   bool traj_is_from_input_port_;
@@ -63,27 +56,25 @@ class OscTrackingData {
     //    *mother_traj = readTrajFromInput();
     // value() and MakeDerivative() are the functions you need. (functions tested)
   // If it's not from input port, users define the fixed desired pos/vel/accel
-  //TODO:(yminchen) In the pos/vel/accel setter, you switch the boolean to true
-  bool user_define_desired_pos_ = false;
-  bool user_define_desired_vel_ = false;
-  bool user_define_desired_accel_ = false;
-  VectorXd desired_pos_;
-  VectorXd desired_vel_;
-  VectorXd desired_accel_;
+  VectorXd* desired_pos_ = nullptr;
+  VectorXd* desired_vel_ = nullptr;
+  VectorXd* desired_accel_ = nullptr;
 
   // Whether or not the traj is in operational space control
-  bool traj_is_in_operational_space_;
   // Tracking in operational space:
-  std::vector<int> body_index_;
-  std::vector<Eigen::VectorXd> pt_on_body_;
+  std::vector<int>* body_index_ = nullptr;
+  std::vector<Eigen::VectorXd>* pt_on_body_ = nullptr;
   // Tracking in joint space:
-  std::vector<int> joint_position_index_;
-  std::vector<int> joint_velocity_index_;
+  std::vector<int>* joint_position_index_ = nullptr;
+  std::vector<int>* joint_velocity_index_ = nullptr;
 
   // Whether the traj is translational or rotational (if tracking in operational
   // space)
   bool traj_is_rotational_;
-  bool desired_pos_is_in_quaternion_;
+  // The RBT and desired position should use quaternion in case of
+  // non-uniqueness RPY.
+  // TODO: (need to test this) You should convert the relative quaternion to
+  // roll-pitch-yaw, and do pd control.
 }
 
 }  // namespace controllers

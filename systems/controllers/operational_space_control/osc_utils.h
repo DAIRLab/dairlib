667c13c46bffa06b4c3e8c74139d604e2c490b52
Yu-Ming Chen <yminchen@umich.edu>
2019-07-11 09:41:36 -0400

    Connect to the correct output port of trajectory by comparing names

--- systems/controllers/operational_space_control/osc_utils.h
+++ systems/controllers/operational_space_control/osc_utils.h
@@ -9,27 +9,36 @@ namespace systems {
 namespace controllers {
 
 //TODO(yminchen): You can actually set the names of systems and connect them
+
+// Requiremnt:
+// The name of traj source block and its output port must have the same name as
+// `TrackingData.name_`
 int ConnectPortsForAllTrackingData(OperationalSpaceControl* osc,
                                    drake::systems::DiagramBuilder<double> & builder) {
   OscTrackingDataSet data_set = osc->GetTrackingDataSet();
   std::vector<OscTrackingData> tracking_data_vec = data_set.GetAllTrackingData();
-  for (int i = 0; i < tracking_data_vec->size(); i++) {
-    string traj_name = tracking_data_vec[i]->traj_name_;
+  for (auto tracking_data : tracking_data_vec) {
+    string traj_name = tracking_data->traj_name_;
 
     std::vector<systems::System<double>*> system_vec = builder.GetMutableSystems();
     bool connect_successfully = false;
-    // TODO: you can actually get the port name
+    // Find trajectory source block
     for (auto system : system_vec) {
       if (traj_name.compare(system->get_name()) == 0) {
-        DRAKE_DEMAND(traj_source->num_output_ports() == 1);
-        builder.Connect(traj_source->get_output_port(0),
-                        osc->get_tracking_data_input_port(traj_name));
-        connect_successfully = true;
-        break;
+        // Find the correspond output port
+        for (int i = 0; i < system->num_output_ports(); i++) {
+          if (traj_name.compare(system->get_output_port(i).get_name()) == 0) {
+            builder.Connect(system->get_output_port(i),
+                            osc->get_tracking_data_input_port(traj_name));
+            connect_successfully = true;
+            break;
+          }
+        }  // end for (ports)
       }
-    }
+      if (connect_successfully) break;
+    }  // end for (leaf systems)
     DRAKE_DEMAND(connect_successfully);
-  }
+  }  // end for (OscTrackingData's)
 }
 
 // Convert rotational matrix to Eular angle (roll pitch yaw)

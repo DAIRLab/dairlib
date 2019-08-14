f63ab5d60ce016744ec66f356a871f9705072382
Nanda <nandav@seas.upenn.edu>
2019-08-14 16:39:25 -0400

    Move log parser files, make parser more generic
    Move the log parser files from `attic/multibody/` to `systems/log_parser`.
    Make function call AddSystem more generic.

--- systems/log_parser/test/parse_generic_log_test.cc
+++ systems/log_parser/test/parse_generic_log_test.cc
@@ -1,5 +1,5 @@
 #include <string>
-#include "attic/multibody/generic_lcm_log_parser.h"
+#include "systems/log_parser/generic_lcm_log_parser.h"
 #include "examples/Cassie/cassie_utils.h"
 #include "systems/robot_lcm_systems.h"
 #include "dairlib/lcmt_robot_output.hpp"
@@ -15,9 +15,10 @@ int main() {
   std::string filename =
     "/home/nanda/DAIR/dairlib/examples/Cassie/lcmlog-2019-05-21.02";
 
-  dairlib::multibody::parseLcmLog<dairlib::lcmt_robot_input,
-    dairlib::systems::RobotInputReceiver>(tree, filename, channel, &t, &x,
-        2.0e6);
+  std::unique_ptr<dairlib::systems::RobotInputReceiver> system =
+      std::make_unique<dairlib::systems::RobotInputReceiver>(tree);
+  dairlib::multibody::parseLcmLog<dairlib::lcmt_robot_input>(
+      std::move(system), filename, channel, &t, &x, 2.0e6);
 
   std::cout << "*****t*****" << std::endl;
   std:: cout << t.rows() << " " << t.cols() << std::endl << std::endl;

badde31eaa2f6b400ad42748ce75562c2d4f032c
William Yang <yangwill@seas.upenn.edu>
2019-10-16 11:49:17 -0400

    fixing compilation bugs

--- lcm/lcm_trajectory.cc
+++ lcm/lcm_trajectory.cc
@@ -19,107 +19,115 @@ using drake::systems::lcm::Serializer;
 
 namespace dairlib {
 
-LcmTrajectory::LcmTrajectory(const vector<Trajectory>& trajectories, 
-				const vector<string>& trajectory_names, 
-				const string& name, 
-				const string& description):
-				trajectory_names_(trajectory_names){
-	int index = 0;
-	for (string name : trajectory_names_){
-		trajectories_[name] = trajectories[index++];
-	}
-	metadata_ = constructMetadataObject(name, description);
-	// serializer = Serializer<lcmt_saved_traj>();
+LcmTrajectory::LcmTrajectory(const vector<Trajectory>& trajectories,
+                             const vector<string>& trajectory_names,
+                             const string& name,
+                             const string& description):
+  trajectory_names_(trajectory_names) {
+  int index = 0;
+  for (string name : trajectory_names_) {
+    trajectories_[name] = trajectories[index++];
+  }
+  metadata_ = constructMetadataObject(name, description);
 }
 
-LcmTrajectory::LcmTrajectory(const lcmt_saved_traj& traj){
-	metadata_ = traj.metadata;
-	trajectories_ = unordered_map<string, Trajectory>();
-	trajectory_names_ = vector<std::string>();
-	for (int i = 0; i < traj.num_trajectories; ++i) {
-		string traj_name = traj.trajectory_names[i];
-		trajectory_names_.push_back(traj_name);
-		trajectories_[traj_name] = Trajectory(traj_name, traj.trajectories[i]);
-	}
-	// serializer = Serializer<lcmt_saved_traj>();
+LcmTrajectory::LcmTrajectory(const lcmt_saved_traj& traj) {
+  metadata_ = traj.metadata;
+  trajectories_ = unordered_map<string, Trajectory>();
+  trajectory_names_ = vector<std::string>();
+  for (int i = 0; i < traj.num_trajectories; ++i) {
+    string traj_name = traj.trajectory_names[i];
+    trajectory_names_.push_back(traj_name);
+    trajectories_[traj_name] = Trajectory(traj_name, traj.trajectories[i]);
+  }
 }
 
-LcmTrajectory::Trajectory::Trajectory(string traj_name, const lcmt_trajectory_block& traj_block){
-	int num_points = traj_block.num_points;
-	int num_datatypes = traj_block.num_datatypes;
-	this->traj_name = traj_name;
-	this->datatypes = vector<string>(traj_block.datatypes);
-	this->time_vector = VectorXd::Map(traj_block.time_vec.data(),
-																		num_points);
-	// this->datapoints = MatrixXd(num_points, num_datatypes);
-	for (int i = 0; i < num_points; ++i){
-		this->datapoints.row(i) = VectorXd::Map(&traj_block.datapoints[i][0], 
-																						num_datatypes);
-	}
+LcmTrajectory::LcmTrajectory(const string& filepath) {
+  return LcmTrajectory(loadFromFile(filepath));
 }
 
-lcmt_saved_traj LcmTrajectory::generateLcmObject() const{
-	lcmt_saved_traj traj;
-	traj.num_trajectories = trajectories_.size();
-	traj.metadata = metadata_;
-
-	int index = 0;
-	for (std::pair<string, Trajectory> traj_el : trajectories_){
-		lcmt_trajectory_block traj_block;
-		Trajectory* cpp_traj = &traj_el.second;
-
-		traj_block.num_points = cpp_traj->time_vector.size();
-		traj_block.num_datatypes = cpp_traj->datatypes.size();
-		memcpy(&traj_block.time_vec, cpp_traj->time_vector.data(), sizeof(traj_block.time_vec));
-		memcpy(&traj_block.datatypes, cpp_traj->datatypes.data(), sizeof(traj_block.datatypes));
-		memcpy(&traj_block.datapoints, cpp_traj->datapoints.data(), sizeof(traj_block.datapoints));
-	
-		traj.trajectories[index] = traj_block;
-		traj.trajectory_names[index] = traj_el.first;
-		++index;
-	}
-	return traj;
-} 
-
-void LcmTrajectory::writeToFile(string filepath){
-	std::vector<uint8_t> bytes;
-	drake::systems::lcm::Serializer<lcmt_saved_traj> serializer;
-	serializer.Serialize(*AbstractValue::Make(generateLcmObject()), &bytes);
-
-	std::ofstream fout(filepath);
-	fout << bytes.data();
-	fout.close();
+LcmTrajectory::Trajectory::Trajectory(string traj_name,
+                                      const lcmt_trajectory_block& traj_block) {
+  int num_points = traj_block.num_points;
+  int num_datatypes = traj_block.num_datatypes;
+  this->traj_name = traj_name;
+  this->datatypes = vector<string>(traj_block.datatypes);
+  this->time_vector = VectorXd::Map(traj_block.time_vec.data(),
+                                    num_points);
+  // this->datapoints = MatrixXd(num_points, num_datatypes);
+  for (int i = 0; i < num_points; ++i) {
+    this->datapoints.row(i) = VectorXd::Map(&traj_block.datapoints[i][0],
+                                            num_datatypes);
+  }
 }
 
-lcmt_saved_traj LcmTrajectory::loadFromFile(string filepath){
-	// std::vector<uint8_t> bytes( std::istreambuf_iterator<char>(infile),
-														// std::istreambuf_iterator<char>());
-	drake::systems::lcm::Serializer<lcmt_saved_traj> serializer;
-	std::ifstream inFile(filepath, std::ios_base::binary);
+lcmt_saved_traj LcmTrajectory::generateLcmObject() const {
+  lcmt_saved_traj traj;
+  traj.num_trajectories = trajectories_.size();
+  traj.metadata = metadata_;
 
-	inFile.seekg(0, std::ios_base::end);
-	size_t length = inFile.tellg();
-	inFile.seekg(0, std::ios_base::beg);
+  int index = 0;
+  for (std::pair<string, Trajectory> traj_el : trajectories_) {
+    lcmt_trajectory_block traj_block;
+    Trajectory* cpp_traj = &traj_el.second;
 
-	std::vector<uint8_t> bytes;
-	bytes.reserve(length);
-	std::copy( std::istreambuf_iterator<char>(inFile),
-	           std::istreambuf_iterator<char>(),
-	           std::back_inserter(bytes) );
+    traj_block.num_points = cpp_traj->time_vector.size();
+    traj_block.num_datatypes = cpp_traj->datatypes.size();
+    memcpy(&traj_block.time_vec, cpp_traj->time_vector.data(),
+           sizeof(traj_block.time_vec));
+    memcpy(&traj_block.datatypes, cpp_traj->datatypes.data(),
+           sizeof(traj_block.datatypes));
+    memcpy(&traj_block.datapoints, cpp_traj->datapoints.data(),
+           sizeof(traj_block.datapoints));
 
-	lcmt_saved_traj traj;
-	std::unique_ptr<AbstractValue> traj_value = AbstractValue::Make(traj);
-	serializer.Deserialize((void *)bytes.data(), (int)bytes.size(), traj_value.release());
-	return traj_value->get_value<lcmt_saved_traj>();
+    traj.trajectories[index] = traj_block;
+    traj.trajectory_names[index] = traj_el.first;
+    ++index;
+  }
+  return traj;
 }
 
-lcmt_metadata LcmTrajectory::constructMetadataObject(string name, string description) const{
-	lcmt_metadata metadata;
-	
-	metadata.name = name;
-	metadata.description = description;
-	// TODO: autofill git details
-	return metadata;
+void LcmTrajectory::writeToFile(string filepath) {
+  std::vector<uint8_t> bytes;
+  drake::systems::lcm::Serializer<lcmt_saved_traj> serializer;
+  serializer.Serialize(*AbstractValue::Make(generateLcmObject()), &bytes);
+
+  std::ofstream fout(filepath);
+  fout << bytes.data();
+  fout.close();
+}
+
+lcmt_saved_traj LcmTrajectory::loadFromFile(const std::string filepath) {
+  // std::vector<uint8_t> bytes( std::istreambuf_iterator<char>(infile),
+  // std::istreambuf_iterator<char>());
+  drake::systems::lcm::Serializer<lcmt_saved_traj> serializer;
+  std::ifstream inFile(filepath, std::ios_base::binary);
+
+  inFile.seekg(0, std::ios_base::end);
+  size_t length = inFile.tellg();
+  inFile.seekg(0, std::ios_base::beg);
+
+  std::vector<uint8_t> bytes;
+  bytes.reserve(length);
+  std::copy( std::istreambuf_iterator<char>(inFile),
+             std::istreambuf_iterator<char>(),
+             std::back_inserter(bytes) );
+
+  lcmt_saved_traj traj;
+  std::unique_ptr<AbstractValue> traj_value = AbstractValue::Make(traj);
+  serializer.Deserialize((void *)bytes.data(), (int)bytes.size(),
+                         traj_value.release());
+  return traj_value->get_value<lcmt_saved_traj>();
+}
+
+lcmt_metadata LcmTrajectory::constructMetadataObject(string name,
+    string description) const {
+  lcmt_metadata metadata;
+
+  metadata.name = name;
+  metadata.description = description;
+  // TODO: autofill git details
+  return metadata;
 }
 
 } // dairlib
--- lcm/lcm_trajectory.h
+++ lcm/lcm_trajectory.h
@@ -6,39 +6,39 @@
 
 #include "dairlib/lcmt_saved_traj.hpp"
 
-namespace dairlib{
+namespace dairlib {
 
-class LcmTrajectory{
-public:
-	struct Trajectory{
-		Trajectory();
-		Trajectory(std::string traj_name, const lcmt_trajectory_block& traj_block);
-		std::string traj_name;
-		Eigen::VectorXd time_vector;
-		Eigen::MatrixXd datapoints;
-		std::vector<std::string> datatypes;
-	};
+class LcmTrajectory {
+ public:
+  struct Trajectory {
+    Trajectory();
+    Trajectory(std::string traj_name, const lcmt_trajectory_block& traj_block);
+    std::string traj_name;
+    Eigen::VectorXd time_vector;
+    Eigen::MatrixXd datapoints;
+    std::vector<std::string> datatypes;
+  };
 
-	LcmTrajectory(const std::vector<Trajectory>& trajectories, 
-				const std::vector<std::string>& trajectory_names, 
-				const std::string& name = "DEFAULT_NAME", 
-				const std::string& description = "DEFAULT_DESCRIPTION");
+  LcmTrajectory(const std::vector<Trajectory>& trajectories,
+                const std::vector<std::string>& trajectory_names,
+                const std::string& name = "DEFAULT_NAME",
+                const std::string& description = "DEFAULT_DESCRIPTION");
 
-	LcmTrajectory(const lcmt_saved_traj& traj);
+  LcmTrajectory(const lcmt_saved_traj& traj);
 
-	lcmt_saved_traj generateLcmObject() const;
+  LcmTrajectory(const std::string filepath);
 
-	void writeToFile(std::string filepath);
+  lcmt_saved_traj generateLcmObject() const;
 
-	lcmt_saved_traj loadFromFile(std::string filepath);
+  void writeToFile(std::string filepath);
 
-private:
-	lcmt_metadata constructMetadataObject(	std::string name, 
-											std::string description) const;
-
-	lcmt_metadata metadata_;
-	std::unordered_map<std::string, Trajectory> trajectories_;
-	std::vector<std::string> trajectory_names_;
+  lcmt_metadata metadata_;
+  std::unordered_map<std::string, Trajectory> trajectories_;
+  std::vector<std::string> trajectory_names_;
+ private:
+  lcmt_metadata constructMetadataObject(	std::string name,
+                                          std::string description) const;
+  lcmt_saved_traj loadFromFile(const std::string filepath);
 };
 
 

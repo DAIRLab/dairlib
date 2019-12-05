5b7af98a32b6b963e5c652fceb61bcfb7ad4a4c5
William Yang <yangwill@seas.upenn.edu>
2019-12-05 14:01:06 -0500

    Explicitly connecting output ports in dispatcher_in (#119)
    Due to the input supervisor now outputting its status, we must explicitly connect its output ports

--- examples/Cassie/dispatcher_robot_in.cc
+++ examples/Cassie/dispatcher_robot_in.cc
@@ -89,7 +89,8 @@ int do_main(int argc, char* argv[]) {
   // Create and connect translator
   auto input_translator =
       builder.AddSystem<systems::CassieInputTranslator>(*tree);
-  builder.Connect(*input_supervisor, *input_translator);
+  builder.Connect(input_supervisor->get_output_port_command(),
+      input_translator->get_input_port(0));
 
   // Create and connect input publisher.
   auto input_pub = builder.AddSystem(
@@ -104,7 +105,8 @@ int do_main(int argc, char* argv[]) {
           "NETWORK_CASSIE_INPUT", &lcm_network,
           {TriggerType::kPeriodic}, FLAGS_pub_rate));
 
-  builder.Connect(*input_supervisor, *net_command_sender);
+  builder.Connect(input_supervisor->get_output_port_command(),
+      net_command_sender->get_input_port(0));
 
   builder.Connect(*net_command_sender, *net_command_pub);
 

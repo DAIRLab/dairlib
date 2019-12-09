b30b1d87309b5cd9a3315333d621102ffdd15eba
Yu-Ming Chen <yminchen@umich.edu>
2019-12-08 22:13:32 -0500

    Update documentation to explain ControllerChannelSender more clearly

--- systems/framework/controller_channel_sender.h
+++ systems/framework/controller_channel_sender.h
@@ -12,27 +12,27 @@ namespace dairlib {
 /// ControllerChannelSender is a single-output block that assigns a string
 /// to the lcm message, MessageType.
 /// The user can choose to publish the string with or without a delay. The delay
-/// is added so that we can sync the publish with the TIME-BASED finite state
-/// machine in the controller.
+/// is added so that we can sync the publish with `TimeBasedFiniteStateMachine`.
 /// If no delay, this class outputs the channel name all the time.
 /// In the case of delay, the user has to provide
 ///   - n_fsm_period, the number of period with which the user wants to delay
 ///   the publish
-///   - period, the period of the time-based fsm
-///   - (optional) fsm_offset, which is the offset of the time-based fsm.
+///   - period, `duration_per_state` of `TimeBasedFiniteStateMachine`
+///   - (optional) fsm_offset, `time_shift` of `TimeBasedFiniteStateMachine`
 /// Let t_init be the time of the simulator/robot when we started running the
 /// diagram containing this class. And let t_current be the current diagram
 /// time. This class outputs the channel name only when
 ///   t_current >= (floor(t_init/period) + n_fsm_period) * period + fsm_offset.
-/// That is, the current time has to be bigger than the initial diagram
-/// time plus the delay.
+/// That is, `ControllerChannelSender` starts publishing the new channel name
+/// when `TimeBasedFiniteStateMachine` switches to a new state for the
+/// `n_fsm_period`-th times after we start running the diagram that contains
+/// `ControllerChannelSender`.
 
 template <typename MessageType>
 class ControllerChannelSender : public drake::systems::LeafSystem<double> {
  public:
   explicit ControllerChannelSender(const std::string& channel_name,
-                                   int n_fsm_period = -1,
-                                   double period = -1,
+                                   int n_fsm_period = -1, double period = -1,
                                    double fsm_offset = 0);
 
  private:

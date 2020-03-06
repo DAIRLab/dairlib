73931d2925a7a295bfc97eadb963622d9b0b8359
William Yang <yangwill@seas.upenn.edu>
2020-03-06 09:58:42 -0500

    Adding leafsystem that allows conversion between floating and fixed base output vectors

--- examples/Cassie/run_pd_controller.cc
+++ examples/Cassie/run_pd_controller.cc
@@ -1,27 +1,36 @@
+#include <gflags/gflags.h>
+
+#include "systems/primitives/output_subvector_pass_through.h"
+#include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/lcm/lcm_interface_system.h"
 #include "drake/systems/lcm/lcm_publisher_system.h"
 #include "drake/systems/lcm/lcm_subscriber_system.h"
-#include "drake/systems/lcm/lcm_interface_system.h"
-#include "drake/systems/analysis/simulator.h"
 
-#include "dairlib/lcmt_robot_output.hpp"
-#include "dairlib/lcmt_robot_input.hpp"
 #include "dairlib/lcmt_pd_config.hpp"
-#include "systems/robot_lcm_systems.h"
+#include "dairlib/lcmt_robot_input.hpp"
+#include "dairlib/lcmt_robot_output.hpp"
+#include "examples/Cassie/cassie_utils.h"
 #include "systems/controllers/linear_controller.h"
 #include "systems/controllers/pd_config_lcm.h"
-#include "examples/Cassie/cassie_utils.h"
+#include "systems/robot_lcm_systems.h"
 
 namespace dairlib {
 
-using drake::systems::lcm::LcmSubscriberSystem;
-using drake::systems::lcm::LcmPublisherSystem;
-using drake::multibody::MultibodyPlant;
 using drake::geometry::SceneGraph;
+using drake::multibody::MultibodyPlant;
 using drake::systems::DiagramBuilder;
+using drake::systems::lcm::LcmPublisherSystem;
+using drake::systems::lcm::LcmSubscriberSystem;
 
+DEFINE_string(channel_x, "CASSIE_STATE_DISPATCHER",
+              "LCM channel for receiving state. "
+              "Use CASSIE_STATE_SIMULATION to get state from simulator, and "
+              "use CASSIE_STATE_DISPATCHER to get state from state estimator");
 
-int doMain() {
+int doMain(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   DiagramBuilder<double> builder;
   DiagramBuilder<double> builder_null;
 
@@ -29,21 +38,39 @@ int doMain() {
       "udpm://239.255.76.67:7667?ttl=0");
 
   SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
+  SceneGraph<double>& scene_graph_floating =
+      *builder_null.AddSystem<SceneGraph>();
   scene_graph.set_name("scene_graph");
 
-  MultibodyPlant<double>& plant =
+  MultibodyPlant<double>& plant = *builder_null.AddSystem<MultibodyPlant>(1.0);
+  MultibodyPlant<double>& plant_floating =
       *builder_null.AddSystem<MultibodyPlant>(1.0);
-  addCassieMultibody(&plant, &scene_graph, false);
+
+  addCassieMultibody(&plant, &scene_graph_floating, false);
+  addCassieMultibody(&plant_floating, &scene_graph_floating, true);
+  plant_floating.Finalize();
   plant.Finalize();
 
-  const std::string channel_x = "CASSIE_STATE";
+  int nq = plant.num_positions();
+  int nv = plant.num_velocities();
+  int nu = plant.num_actuators();
+  int nq_float = plant_floating.num_positions();
+  int nv_float = plant_floating.num_velocities();
+  int nu_float = plant_floating.num_actuators();
+
+  std::cout << "nq" << nq << std::endl;
+  std::cout << "nv" << nv << std::endl;
+  std::cout << "nu" << nu << std::endl;
+
+  const std::string channel_x = FLAGS_channel_x;
   const std::string channel_u = "CASSIE_INPUT";
   const std::string channel_config = "PD_CONFIG";
 
   // Create state receiver.
   auto state_sub = builder.AddSystem(
       LcmSubscriberSystem::Make<dairlib::lcmt_robot_output>(channel_x, lcm));
-  auto state_receiver = builder.AddSystem<systems::RobotOutputReceiver>(plant);
+  auto state_receiver =
+      builder.AddSystem<systems::RobotOutputReceiver>(plant_floating);
   builder.Connect(state_sub->get_output_port(),
                   state_receiver->get_input_port(0));
 
@@ -55,19 +82,27 @@ int doMain() {
                   config_receiver->get_input_port(0));
 
   // Create command sender.
-  auto command_pub = builder.AddSystem(
-      LcmPublisherSystem::Make<dairlib::lcmt_robot_input>(channel_u, lcm,
-                                                          1.0/1000.0));
+  auto command_pub =
+      builder.AddSystem(LcmPublisherSystem::Make<dairlib::lcmt_robot_input>(
+          channel_u, lcm, 1.0 / 1000.0));
   auto command_sender = builder.AddSystem<systems::RobotCommandSender>(plant);
 
   builder.Connect(command_sender->get_output_port(0),
                   command_pub->get_input_port());
 
-
   auto controller = builder.AddSystem<systems::LinearController>(
-      plant.num_positions(), plant.num_velocities(),
-      plant.num_actuators());
+      plant.num_positions(), plant.num_velocities(), plant.num_actuators());
+
+  auto floating_to_fixed_base =
+      builder.AddSystem<systems::OutputSubvectorPassThrough<double>>(
+          nq_float, nv_float, nu_float, nq_float - nq, nq,
+          nq_float + nv_float - nv, nv, nq_float + nv_float + nu_float - nu,
+          nu);
+
   builder.Connect(state_receiver->get_output_port(0),
+                  floating_to_fixed_base->get_input_port());
+
+  builder.Connect(floating_to_fixed_base->get_output_port(),
                   controller->get_input_port_output());
 
   builder.Connect(config_receiver->get_output_port(0),
@@ -82,14 +117,14 @@ int doMain() {
   auto context = diagram->CreateDefaultContext();
 
   /// Use the simulator to drive at a fixed rate
-  /// If set_publish_every_time_step is true, this publishes twice 
+  /// If set_publish_every_time_step is true, this publishes twice
   /// Set realtime rate. Otherwise, runs as fast as possible
-  auto stepper = std::make_unique<drake::systems::Simulator<double>>(*diagram, std::move(context));
+  auto stepper = std::make_unique<drake::systems::Simulator<double>>(
+      *diagram, std::move(context));
   stepper->set_publish_every_time_step(false);
   stepper->set_publish_at_initialization(false);
   stepper->set_target_realtime_rate(1.0);
   stepper->Initialize();
-
 
   drake::log()->info("controller started");
 
@@ -98,7 +133,6 @@ int doMain() {
   return 0;
 }
 
+}  // namespace dairlib
 
-}
-
-int main() { return dairlib::doMain(); }
+int main(int argc, char* argv[]) { return dairlib::doMain(argc, argv); }

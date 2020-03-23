efc4147a563cfacbbfd26c14f48897476cb28361
Jianshu-Hu <jianshu@seas.upenn.edu>
2020-03-23 18:04:23 -0400

    combine and test set_initial_guess

--- examples/goldilocks_models/find_goldilocks_models.cc
+++ examples/goldilocks_models/find_goldilocks_models.cc
@@ -45,7 +45,7 @@ using dairlib::FindResourceOrThrow;
 namespace dairlib::goldilocks_models {
 
 // Robot models
-DEFINE_int32(robot_option, 1, "0: plannar robot. 1: cassie_fixed_spring");
+DEFINE_int32(robot_option, 0, "0: plannar robot. 1: cassie_fixed_spring");
 // Reduced order models
 DEFINE_int32(rom_option, -1, "");
 
@@ -233,13 +233,13 @@ void setInitialTheta(VectorXd& theta_s, VectorXd& theta_sDDot,
 }
 
 //edited by Jianshu to try a new way of setting initial guess
-string set_initial_guess(const string directory, int iter, int sample,int total_sample_num=1)
+string set_initial_guess(const string directory, int iter, int sample, int total_sample_num)
 {
 /* define some parameters used in interpolation
 * theta_range :decide the range of theta to use in interpolation
 * theta_sclae,gamma_scale :used to scale the theta and gamma in interpolation
 */
-    double theta_range = 0.2;
+    double theta_range = 0.1;
     double theta_scale = 1;
     double gamma_scale = 1;
     int gamma_dimension = 2;
@@ -272,16 +272,26 @@ string set_initial_guess(const string directory, int iter, int sample,int total_
                 past_gamma << past_ground_incline(0,0),past_stride_length(0,0);
                 double distance = ((past_theta - current_theta)/theta_scale).squaredNorm()
                                   + ((past_gamma - current_gamma)/gamma_scale).squaredNorm();
+//                debug for getting wrong weight
+                if(distance == 0){
+                    return to_string(iter-1)+"_"+to_string(sample)+string("_w.csv");
+                }
                 weight.conservativeResize(weight.rows()+1);
                 weight(weight.rows()-1) = 1 / sqrt(distance);
-                w_near.conservativeResize(w_near.rows(),w_near.cols()+1);
-                w_near.rightCols(1) = readCSV(directory+to_string(past_iter)+string("_")
-                        +to_string(sample_num)+string("_w.csv"));
+                VectorXd w_to_interpolate = readCSV(directory+to_string(past_iter)+string("_")
+                                                    +to_string(sample_num)+string("_w.csv"));
+                w_near.conservativeResize(w_to_interpolate.rows(),w_near.cols()+1);
+                w_near.col(w_near.cols()-1) = w_to_interpolate;
             }
         }
         else {
             break;
         }
+    }
+//    debug for no appropriate theta for interpolation
+    if(weight.rows()==0){
+        cout<<"wrong setting of theta range";
+        return to_string(iter-1)+"_"+to_string(sample)+string("_w.csv");
     }
 //    normalize weight
     weight = weight/weight.sum();
@@ -293,7 +303,7 @@ string set_initial_guess(const string directory, int iter, int sample,int total_
     return initial_file_name;
 }
 
-void getInitFileName(const string dir, string * init_file, const string & nominal_traj_init_file,
+void getInitFileName(const string dir,int total_sample_num, string * init_file, const string & nominal_traj_init_file,
                      int iter, int sample, bool is_get_nominal,
                      bool rerun_current_iteration, bool has_been_all_success,
                      bool step_size_shrinked_last_loop, int n_rerun,
@@ -309,12 +319,11 @@ void getInitFileName(const string dir, string * init_file, const string & nomina
                  string("_w.csv");
   } else if (rerun_current_iteration) {
     *init_file = to_string(iter) + "_" + to_string(sample) + string("_w.csv");
-  } else {
-    *init_file = to_string(iter - 1) +  "_" +
-                 to_string(sample) + string("_w.csv");
-
-//      modified by Jianshu
-//    *init_file = set_initial_guess(dir, iter, sample);
+  }else{
+//      modified by Jianshu to test new initial guess
+//      *init_file = to_string(iter - 1) +  "_" +
+//                   to_string(sample) + string("_w.csv");
+      *init_file = set_initial_guess(dir, iter, sample, total_sample_num);
   }
 
   // Testing:
@@ -1968,7 +1977,8 @@ int findGoldilocksModels(int argc, char* argv[]) {
 
           // Get file name of initial seed
           string init_file_pass_in;
-          getInitFileName(dir, &init_file_pass_in, init_file, iter, sample_idx,
+          int total_sample_num = N_sample_sl*N_sample_gi;
+          getInitFileName(dir, total_sample_num, &init_file_pass_in, init_file, iter, sample_idx,
                           is_get_nominal,
                           current_sample_is_a_rerun, has_been_all_success,
                           step_size_shrinked_last_loop, n_rerun[sample_idx],
@@ -2550,8 +2560,84 @@ int findGoldilocksModels(int argc, char* argv[]) {
   return 0;
 }  // int findGoldilocksModels
 
+//used for testing the result of set_initial_guess
+string test_set_initial_guess(int argc, char* argv[])
+{
+/* define some parameters used in interpolation
+* theta_range :decide the range of theta to use in interpolation
+* theta_sclae,gamma_scale :used to scale the theta and gamma in interpolation
+*/
+    const string directory = "../dairlib_data/goldilocks_models/find_models/robot_" +
+                       to_string(0) + "/";
+    int iter = 2;
+    int sample = 0;
+    int total_sample_num = 1;
+    double theta_range = 1;
+    double theta_scale = 1;
+    double gamma_scale = 1;
+    int gamma_dimension = 2;
+//    initialize variables used for setting initial guess
+    VectorXd initial_guess;
+    VectorXd weight;
+    MatrixXd w_near;
+    int past_iter;
+    int sample_num;
+//    get theta of current iteration and task of current sample
+    VectorXd current_theta = readCSV(directory+to_string(iter)+string("_theta_s.csv"));
+    MatrixXd current_ground_incline = readCSV(directory+to_string(iter)+string("_")+to_string(sample)
+                                              +string("_ground_incline.csv"));
+    MatrixXd current_stride_length = readCSV(directory+to_string(iter)+string("_")+to_string(sample)
+                                             +string("_stride_length.csv"));
+    VectorXd current_gamma(gamma_dimension);
+    current_gamma << current_ground_incline(0,0),current_stride_length(0,0);
+    for(past_iter=iter-1;past_iter>=0;past_iter--) {
+//        find useful theta according to the difference between previous theta and new theta
+        VectorXd past_theta = readCSV(directory+to_string(past_iter)+string("_theta_s.csv"));
+        double theta_diff = (past_theta - current_theta).norm() / current_theta.norm();
+        if (theta_diff < theta_range) {
+//            take out corresponding w and calculate the weight for interpolation
+            for (sample_num = 0; sample_num < total_sample_num; sample_num++) {
+                MatrixXd past_ground_incline = readCSV(directory+to_string(past_iter)+string("_")
+                                                       +to_string(sample_num)+string("_ground_incline.csv"));
+                MatrixXd past_stride_length = readCSV(directory+to_string(past_iter)+string("_")
+                                                      +to_string(sample_num)+string("_stride_length.csv"));
+                VectorXd past_gamma(gamma_dimension);
+                past_gamma << past_ground_incline(0,0),past_stride_length(0,0);
+                double distance = ((past_theta - current_theta).squaredNorm())/theta_scale
+                                  + (past_gamma - current_gamma).squaredNorm()/gamma_scale;
+                cout<<"theta distance"<<(past_theta - current_theta).squaredNorm()<<endl;
+                cout<<"gamma distance"<<(past_gamma - current_gamma).squaredNorm()<<endl;
+                cout<<"distance"<<distance<<endl;
+                weight.conservativeResize(weight.rows()+1);
+                weight(weight.rows()-1) = 1 / sqrt(distance);
+                VectorXd w_to_interpolate = readCSV(directory+to_string(past_iter)+string("_")
+                                              +to_string(sample_num)+string("_w.csv"));
+                w_near.conservativeResize(w_to_interpolate.rows(),w_near.cols()+1);
+                w_near.col(w_near.cols()-1) = w_to_interpolate;
+            }
+        }
+        else {
+            break;
+        }
+    }
+//    normalize weight
+    weight = weight/weight.sum();
+    initial_guess = w_near*weight;
+    cout<<"w_near"<<w_near<<endl;
+//    cout<<initial_guess<<endl;
+//    save initial guess and set init file
+    string initial_file_name = to_string(iter)+"_"+to_string(sample)+string("_initial_guess.csv");
+    writeCSV(directory+initial_file_name,initial_guess);
+
+    return initial_file_name;
+}
+
+
 }  // namespace dairlib::goldilocks_models
 
 int main(int argc, char* argv[]) {
   return dairlib::goldilocks_models::findGoldilocksModels(argc, argv);
+//    string init = dairlib::goldilocks_models::test_set_initial_guess(argc,argv);
+//    cout<<init<<endl;
+//    return 0;
 }

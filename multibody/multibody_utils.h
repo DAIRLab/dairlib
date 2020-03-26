358c25ee48037c0b097661f65f04d464ddd4ef55
Yu-Ming Chen <yminchen@umich.edu>
2020-03-26 11:34:58 -0400

    Fix a bug in initial normal direction

--- multibody/multibody_utils.h
+++ multibody/multibody_utils.h
@@ -22,7 +22,7 @@ template <typename T>
 void addTerrain(drake::multibody::MultibodyPlant<T>* plant,
                 drake::geometry::SceneGraph<T>* scene_graph,
                 double mu_static, double mu_kinetic,
-                Eigen::Vector3d normal_W = Eigen::Vector3d::Zero());
+                Eigen::Vector3d normal_W = Eigen::Vector3d(0, 0 , 1));
 
 /// Given a MultiBodyTree, builds a map from position name to position index
 template <typename T>

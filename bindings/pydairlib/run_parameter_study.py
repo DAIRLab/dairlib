ba4bed33170fdc02582df35e818d38167a12b161
William Yang <yangwill@seas.upenn.edu>
2020-04-30 16:44:23 -0400

    making sure to use floats when inserting into strings

--- bindings/pydairlib/run_parameter_study.py
+++ bindings/pydairlib/run_parameter_study.py
@@ -13,20 +13,22 @@ def main():
     controller_cmd = ['bazel-bin/examples/five_link_biped/run_lqr',
                 '--folder_path=%s' % folder_path,
                 '--trajectory_name=%s' % trajectory_name,
-                '--time_offset=%d' % 0.00,
+                '--time_offset=%f' % 0.00,
                 ]
     simulator_cmd = ['bazel-bin/examples/five_link_biped/rabbit_sim',
                 '--folder_path=%s' % folder_path,
                 '--trajectory_name=%s' % trajectory_name,
-                '--sim_time=%d' % sim_time,
-                '--dt=%d' % 1e-5,
-                '--penetration_allowance=%d' % penetration_allowance,
+                '--sim_time=%f' % sim_time,
+                '--dt=%f' % 1e-5,
+                '--penetration_allowance=%f' % penetration_allowance,
                 ]
     lcm_logger_cmd = ['lcm-logger',
                 '-f',
                 '%s' % log_path,
                 ]
 
+    # print(simulator_cmd)
+    # import pdb; pdb.set_trace()
     simulator_process = subprocess.run(simulator_cmd)
     time.sleep(3)
     controller_process = subprocess.run(controller_cmd)

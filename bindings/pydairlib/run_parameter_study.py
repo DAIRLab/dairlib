c7e1ef315ae7c935459b940b3f85918ebac77d4c
William Yang <yangwill@seas.upenn.edu>
2020-05-08 10:42:27 -0400

    varying ground stiffness as another parameter

--- bindings/pydairlib/run_parameter_study.py
+++ bindings/pydairlib/run_parameter_study.py
@@ -9,9 +9,10 @@ def main():
     folder_path = "/home/yangwill/Documents/research/projects/five_link_biped/hybrid_lqr/saved_trajs/"
     trajectory_name = "walking_4_24"
     log_folder = "/home/yangwill/Documents/research/projects/five_link_biped" \
-               "/hybrid_lqr/logs/parameter_study/rfoot_z_velocity/"
+               "/hybrid_lqr/logs/parameter_study/vary_ground_penetration" \
+                 "/rfoot_z_velocity_adjusted/"
     sim_time = 1.0
-    penetration_allowance = 2e-4
+    penetration_allowances = 2e-4
     # error_idx = 1 + 7
     # error_idx = 14, 15, 16, 17
     # order goes: lfoot x, rfoot x, lfoot z, rfoot z
@@ -20,7 +21,9 @@ def main():
 
     # time_offsets = np.linspace(0.0, 0.01, 21)
     # x_error_values = np.linspace(-0.05, 0.05, 11)
-    time_offsets = np.linspace(0.0, 0.01, 21)
+    # time_offsets = np.linspace(0.0, 0.01, 21)
+    penetration_allowances = np.array([1e-5, 1e-4, 2e-4])
+    time_offsets = np.linspace(0.0, 0.005, 11)
     x_error_values = np.linspace(-0.05, 0.05, 11)
     # x_error_values = np.array([0.0])
     parameter_file = open(log_folder + 'command_list.txt', 'w')
@@ -29,44 +32,48 @@ def main():
 
     for i in range(time_offsets.shape[0]):
         for j in range(x_error_values.shape[0]):
-            log_path = log_folder + 'lcmlog-delay_%.4f-error_%.2f' % (
-                    time_offsets[i], x_error_values[j])
-            print(log_path)
-            controller_cmd = ['bazel-bin/examples/five_link_biped/run_lqr',
-                              '--contact_driven=%d' % False,
-                              '--folder_path=%s' % folder_path,
-                              '--trajectory_name=%s' % trajectory_name,
-                              '--time_offset=%.4f' % time_offsets[i],
-                              ]
-            simulator_cmd = ['bazel-bin/examples/five_link_biped/rabbit_sim',
-                             '--folder_path=%s' % folder_path,
-                             '--trajectory_name=%s' % trajectory_name,
-                             '--publish_rate=%d' % 8000.0,
-                             '--sim_time=%.3f' % sim_time,
-                             '--dt=%.5f' % 1e-5,
-                             '--penetration_allowance=%.4f' %
-                             penetration_allowance,
-                             '--start_time=%.3f' % start_time,
-                             '--error_idx=%d' % error_idx,
-                             '--error=%.3f' % x_error_values[j]
-                             ]
-            lcm_logger_cmd = ['lcm-logger',
-                              '-f',
-                              '%s' % log_path,
-                              ]
-            parameter_file.write(log_path + '\n')
-            parameter_file.write(' '.join(controller_cmd) + '\n')
-            parameter_file.write(' '.join(simulator_cmd) + '\n')
-            parameter_file.write('**********\n')
-            # print(log_path)
-            # print(controller_cmd)
-            controller_process = subprocess.Popen(controller_cmd)
-            logger_process = subprocess.Popen(lcm_logger_cmd)
-            time.sleep(3)
-            simulator_process = subprocess.Popen(simulator_cmd)
-            time.sleep(5)
-            controller_process.kill()
-            logger_process.kill()
+            for k in range(penetration_allowances.shape[0]):
+                log_path = log_folder + \
+                           'lcmlog-delay_%.4f-error_%.2f_-pen_%.5f' % (
+                        time_offsets[i], x_error_values[j],
+                        penetration_allowances[k])
+                print(log_path)
+                controller_cmd = ['bazel-bin/examples/five_link_biped/run_lqr',
+                                  '--contact_driven=%d' % False,
+                                  '--folder_path=%s' % folder_path,
+                                  '--naive=%d' % True,
+                                  '--trajectory_name=%s' % trajectory_name,
+                                  '--time_offset=%.4f' % time_offsets[i],
+                                  ]
+                simulator_cmd = ['bazel-bin/examples/five_link_biped/rabbit_sim',
+                                 '--folder_path=%s' % folder_path,
+                                 '--trajectory_name=%s' % trajectory_name,
+                                 '--publish_rate=%d' % 8000.0,
+                                 '--sim_time=%.3f' % sim_time,
+                                 '--dt=%.5f' % 1e-5,
+                                 '--penetration_allowance=%.5f' %
+                                 penetration_allowances[k],
+                                 '--start_time=%.3f' % start_time,
+                                 '--error_idx=%d' % error_idx,
+                                 '--error=%.3f' % x_error_values[j]
+                                 ]
+                lcm_logger_cmd = ['lcm-logger',
+                                  # '-f',
+                                  '%s' % log_path,
+                                  ]
+                parameter_file.write(log_path + '\n')
+                parameter_file.write(' '.join(controller_cmd) + '\n')
+                parameter_file.write(' '.join(simulator_cmd) + '\n')
+                parameter_file.write('**********\n')
+                # print(log_path)
+                # print(controller_cmd)
+                controller_process = subprocess.Popen(controller_cmd)
+                logger_process = subprocess.Popen(lcm_logger_cmd)
+                time.sleep(3)
+                simulator_process = subprocess.Popen(simulator_cmd)
+                time.sleep(5)
+                controller_process.kill()
+                logger_process.kill()
     parameter_file.close()
 if __name__ == "__main__":
     main()

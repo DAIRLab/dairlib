2fe7085ca63030293591155f7a20f32635922600
posa <posa@seas.upenn.edu>
2020-05-28 22:10:49 -0400

    Add fixed point constraint.

--- multibody/multibody_solvers.cc
+++ multibody/multibody_solvers.cc
@@ -1,4 +1,5 @@
 #include "multibody/multibody_solvers.h"
+#include "multibody/multibody_utils.h"
 
 namespace dairlib {
 namespace multibody {
@@ -6,6 +7,7 @@ namespace multibody {
 using Eigen::VectorXd;
 using drake::multibody::MultibodyPlant;
 using drake::systems::Context;
+using drake::MatrixX;
 using drake::VectorX;
 using solvers::NonlinearConstraint;
 
@@ -14,21 +16,14 @@ KinematicPositionConstraint<T>::KinematicPositionConstraint(
     const MultibodyPlant<T>& plant,
     const std::vector<KinematicEvaluator<T>>& evaluators,
     std::shared_ptr<Context<T>> context, const std::string& description)
-    : NonlinearConstraint<T>(0, plant.num_positions(), VectorXd::Zero(0),
-          VectorXd::Zero(0), description),
+    : NonlinearConstraint<T>(KinematicEvaluator<T>::CountActive(evaluators),
+          plant.num_positions(),
+          VectorXd::Zero(KinematicEvaluator<T>::CountActive(evaluators)),
+          VectorXd::Zero(KinematicEvaluator<T>::CountActive(evaluators)),
+          description),
       plant_(plant), 
       evaluators_(evaluators),
-      context_(context) {
-  // Length and bounds set above are fake. Count up constraints now.
-  int num_constraints = 0;
-  for (const auto& e : evaluators_) {
-    num_constraints += e.num_active();
-  }
-
-  set_num_outputs(num_constraints);
-  set_bounds(VectorXd::Zero(num_constraints), VectorXd::Zero(num_constraints));
-
-}
+      context_(context) {}
 
 template <typename T>
 KinematicPositionConstraint<T>::KinematicPositionConstraint(
@@ -54,6 +49,66 @@ void KinematicPositionConstraint<T>::EvaluateConstraint(
     y->segment(ind, e.num_active()) = e.EvalActive(*context_);
     ind += e.num_active();
   }
+}
+
+template <typename T>
+FixedPointConstraint<T>::FixedPointConstraint(
+    const MultibodyPlant<T>& plant,
+    const std::vector<KinematicEvaluator<T>>& evaluators,
+    std::shared_ptr<Context<T>> context, const std::string& description)
+    : NonlinearConstraint<T>(plant.num_positions() + plant.num_velocities(),
+          plant.num_positions() + plant.num_velocities() + plant.num_actuators()
+          + KinematicEvaluator<T>::CountFull(evaluators),
+          VectorXd::Zero(plant.num_positions() + plant.num_velocities()),
+          VectorXd::Zero(plant.num_positions() + plant.num_velocities()),
+          description),
+      plant_(plant), 
+      evaluators_(evaluators),
+      context_(context) {}
+
+template <typename T>
+FixedPointConstraint<T>::FixedPointConstraint(
+    const MultibodyPlant<T>& plant,
+    const std::vector<KinematicEvaluator<T>>& evaluators,
+    const std::string& description) 
+    : FixedPointConstraint<T>(plant, evaluators,
+      std::shared_ptr<Context<T>>(plant_.CreateDefaultContext().release()),
+      description) {}
+
+template <typename T>
+void FixedPointConstraint<T>::EvaluateConstraint(
+    const Eigen::Ref<const VectorX<T>>& input, VectorX<T>* y) const {
+
+  auto u = input.segment(plant_.num_positions() + plant_.num_velocities(),
+      plant_.num_actuators());
+  auto lambda = input.segment(plant_.num_positions() + plant_.num_velocities()
+      + plant_.num_actuators(), KinematicEvaluator<T>::CountFull(evaluators_));
+
+  // input order is x, u, lambda
+  setContext(plant_,
+      input.head(plant_.num_positions() + plant_.num_velocities()),
+      u, context_.get());
+  
+  // Build kinematic Jacobian
+  MatrixX<T> J(lambda.size(), plant_.num_velocities());
+  int ind = 0;
+  for (const auto& e : evaluators_) {
+    J.block(ind, 0, e.length(), plant_.num_velocities()) =
+        e.EvalFullJacobian(*context_);
+    ind += e.length();
+  }
+
+  // right_hand_side is the right hand side of the system's equations:
+  // M*vdot -J^T*f = right_hand_side.
+  // BiasTerm is C(q,v) in manipulator equations
+  VectorX<T> generalized_forces(plant_.num_velocities());
+  plant_.CalcBiasTerm(*context_, &generalized_forces);
+
+  generalized_forces = -generalized_forces +
+      plant_.MakeActuationMatrix() * u +
+      plant_.CalcGravityGeneralizedForces(*context_) +
+      J.transpose() * lambda;
+  *y = generalized_forces;
 }
 
 }  // namespace multibody
--- multibody/multibody_solvers.h
+++ multibody/multibody_solvers.h
@@ -24,8 +24,31 @@ class KinematicPositionConstraint : public solvers::NonlinearConstraint<T> {
       const std::vector<KinematicEvaluator<T>>& evaluators,
       const std::string& description = "");
 
-  using drake::solvers::Constraint::set_bounds;
-  using drake::solvers::EvaluatorBase::set_num_outputs;
+  void EvaluateConstraint(const Eigen::Ref<const drake::VectorX<T>>& x,
+                                  drake::VectorX<T>* y) const = 0;
+
+ private:
+  const drake::multibody::MultibodyPlant<T>& plant_;
+  const std::vector<KinematicEvaluator<T>>& evaluators_;
+  std::shared_ptr<drake::systems::Context<T>> context_;
+};
+
+template <typename T>
+class FixedPointConstraint : public solvers::NonlinearConstraint<T> {
+ public:
+  /// This constructor takes a shared_ptr<Context> as an argument to share
+  /// cached kinematic/dynamic computation within the ccontext
+  FixedPointConstraint(
+      const drake::multibody::MultibodyPlant<T>& plant,
+      const std::vector<KinematicEvaluator<T>>& evaluators,
+      std::shared_ptr<drake::systems::Context<T>> context,
+      const std::string& description = "");
+
+  /// This constructor will build its own shared_ptr<Context>
+  FixedPointConstraint(
+      const drake::multibody::MultibodyPlant<T>& plant,
+      const std::vector<KinematicEvaluator<T>>& evaluators,
+      const std::string& description = "");
 
   void EvaluateConstraint(const Eigen::Ref<const drake::VectorX<T>>& x,
                                   drake::VectorX<T>* y) const = 0;

be75abb0c313550468fc6a7f616c862e5c98de54
Michael Posa <posa@seas.upenn.edu>
2020-07-03 20:25:03 -0400

    Early pass at attic deprecation (#177)
    -Add deprecation and manual Bazel flags to attic/
    -Remove some unused RBT references
    - Removed old RBT tests.

--- examples/Cassie/networking/cassie_input_translator.cc
+++ examples/Cassie/networking/cassie_input_translator.cc
@@ -35,24 +35,6 @@ CassieInputTranslator::CassieInputTranslator(
       "cassie_user_out_t", &CassieInputTranslator::Output);
 }
 
-CassieInputTranslator::CassieInputTranslator(
-    const RigidBodyTree<double>& tree) {
-  std::map<std::string, int> actuatorIndexMap =
-    multibody::makeNameToActuatorsMap(tree);
-  // build index map
-  DRAKE_ASSERT(kNumActuators == cassieEffortNames.size());
-  for (auto const& name : cassieEffortNames) {
-    userin_to_uvector_index_.push_back(actuatorIndexMap.at(name));
-  }
-
-  this->DeclareVectorInputPort("vector_command",
-      TimestampedVector<double>(kNumActuators));
-  this->DeclareAbstractOutputPort(
-      "cassie_user_out_t", &CassieInputTranslator::Output); 
-}
-
-
-
 void CassieInputTranslator::Output(const Context<double>& context,
     cassie_user_in_t* cassie_in) const {
   const TimestampedVector<double>* command = (TimestampedVector<double>*)

4deb7ab9d0bc8253bbd7881201fbc3944366041c
Yu-Ming Chen <yminchen@umich.edu>
2020-08-19 00:29:48 -0400

    Add multipose visualization for the planner

--- examples/goldilocks_models/planning/visualization/visualize_FoM_pose_MBP.cc
+++ examples/goldilocks_models/planning/visualization/visualize_FoM_pose_MBP.cc
@@ -1,28 +1,24 @@
-#include <gflags/gflags.h>
 #include <chrono>
 #include <memory>
 #include <string>
+#include <gflags/gflags.h>
 
-#include "drake/systems/analysis/simulator.h"
-#include "drake/systems/framework/diagram.h"
-#include "drake/systems/framework/diagram_builder.h"
-#include "drake/systems/primitives/trajectory_source.h"
-#include "drake/lcm/drake_lcm.h"
-#include "drake/geometry/geometry_visualization.h"
-#include "drake/multibody/parsing/parser.h"
-#include "drake/systems/rendering/multibody_position_to_geometry_pose.h"
-#include "common/find_resource.h"
-#include "systems/primitives/subvector_pass_through.h"
-#include "multibody/multibody_utils.h"
-#include "multibody/visualization_utils.h"
-#include "drake/lcm/drake_lcm.h"
-#include "drake/systems/analysis/simulator.h"
-#include "drake/systems/framework/diagram.h"
-#include "drake/systems/framework/diagram_builder.h"
-#include "drake/systems/primitives/trajectory_source.h"
-#include "drake/common/trajectories/piecewise_polynomial.h"
 #include "common/file_utils.h"
+#include "common/find_resource.h"
 #include "examples/goldilocks_models/goldilocks_utils.h"
+#include "multibody/multibody_utils.h"
+#include "multibody/multipose_visualizer.h"
+#include "multibody/visualization_utils.h"
+#include "systems/primitives/subvector_pass_through.h"
+#include "drake/common/trajectories/piecewise_polynomial.h"
+#include "drake/geometry/geometry_visualization.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/framework/diagram.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/primitives/trajectory_source.h"
+#include "drake/systems/rendering/multibody_position_to_geometry_pose.h"
 
 using drake::geometry::SceneGraph;
 using drake::multibody::Body;
@@ -48,6 +44,18 @@ namespace goldilocks_models {
 namespace planning {
 
 DEFINE_int32(robot_option, 0, "0: plannar robot. 1: cassie_fixed_spring");
+
+// There are two modes of this visualizer.
+// The first mode visualizes all poses at once, and the second mode visualizes
+// them in sequence
+DEFINE_bool(view_multipose_at_once, false, "Visualize the poses at once");
+
+// Flags for the first mode
+DEFINE_bool(only_start_and_end_poses, false,
+            "Visualize only the start and the end poses");
+DEFINE_double(alpha, 0.15, "Transparency of the robots");
+
+// Flags for the second mode
 DEFINE_int32(start_mode, 0, "Starting at mode #");
 DEFINE_bool(start_is_head, true, "Starting with x0 or xf");
 DEFINE_int32(end_mode, -1, "Ending at mode #");
@@ -70,51 +78,69 @@ void visualizeFullOrderModelPose(int argc, char* argv[]) {
   MatrixXd x0_each_mode = readCSV(directory + string("x0_each_mode.csv"));
   MatrixXd xf_each_mode = readCSV(directory + string("xf_each_mode.csv"));
 
-  bool is_head = FLAGS_start_is_head;
-  bool last_visited_mode = -1;
-  for (int mode = FLAGS_start_mode; mode <= end_mode; mode++) {
-    cout << "(mode, is_head) = (" << mode << ", " << is_head << ")\n";
-    if (last_visited_mode != mode) {
-      last_visited_mode = mode;
-    }
-
-    // Create a testing piecewise polynomial
-    std::vector<double> T_breakpoint{0, FLAGS_step_time};
-    std::vector<MatrixXd> Y;
-    if (is_head) {
-      Y.push_back(x0_each_mode.col(mode));
-      Y.push_back(x0_each_mode.col(mode));
-      cout << "x0 = \n" << x0_each_mode.col(mode).transpose() << endl;
+  if (FLAGS_view_multipose_at_once) {
+    MatrixXd poses;
+    if (FLAGS_only_start_and_end_poses) {
+      poses.resize(x0_each_mode.rows(), 2);
+      poses << x0_each_mode.leftCols<1>(), xf_each_mode.rightCols<1>();
     } else {
-      Y.push_back(xf_each_mode.col(mode));
-      Y.push_back(xf_each_mode.col(mode));
-      cout << "xf = \n" << xf_each_mode.col(mode).transpose() << endl;
+      poses.resize(x0_each_mode.rows(), x0_each_mode.cols() + 1);
+      poses << x0_each_mode, xf_each_mode.rightCols<1>();
     }
-    PiecewisePolynomial<double> pp_xtraj =
-        PiecewisePolynomial<double>::FirstOrderHold(T_breakpoint, Y);
+    VectorXd alpha_vec = FLAGS_alpha * VectorXd::Ones(poses.cols());
+    alpha_vec.head(1) << 1;
+    alpha_vec.tail(1) << 1;
+    multibody::MultiposeVisualizer visualizer = multibody::MultiposeVisualizer(
+        FindResourceOrThrow("examples/Cassie/urdf/cassie_fixed_springs.urdf"),
+        poses.cols(), alpha_vec);
+    visualizer.DrawPoses(poses);
+  } else {
+    bool is_head = FLAGS_start_is_head;
+    bool last_visited_mode = -1;
+    for (int mode = FLAGS_start_mode; mode <= end_mode; mode++) {
+      cout << "(mode, is_head) = (" << mode << ", " << is_head << ")\n";
+      if (last_visited_mode != mode) {
+        last_visited_mode = mode;
+      }
 
-    // Create MBP for visualization
-    drake::systems::DiagramBuilder<double> builder;
-    MultibodyPlant<double> plant_vis(0.0);
-    SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
-    Vector3d ground_normal(0, 0, 1);
-    CreateMBPForVisualization(&plant_vis, &scene_graph, ground_normal,
-                              FLAGS_robot_option);
+      // Create a testing piecewise polynomial
+      std::vector<double> T_breakpoint{0, FLAGS_step_time};
+      std::vector<MatrixXd> Y;
+      if (is_head) {
+        Y.push_back(x0_each_mode.col(mode));
+        Y.push_back(x0_each_mode.col(mode));
+        cout << "x0 = \n" << x0_each_mode.col(mode).transpose() << endl;
+      } else {
+        Y.push_back(xf_each_mode.col(mode));
+        Y.push_back(xf_each_mode.col(mode));
+        cout << "xf = \n" << xf_each_mode.col(mode).transpose() << endl;
+      }
+      PiecewisePolynomial<double> pp_xtraj =
+          PiecewisePolynomial<double>::FirstOrderHold(T_breakpoint, Y);
 
-    // visualizer
-    multibody::connectTrajectoryVisualizer(&plant_vis, &builder, &scene_graph,
-                                           pp_xtraj);
-    auto diagram = builder.Build();
-    // while (true)
-    drake::systems::Simulator<double> simulator(*diagram);
-    simulator.set_target_realtime_rate(1);
-    simulator.Initialize();
-    simulator.AdvanceTo(pp_xtraj.end_time());
+      // Create MBP for visualization
+      drake::systems::DiagramBuilder<double> builder;
+      MultibodyPlant<double> plant_vis(0.0);
+      SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
+      Vector3d ground_normal(0, 0, 1);
+      CreateMBPForVisualization(&plant_vis, &scene_graph, ground_normal,
+                                FLAGS_robot_option);
 
-    if ((is_head == FLAGS_end_is_head) && (mode == end_mode)) continue;
-    if (is_head == true) mode--;
+      // visualizer
+      multibody::connectTrajectoryVisualizer(&plant_vis, &builder, &scene_graph,
+                                             pp_xtraj);
+      auto diagram = builder.Build();
+      // while (true)
+      drake::systems::Simulator<double> simulator(*diagram);
+      simulator.set_target_realtime_rate(1);
+      simulator.Initialize();
+      simulator.AdvanceTo(pp_xtraj.end_time());
 
-    is_head = !is_head;
+      if ((is_head == FLAGS_end_is_head) && (mode == end_mode)) continue;
+      if (is_head == true) mode--;
+
+      is_head = !is_head;
+    }
   }
 
   return;

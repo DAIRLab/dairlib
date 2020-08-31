0e69614d842bb326e835d6bbcd1ad1ab9fda2f0d
Jianshu-Hu <jianshuhu98@gmail.com>
2020-08-31 15:09:41 -0400

    update plotting functions

--- examples/goldilocks_models/find_boundary_utils/compare_boundary.py
+++ examples/goldilocks_models/find_boundary_utils/compare_boundary.py
@@ -14,15 +14,15 @@ import numpy as np
 import os
 
 robot_option = 1
-file_dir = '/Users/jason-hu/'
+file_dir = "../dairlib_data/goldilocks_models/find_boundary/"
 if robot_option == 1:
     robot = 'cassie/'
 else:
     robot = 'five_link/'
-dir1 = file_dir + 'dairlib_data/find_boundary/' + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + \
-        '_range1_iter300/'
-dir2 = file_dir + 'dairlib_data/find_boundary/' + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + \
-       '_grid_iter50_sl_tr/'
+dir1 = file_dir + robot + '2D_rom/2D_task_space/' + 'robot_' + str(robot_option) + \
+        '_large_iter100/'
+dir2 = file_dir + robot + '2D_rom/2D_task_space/' + 'robot_' + str(robot_option) + \
+       '_small_iter200/'
 
 # number of searching directions
 n_direction = 16
@@ -38,79 +38,85 @@ plot_optimization_range = 0
 # Eg. column index 0 corresponds to stride length
 task_name = ['Stride length', 'Ground incline', 'Velocity', 'Turning rate']
 task_1_idx = 0
-task_2_idx = 3
+task_2_idx = 1
 
-
-def process_data_from_direction(dir1, dir_nominal):
+def process_data_from_direction(i, dir1, dir_nominal):
     # need to add central point on the points list
     task0 = np.genfromtxt(dir1 + str(0) + '_' + str(0) + '_task.csv', delimiter=",")
-    x0 = [task0[task_1_idx]]
-    y0 = [task0[task_2_idx]]
-    cost1 = np.genfromtxt(dir1 + str(0) + '_' + str(0) + '_c.csv', delimiter=",")
-    cost2 = np.genfromtxt(dir_nominal + str(0) + '_' + str(0) + '_c.csv', delimiter=",")
-    z0 = [cost1 / cost2]
-    for i in range(n_direction):
-        data_dir1 = np.genfromtxt(dir1 + str(i+1) + '_cost_list.csv', delimiter=",")
-        data_dir2 = np.genfromtxt(dir_nominal + str(i+1) + '_cost_list.csv', delimiter=",")
+    x = [task0[task_1_idx]]
+    y = [task0[task_2_idx]]
+    cost1 = float(np.genfromtxt(dir1 + str(0) + '_' + str(0) + '_c.csv', delimiter=","))
+    cost2 = float(np.genfromtxt(dir_nominal + str(0) + '_' + str(0) + '_c.csv', delimiter=","))
+    z = [cost1 / cost2]
 
+    data_dir1 = np.genfromtxt(dir1 + str(int(i+1)) + '_cost_list.csv', delimiter=",")
+    data_dir2 = np.genfromtxt(dir_nominal + str(int(i+1)) + '_cost_list.csv', delimiter=",")
+
+    if data_dir1.shape[0] >= data_dir2.shape[0]:
+        num_small = data_dir2.shape[0]
+        num_large = data_dir1.shape[0]
+    else:
+        num_small = data_dir1.shape[0]
+        num_large = data_dir2.shape[0]
+
+    # process the points on the line
+    # set the value for intersected parts
+    for j in range(num_small):
+        cost1 = data_dir1[j, 1]
+        cost2 = data_dir2[j, 1]
+        # we only append reasonable point
+        if (cost1 < 35) & (cost2 < 35) & (cost1/cost2 < 4):
+            task = np.genfromtxt(dir1 + str(int(data_dir1[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
+            x.append(task[task_1_idx])
+            y.append(task[task_2_idx])
+            z.append(cost1/cost2)
+    for j in range(num_small, num_large):
         if data_dir1.shape[0] >= data_dir2.shape[0]:
-            num_small = data_dir2.shape[0]
-            num_large = data_dir1.shape[0]
+            # extended range
+            task = np.genfromtxt(dir1 + str(int(data_dir1[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
+            x.append(task[task_1_idx])
+            y.append(task[task_2_idx])
+            z.append(-1)
         else:
-            num_small = data_dir1.shape[0]
-            num_large = data_dir2.shape[0]
+            task = np.genfromtxt(dir_nominal + str(int(data_dir2[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
+            x.append(task[task_1_idx])
+            y.append(task[task_2_idx])
+            z.append(2.5)
+    return x, y, z
 
-        # process the points on the line
-        x = []
-        y = []
-        z = []
 
-        if num_small > 10:
-            # set the value for intersected parts
-            for j in range(num_small):
-                cost1 = data_dir1[j, 1]
-                cost2 = data_dir2[j, 1]
-                # only consider reasonable point
-                if (cost1 < 30) & (cost2 < 30):
-                    z.append(cost1 / cost2)
-                    task = np.genfromtxt(dir1 + str(int(data_dir1[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
-                    x.append(task[task_1_idx])
-                    y.append(task[task_2_idx])
-            for j in range(num_small, num_large):
-                if data_dir1.shape[0] >= data_dir2.shape[0]:
-                    # extended range
-                    task = np.genfromtxt(dir1 + str(int(data_dir1[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
-                    x.append(task[task_1_idx])
-                    y.append(task[task_2_idx])
-                    z.append(-0.5)
-                else:
-                    # shrunk range
-                    task = np.genfromtxt(dir_nominal + str(int(data_dir2[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
-                    x.append(task[task_1_idx])
-                    y.append(task[task_2_idx])
-                    z.append(2.5)
-            x0 = x0 + x
-            y0 = y0 + y
-            z0 = z0 + z
-    return np.array(x0), np.array(y0), np.array(z0)
+def generateplot(dir1, dir_nominal, adj_index):
+    # discrete color map
+    levels = [0, 0.7, 0.8, 0.9, 1, 2]
+    colors = ['darkgreen', 'green', 'seagreen', 'mediumseagreen', 'blue']
+    cmap, norm = matplotlib.colors.from_levels_and_colors(levels, colors)
+    cmap.set_over('yellow')
+    cmap.set_under('red')
+
+    total_max = 0
+    for i in range(n_direction):
+        # process data on one line
+        x1, y1, z1 = process_data_from_direction(i, dir1, dir_nominal)
+        # process data on adjacent line
+        x2, y2, z2 = process_data_from_direction(adj_index[i], dir1,dir_nominal)
+        # plot
+        x = x1+x2
+        y = y1+y2
+        z = z1+z2
+        surf = ax.tricontourf(x, y, z, cmap=cmap, norm=norm, levels=levels, extend='both')
+        if max(z) > total_max:
+            total_max = max(z)
+    print('max', total_max)
+    cbar = fig.colorbar(surf, shrink=0.9, aspect=10, extend='both')
+    cbar.ax.set_yticklabels(['0', '0.7', '0.8', '0.9', '1', 'Inf'])
 
 
 # plt.rcParams.update({'font.size': 28})
 # fig, ax = plt.subplots(figsize=(11,5.5))
 fig, ax = plt.subplots()
-x, y, z = process_data_from_direction(dir1, dir2)
 # ax.scatter(x, y)
-# discrete color map
-print('max', max(z))
-levels = [0, 0.25, 0.5, 0.75, 1, 2]
-colors = ['darkgreen', 'green', 'seagreen', 'mediumseagreen', 'blue']
-cmap, norm = matplotlib.colors.from_levels_and_colors(levels, colors)
-cmap.set_over('yellow')
-cmap.set_under('red')
-surf = ax.tricontourf(x, y, z, cmap=cmap, norm=norm, levels=levels, extend='both')
-cbar = fig.colorbar(surf, shrink=0.9, aspect=10, extend='both')
-cbar.ax.set_yticklabels(['0', '0.25', '0.5', '0.75', '1', 'Inf'])
-
+adjacent = np.genfromtxt('adjacent.csv', delimiter=",")
+generateplot(dir1, dir2, adjacent)
 ax.set_xlabel(task_name[task_1_idx])
 ax.set_ylabel(task_name[task_2_idx])
 ax.set_title('Compare two cost landscapes')
--- examples/goldilocks_models/find_boundary_utils/plot_landscape.py
+++ examples/goldilocks_models/find_boundary_utils/plot_landscape.py
@@ -1,73 +1,94 @@
+"""
+Given points on two adjacent lines, fill the region between two lines with the color corresponding to the cost.
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 robot_option = 1
-file_dir = "/home/jianshu/workspace/dairlib_data/goldilocks_models/find_boundary/"
-# file_dir = "../dairlib_data/goldilocks_models/find_boundary/"
-
-# optimization setting
-# large range
-min_gi1 = -0.325
-max_gi1 = 0.275
-min_sl1 = 0.0925
-max_sl1 = 0.3925
-# small range
-min_gi2 = -0.175
-max_gi2 = 0.125
-min_sl2 = 0.1675
-max_sl2 = 0.3175
-plot_large_range = 0
-plot_small_range = 0
-
-if robot_option == 0:
-    robot = 'five_link/'
-    dir = file_dir + robot + 'robot_' + str(robot_option) + '_nominal/'
+file_dir = "../dairlib_data/goldilocks_models/find_boundary/"
 if robot_option == 1:
     robot = 'cassie/'
-    dir = file_dir + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + '_grid_iter50_sl_tr/'
+    dir1 = file_dir + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + \
+           '_2d_nominal/'
+    dir2 = file_dir + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + \
+           '_2d_initial_model/'
+    dir3 = file_dir + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + \
+           '_large_iter100/'
+    dir4 = file_dir + robot + '2D_rom/4D_task_space/' + 'robot_' + str(robot_option) + \
+           '_small_iter200/'
+
+# number of searching directions
+n_direction = 16
+
+# Note: we need to decide which column of the searching direction to use
+# Eg. column index 0 corresponds to stride length
+task_name = ['Stride length', 'Ground incline', 'Velocity', 'Turning rate']
+task_1_idx = 0
+task_2_idx = 1
+
+def ExtractAdjacentLine(dir):
+    adj_direction = np.zeros([n_direction]).astype(int)
+    for i in range(n_direction):
+        min_sin = 1
+        line1 = np.genfromtxt(dir + str(i + 1) + '_searching_direction.csv', delimiter=",")
+        vec1 = np.array([line1[task_1_idx], line1[task_2_idx]])
+        for j in range(n_direction):
+            line2 = np.genfromtxt(dir + str(j+1) + '_searching_direction.csv', delimiter=",")
+            vec2 = np.array([line2[task_1_idx], line2[task_2_idx]])
+            sin = np.cross(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+            cos = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+            # restrict the direction
+            if (cos > 0) & (sin > 0):
+                # find the adjacent pair
+                if sin < min_sin:
+                    adj_index = j
+                    min_sin = sin
+        adj_direction[i] = adj_index
+    return adj_direction
 
 
-i = 0
-x = []
-y = []
-z = []
-while os.path.isfile(dir+str(i)+'_'+str(0)+'_task.csv'):
-    gamma = np.genfromtxt(dir + str(i) + '_' + str(0) + '_task.csv', delimiter=",")
-    cost = float(np.genfromtxt(dir + str(i) + '_' + str(0) + '_c.csv', delimiter=","))
-    # only consider reasonable point
-    if cost < 35:
-        x.append(gamma[0])
-        y.append(gamma[3])
-        z.append(cost)
-    i = i+1
+def process_data_from_direction(i, dir1):
+    data_dir = np.genfromtxt(dir1 + str(int(i+1)) + '_cost_list.csv', delimiter=",")
+
+    # need to add central point on the points list
+    task0 = np.genfromtxt(dir1 + str(0) + '_' + str(0) + '_task.csv', delimiter=",")
+    x = [task0[task_1_idx]]
+    y = [task0[task_2_idx]]
+    z = [float(np.genfromtxt(dir1 + str(0) + '_' + str(0) + '_c.csv', delimiter=","))]
+
+    # process the points on the line
+    for j in range(data_dir.shape[0]):
+        task = np.genfromtxt(dir1 + str(int(data_dir[j, 0])) + '_' + str(0) + '_task.csv', delimiter=",")
+        cost = data_dir[j, 1]
+        if cost < 35:
+            x.append(task[task_1_idx])
+            y.append(task[task_2_idx])
+            z.append(cost)
+
+    return x, y, z
+
+
+def generateplot(dir1, adj_index):
+    for i in range(n_direction):
+        # process data on one line
+        x1, y1, z1 = process_data_from_direction(i, dir1)
+        # process data on adjacent line
+        x2, y2, z2 = process_data_from_direction(adj_index[i], dir1)
+        # plot
+        x = x1+x2
+        y = y1+y2
+        z = z1+z2
+        surf = ax.tricontourf(x, y, z, levels=[0, 4, 8, 12, 16, 20, 24, 28, 32])
+    fig.colorbar(surf, shrink=0.5, aspect=6)
+
+# adjacent = ExtractAdjacentLine(dir1)
+# np.savetxt('adjacent.csv', adjacent, delimiter=',')
 
 fig, ax = plt.subplots()
-
-print(np.shape(x))
-print(np.shape(y))
-print(np.shape(z))
-
-# scatter
-# surf = ax.scatter(x, y, z)
-
-# continuous color map
-surf = ax.tricontourf(x, y, z)
-fig.colorbar(surf, shrink=0.5, aspect=6)
-ax.set_xlabel('Stride length')
-ax.set_ylabel('Turning rate')
+ax.set_xlabel(task_name[task_1_idx])
+ax.set_ylabel(task_name[task_2_idx])
 ax.set_title('cost landscape')
-
-# optimization range
-# large range
-if plot_large_range == 1:
-    x_line = np.array([min_sl1, max_sl1, max_sl1, min_sl1, min_sl1])
-    y_line = np.array([min_gi1, min_gi1, max_gi1, max_gi1, min_gi1])
-    ax.plot(x_line, y_line, 'black', linewidth=3)
-# small range
-if plot_small_range == 1:
-    x_line2 = np.array([min_sl2, max_sl2, max_sl2, min_sl2, min_sl2])
-    y_line2 = np.array([min_gi2, min_gi2, max_gi2, max_gi2, min_gi2])
-    ax.plot(x_line2, y_line2, 'black', linewidth=3)
-
+adjacent = np.genfromtxt('adjacent.csv', delimiter=",")
+generateplot(dir3, adjacent)
 plt.show()

3c6fe445b1aa286d44cdcd43bc653ca4162a2aa3
Yu-Ming Chen <yminchen@umich.edu>
2020-09-30 11:57:58 -0400

    Fix a legend bug in the force plot when create multiple figure with the same name

--- examples/goldilocks_models/find_models/dircon_traj_plotter.py
+++ examples/goldilocks_models/find_models/dircon_traj_plotter.py
@@ -13,20 +13,15 @@ from pydrake.systems.framework import DiagramBuilder
 import pydairlib.multibody
 
 def main():
+  filename = FindResourceOrThrow('../dairlib_data/goldilocks_models/find_models/robot_1/dircon_trajectory')
   # filename = FindResourceOrThrow('../dairlib_data/goldilocks_models/find_models/robot_1/dircon_trajectory_iter0')
-  filename = FindResourceOrThrow('../dairlib_data/goldilocks_models/find_models/dircon_trajectory_iter1')
+  # filename = FindResourceOrThrow('../dairlib_data/goldilocks_models/find_models/dircon_trajectory_iter1')
+  # abs_path = "/home/yuming/Desktop/20200926 try to impose lipm constraint/4 penalize swing toe vel x100/robot_1"
+  # filename = abs_path + "/dircon_trajectory"
   if len(sys.argv) == 2:
     filename = sys.argv[1]
   dircon_traj = pydairlib.lcm_trajectory.DirconTrajectory(filename)
 
-  """
-  States, inputs, forces trajectories
-  """
-  PlotStateInputForce(dircon_traj)
-
-  """
-  Center of mass trajectories
-  """
   global plant, context, world, nq, nv, nx, nu
 
   # Build MBP
@@ -36,40 +31,98 @@ def main():
   plant.mutable_gravity_field().set_gravity_vector(-9.81 * np.array([0, 0, 1]))
   plant.Finalize()
 
+  # Conext and world
+  context = plant.CreateDefaultContext()
+  world = plant.world_frame()
+
   # MBP params
   nq = plant.num_positions()
   nv = plant.num_velocities()
   nx = plant.num_positions() + plant.num_velocities()
   nu = plant.num_actuators()
 
-  # Conext and world
-  context = plant.CreateDefaultContext()
-  world = plant.world_frame()
+  """
+  States, inputs, forces trajectories
+  """
+  # Indexing
+  x_idx_start = 0
+  x_idx_end = 7
+  PlotState(dircon_traj, x_idx_start, x_idx_end)
+  x_idx_start = 7
+  x_idx_end = nq
+  PlotState(dircon_traj, x_idx_start, x_idx_end)
+  # x_idx_start = nq
+  # x_idx_end = nq + 6
+  # PlotStateInputForce(dircon_traj, x_idx_start, x_idx_end)
+  x_idx_start = nq + 6
+  x_idx_end = nx
+  PlotState(dircon_traj, x_idx_start, x_idx_end)
 
-  PlotCenterOfMass(dircon_traj, True)
+  PlotInput(dircon_traj)
+  PlotForce(dircon_traj)
+
+  """
+  Center of mass trajectories
+  """
+  # PlotCenterOfMass(dircon_traj, True)
   PlotCenterOfMass(dircon_traj, False)
 
   plt.show()
 
-def PlotStateInputForce(dircon_traj):
-  # Indexing
-  x_idx_start = 0
-  x_idx_end = 19
-  # x_idx_start = 19
-  # x_idx_end = 19 + 18
-
+def PlotState(dircon_traj, x_idx_start=0, x_idx_end=19):
   # Get data at knot points
   t_knot = dircon_traj.GetStateBreaks(0)
   x_knot = dircon_traj.GetStateSamples(0)[x_idx_start:x_idx_end, :]
+
+  # Reconstructing state and input trajectory as piecewise polynomials
+  state_traj = dircon_traj.ReconstructStateTrajectory()
+  state_datatypes = dircon_traj.GetTrajectory("state_traj0").datatypes
+
+  # Sampling the spline for visualization
+  n_points = 10000
+  t = np.linspace(state_traj.start_time(), state_traj.end_time(), n_points)
+  # state_samples = np.zeros((n_points, state_traj.value(0).shape[0]))
+  state_samples = np.zeros((n_points, x_idx_end - x_idx_start))
+  for i in range(n_points):
+    state_samples[i] = state_traj.value(t[i])[x_idx_start:x_idx_end, 0]
+
+  # Plotting reconstructed state trajectories
+  plt.figure("state trajectory " + str(x_idx_start) + ":" + str(x_idx_end))
+  plt.plot(t, state_samples)
+  plt.plot(t_knot, x_knot.T, 'ko', markersize=2)
+  plt.legend(state_datatypes[x_idx_start:x_idx_end])
+
+def PlotInput(dircon_traj):
+  # Get data at knot points
+  t_knot = dircon_traj.GetStateBreaks(0)
+  t_coll = dircon_traj.GetCollocationForceBreaks(0)
+
+  # Reconstructing state and input trajectory as piecewise polynomials
+  state_traj = dircon_traj.ReconstructStateTrajectory()
+  input_traj = dircon_traj.ReconstructInputTrajectory()
+  input_datatypes = dircon_traj.GetTrajectory("input_traj").datatypes
+
+  # Sampling the spline for visualization
+  n_points = 10000
+  t = np.linspace(state_traj.start_time(), state_traj.end_time(), n_points)
+  input_samples = np.zeros((n_points, input_traj.value(0).shape[0]))
+  for i in range(n_points):
+    input_samples[i] = input_traj.value(t[i])[:, 0]
+
+  # Plotting reconstructed state trajectories
+  plt.figure("input trajectory")
+  plt.plot(t, input_samples)
+  plt.legend(input_datatypes)
+
+def PlotForce(dircon_traj):
+  # Get data at knot points
+  t_knot = dircon_traj.GetStateBreaks(0)
   force_knot = dircon_traj.GetForceSamples(0)
   t_coll = dircon_traj.GetCollocationForceBreaks(0)
   force_coll = dircon_traj.GetCollocationForceSamples(0)
 
   # Reconstructing state and input trajectory as piecewise polynomials
   state_traj = dircon_traj.ReconstructStateTrajectory()
-  state_datatypes = dircon_traj.GetTrajectory("state_traj0").datatypes
-  input_traj = dircon_traj.ReconstructInputTrajectory()
-  input_datatypes = dircon_traj.GetTrajectory("input_traj").datatypes
   force_traj = PiecewisePolynomial.ZeroOrderHold(dircon_traj.GetForceBreaks(0), dircon_traj.GetForceSamples(0))
   force_datatypes = dircon_traj.GetTrajectory("force_vars0").datatypes
   force_c_traj = PiecewisePolynomial.ZeroOrderHold(dircon_traj.GetCollocationForceBreaks(0), dircon_traj.GetCollocationForceSamples(0))
@@ -78,26 +131,13 @@ def PlotStateInputForce(dircon_traj):
   # Sampling the spline for visualization
   n_points = 10000
   t = np.linspace(state_traj.start_time(), state_traj.end_time(), n_points)
-  # state_samples = np.zeros((n_points, state_traj.value(0).shape[0]))
-  state_samples = np.zeros((n_points, x_idx_end - x_idx_start))
-  input_samples = np.zeros((n_points, input_traj.value(0).shape[0]))
   force_samples = np.zeros((n_points, force_traj.value(0).shape[0]))
   force_c_samples = np.zeros((n_points, force_c_traj.value(0).shape[0]))
   for i in range(n_points):
-    state_samples[i] = state_traj.value(t[i])[x_idx_start:x_idx_end, 0]
-    input_samples[i] = input_traj.value(t[i])[:, 0]
     force_samples[i] = force_traj.value(t[i])[:, 0]
     force_c_samples[i] = force_c_traj.value(t[i])[:, 0]
 
   # Plotting reconstructed state trajectories
-  plt.figure("state trajectory")
-  plt.plot(t, state_samples)
-  plt.plot(t_knot, x_knot.T, 'ko', markersize=2)
-  plt.legend(state_datatypes[x_idx_start:x_idx_end])
-
-  plt.figure("input trajectory")
-  plt.plot(t, input_samples)
-  plt.legend(input_datatypes)
 
   # plt.figure("force trajectory")
   # plt.plot(t, force_samples)
@@ -125,7 +165,6 @@ def PlotStateInputForce(dircon_traj):
   plt.plot(t_knot_col, force_knot_col.T)
   plt.plot(t_coll, force_coll.T, 'ko', markersize=2)
   plt.legend(force_datatypes + force_c_datatypes)
-
 
 def PlotCenterOfMass(dircon_traj, visualize_only_collocation_point=False):
   # Get data at knots

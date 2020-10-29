3e0780d6bfb4c1737e9f43862335fe0065fd8242
Yu-Ming Chen <yminchen@umich.edu>
2020-10-28 23:49:57 -0400

    Adding PlotDynamicError but unfinished

--- examples/goldilocks_models/find_models/dircon_traj_plotter.py
+++ examples/goldilocks_models/find_models/dircon_traj_plotter.py
@@ -12,6 +12,8 @@ from pydrake.multibody.tree import JacobianWrtVariable
 from pydrake.systems.framework import DiagramBuilder
 import pydairlib.multibody
 
+from pydairlib.multibody.kinematic import DistanceEvaluator
+from pydairlib.cassie.cassie_utils import *
 
 def main():
   filename = FindResourceOrThrow(
@@ -51,6 +53,15 @@ def main():
   # Conext and world
   context = plant.CreateDefaultContext()
   world = plant.world_frame()
+  global l_toe_frame, r_toe_frame
+  global front_contact_disp, rear_contact_disp
+  global l_loop_closure, r_loop_closure
+  l_toe_frame = plant.GetBodyByName("toe_left").body_frame()
+  r_toe_frame = plant.GetBodyByName("toe_right").body_frame()
+  front_contact_disp = np.array((-0.0457, 0.112, 0))
+  rear_contact_disp = np.array((0.088, 0, 0))
+  l_loop_closure = LeftLoopClosureEvaluator(plant)
+  r_loop_closure = RightLoopClosureEvaluator(plant)
 
   # MBP params
   nq = plant.num_positions()
@@ -81,9 +92,13 @@ def main():
   """
   PlotPelvis(dircon_traj, False)
 
+  """
+  Dynamic error
+  """
+  # PlotDynamicError(dircon_traj, nq + 6, nx)
+
   if not savefig:
       plt.show()
-
 
 def PlotState(dircon_traj, x_idx_start=0, x_idx_end=19):
   # Get data at knot points
@@ -111,7 +126,6 @@ def PlotState(dircon_traj, x_idx_start=0, x_idx_end=19):
   plt.legend(state_datatypes[x_idx_start:x_idx_end])
   if savefig:
     plt.savefig(save_path + figname + ".png")
-
 
 def PlotInput(dircon_traj):
   # Get data at knot points
@@ -169,9 +183,9 @@ def PlotForce(dircon_traj):
 
   # Plotting reconstructed state trajectories
 
-  # plt.figure("force trajectory")
-  # plt.plot(t, force_samples)
-  # plt.legend(force_datatypes)
+  plt.figure("force trajectory")
+  plt.plot(t, force_samples)
+  plt.legend(force_datatypes)
   #
   # plt.figure("collocation force trajectory")
   # plt.plot(t, force_c_samples)
@@ -450,6 +464,101 @@ def PlotPelvis(dircon_traj, visualize_only_collocation_point=False):
     if savefig:
       plt.savefig(save_path + figname + ".png")
 
+def PlotDynamicError(dircon_traj, x_idx_start=0, x_idx_end=19):
+  # Get data at knot points
+  t_knot = dircon_traj.GetStateBreaks(0)
+  x_knot = dircon_traj.GetStateSamples(0)[x_idx_start:x_idx_end, :]
+
+  # Reconstructing state and input trajectory as piecewise polynomials
+  state_traj = dircon_traj.ReconstructStateTrajectory()
+  state_datatypes = dircon_traj.GetTrajectory("state_traj0").datatypes
+  input_traj = dircon_traj.ReconstructInputTrajectory()
+  input_datatypes = dircon_traj.GetTrajectory("input_traj").datatypes
+  force_traj = PiecewisePolynomial.ZeroOrderHold(dircon_traj.GetForceBreaks(0),
+    dircon_traj.GetForceSamples(0))
+  force_datatypes = dircon_traj.GetTrajectory("force_vars0").datatypes
+
+  # Create time and state samples
+  n_points = 10000
+  t = np.linspace(state_traj.start_time(), state_traj.end_time(), n_points)
+  state_samples = np.zeros((n_points, state_traj.value(0).shape[0]))
+  for i in range(n_points):
+    state_samples[i] = state_traj.value(t[i])[:, 0]
+  input_samples = np.zeros((n_points, input_traj.value(0).shape[0]))
+  for i in range(n_points):
+    input_samples[i] = input_traj.value(t[i])[:, 0]
+  force_samples = np.zeros((n_points, force_traj.value(0).shape[0]))
+  for i in range(n_points):
+    force_samples[i] = force_traj.value(t[i])[:, 0]
+
+  # Sampling the spline for visualization
+  n_points = 10000
+  t = np.linspace(state_traj.start_time(), state_traj.end_time(), n_points)
+
+  # From time derivatives of cubic spline
+  xdot_traj = state_traj.MakeDerivative(1)
+  xdot_from_spline = np.zeros((n_points, nx))
+  for i in range(n_points):
+    xdot_from_spline[i] = xdot_traj.value(t[i])[:, 0]
+
+  # From dynamics function
+  xdot_from_func = np.zeros((n_points, nx))
+  for i in range(n_points):
+    plant.SetPositionsAndVelocities(context, state_traj.value(t[i]))
+
+    u = input_samples[i]
+    lamb = force_samples[i]
+
+    M = plant.CalcMassMatrixViaInverseDynamics(context)
+    M_inv = np.linalg.inv(M)
+    B = plant.MakeActuationMatrix()
+    g = plant.CalcGravityGeneralizedForces(context)
+    Cv = plant.CalcBiasTerm(context)
+
+    # The constraint order should be the same as trajopt's
+    J_lt = plant.CalcJacobianTranslationalVelocity(
+      context, JacobianWrtVariable.kV, l_toe_frame, front_contact_disp, world, world)
+    J_lh = plant.CalcJacobianTranslationalVelocity(
+      context, JacobianWrtVariable.kV, l_toe_frame, rear_contact_disp, world, world)
+    J_rt = plant.CalcJacobianTranslationalVelocity(
+      context, JacobianWrtVariable.kV, r_toe_frame, front_contact_disp, world, world)
+    J_rh = plant.CalcJacobianTranslationalVelocity(
+      context, JacobianWrtVariable.kV, r_toe_frame, rear_contact_disp, world, world)
+    J_l_loop_closure = l_loop_closure.EvalFullJacobian(context)
+    J_r_loop_closure = r_loop_closure.EvalFullJacobian(context)
+    # J = np.vstack((J_lt, J_lh, J_rt, J_rh, J_l_loop_closure, J_r_loop_closure))
+    J = np.vstack((J_lt, J_lh, J_l_loop_closure, J_r_loop_closure))
+
+    # xdot_from_func[i, :nq] = state_samples[i][nq:]
+    # import pdb; pdb.set_trace()
+    xdot_from_func[i, nq:] = M_inv @ (-Cv + g + B@u + J.T @ lamb)
+
+  # Plotting reconstructed state trajectories
+  figname = "xdot trajectory " + str(x_idx_start) + "-" + str(x_idx_end)
+  plt.figure(figname, figsize=figsize)
+  # plt.plot(t, (xdot_from_spline)[:, x_idx_start:x_idx_end])
+  plt.plot(t, (xdot_from_func)[:, x_idx_start:x_idx_end])
+  # plt.plot(t, (xdot_from_spline - xdot_from_func)[:, x_idx_start:x_idx_end], 'ko', markersize=1)
+  # plt.plot(t_knot, x_knot.T, 'ko', markersize=2)
+  plt.xlabel('time (s)')
+  plt.legend(state_datatypes[x_idx_start:x_idx_end])
+  if savefig:
+    plt.savefig(save_path + figname + ".png")
+
+# PlotStateAtKnots is for testing (TODO: delete later)
+def PlotStateAtKnots(dircon_traj, x_idx_start=0, x_idx_end=19):
+  # Get data at knot points
+  t_knot = dircon_traj.GetStateBreaks(0)
+  x_knot = dircon_traj.GetStateSamples(0)[x_idx_start:x_idx_end, :]
+
+  state_datatypes = dircon_traj.GetTrajectory("state_traj0").datatypes
+
+  # Plotting reconstructed state trajectories
+  figname = "state trajectory " + str(x_idx_start) + "-" + str(x_idx_end) + "(at knots)"
+  plt.figure(figname, figsize=figsize)
+  plt.plot(t_knot, x_knot.T)
+  plt.xlabel('time (s)')
+  plt.legend(state_datatypes[x_idx_start:x_idx_end])
 
 if __name__ == "__main__":
   main()

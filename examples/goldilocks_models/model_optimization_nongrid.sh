a05688ea7e3315211215141b890f8a501f16292b
Jianshu-Hu <jianshuhu98@gmail.com>
2020-11-10 16:12:27 -0500

    minor change in bash file

--- examples/goldilocks_models/model_optimization_nongrid.sh
+++ examples/goldilocks_models/model_optimization_nongrid.sh
@@ -2,7 +2,7 @@ echo If you ssh, run this sciprt in the background to prevent the program from b
 
 # Set robot id and model id
 robot=1
-model=0 # 1Drom 0,1   2Drom 2,3  3Drom 4,5
+model=0 # 1Drom 2,3   2Drom 0,1  3Drom 4,5
 echo robot_option = $robot, rom_option = $model
 
 # Set sample size
@@ -23,6 +23,8 @@ tr_min=0
 tr_max=0
 # large range
 # Other parameters
+is_grid=false
+iter_start=0
 final_iter=1
 snopt_scaling=true
 N_rerun=2
@@ -37,6 +39,6 @@ fi
 
 ##########################
 echo ===== optimize rom0 in small 3D task space \(sl gi v\) with snopt scaling =====
-./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=$final_iter --robot_option=$robot --rom_option=$model --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_v=$n_v --N_sample_tr=$n_tr --sl_min=$sl_min --sl_max=$sl_max --gi_min=$gi_min --gi_max=$gi_max --v_min=$v_min --v_max=$v_max --tr_min=$tr_min --tr_max=$tr_max --is_grid_task=false --fix_node_number=true --snopt_scaling=$snopt_scaling --N_rerun=$N_rerun | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log.txt
+./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=$iter_start --max_outer_iter=$final_iter --robot_option=$robot --rom_option=$model --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_v=$n_v --N_sample_tr=$n_tr --sl_min=$sl_min --sl_max=$sl_max --gi_min=$gi_min --gi_max=$gi_max --v_min=$v_min --v_max=$v_max --tr_min=$tr_min --tr_max=$tr_max --is_grid_task=$is_grid --fix_node_number=true --snopt_scaling=$snopt_scaling --N_rerun=$N_rerun | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log.txt
 
 

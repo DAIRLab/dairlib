28870bd51658884124e07687b3a535ac1f552cea
Jianshu Hu <58180987+Jianshu-Hu@users.noreply.github.com>
2020-12-04 11:29:18 -0500

    Update model_optimization_nongrid.sh
    Add a flag used to change momentum term in bash file

--- examples/goldilocks_models/model_optimization_nongrid.sh
+++ examples/goldilocks_models/model_optimization_nongrid.sh
@@ -28,6 +28,7 @@ iter_start=0
 final_iter=1
 snopt_scaling=true
 N_rerun=2
+momentum=0.8
 
 # Delete and create a new data folder if specified in the argument
 if [ "$1" = "rm" ]; then
@@ -39,6 +40,6 @@ fi
 
 ##########################
 echo ===== optimize rom$model in small 3D task space \(sl gi v\) with snopt scaling =====
-./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=$iter_start --max_outer_iter=$final_iter --robot_option=$robot --rom_option=$model --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_v=$n_v --N_sample_tr=$n_tr --sl_min=$sl_min --sl_max=$sl_max --gi_min=$gi_min --gi_max=$gi_max --v_min=$v_min --v_max=$v_max --tr_min=$tr_min --tr_max=$tr_max --is_grid_task=$is_grid --fix_node_number=true --snopt_scaling=$snopt_scaling --N_rerun=$N_rerun | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log.txt
+./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=$iter_start --max_outer_iter=$final_iter --robot_option=$robot --rom_option=$model --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_v=$n_v --N_sample_tr=$n_tr --sl_min=$sl_min --sl_max=$sl_max --gi_min=$gi_min --gi_max=$gi_max --v_min=$v_min --v_max=$v_max --tr_min=$tr_min --tr_max=$tr_max --is_grid_task=$is_grid --fix_node_number=true --snopt_scaling=$snopt_scaling --N_rerun=$N_rerun --beta_momentum=$momentum | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log.txt
 
 

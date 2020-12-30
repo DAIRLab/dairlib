43aff978de2395f2e447d052608e77fb79a01b1e
J Diego Caporale <jdcap@seas.upenn.edu>
2020-12-30 05:46:27 -0500

    Spirit Stand in debug, based on spirit_vis and run_dircon_squatting for cassie. Looks like reference arrays are illegal and many other errors.

---
+++ examples/Spirit/run_spirit_stand.cc
@@ -0,0 +1,385 @@
+#include <memory>
+#include <chrono>
+#include <unistd.h>
+#include <gflags/gflags.h>
+#include <string.h>
+
+#include "drake/solvers/snopt_solver.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/framework/diagram.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/geometry/geometry_visualization.h"
+#include "drake/solvers/solve.h"
+
+#include "common/find_resource.h"
+#include "systems/trajectory_optimization/dircon/dircon.h"
+#include "multibody/kinematic/world_point_evaluator.h"
+#include "multibody/kinematic/distance_evaluator.h"
+#include "multibody/multibody_utils.h"
+#include "multibody/visualization_utils.h"
+#include "multibody/kinematic/kinematic_constraints.h"
+
+DEFINE_double(duration, 1, "The stand duration");
+DEFINE_double(front2BackToeDistance, 0.35, "Nominal distance between the back and front toes.");
+DEFINE_double(side2SideToeDistance, 0.2, "Nominal distance between the back and front toes.");
+DEFINE_double(bodyHeight, 0.104, "The spirit body start height (defined in URDF)");
+DEFINE_double(standHeight, 0.25, "The standing height.");
+DEFINE_bool(autodiff, false, "Double or autodiff version");
+
+using drake::AutoDiffXd;
+using drake::multibody::MultibodyPlant;
+using drake::geometry::SceneGraph;
+using drake::multibody::Parser;
+using drake::trajectories::PiecewisePolynomial;
+
+using Eigen::Vector3d;
+using Eigen::VectorXd;
+using Eigen::Matrix3d;
+using Eigen::MatrixXd;
+
+namespace dairlib {
+namespace {
+
+using systems::trajectory_optimization::DirconModeSequence;
+using systems::trajectory_optimization::DirconMode;
+using systems::trajectory_optimization::Dircon;
+using systems::trajectory_optimization::KinematicConstraintType;
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+  /// See runAnimate(
+  ///    std::unique_ptr<MultibodyPlant<T>> plant_ptr,
+  ///    MultibodyPlant<double>* plant_double_ptr,
+  ///    std::unique_ptr<SceneGraph<double>> scene_graph_ptr,
+  ///    PiecewisePolynomial<double> pp_xtraj )
+  ///
+  /// Takes the plants and scenegraph and a trajectory and 
+  /// creates a visualization of that trajectory (example
+  /// built in the main file)
+template <typename T>
+void runSpiritStand(
+    std::unique_ptr<MultibodyPlant<T>> plant_ptr,
+    MultibodyPlant<double>* plant_double_ptr,
+    std::unique_ptr<SceneGraph<double>> scene_graph_ptr,
+    double duration,
+    PiecewisePolynomial<double> init_x_traj,
+    PiecewisePolynomial<double> init_u_traj,
+    vector<PiecewisePolynomial<double>> init_l_traj,
+    vector<PiecewisePolynomial<double>> init_lc_traj,
+    vector<PiecewisePolynomial<double>> init_vc_traj
+    ) {
+
+  drake::systems::DiagramBuilder<double> builder;
+  MultibodyPlant<T>& plant = *plant_ptr;
+  SceneGraph<double>& scene_graph =
+      *builder.AddSystem(std::move(scene_graph_ptr));
+
+  auto positions_map = multibody::makeNameToPositionsMap(plant);
+  auto velocities_map = multibody::makeNameToVelocitiesMap(plant);
+
+  // Print joint dictionary
+  for (auto const& element : positions_map)
+    cout << element.first << " = " << element.second << endl;
+  for (auto const& element : velocities_map)
+    cout << element.first << " = " << element.second << endl;
+  
+
+  /// For Spirit front left leg->0, back left leg->1, front right leg->2, back right leg->3
+  /// Get the frame of each toe and attach a world point to the toe tip (frame is at toe ball center).
+
+  int num_legs = 4;
+  double toeRadius = 0.02; // Radius of toe ball
+  Vector3d toeOffset(toeRadius,0,0); // vector to "contact point"
+  double mu = 1; //friction
+  ??????????????????????????????????????
+//// I hate this but copy and paste everything for now and then see how you can do it later
+  // const drake::multibody::Frame<T> * toes[num_legs];
+  const auto& toes[num_legs]; //Initialize toeframes //Doesnt work
+  // const auto& toe = plant.GetFrameByName( "toe"+std::to_string(i) );
+
+  // multibody::WorldPointEvaluator<T> toe_eval[num_legs]; //Initialize worldpoints
+  auto toe_eval[num_legs];
+  auto evaluators = multibody::KinematicEvaluatorSet<T>(plant); //Initialize kinematic evaluator set
+  
+  for ( int i = 0; i < num_legs; i++) {
+    // For each toe, set up world point evaluator
+    toes[i] = plant.GetFrameByName( "toe"+std::to_string(i) ); //Get toe frame
+    toe_eval[i] = multibody::WorldPointEvaluator<T>(plant, toeOffset, toes[i], Matrix3d::Identity(), Vector3d::Zero(), {0, 1, 2}); //Shift to tip
+    toe_eval[i].set_frictional(); //
+    toe_eval[i].set_mu(mu);
+    evaluators.add_evaluator(&(toe_eval[i]));
+  }
+
+
+  int num_knotpoints = 10; //
+  auto full_support = DirconMode<T>(evaluators,num_knotpoints); //No min and max mode times
+  for (int i; i < num_legs; i++ ){
+    full_support.MakeConstraintRelative(i, 0);  // x-coordinate
+    full_support.MakeConstraintRelative(i, 1);  // y-coordinate
+  }
+  
+  ///Mode Sequence
+  // Adding the ONE mode to the sequence, will not leave full_support
+  auto sequence = DirconModeSequence<T>(plant);
+  sequence.AddMode(&full_support);
+
+  ///Setup trajectory optimization
+  auto trajopt = Dircon<T>(sequence);
+
+  // Set up Trajectory Optimization options
+  trajopt.SetSolverOption(drake::solvers::SnoptSolver::id(),
+                           "Print file", "../snopt.out");
+  trajopt.SetSolverOption(drake::solvers::SnoptSolver::id(),
+                           "Major iterations limit", 20000);
+  trajopt.SetSolverOption(drake::solvers::SnoptSolver::id(), "Iterations limit", 100000);
+  // Add duration constraint, currently constrained not bounded
+  trajopt.AddDurationBounds(duration, duration);
+  // Initialize the trajectory control state and forces
+  for (int j = 0; j < sequence.num_modes(); j++) {
+    trajopt.drake::systems::trajectory_optimization::MultipleShooting::
+        SetInitialTrajectory(init_u_traj, init_x_traj);
+    trajopt.SetInitialForceTrajectory(j, init_l_traj[j], init_lc_traj[j],
+                                      init_vc_traj[j]);
+  }
+
+  /// Setup all the optimization constraints 
+  int n_v = plant.num_velocities();
+  int n_q = plant.num_positions();
+  auto u = trajopt.input();
+  auto x = trajopt.state();
+  auto x0 = trajopt.initial_state();
+  auto xf = trajopt.final_state();
+  
+  // Initial body positions
+  trajopt.AddBoundingBoxConstraint(0, 0, x0(positions_map.at("base_x")));
+  trajopt.AddBoundingBoxConstraint(0, 0, x0(positions_map.at("base_y")));
+  trajopt.AddBoundingBoxConstraint(FLAGS_bodyHeight/2, FLAGS_bodyHeight/2, x0(positions_map.at("base_z")));
+  
+  // Final body positions
+  trajopt.AddBoundingBoxConstraint(-0.1, 0.1, xf(positions_map.at("base_x")));
+  trajopt.AddBoundingBoxConstraint(   0,   0, xf(positions_map.at("base_y")));
+  trajopt.AddBoundingBoxConstraint(FLAGS_standHeight, FLAGS_standHeight, xf(positions_map.at("base_z")));
+
+  // Body pose constraints (keep the body flat) at all the knotpoints including the ends
+  for (int i = 0; i < num_knotpoints; i++) {
+    auto xi = trajopt.state(i);
+    trajopt.AddBoundingBoxConstraint(1, 1, xi(positions_map.at("base_qw")));
+    trajopt.AddBoundingBoxConstraint(0, 0, xi(positions_map.at("base_qx")));
+    trajopt.AddBoundingBoxConstraint(0, 0, xi(positions_map.at("base_qy")));
+    trajopt.AddBoundingBoxConstraint(0, 0, xi(positions_map.at("base_qz")));
+  }
+
+  // start/end velocity constraints
+  trajopt.AddBoundingBoxConstraint(VectorXd::Zero(n_v), VectorXd::Zero(n_v),
+                                   x0.tail(n_v));
+  trajopt.AddBoundingBoxConstraint(VectorXd::Zero(n_v), VectorXd::Zero(n_v),
+                                   xf.tail(n_v));
+
+
+//     Might need this currently I personally parse this as constraining the first force_vars element
+//     in the 0th mode to zero at all the knotpoints not sure why? 
+  // // Not sure what this is doing
+  // for (int i = 0; i < num_knotpoints; i++) {
+  //   trajopt.AddBoundingBoxConstraint(0, 0, trajopt.force_vars(0, i)(1));
+  // }
+
+  /// Set foot distances
+  // Distance evaluators for constraints (note one front back, one side to side, front back in line, then symmetry)
+  auto toe_dist_eval_front_back =  multibody::DistanceEvaluator<T>(plant, toeOffset, toes[0], toeOffset, toes[1], FLAGS_front2BackToeDistance);
+  auto toe_dist_eval_side_side  =  multibody::DistanceEvaluator<T>(plant, toeOffset, toes[0], toeOffset, toes[2], FLAGS_side2SideToeDistance);
+  
+  auto toe_distance_evaluators = multibody::KinematicEvaluatorSet<T>(plant);
+  toe_distance_evaluators.add_evaluator(&toe_dist_eval_front_back);
+  toe_distance_evaluators.add_evaluator(&toe_dist_eval_side_side);
+
+  auto toe_distance_lb = Eigen::VectorXd(2);
+  auto toe_distance_ub = Eigen::VectorXd(2);
+
+  toe_distance_lb(0) = -0.1;
+  toe_distance_lb(1) = -0.1;
+  toe_distance_ub(0) =  0.1;
+  toe_distance_ub(1) =  0.1;
+
+  auto toe_constraints = std::make_shared<multibody::KinematicPositionConstraint<T>>(plant,
+              toe_distance_evaluators, toe_distance_lb, toe_distance_ub);
+  
+  for (int i = 0; i < num_knotpoints; i++){
+    auto xi = trajopt.state(i);
+    trajopt.AddConstraint(toe_constraints,xi.head(n_q));
+
+    //legs lined up (front and back hips equal)
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_8") ) ==  xi( positions_map.at("joint_9") ) );
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_10") ) ==  xi( positions_map.at("joint_11") ) );
+  
+    // Symmetry constraints (mirrored hips all else equal across)
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_8") ) == -xi( positions_map.at("joint_10") ) );
+    
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_0") ) == xi( positions_map.at("joint_2") ) );
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_1") ) == xi( positions_map.at("joint_3") ) );
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_4") ) == xi( positions_map.at("joint_6") ) );
+    trajopt.AddLinearConstraint( xi( positions_map.at("joint_5") ) == xi( positions_map.at("joint_7") ) );
+  }
+  
+
+
+  const double R = 100;  // Cost on input effort
+  const MatrixXd Q = 10  * MatrixXd::Identity(n_v, n_v); // Cost on velocity
+  trajopt.AddRunningCost(x.tail(n_v).transpose() * Q * x.tail(n_v));
+  trajopt.AddRunningCost(u.transpose()*R*u);
+
+  std::vector<unsigned int> visualizer_poses;
+  visualizer_poses.push_back(5);
+
+  trajopt.CreateVisualizationCallback(
+      dairlib::FindResourceOrThrow("examples/Spirit/spirit_drake.urdf"),
+      visualizer_poses, 0.2, "base");
+
+  auto start = std::chrono::high_resolution_clock::now();
+  const auto result = Solve(trajopt, trajopt.initial_guess());
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = finish - start;
+  std::cout << "Solve time:" << elapsed.count() <<std::endl;
+  std::cout << "Cost:" << result.get_optimal_cost() <<std::endl;
+  std::cout << (result.is_success() ? "Optimization Success" : "Optimization Fail") << std::endl;
+
+  // visualizer
+  const drake::trajectories::PiecewisePolynomial<double> pp_xtraj =
+      trajopt.ReconstructStateTrajectory(result);
+  multibody::connectTrajectoryVisualizer(plant_double_ptr,
+      &builder, &scene_graph, pp_xtraj);
+  auto diagram = builder.Build();
+  while (true) {
+    
+    drake::systems::Simulator<double> simulator(*diagram);
+    simulator.set_target_realtime_rate(.25);
+    simulator.Initialize();
+    simulator.AdvanceTo(pp_xtraj.end_time());
+    sleep(2);
+  }
+}
+}  // namespace
+}  // namespace dairlib
+
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::srand(time(0));  // Initialize random number generator.
+ 
+  auto plant = std::make_unique<MultibodyPlant<double>>(0.0);
+  auto plant_vis = std::make_unique<MultibodyPlant<double>>(0.0);
+  auto scene_graph = std::make_unique<SceneGraph<double>>();
+  Parser parser(plant.get());
+  Parser parser_vis(plant_vis.get(), scene_graph.get());
+  std::string full_name =
+      dairlib::FindResourceOrThrow("examples/Spirit/spirit_drake.urdf");
+
+  parser.AddModelFromFile(full_name);
+  parser_vis.AddModelFromFile(full_name);
+
+  plant->Finalize();
+  plant_vis->Finalize();
+
+  Eigen::VectorXd x0 = Eigen::VectorXd::Zero(plant->num_positions() +
+                       plant->num_velocities());
+
+  int nu = plant->num_actuators();
+  int nx = plant->num_positions() + plant->num_velocities();
+  int nq = plant->num_positions();
+  int N = 20; // number of timesteps
+
+  std::vector<MatrixXd> init_x;
+  std::vector<MatrixXd> init_u;
+  std::vector<PiecewisePolynomial<double>> init_l_traj;
+  std::vector<PiecewisePolynomial<double>> init_lc_traj;
+  std::vector<PiecewisePolynomial<double>> init_vc_traj;
+
+  // Initialize state trajectory
+  std::vector<double> init_time;
+  VectorXd xState(nx);
+  xState = Eigen::VectorXd::Zero(plant->num_positions() + plant->num_velocities());
+  auto positions_map = dairlib::multibody::makeNameToPositionsMap(*plant);
+  auto velocities_map = dairlib::multibody::makeNameToVelocitiesMap(*plant);
+  int num_joints = 12;
+
+  
+  xState(positions_map.at("base_qw")) = 1;
+//   std::cout << "Debug:So it begins" << std::endl;
+
+  for (int j = 0; j < num_joints; j++){
+  //   // std::cout << std::to_string(j) << std::endl;
+  //   // std::cout << velocities_map.at( std::to_string(j)+"dot" ) << std::endl;
+    int flipFlag = 1;
+    if (j==10||j==11){
+      flipFlag = -1;
+    }
+    xState(nq + velocities_map.at( "joint_" + std::to_string(j)+"dot" )) = 0.1*flipFlag;
+  }
+  
+
+  double time = 0;
+  for (int i = 0; i < N; i++) {
+    time=i*FLAGS_duration/(N-1);
+    init_time.push_back(time);
+
+    for (int j = 0; j < num_joints; j++){
+    //     xState(nq + velocities_map.at( std::to_string(j)+"dot"  )) = 0;
+        xState(positions_map.at(std::to_string(j))) = xState(nq + velocities_map.at(std::to_string(j)+"dot" )) * time;
+    }
+    
+
+    init_x.push_back(xState);
+    init_u.push_back(Eigen::VectorXd::Zero(nu));
+    // init_u.push_back(VectorXd::Random(nu));
+  }
+  auto init_x_traj = PiecewisePolynomial<double>::ZeroOrderHold(init_time, init_x);
+  auto init_u_traj = PiecewisePolynomial<double>::ZeroOrderHold(init_time, init_u);
+
+
+  ///TODO:JDC *********************************************************************
+  
+  // Four contacts so forces are 12 dimensional
+  Eigen::VectorXd init_l_vec(12);
+  init_l_vec << 0, 0, 12*9.81, 0, 0, 12*9.81, 0, 0, 12*9.81, 0, 0, 12*9.81; //Maybe add for loop?
+  //Initialize force trajectories
+  int num_modes = 1; // MAKE DYNAMIC DEBUG
+  for (int j = 0; j < num_modes; j++) {    
+    std::vector<MatrixXd> init_l_j;
+    std::vector<MatrixXd> init_lc_j;
+    std::vector<MatrixXd> init_vc_j;
+    std::vector<double> init_time_j;
+    for (int i = 0; i < N; i++) {
+      init_time_j.push_back(i*FLAGS_duration/(N-1));
+      init_l_j.push_back(init_l_vec);
+      init_lc_j.push_back(init_l_vec);
+      init_vc_j.push_back(VectorXd::Zero(12));
+    }
+
+    auto init_l_traj_j = PiecewisePolynomial<double>::ZeroOrderHold(init_time_j,init_l_j);
+    auto init_lc_traj_j = PiecewisePolynomial<double>::ZeroOrderHold(init_time_j,init_lc_j);
+    auto init_vc_traj_j = PiecewisePolynomial<double>::ZeroOrderHold(init_time_j,init_vc_j);
+
+    init_l_traj.push_back(init_l_traj_j);
+    init_lc_traj.push_back(init_lc_traj_j);
+    init_vc_traj.push_back(init_vc_traj_j);
+  }
+  ///*****************************************************************************
+
+  if (FLAGS_autodiff) {
+    std::unique_ptr<MultibodyPlant<drake::AutoDiffXd>> plant_autodiff =
+        drake::systems::System<double>::ToAutoDiffXd(*plant);
+    dairlib::runSpiritStand<drake::AutoDiffXd>(
+      std::move(plant_autodiff), plant_vis.get(), std::move(scene_graph),
+      FLAGS_duration, init_x_traj, init_u_traj, init_l_traj,
+      init_lc_traj, init_vc_traj);
+  } else {
+    dairlib::runSpiritStand<double>(
+      std::move(plant), plant_vis.get(), std::move(scene_graph),
+      FLAGS_duration, init_x_traj, init_u_traj, init_l_traj,
+      init_lc_traj, init_vc_traj);
+  }
+
+}
+

1c99b5545262710c17d0eb30474c2c5c7adbea16
Yu-Ming Chen <yminchen@umich.edu>
2021-01-09 16:54:15 -0500

    Add a comment

--- examples/goldilocks_models/find_models/traj_opt_given_weigths.cc
+++ examples/goldilocks_models/find_models/traj_opt_given_weigths.cc
@@ -3396,8 +3396,9 @@ void cassieTrajOpt(const MultibodyPlant<double>& plant,
   }
 
   if (is_print_for_debugging) {
-    // Impulse variable's value
+    // w_sol is not assigned when snopt didn't solve the problem successfully 
     if (w_sol.size() > 0) {
+      // Impulse variable's value
       for (int i = w_sol.size() - rs_dataset.countConstraints() - 1;
            i < w_sol.size(); i++) {
         cout << i << ": " << gm_traj_opt.dircon->decision_variables()[i] << ", "

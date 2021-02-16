42495f933e45b85ef632720f31ca89e78b8fc98c
Brian Acosta <bjacosta@seas.upenn.edu>
2021-02-16 00:00:59 -0500

    added traj saving as CVS to plotting script

--- systems/trajectory_optimization/centroidal_to/centroidal_trajectory_plotter.py
+++ systems/trajectory_optimization/centroidal_to/centroidal_trajectory_plotter.py
@@ -6,7 +6,6 @@ import numpy as np
 
 stance_dim = 2
 
-
 def ReconstructStateTrajectory(x, x_dot):
     traj = PiecewisePolynomial.CubicHermite(x.time_vector, x.datapoints, x_dot.datapoints)
     return traj
@@ -19,8 +18,36 @@ def ReconstructForceTrajectory(f):
     traj = PiecewisePolynomial.FirstOrderHold(f.time_vector, f.datapoints)
     return traj
 
-def SaveTrajDatapoints(x_poly, theta_poly, stance_polys, force_poly):
-    npoints = 10000
+def SaveTrajDatapoints(x_poly, y_poly, theta_poly, left_poly, right_poly, left_force_poly, right_force_poly):
+    npoints = 10001
+
+    t = np.linspace(x_poly.start_time(), x_poly.end_time(), npoints)
+    traj = np.zeros((18, npoints))
+    input = np.zeros((8,npoints))
+
+    for i in range(npoints):
+        traj[0:1,i] = x_poly.value(t[i])[:,0]
+        traj[1:2,i] = y_poly.value(t[i])[:,0]
+        traj[2:3,i] = theta_poly.value(t[i])[:,0]
+        traj[3:5,i] = left_poly.value(t[i])[:,0]
+        traj[5:7,i] = right_poly.value(t[i])[:,0]
+        traj[7:8,i] = x_poly.derivative(1).value(t[i])[:,0]
+        traj[8:9,i] = y_poly.derivative(1).value(t[i])[:,0]
+        traj[9:10,i] = theta_poly.derivative(1).value(t[i])[:,0]
+        traj[10:12,i] = left_poly.derivative(1).value(t[i])[:,0]
+        traj[12:14,i] = right_poly.derivative(1).value(t[i])[:,0]
+        traj[14:16,i] = left_force_poly.value(t[i])[:,0]
+        traj[16:18,i] = right_force_poly.value(t[i])[:,0]
+
+        input[0:2,i] = left_poly.derivative(2).value(t[i])[:,0]
+        input[2:4,i] = right_poly.derivative(2).value(t[i])[:,0]
+        input[4:6,i] = left_force_poly.derivative(1).value(t[i])[:,0]
+        input[6:8,i] = right_force_poly.derivative(1).value(t[i])[:,0]
+
+    np.savetxt("/home/brian/Documents/Research/Quals/Traj.csv", traj, delimiter=",")
+    np.savetxt("/home/brian/Documents/Research/Quals/Input.csv", input, delimiter="," )
+    np.savetxt("/home/brian/Documents/Research/Quals/Time.csv", t, delimiter=",")
+
     return 0
 
 
@@ -68,20 +95,30 @@ def main():
                             "trajectory_optimization/centroidal_to/forcetraj.lcmtraj")
 
     print(stateTrajs.GetTrajectoryNames())
-    print(sorted(stanceTrajs.GetTrajectoryNames()))
+
     n = len(stanceTrajs.GetTrajectoryNames())
     left_stance = sorted(stanceTrajs.GetTrajectoryNames())[0:n//2]
+    print(left_stance)
     right_stance = sorted(stanceTrajs.GetTrajectoryNames())[n//2:-1]
+    print(right_stance)
 
     traj_names = ['x', 'y', 'theta']
     stance_names = stanceTrajs.GetTrajectoryNames()
-    force_names = forceTrajs.GetTrajectoryNames()
+    force_names = sorted(forceTrajs.GetTrajectoryNames())
+    print(force_names)
+
+    state_traj_polys = []
+    foot_traj_polys = []
+    force_traj_polys = []
 
     n_points = 5001
     for name in traj_names:
         q = stateTrajs.GetTrajectory(name)
         qdot = stateTrajs.GetTrajectory(name + '_dot')
         pp = ReconstructStateTrajectory(q, qdot)
+
+        state_traj_polys.append(pp)
+
         t = np.linspace(pp.start_time(), pp.end_time(), n_points)
         qsamples = np.zeros((n_points, pp.value(0).shape[0]))
         qdotsamples = np.zeros((n_points, pp.value(0).shape[0]))
@@ -98,6 +135,7 @@ def main():
     for name in force_names:
         f = forceTrajs.GetTrajectory(name)
         pp = ReconstructForceTrajectory(f)
+        force_traj_polys.append(pp)
         force_samples = np.zeros((n_points, pp.value(0).shape[0]))
         for i in range(n_points):
             force_samples[i] = pp.value(t[i])[:,0]
@@ -115,12 +153,14 @@ def main():
         s = stanceTrajs.GetTrajectory(name)
         l_trajs.append(ReconstructStanceTrajectory(s))
     left_foot_traj = ReconstructFootTrajectory(l_trajs)
+    foot_traj_polys.append(left_foot_traj)
 
     for name in right_stance:
         s = stanceTrajs.GetTrajectory(name)
         r_trajs.append(ReconstructStanceTrajectory(s))
 
     right_foot_traj = ReconstructFootTrajectory(r_trajs)
+    foot_traj_polys.append(right_foot_traj)
 
     left_samples = np.zeros((n_points, left_foot_traj.value(0).shape[0]))
     right_samples = np.zeros((n_points, right_foot_traj.value(0).shape[0]))
@@ -137,8 +177,12 @@ def main():
     plt.legend(['x', 'y'])
 
 
+    #plt.show()
+
+    SaveTrajDatapoints(state_traj_polys[0], state_traj_polys[1], state_traj_polys[2],
+                       foot_traj_polys[0], foot_traj_polys[1],
+                       force_traj_polys[0], force_traj_polys[1])
+
     plt.show()
-
-
 if __name__ == "__main__":
     main()

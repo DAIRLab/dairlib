9d0764656801ddad603aa0ec450ece6ffbe86e96
Yu-Ming Chen <yminchen@umich.edu>
2021-05-01 10:46:45 -0400

    Create a function to contruct the ball plant for COM visualization

--- multibody/visualization_utils.cc
+++ multibody/visualization_utils.cc
@@ -17,11 +17,39 @@ using drake::systems::rendering::MultibodyPositionToGeometryPose;
 using drake::trajectories::Trajectory;
 using systems::SubvectorPassThrough;
 
+std::unique_ptr<MultibodyPlant<double>> ConstructBallPlant(
+    drake::geometry::SceneGraph<double>* scene_graph) {
+  auto ball_plant = std::make_unique<MultibodyPlant<double>>(0.0);
+  double radius = .02;
+  UnitInertia<double> G_Bcm = UnitInertia<double>::SolidSphere(radius);
+  SpatialInertia<double> M_Bcm(1, Eigen::Vector3d::Zero(), G_Bcm);
+  const RigidBody<double>& ball = ball_plant->AddRigidBody("Ball", M_Bcm);
+  ball_plant->RegisterAsSourceForSceneGraph(scene_graph);
+  const Eigen::Vector4d orange(1.0, 0.55, 0.0, 1.0);
+  const RigidTransformd X_BS = RigidTransformd::Identity();
+  ball_plant->RegisterVisualGeometry(ball, X_BS, Sphere(radius), "visual",
+                                     orange);
+  ball_plant->Finalize();
+
+  return ball_plant;
+}
+
 void connectTrajectoryVisualizer(
     const MultibodyPlant<double>* plant,
     drake::systems::DiagramBuilder<double>* builder,
     drake::geometry::SceneGraph<double>* scene_graph,
-    const Trajectory<double>& trajectory, MultibodyPlant<double>* ball_plant) {
+    const Trajectory<double>& trajectory) {
+  auto empty_plant = std::make_unique<MultibodyPlant<double>>(0.0);
+  connectTrajectoryVisualizer(plant, builder, scene_graph, trajectory,
+                              *empty_plant);
+}
+
+void connectTrajectoryVisualizer(
+    const MultibodyPlant<double>* plant,
+    drake::systems::DiagramBuilder<double>* builder,
+    drake::geometry::SceneGraph<double>* scene_graph,
+    const Trajectory<double>& trajectory,
+    const MultibodyPlant<double>& ball_plant) {
   auto traj_source =
       builder->AddSystem<drake::systems::TrajectorySource>(trajectory);
   auto passthrough = builder->AddSystem<SubvectorPassThrough>(
@@ -38,19 +66,7 @@ void connectTrajectoryVisualizer(
       scene_graph->get_source_pose_port(plant->get_source_id().value()));
 
   // *******Add COM visualization**********
-  if (ball_plant != nullptr) {
-    // Construct the ball plant
-    double radius = .02;
-    UnitInertia<double> G_Bcm = UnitInertia<double>::SolidSphere(radius);
-    SpatialInertia<double> M_Bcm(1, Eigen::Vector3d::Zero(), G_Bcm);
-    const RigidBody<double>& ball = ball_plant->AddRigidBody("Ball", M_Bcm);
-    ball_plant->RegisterAsSourceForSceneGraph(scene_graph);
-    const Eigen::Vector4d orange(1.0, 0.55, 0.0, 1.0);
-    const RigidTransformd X_BS = RigidTransformd::Identity();
-    ball_plant->RegisterVisualGeometry(ball, X_BS, Sphere(radius), "visual",
-                                       orange);
-    ball_plant->Finalize();
-
+  if (ball_plant.is_finalized()) {
     // connect
     auto q_passthrough = builder->AddSystem<SubvectorPassThrough>(
         traj_source->get_output_port().size(), 0, plant->num_positions());
@@ -59,14 +75,13 @@ void connectTrajectoryVisualizer(
     auto rbt_passthrough = builder->AddSystem<multibody::ComPoseSystem>(*plant);
 
     auto ball_to_pose =
-        builder->AddSystem<MultibodyPositionToGeometryPose<double>>(
-            *ball_plant);
+        builder->AddSystem<MultibodyPositionToGeometryPose<double>>(ball_plant);
     builder->Connect(*q_passthrough, *rbt_passthrough);
     builder->Connect(rbt_passthrough->get_xy_com_output_port(),
                      ball_to_pose->get_input_port());
     builder->Connect(
         ball_to_pose->get_output_port(),
-        scene_graph->get_source_pose_port(ball_plant->get_source_id().value()));
+        scene_graph->get_source_pose_port(ball_plant.get_source_id().value()));
   }
 
   drake::geometry::DrakeVisualizer::AddToBuilder(builder, *scene_graph);
--- multibody/visualization_utils.h
+++ multibody/visualization_utils.h
@@ -9,19 +9,29 @@
 namespace dairlib {
 namespace multibody {
 
+/// Construct a ball plant for visualization
+std::unique_ptr<drake::multibody::MultibodyPlant<double>> ConstructBallPlant(
+    drake::geometry::SceneGraph<double>* scene_graph);
+
 /// Creates and connects a trajectory playback visualizers using the given
 /// builder and scene_graph.
 /// Note:
 ///  1. the plant cannot previously have been connected to a visualizer and the
 ///     diagram must still be built
-///  2. pass in a empty plant, ball_plant, if you want to visualize the center
-///     of mass position. The plant cannot be previously finalized.
+///  2. if you want to visualize the center of mass position, provide ball_plant
+///     as an input. The ball can be constructed by the function
+///     ConstructBallPlant().
+void connectTrajectoryVisualizer(
+    const drake::multibody::MultibodyPlant<double>* plant,
+    drake::systems::DiagramBuilder<double>* builder,
+    drake::geometry::SceneGraph<double>* scene_graph,
+    const drake::trajectories::Trajectory<double>& trajectory);
 void connectTrajectoryVisualizer(
     const drake::multibody::MultibodyPlant<double>* plant,
     drake::systems::DiagramBuilder<double>* builder,
     drake::geometry::SceneGraph<double>* scene_graph,
     const drake::trajectories::Trajectory<double>& trajectory,
-    drake::multibody::MultibodyPlant<double>* ball_plant = nullptr);
+    const drake::multibody::MultibodyPlant<double>& ball_plant);
 
 }  // namespace multibody
 }  // namespace dairlib

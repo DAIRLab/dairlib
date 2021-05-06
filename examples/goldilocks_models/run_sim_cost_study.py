e8f105d566bfa29e044fd2c58b27439e2fed53eb
Yu-Ming Chen <yminchen@umich.edu>
2021-05-06 11:29:05 -0400

    Little update

--- examples/goldilocks_models/run_sim_cost_study.py
+++ examples/goldilocks_models/run_sim_cost_study.py
@@ -527,6 +527,7 @@ if __name__ == "__main__":
   turning_rate = 0.0
 
   # stride_length = np.linspace(-0.2, -0.1, n_task)
+  # stride_length = np.linspace(-0.2, -0.1, n_task, endpoint=False)
 
   task_list = np.zeros((n_task, 4))
   task_list[:, 0] = stride_length

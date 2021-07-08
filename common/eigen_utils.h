d8d6c056a766c2c9dc9599c71487596d005e0ad1
William Yang <yangwill@seas.upenn.edu>
2021-07-08 14:38:27 -0400

    need to remove the external dependency

--- common/eigen_utils.h
+++ common/eigen_utils.h
@@ -3,11 +3,11 @@
 #include <utility>
 #include <vector>
 #include <Eigen/Dense>
-#include <cnpy.h>
+//#include <cnpy.h>
 
 /// CopyVectorXdToStdVector returns an std::vector<double> which is converted
 /// from an Eigen::VectorXd.
 std::vector<double> CopyVectorXdToStdVector(
     const Eigen::VectorXd& eigen_vec);
 
-void cnpy2eigen(std::string data_fname, Eigen::MatrixXd& mat_out);
+//void cnpy2eigen(std::string data_fname, Eigen::MatrixXd& mat_out);

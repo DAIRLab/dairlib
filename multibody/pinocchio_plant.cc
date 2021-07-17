df38639e492cc2e374481c44559ed7c42311bc5b
posa <posa@seas.upenn.edu>
2021-07-17 00:07:11 -0400

    Add coordinate permutations and mass matrix test. Works for fixed-base plant, not implemented for floating base.

--- multibody/pinocchio_plant.cc
+++ multibody/pinocchio_plant.cc
@@ -1,28 +1,97 @@
 #include "multibody/pinocchio_plant.h"
+#include "multibody/multibody_utils.h"
+#include "drake/common/test_utilities/eigen_matrix_compare.h"
 
 namespace dairlib {
 namespace multibody {
 
 using drake::AutoDiffXd;
+using drake::multibody::MultibodyPlant;
+using drake::systems::Context;
+using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::map;
+using std::string;
+
+template <typename T>
+PinocchioPlant<T>::PinocchioPlant(double time_step, const std::string& urdf) :
+      MultibodyPlant<T>(time_step) {
+    pinocchio::urdf::buildModel(urdf, pinocchio_model_);
+    pinocchio_data_ = pinocchio::Data(pinocchio_model_);
+}
+
+template<typename T>
+void PinocchioPlant<T>::BuildPermutations() {
+  map<string, int> pos_map = makeNameToPositionsMap(*this);
+  map<string, int> vel_map = makeNameToVelocitiesMap(*this);
+  int nq = this->num_positions();
+  int nv = this->num_velocities();
+  Eigen::VectorXi pos_indices(nq);
+  Eigen::VectorXi vel_indices(nv);
+
+    // for (auto const &pair: vel_map) {
+    //     std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+    // }
+
+  for (int i = 1; i < pinocchio_model_.names.size(); i++) {
+    // TODO: floating base options
+    // Skipping i=0 for the world (TODO--doesn't handle floating base yet)
+    // Assumes that URDF root is welded to the world
+    const auto& name = pinocchio_model_.names[i];
+    int pos_ind = pos_map[name];
+    int vel_ind = vel_map[name + "dot"];
+    pos_indices(i - 1) = pos_ind;
+    vel_indices(i - 1) = vel_ind;
+
+    // std::cout << pos_ind << ": " << name << std::endl;
+    // std::cout << vel_ind << ": " << name << std::endl;
+  }
+
+  q_perm_.indices() = pos_indices;
+  v_perm_.indices() = vel_indices;
+}
 
 template <>
-void PinocchioPlant<double>::CalcMassMatrix(
-    const drake::systems::Context<double>& context,
+void PinocchioPlant<double>::CalcMassMatrix(const Context<double>& context,
     drake::EigenPtr<Eigen::MatrixXd> M) const {
   VectorXd q = GetPositions(context);
-  // probably not the right num. joints for fixed/floating
-  // definitely not the right ordering  
-  pinocchio::crba(pinocchio_model_, pinocchio_data_, q);
+  pinocchio::crba(pinocchio_model_, pinocchio_data_, q_perm_.inverse() * q);
+
+  // Pinocchio builds an upper triangular matrix, skipping the parts
+  // below the diagonal
   *M = pinocchio_data_.M;
+  for (int i = 0; i < M->cols(); i++) {
+    for (int j = i + 1; j < M->rows(); j++) {
+      (*M)(j, i) = (*M)(i, j);
+    }
+  }
+  *M = v_perm_ * (*M) * v_perm_.inverse();
 }
 
 template<>
 void PinocchioPlant<AutoDiffXd>::CalcMassMatrix(
-    const drake::systems::Context<AutoDiffXd>& context, 
+    const Context<AutoDiffXd>& context, 
     drake::EigenPtr<drake::MatrixX<AutoDiffXd>> M) const {
-  
+}
+
+template<>
+::testing::AssertionResult PinocchioPlant<double>::TestMassMatrix(
+    const Context<double>& context, double tol) const {
+  int nv = num_velocities();
+
+  MatrixXd M(nv, nv);
+  MatrixXd pin_M(nv, nv);
+
+  MultibodyPlant<double>::CalcMassMatrix(context, &M);
+
+  CalcMassMatrix(context, &pin_M);
+
+  return drake::CompareMatrices(M, pin_M, tol);
 }
 
 }  // namespace multibody
 }  // namespace dairlib
+
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class dairlib::multibody::PinocchioPlant)
--- multibody/pinocchio_plant.h
+++ multibody/pinocchio_plant.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <gtest/gtest.h>
 
 #include "drake/multibody/plant/multibody_plant.h"
 
@@ -11,29 +12,39 @@
 #include "pinocchio/multibody/joint/joint-free-flyer.hpp"
 #include "pinocchio/parsers/urdf.hpp"
 
+// TODO: Needs a fixed vs. floating base mechanism
+// Move test methods here as self-verification steps
+// Needs joint correspondance
+
 namespace dairlib {
 namespace multibody {
 template <typename T>
 class PinocchioPlant : public drake::multibody::MultibodyPlant<T> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(PinocchioPlant)
-  
-  explicit PinocchioPlant(double time_step, const std::string& urdf) :
-      drake::multibody::MultibodyPlant<T>(time_step) {
-    pinocchio::urdf::buildModel(urdf, pinocchio_model_);
-    pinocchio_data_ = pinocchio::Data(pinocchio_model_);
-  }
+
+  explicit PinocchioPlant(double time_step, const std::string& urdf);
+
+  void BuildPermutations();
 
   void CalcMassMatrix(const drake::systems::Context<T>& context,
                       drake::EigenPtr<drake::MatrixX<T>> M) const;
 
-  private:
-   pinocchio::Model pinocchio_model_;
-   mutable pinocchio::Data pinocchio_data_;;
+
+  //
+  // Comparisons against MultibodyPlant
+  //
+
+  ::testing::AssertionResult TestMassMatrix(
+      const drake::systems::Context<T>& context, double tol = 1e-5) const;
+
+ private:
+  pinocchio::Model pinocchio_model_;
+  mutable pinocchio::Data pinocchio_data_;
+
+  // permutation matrices maps from Pinocchio to MBP
+  Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> q_perm_;
+  Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> v_perm_;
 };
 }  // namespace multibody
 }  // namespace dairlib
-
-
-DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
-    class dairlib::multibody::PinocchioPlant)

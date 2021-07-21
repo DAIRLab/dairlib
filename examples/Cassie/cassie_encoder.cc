4dbbd9495e25408b5d6be35f8912ee2d285a14b8
William Yang <yangwill@seas.upenn.edu>
2021-07-21 13:45:43 -0400

    addressing comments

--- examples/Cassie/cassie_encoder.cc
+++ examples/Cassie/cassie_encoder.cc
@@ -1,31 +1,32 @@
-#include "systems/primitives/encoder.h"
+#include "cassie_encoder.h"
 
 #include <drake/common/eigen_types.h>
 
 namespace dairlib {
-namespace systems {
 
 using Eigen::VectorXd;
 
-Encoder::Encoder(const drake::multibody::MultibodyPlant<double>& plant,
-                 std::vector<int>& joint_indices,
+CassieEncoder::CassieEncoder(const drake::multibody::MultibodyPlant<double>& plant,
+                 std::vector<int>& joint_pos_indices,
+                 std::vector<int>& joint_vel_indices,
                  std::vector<int>& ticks_per_revolution)
     : num_positions_(plant.num_positions()),
       num_velocities_(plant.num_velocities()),
-      joint_indices_(joint_indices),
+      joint_pos_indices_(joint_pos_indices),
+      joint_vel_indices_(joint_vel_indices),
       ticks_per_revolution_(ticks_per_revolution),
-      joint_filters_(std::vector<JointFilter>(joint_indices_.size())) {
-  this->DeclareVectorInputPort(
+      joint_filters_(std::vector<JointFilter>(ticks_per_revolution_.size())) {
+  this->DeclareVectorInputPort("robot_state",
       systems::BasicVector<double>(num_positions_ + num_velocities_));
-  this->DeclareVectorOutputPort(
+  this->DeclareVectorOutputPort("filtered_state",
       systems::BasicVector<double>(num_positions_ + num_velocities_),
-      &Encoder::UpdateFilter);
+      &CassieEncoder::UpdateFilter);
 }
 
-void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
+void CassieEncoder::UpdateFilter(const drake::systems::Context<double>& context,
                            systems::BasicVector<double>* output) const {
   const systems::BasicVector<double>& input =
-      *this->template EvalVectorInput<BasicVector>(context, 0);
+      *this->template EvalVectorInput<systems::BasicVector>(context, 0);
 
   VectorXd q = input.get_value().head(num_positions_);
   VectorXd v = input.get_value().head(num_velocities_);
@@ -34,7 +35,7 @@ void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
   VectorXd v_filtered = v;
 
   // joint_encoders
-  for (int joint_index = 0; joint_index < joint_indices_.size();
+  for (int joint_index = 0; joint_index < joint_pos_indices_.size();
        ++joint_index) {
     JointFilter filter = joint_filters_[joint_index];
 
@@ -42,8 +43,8 @@ void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
     using std::floor;
     const double ticks_per_radian =
         ticks_per_revolution_[joint_index] / (2.0 * M_PI);
-    q_filtered[joint_indices_[joint_index]] =
-        floor(q[joint_indices_[joint_index]] * ticks_per_radian) /
+    q_filtered[joint_pos_indices_[joint_index]] =
+        floor(q[joint_pos_indices_[joint_index]] * ticks_per_radian) /
         ticks_per_radian;
 
     // Velocity
@@ -56,7 +57,7 @@ void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
       // If all filter values are zero, initialize the signal array
       // with the current encoder value
       for (int i = 0; i < JOINT_FILTER_NB; ++i) {
-        filter.x[i] = q_filtered[joint_indices_[joint_index]];
+        filter.x[i] = q_filtered[joint_pos_indices_[joint_index]];
       }
     }
 
@@ -64,7 +65,7 @@ void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
     for (int i = JOINT_FILTER_NB - 1; i > 0; --i) {
       filter.x[i] = filter.x[i - 1];
     }
-    filter.x[0] = q[joint_indices_[joint_index]];
+    filter.x[0] = q[joint_pos_indices_[joint_index]];
     for (int i = JOINT_FILTER_NA - 1; i > 0; --i) {
       filter.y[i] = filter.y[i - 1];
     }
@@ -77,7 +78,7 @@ void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
     for (int i = 1; i < JOINT_FILTER_NA; ++i) {
       filter.y[0] -= filter.y[i] * joint_filter_a[i];
     }
-    v_filtered[joint_indices_[joint_index]] = filter.y[0];
+    v_filtered[joint_vel_indices_[joint_index]] = filter.y[0];
   }
 
   VectorXd x_filtered = VectorXd::Zero(num_positions_ + num_velocities_);
@@ -85,5 +86,4 @@ void Encoder::UpdateFilter(const drake::systems::Context<double>& context,
   output->set_value(x_filtered);
 }
 
-}  // namespace systems
 }  // namespace dairlib

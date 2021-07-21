ba016a112b15ebc06c5e0e0ce093e5d2235b0bab
Brian Acosta <bjacosta@seas.upenn.edu>
2021-07-21 15:11:41 -0400

    mpc cost tuning

--- systems/controllers/mpc/mpc_trajectory_reciever.cc
+++ systems/controllers/mpc/mpc_trajectory_reciever.cc
@@ -111,8 +111,8 @@ void MpcTrajectoryReceiver::MakeSwingFtTrajFromLcm(
       (PiecewisePolynomial<double>*)dynamic_cast<PiecewisePolynomial<double>*>(
           traj);
 
-  *casted_traj = drake::trajectories::PiecewisePolynomial<double>::CubicHermite(
-      swing_ft_traj.time_vector, knots, knots_dot);
+  *casted_traj = drake::trajectories::PiecewisePolynomial<double>::FirstOrderHold(
+      swing_ft_traj.time_vector, knots);
 }
 
 MatrixXd MpcTrajectoryReceiver::Make3dFromPlanar(MatrixXd planar_knots) const {

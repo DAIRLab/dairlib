1fdc6925d62bae6b095ca6d65b4488d91a6e2aa1
Brian Acosta <bjacosta@seas.upenn.edu>
2021-08-02 16:02:08 -0400

    added parameter saving and visualiztion for drake trajectories

--- examples/contact_parameter_learning/learn_parameters.py
+++ examples/contact_parameter_learning/learn_parameters.py
@@ -2,8 +2,17 @@ import nevergrad as ng
 import cube_sim
 import drake_cube_sim
 import mujoco_cube_sim
+import os
+from json import dump, load
 
-cube_data_folder = '/home/brian/workspace/contact-nets/data/tosses_processed/'
+cube_data_folder = os.path.join(os.getcwd(), '..', 'contact-nets/data/tosses_processed/')
+model_folder = os.path.join(os.getcwd(), 'examples/contact_parameter_learning/learned_parameters')
+
+def save_params(simulator, id, params):
+    filename = os.path.join(model_folder, simulator + str(id) +'.json')
+    with open(filename, 'w+') as fp:
+        dump(params, fp)
+
 
 def get_drake_loss(params):
     get_drake_loss.sim = drake_cube_sim.DrakeCubeSim()
@@ -21,7 +30,7 @@ def learn_drake_params():
     optimization_param.value=drake_cube_sim.default_drake_contact_params
     optimizer = ng.optimizers.NGOpt(parametrization=optimization_param, budget=10000)
     params = optimizer.minimize(get_drake_loss)
-    print(params.value)
+    save_params('drake', 33, params.value)
 
 
 def get_mujoco_loss(params):
@@ -38,10 +47,25 @@ def learn_mujoco_params():
         mu_rolling=ng.p.Scalar(lower=0.000001, upper=0.1)
     )
     optimization_param.value=mujoco_cube_sim.default_mujoco_contact_params
-    optimizer = ng.optimizers.NGOpt(parametrization=optimization_param, budget=10000)
+    optimizer = ng.optimizers.NGOpt(parametrization=optimization_param, budget=100)
     params = optimizer.minimize(get_mujoco_loss)
     print(params.value)
+    save_params('mujoco', 33, params.value)
+    visualize_learned_params(params.value, 'mujoco', 33)
 
+def visualize_learned_params(params, sim, toss_id):
+    cube_data = cube_sim.load_cube_toss(cube_sim.make_cube_toss_filename(cube_data_folder, toss_id))
+    initial_state = cube_data[0].ravel()
+    
+    if (sim == 'mujoco'):
+        vis_sim_params = mujoco_cube_sim.MujocoCubeSim(True)
+        vis_sim_data = mujoco_cube_sim.MujocoCubeSim(True)
+        
+
+    vis_sim_data.init_sim(mujoco_cube_sim.default_mujoco_contact_params)
+    #vis_sim_data.visualize_data_rollout(cube_data)
+    vis_sim_params.visualize_sim_rollout(params, initial_state, cube_data.shape[0])
+    
 
 if (__name__ == '__main__'):
     learn_mujoco_params()

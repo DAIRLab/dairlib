d3d94b76b89b74700966d38a7d0f370c238248a2
William Yang <yangwill@seas.upenn.edu>
2021-08-04 16:15:42 -0400

    improving where the data is saved

--- examples/contact_parameter_learning/learn_cassie_parameters.py
+++ examples/contact_parameter_learning/learn_cassie_parameters.py
@@ -1,31 +1,36 @@
 import nevergrad as ng
 import drake_cassie_sim
 import os
+import pickle
 from json import dump, load
+import cassie_loss_utils
+
 
 
 def get_drake_loss(params):
+  log_num = '27'
   sim = drake_cassie_sim.DrakeCassieSim()
-  sim_id = sim.run()
+  sim_id = sim.run(params, log_num)
   loss = sim.compute_loss(log_num, sim_id)
 
   return
 
 def learn_drake_cassie_params():
+  # loss_weights_file = 'default_loss_weights'
   sim = drake_cassie_sim.DrakeCassieSim()
   # sim.run(sim.default_drake_contact_params, '27')
 
   optimization_param = ng.p.Dict(
     mu_static = ng.p.Scalar(lower=0.001, upper=1.0),
     mu_ratio = ng.p.Scalar(lower=0.001, upper=1.0),
-    pen_allow=ng.p.Log(lower=1e-10, upper=1e-1),
+    pen_allow=ng.p.Log(lower=1e-6, upper=1e-1),
     stiction_tol=ng.p.Log(lower=1e-6, upper=1e-1)
   )
 
-  optimization_param.value=default_drake_contact_params
+  optimization_param.value=sim.default_drake_contact_params
   optimizer = ng.optimizers.NGOpt(parametrization=optimization_param, budget=10000)
   params = optimizer.minimize(get_drake_loss)
-  save_params('drake', 33, params.value)
+  sim.save_params(params, "optimized_params")
 
 if (__name__ == '__main__'):
   learn_drake_cassie_params()

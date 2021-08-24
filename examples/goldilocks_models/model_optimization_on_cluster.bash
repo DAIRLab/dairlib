2dc5d4098d9a55131866b035dcbd5cf77bedfc10
Yu-Ming Chen <yminchen@umich.edu>
2021-08-24 13:32:51 -0400

    update model optimization script

--- examples/goldilocks_models/model_optimization_on_cluster.bash
+++ examples/goldilocks_models/model_optimization_on_cluster.bash
@@ -24,11 +24,14 @@
 #SBATCH --ntasks=1
 #SBATCH --array=0-75%1
 
-# This script hasn't been not tested yet.
-
-# Note that you need to bazel build the binary, because not all machines/nodes have it.
+# This script hasn't been fully tested yet.
 
 cd /scratch/$USER/dairlib
+
+# Note that you need to bazel build the binary, because not all machines/nodes have it. (even though it's the same file path...)
+# Build the program
+bazel build examples/goldilocks_models:find_goldilocks_models
+
 
 ###########model optimization setting##########
 # Set robot id and model id
@@ -47,64 +50,61 @@ n_ph=5
 #final_iter=100
 
 if [ $SLURM_ARRAY_TASK_ID = 0 ]; then
-    # Delete and create a new data folder if specified in the argument
-    if [ "$1" = "rm" ]; then
-    	echo Delete and create a new folder dairlib_data/goldilocks_models/find_models/robot_$robot/
-    	rm -rf ../dairlib_data/goldilocks_models/find_models/robot_$robot/
-    fi
-    mkdir -p ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_no_constraint_traj/
-    mkdir -p ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_traj_cubic_swing_foot/
+  # Delete and create a new data folder if specified in the argument
+  if [ "$1" = "rm" ]; then
+    echo Delete and create a new folder dairlib_data/goldilocks_models/find_models/robot_$robot/
+    rm -rf ../dairlib_data/goldilocks_models/find_models/robot_$robot/
+  fi
+  mkdir -p ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_no_constraint_traj/
+  mkdir -p ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_traj_cubic_swing_foot/
 
-    # Build the program
-    bazel build examples/goldilocks_models:find_goldilocks_models
+  # Optimize the model
+  echo ===== evaluate nomial traj \(without snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=false --start_current_iter_as_rerun=false \
+  --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
-    # Optimize the model
-    echo ===== evaluate nomial traj \(without snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=false --start_current_iter_as_rerun=false \
-    --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== evaluate nomial traj \(with snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=true --start_current_iter_as_rerun=true \
+  --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
-    echo ===== evaluate nomial traj \(with snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=true --start_current_iter_as_rerun=true \
-    --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== copy files for nomial gaits =====
+  cp ../dairlib_data/goldilocks_models/find_models/robot_$robot/0_* ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_no_constraint_traj/
 
-    echo ===== copy files for nomial gaits =====
-    cp ../dairlib_data/goldilocks_models/find_models/robot_$robot/0_* ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_no_constraint_traj/
+  echo ===== evaluate nomial traj \(without snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=false --start_current_iter_as_rerun=true \
+   --swing_foot_cublic_spline=true \
+  --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
-    echo ===== evaluate nomial traj \(without snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=false --start_current_iter_as_rerun=true \
-     --swing_foot_cublic_spline=true \
-    --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== evaluate nomial traj \(with snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=true --start_current_iter_as_rerun=true \
+   --swing_foot_cublic_spline=true \
+  --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
-    echo ===== evaluate nomial traj \(with snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=true --start_current_iter_as_rerun=true \
-     --swing_foot_cublic_spline=true \
-    --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== copy files for nomial gaits with cubic swing foot constraint =====
+  cp ../dairlib_data/goldilocks_models/find_models/robot_$robot/0_* ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_traj_cubic_swing_foot/
 
-    echo ===== copy files for nomial gaits with cubic swing foot constraint =====
-    cp ../dairlib_data/goldilocks_models/find_models/robot_$robot/0_* ../dairlib_data/goldilocks_models/find_models/robot_$robot/nominal_traj_cubic_swing_foot/
+  echo ===== evaluate nomial traj with com accel constraint  \(without snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=false --start_current_iter_as_rerun=true \
+   --com_accel_constraint=true --swing_foot_cublic_spline=true \
+   --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
-    echo ===== evaluate nomial traj with com accel constraint  \(without snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=false --start_current_iter_as_rerun=true \
-     --com_accel_constraint=true --swing_foot_cublic_spline=true \
-     --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
-
-    echo ===== evaluate nomial traj with com accel constraint \(with snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=true --start_current_iter_as_rerun=true \
-     --com_accel_constraint=true --swing_foot_cublic_spline=true \
-     --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== evaluate nomial traj with com accel constraint \(with snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=0 --max_outer_iter=0 --snopt_scaling=true --start_current_iter_as_rerun=true \
+   --com_accel_constraint=true --swing_foot_cublic_spline=true \
+   --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
 
-    echo ===== evaluate initial rom \(without snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=1 --max_outer_iter=1 --snopt_scaling=false --start_current_iter_as_rerun=false \
-     --swing_foot_cublic_spline=true \
-     --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== evaluate initial rom \(without snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=1 --max_outer_iter=1 --snopt_scaling=false --start_current_iter_as_rerun=false \
+   --swing_foot_cublic_spline=true \
+   --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 
 else
 
-    echo ===== evaluate \(with snopt scaling\) =====
-    ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=$SLURM_ARRAY_TASK_ID --max_outer_iter=$SLURM_ARRAY_TASK_ID --snopt_scaling=true --start_current_iter_as_rerun=true \
-     --swing_foot_cublic_spline=true \
-     --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
+  echo ===== evaluate \(with snopt scaling\) =====
+  ./bazel-bin/examples/goldilocks_models/find_goldilocks_models --iter_start=$SLURM_ARRAY_TASK_ID --max_outer_iter=$SLURM_ARRAY_TASK_ID --snopt_scaling=true --start_current_iter_as_rerun=true \
+   --swing_foot_cublic_spline=true \
+   --rom_option=$model --robot_option=$robot --N_sample_sl=$n_sl --N_sample_gi=$n_gi --N_sample_du=$n_du --N_sample_tr=$n_tr --N_sample_ph=$n_ph --fix_node_number=true 2>&1 | tee -a ../dairlib_data/goldilocks_models/find_models/robot_$robot/terminal_log
 fi
 
 wait

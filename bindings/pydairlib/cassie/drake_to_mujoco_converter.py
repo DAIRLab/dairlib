quaternion looks correct in visualization but breaks sim

bindings/pydairlib/cassie/drake_to_mujoco_converter.py
@@ -81,12 +81,11 @@ class DrakeToMujocoConverter():
 
     self.left_achilles_frame = np.zeros((3, 3))
     self.left_achilles_frame[:, 0] = np.array([0.7922, -0.60599, -0.072096])
-    self.left_achilles_frame[:, 1] = np.array([0.60349, 0.79547, -0.054922])
+    self.left_achilles_frame[:, 1] = -np.array([0.60349, 0.79547, -0.054922])
     self.left_achilles_frame[:, 2] = np.cross(self.left_achilles_frame[:, 0], self.left_achilles_frame[:, 1])
-    self.left_achilles_frame = self.left_achilles_frame.T
     self.right_achilles_frame = np.zeros((3, 3))
     self.right_achilles_frame[:, 0] = np.array([0.7922, -0.60599, 0.072096])
-    self.right_achilles_frame[:, 1] = np.array([0.60349, 0.79547, 0.054922])
+    self.right_achilles_frame[:, 1] = -np.array([0.60349, 0.79547, 0.054922])
     self.right_achilles_frame[:, 2] = np.cross(self.right_achilles_frame[:, 0], self.right_achilles_frame[:, 1])
 
     self.plantar_rod_frame = self.foot_crank_plant.GetBodyByName('plantar_rod_left').body_frame()
@@ -174,21 +173,17 @@ class DrakeToMujocoConverter():
     sim.Initialize()
     sim.AdvanceTo(0.1)
 
-  def visualize_state_upper(self, quat, x):
-    # self.plant.SetPositionsAndVelocities(self.context, x)
-    # toe_left_ang = x[self.pos_map['toe_left']]
-    # toe_left_ang = -1.5968
-    # self.toe_angle_constraint.evaluator().UpdateCoefficients(Aeq=[[1]], beq=[toe_left_ang])
-    # self.ik_solver.prog().SetInitialGuess(self.ik_solver.q(), np.array([1, 0, 0, 0, 0, 0, 0, toe_left_ang, toe_left_ang, -toe_left_ang]))
-    # result = Solve(self.ik_solver.prog())
-    # left_foot_crank_state = result.GetSolution()
-    # print(left_foot_crank_state)
-    # print(result.get_solution_result())
+  def visualize_state_upper(self, x):
+
+    q_missing, _ = self.solve_IK(x)
+    quat = q_missing[10:14]
 
     rot = R.from_quat(np.hstack((quat[1:4], quat[0])))
     euler_vec = rot.as_euler('xyz')
 
-    knee_linkage_state = np.array([1, 0, 0, 0, 0, 0, 0, -0.03944253, 0.02825365, -0.41339635, -1.1997, 0, 1.4267, 0])
+    fb_state = np.array([-0.14644661,  0.35355339,  0.35355339,  0.85355339, 0, 0, 1])
+    # knee_linkage_state = np.array([-0.5,  0.5,  0.5,  0.5, 0, 0, 1, -0.03944253, 0.02825365, -0.41339635, -1.1997, 0, 1.4267, 0])
+    knee_linkage_state = np.hstack((fb_state, np.array([-0.03944253, 0.02825365, -0.41339635, -1.1997, 0, 1.4267, 0])))
     knee_linkage_state[7:10] = -euler_vec
     knee_linkage_state[10] = x[13]
     knee_linkage_state[11] = x[15]
@@ -277,7 +272,8 @@ class DrakeToMujocoConverter():
                                                 self.world)
     right_thigh = self.plant.CalcPointsPositions(self.context, self.right_thigh_rod[1], self.right_thigh_rod[0],
                                                  self.world)
-    left_heel = self.plant.CalcPointsPositions(self.context, self.left_heel_rod[1], self.left_heel_rod[0], self.world)
+    left_heel = self.plant.CalcPointsPositions(self.context, self.left_heel_rod[1], self.left_heel_rod[0],
+                                               self.world)
     right_heel = self.plant.CalcPointsPositions(self.context, self.right_heel_rod[1], self.right_heel_rod[0],
                                                 self.world)
 
@@ -298,22 +294,16 @@ class DrakeToMujocoConverter():
 
     l_hip_pitch_frame = self.plant.CalcRelativeTransform(self.context, self.world, self.left_thigh_rod[1])
     r_hip_pitch_frame = self.plant.CalcRelativeTransform(self.context, self.world, self.right_thigh_rod[1])
-    # l_bar_frame = np.vstack((l_x_vec, l_y_vec, l_z_vec)).T
-    # r_bar_frame = np.vstack((r_x_vec, r_y_vec, r_z_vec)).T
     l_bar_frame = R.from_matrix(np.vstack((l_x_vec, l_y_vec, l_z_vec)).T)
-    # print(l_bar_frame.as_matrix())
     r_bar_frame = R.from_matrix(np.vstack((r_x_vec, r_y_vec, r_z_vec)).T)
-    # l_bar_quat = R.from_matrix(
-    #   self.left_achilles_frame.T @ l_hip_pitch_frame.rotation().matrix().T @ l_bar_frame.as_matrix()).as_quat()
     l_bar_quat = R.from_matrix(
-      self.left_achilles_frame.T @ l_hip_pitch_frame.rotation().matrix().T @ l_bar_frame.as_matrix()).as_quat()
+      self.left_achilles_frame @ l_hip_pitch_frame.rotation().matrix().T @ l_bar_frame.as_matrix()).as_quat()
     r_bar_quat = R.from_matrix(
-      self.right_achilles_frame.T @ r_hip_pitch_frame.rotation().matrix().T @ r_bar_frame.as_matrix()).as_quat()
-    # import pdb; pdb.set_trace()
+      self.right_achilles_frame @ r_hip_pitch_frame.rotation().matrix().T @ r_bar_frame.as_matrix()).as_quat()
     l_bar_quat = np.hstack((l_bar_quat[3], l_bar_quat[0:3]))
     r_bar_quat = np.hstack((r_bar_quat[3], r_bar_quat[0:3]))
 
-    self.visualize_state_upper(l_bar_quat, x)
+    # self.visualize_state_upper(l_bar_quat, x)
 
     q_missing = np.zeros(35)
     v_missing = np.zeros(32)

bindings/pydairlib/cassie/mujoco_sim.py
@@ -106,7 +106,7 @@ class MujocoCassieSim():
       q, v, u, t = self.sim_step(u)
       if (self.publish_state):
         pack_robot_output(self.robot_output, q, v, u, t)
-        self.lcm.Publish('CASSIE_STATE_SIMULATION', self.robot_output.encode())
+        self.lcm.Publish('CASSIE_STATE_MUJOCO', self.robot_output.encode())
       q, v = self.drake_to_mujoco_converter.convert_to_drake(q, v)
       x_traj.append(np.hstack((q, v)))
       u_traj.append(u)
@@ -134,7 +134,7 @@ class MujocoCassieSim():
       q, v, u, t = self.sim_step(u)
       if (self.publish_state):
         pack_robot_output(self.robot_output, q, v, u, t)
-        self.lcm.Publish('CASSIE_STATE_SIMULATION', self.robot_output.encode())
+        self.lcm.Publish('CASSIE_STATE_MUJOCO', self.robot_output.encode())
       x_traj.append([q, v])
       u_traj.append(u)
       t_traj.append(t)

examples/contact_parameter_learning/learn_cassie_parameters.py
@@ -13,10 +13,10 @@ import time
 
 SIM_ERROR_LOSS = 200
 
-# drake_sim = drake_cassie_sim.DrakeCassieSim(drake_sim_dt=8e-5, loss_filename='pos_loss_weights')
-# mujoco_sim = mujoco_cassie_sim.LearningMujocoCassieSim(loss_filename='pos_loss_weights')
-drake_sim = drake_cassie_sim.DrakeCassieSim(drake_sim_dt=8e-5, loss_filename=time.strftime("%Y_%m_%d") + '_weights')
-mujoco_sim = mujoco_cassie_sim.LearningMujocoCassieSim(loss_filename=time.strftime("%Y_%m_%d") + '_weights')
+drake_sim = drake_cassie_sim.DrakeCassieSim(drake_sim_dt=8e-5, loss_filename='2021_08_27_weights')
+mujoco_sim = mujoco_cassie_sim.LearningMujocoCassieSim(loss_filename='2021_08_27_weights')
+# drake_sim = drake_cassie_sim.DrakeCassieSim(drake_sim_dt=8e-5, loss_filename=time.strftime("%Y_%m_%d") + '_weights')
+# mujoco_sim = mujoco_cassie_sim.LearningMujocoCassieSim(loss_filename=time.strftime("%Y_%m_%d") + '_weights')
 loss_over_time = []
 stiffness_over_time = []
 log_num = 'training'
@@ -95,7 +95,8 @@ def learn_drake_cassie_params():
   stiffness = np.array(stiffness_over_time)
   np.save(drake_sim.params_folder + log_num + '_loss_trajectory_' + str(budget), loss)
   np.save(drake_sim.params_folder + log_num + '_stiffness_trajectory_' + str(budget), stiffness)
-  drake_sim.save_params(params, '_training_' + str(budget))
+  # drake_sim.save_params(params, '_training_' + str(budget))
+  drake_sim.save_params(params, log_num + '_x_offsets_' + str(budget))
   print('optimal params:')
   print(params)
 
@@ -213,6 +214,7 @@ def print_params():
 
 def learn_x_offsets():
   global training_idxs
+  global log_num
   for i in all_logs:
     print(i)
     log_num = i
@@ -221,7 +223,7 @@ def learn_x_offsets():
 
 if (__name__ == '__main__'):
   # print_loss_weights('pos_loss_weights')
-  # learn_x_offsets()
+  learn_x_offsets()
   # save_x_offsets()
   # print_params()
   # import pdb; pdb.set_trace()
@@ -232,6 +234,6 @@ if (__name__ == '__main__'):
   # plot_per_log_loss_mujoco()
   # print_mujoco_cassie_params()
   # log_num = '33'
-  print_drake_cassie_params('16', False)
+  # print_drake_cassie_params('16', False)
   # plot_loss_trajectory()
   pass

examples/contact_parameter_learning/test_mujoco_sim.py
@@ -12,25 +12,25 @@ def test_default():
 
 
 def test_initial_state():
-  sim = MujocoCassieSim(realtime_rate=1e-1)
-  # q_init = np.array([1, 0, 0, 0, 0, 0, 10.0,
-  #                    0.0216644, -0.0216644,
-  #                    0, 0,
-  #                    0.62496, 0.62496,
-  #                    -1.652, -1.652,
-  #                    -0.00, -0.00,
-  #                    1.77012, 1.77013,
-  #                    -0.0, -1.7677,
-  #                    -0.0, -1.7677])
-  q_init = np.array([1, 0, 0, 0, 0, 0, 10.0,
-                     0.0045, -0.0045,
+  sim = MujocoCassieSim(realtime_rate=1e-2)
+  q_init = np.array([1, 0, 0, 0, 0, 0, 2.0,
+                     0.0216644, -0.0216644,
                      0, 0,
-                     0.4973, 0.4973,
-                     -1.1997, -1.1997,
+                     0.62496, 0.62496,
+                     -1.652, -1.652,
                      -0.00, -0.00,
-                     1.4267, 1.4267,
-                     -0.0, -1.5244,
-                     -0.0, -1.5244])
+                     1.77012, 1.77013,
+                     -0.0, -1.7677,
+                     -0.0, -1.7677])
+  # q_init = np.array([1, 0, 0, 0, 0, 0, 2.0,
+  #                    0.0045, -0.0045,
+  #                    0, 0,
+  #                    0.4973, 0.4973,
+  #                    -1.1997, -1.1997,
+  #                    -0.00, -0.00,
+  #                    1.4267, 1.4267,
+  #                    -0.0, -1.5244,
+  #                    -0.0, -1.5244])
   # 0.0045, 0, 0.4973, 0.9785, -0.0164, 0.01787, -0.2049,
   # -1.1997, 0, 1.4267, 0, -1.5244, 1.5244, -1.5968,
   # -0.0045, 0, 0.4973, 0.9786, 0.00386, -0.01524, -0.2051,
@@ -58,13 +58,26 @@ def test_playback():
   x_init = x_interp(start_time)
   # import pdb; pdb.set_trace()
 
-  # sim.run_sim_playback(x_init, start_time, end_time)
+  sim.run_sim_playback(x_init, start_time, end_time)
   # sim.run_sim_playback(x_init, start_time, end_time, u_init_traj)
   # sim.visualize_IK_lower(x_init)
-  sim.visualize_IK_upper(x_init)
+  # sim.visualize_IK_upper(x_init)
 
+def visualize_loop_closures():
+  folder_path = "/home/yangwill/Documents/research/projects/impact_uncertainty/data/"
+  log_num = '02'
+  x_traj = np.load(folder_path + 'x_' + log_num + '.npy')
+  t = np.load(folder_path + 't_x_' + log_num + '.npy')
+  sim = MujocoCassieSim(realtime_rate=1.0)
+  time = 30.495
+  x_interp = interpolate.interp1d(t[:, 0], x_traj, axis=0, bounds_error=True)
+  x_init = x_interp(time)
+
+  # sim.visualize_IK_lower(x_init)
+  sim.visualize_IK_upper(x_init)
 
 if __name__ == '__main__':
   # test_default()
   # test_playback()
+  # visualize_loop_closures()
   test_initial_state()
62564e9659c9720b9a009c6653142f7c5b4c0186
Yu-Ming Chen <yminchen@umich.edu>
2021-09-18 15:16:30 -0400

    Update DRAKE_DEMAND for the new model

--- examples/goldilocks_models/find_goldilocks_models.cc
+++ examples/goldilocks_models/find_goldilocks_models.cc
@@ -1462,7 +1462,7 @@ int findGoldilocksModels(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   DRAKE_DEMAND((FLAGS_robot_option == 0) || FLAGS_robot_option == 1);
-  DRAKE_DEMAND((FLAGS_rom_option >= 0) && FLAGS_rom_option <= 8);
+  DRAKE_DEMAND((FLAGS_rom_option >= 0) && FLAGS_rom_option <= 9);
   if (FLAGS_robot_option == 0) {
     DRAKE_DEMAND(FLAGS_rom_option != 4);
     DRAKE_DEMAND(FLAGS_rom_option != 5);

6b4761a3d069aed9db39d10dda519f4f2f16b406
Yu-Ming Chen <yminchen@umich.edu>
2021-10-08 14:34:13 -0400

    update install script for pinocchio (for CI)

--- install/install_prereqs_focal.sh
+++ install/install_prereqs_focal.sh
@@ -34,3 +34,16 @@ cd ..
 rm -rf tmp/
 # In addition to drake, install lcm and libbot2
 apt install lcm libbot2
+
+# Install Pinocchio
+sudo apt install -qqy lsb-release gnupg2 curl
+echo "deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" | sudo tee /etc/apt/sources.list.d/robotpkg.list
+curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt install -qqy robotpkg-py38-pinocchio  # Adapt your desired python version here
+
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python3.8/site-packages:$PYTHONPATH # Adapt your desired python version here
+export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH

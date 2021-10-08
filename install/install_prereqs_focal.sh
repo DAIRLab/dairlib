76e8937b6b0d413afba0fddc25a1d942ab44d4ec
Yu-Ming Chen <yminchen@umich.edu>
2021-10-08 16:07:27 -0400

    Test -- use drake's official repo to build docker image on CI

--- install/install_prereqs_focal.sh
+++ install/install_prereqs_focal.sh
@@ -14,19 +14,19 @@ echo "Using Drake commit '${DRAKE_COMMIT}'"
 # Download and run drake install scripts
 mkdir tmp
 cd tmp
-wget "https://raw.githubusercontent.com/yminchen/drake/${DRAKE_COMMIT}/setup/ubuntu/install_prereqs.sh"
+wget "https://raw.githubusercontent.com/RobotLocomotion/drake/${DRAKE_COMMIT}/setup/ubuntu/install_prereqs.sh"
 mkdir source_distribution
 cd source_distribution
-wget "https://raw.githubusercontent.com/yminchen/drake/${DRAKE_COMMIT}/setup/ubuntu/source_distribution/install_prereqs.sh"
+wget "https://raw.githubusercontent.com/RobotLocomotion/drake/${DRAKE_COMMIT}/setup/ubuntu/source_distribution/install_prereqs.sh"
 # skipping drake's "install_prereqs_user_environment.sh"
 touch install_prereqs_user_environment.sh
-wget "https://raw.githubusercontent.com/yminchen/drake/${DRAKE_COMMIT}/setup/ubuntu/source_distribution/packages-${ubuntu_codename}.txt"
-wget "https://raw.githubusercontent.com/yminchen/drake/${DRAKE_COMMIT}/setup/ubuntu/source_distribution/packages-${ubuntu_codename}-test-only.txt"
+wget "https://raw.githubusercontent.com/RobotLocomotion/drake/${DRAKE_COMMIT}/setup/ubuntu/source_distribution/packages-${ubuntu_codename}.txt"
+wget "https://raw.githubusercontent.com/RobotLocomotion/drake/${DRAKE_COMMIT}/setup/ubuntu/source_distribution/packages-${ubuntu_codename}-test-only.txt"
 cd ..
 mkdir binary_distribution
 cd binary_distribution
-wget "https://raw.githubusercontent.com/yminchen/drake/${DRAKE_COMMIT}/setup/ubuntu/binary_distribution/install_prereqs.sh"
-wget "https://raw.githubusercontent.com/yminchen/drake/${DRAKE_COMMIT}/setup/ubuntu/binary_distribution/packages-${ubuntu_codename}.txt"
+wget "https://raw.githubusercontent.com/RobotLocomotion/drake/${DRAKE_COMMIT}/setup/ubuntu/binary_distribution/install_prereqs.sh"
+wget "https://raw.githubusercontent.com/RobotLocomotion/drake/${DRAKE_COMMIT}/setup/ubuntu/binary_distribution/packages-${ubuntu_codename}.txt"
 cd ..
 chmod +x install_prereqs.sh
 ./install_prereqs.sh

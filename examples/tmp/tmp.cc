3b156b135cf5669c875d5bdf3496d6c90d167559
Michael Posa <posa@seas.upenn.edu>
2021-11-09 14:07:29 -0500

    C example

--- examples/tmp/tmp.cc
+++ examples/tmp/tmp.cc
@@ -1,4 +1,4 @@
-* Copyright 2021, Gurobi Optimization, LLC */
+/* Copyright 2021, Gurobi Optimization, LLC */
 
 /* This example formulates and solves the following simple MIP model:
 
@@ -8,55 +8,106 @@
                  x, y, z binary
 */
 
-#include "gurobi_c++.h"
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+#include "gurobi_c.h"
 
 int
 main(int   argc,
      char *argv[])
 {
-  try {
+  GRBenv   *env   = NULL;
+  GRBmodel *model = NULL;
+  int       error = 0;
+  double    sol[3];
+  int       ind[3];
+  double    val[3];
+  double    obj[3];
+  char      vtype[3];
+  int       optimstatus;
+  double    objval;
 
-    // Create an environment
-    GRBEnv env = GRBEnv(true);
-    env.set("LogFile", "mip1.log");
-    env.start();
+  /* Create environment */
+  error = GRBemptyenv(&env);
+  if (error) goto QUIT;
 
-    // Create an empty model
-    GRBModel model = GRBModel(env);
+  error = GRBsetstrparam(env, "LogFile", "mip1.log");
+  if (error) goto QUIT;
 
-    // Create variables
-    GRBVar x = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x");
-    GRBVar y = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "y");
-    GRBVar z = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "z");
+  error = GRBstartenv(env);
+  if (error) goto QUIT;
 
-    // Set objective: maximize x + y + 2 z
-    model.setObjective(x + y + 2 * z, GRB_MAXIMIZE);
+  /* Create an empty model */
+  error = GRBnewmodel(env, &model, "mip1", 0, NULL, NULL, NULL, NULL, NULL);
+  if (error) goto QUIT;
 
-    // Add constraint: x + 2 y + 3 z <= 4
-    model.addConstr(x + 2 * y + 3 * z <= 4, "c0");
+  /* Add variables */
+  obj[0] = 1; obj[1] = 1; obj[2] = 2;
+  vtype[0] = GRB_BINARY; vtype[1] = GRB_BINARY; vtype[2] = GRB_BINARY;
+  error = GRBaddvars(model, 3, 0, NULL, NULL, NULL, obj, NULL, NULL, vtype,
+                     NULL);
+  if (error) goto QUIT;
 
-    // Add constraint: x + y >= 1
-    model.addConstr(x + y >= 1, "c1");
+  /* Change objective sense to maximization */
+  error = GRBsetintattr(model, GRB_INT_ATTR_MODELSENSE, GRB_MAXIMIZE);
+  if (error) goto QUIT;
 
-    // Optimize model
-    model.optimize();
+  /* First constraint: x + 2 y + 3 z <= 4 */
+  ind[0] = 0; ind[1] = 1; ind[2] = 2;
+  val[0] = 1; val[1] = 2; val[2] = 3;
 
-    cout << x.get(GRB_StringAttr_VarName) << " "
-         << x.get(GRB_DoubleAttr_X) << endl;
-    cout << y.get(GRB_StringAttr_VarName) << " "
-         << y.get(GRB_DoubleAttr_X) << endl;
-    cout << z.get(GRB_StringAttr_VarName) << " "
-         << z.get(GRB_DoubleAttr_X) << endl;
+  error = GRBaddconstr(model, 3, ind, val, GRB_LESS_EQUAL, 4.0, "c0");
+  if (error) goto QUIT;
 
-    cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+  /* Second constraint: x + y >= 1 */
+  ind[0] = 0; ind[1] = 1;
+  val[0] = 1; val[1] = 1;
 
-  } catch(GRBException e) {
-    cout << "Error code = " << e.getErrorCode() << endl;
-    cout << e.getMessage() << endl;
-  } catch(...) {
-    cout << "Exception during optimization" << endl;
+  error = GRBaddconstr(model, 2, ind, val, GRB_GREATER_EQUAL, 1.0, "c1");
+  if (error) goto QUIT;
+
+  /* Optimize model */
+  error = GRBoptimize(model);
+  if (error) goto QUIT;
+
+  /* Write model to 'mip1.lp' */
+  error = GRBwrite(model, "mip1.lp");
+  if (error) goto QUIT;
+
+  /* Capture solution information */
+  error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
+  if (error) goto QUIT;
+
+  error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);
+  if (error) goto QUIT;
+
+  error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, 3, sol);
+  if (error) goto QUIT;
+
+  printf("\nOptimization complete\n");
+  if (optimstatus == GRB_OPTIMAL) {
+    printf("Optimal objective: %.4e\n", objval);
+
+    printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
+  } else if (optimstatus == GRB_INF_OR_UNBD) {
+    printf("Model is infeasible or unbounded\n");
+  } else {
+    printf("Optimization was stopped early\n");
   }
+
+QUIT:
+
+  /* Error reporting */
+  if (error) {
+    printf("ERROR: %s\n", GRBgeterrormsg(env));
+    exit(1);
+  }
+
+  /* Free model */
+  GRBfreemodel(model);
+
+  /* Free environment */
+  GRBfreeenv(env);
 
   return 0;
 }

0a0ebcdeb326527445f29daf6b673744c2a3f766
Alp <alpaydinogl@gmail.com>
2021-11-12 15:02:27 -0500

    without virtual

--- examples/tmp/tmp.cc
+++ examples/tmp/tmp.cc
@@ -12,102 +12,116 @@
 #include <stdio.h>
 #include "gurobi_c.h"
 
-int
-main(int   argc,
+//For using cout
+#include<iostream>
+using namespace std;
+//
+
+class C3 {        // The class
+  public:              // Access specifier
+    int SolveSingleProjection() {  // Method/function defined inside the class
+      GRBenv   *env   = NULL;
+      GRBmodel *model = NULL;
+      int       error = 0;
+      double    sol[3];
+      int       ind[3];
+      double    val[3];
+      double    obj[3];
+      char      vtype[3];
+      int       optimstatus;
+      double    objval;
+
+      /* Create environment */
+      error = GRBemptyenv(&env);
+      if (error) goto QUIT;
+
+      error = GRBsetstrparam(env, "LogFile", "mip1.log");
+      if (error) goto QUIT;
+
+      error = GRBstartenv(env);
+      if (error) goto QUIT;
+
+      /* Create an empty model */
+      error = GRBnewmodel(env, &model, "mip1", 0, NULL, NULL, NULL, NULL, NULL);
+      if (error) goto QUIT;
+
+      /* Add variables */
+      obj[0] = 1; obj[1] = 1; obj[2] = 2;
+      vtype[0] = GRB_BINARY; vtype[1] = GRB_BINARY; vtype[2] = GRB_BINARY;
+      error = GRBaddvars(model, 3, 0, NULL, NULL, NULL, obj, NULL, NULL, vtype,
+                         NULL);
+      if (error) goto QUIT;
+
+      /* Change objective sense to maximization */
+      error = GRBsetintattr(model, GRB_INT_ATTR_MODELSENSE, GRB_MAXIMIZE);
+      if (error) goto QUIT;
+
+      /* First constraint: x + 2 y + 3 z <= 4 */
+      ind[0] = 0; ind[1] = 1; ind[2] = 2;
+      val[0] = 1; val[1] = 2; val[2] = 3;
+
+      error = GRBaddconstr(model, 3, ind, val, GRB_LESS_EQUAL, 4.0, "c0");
+      if (error) goto QUIT;
+
+      /* Second constraint: x + y >= 1 */
+      ind[0] = 0; ind[1] = 1;
+      val[0] = 1; val[1] = 1;
+
+      error = GRBaddconstr(model, 2, ind, val, GRB_GREATER_EQUAL, 1.0, "c1");
+      if (error) goto QUIT;
+
+      /* Optimize model */
+      error = GRBoptimize(model);
+      if (error) goto QUIT;
+
+      /* Write model to 'mip1.lp' */
+      error = GRBwrite(model, "mip1.lp");
+      if (error) goto QUIT;
+
+      /* Capture solution information */
+      error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
+      if (error) goto QUIT;
+
+      error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);
+      if (error) goto QUIT;
+
+      error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, 3, sol);
+      if (error) goto QUIT;
+
+      printf("\nOptimization complete\n");
+      if (optimstatus == GRB_OPTIMAL) {
+        printf("Optimal objective: %.4e\n", objval);
+
+        printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
+      } else if (optimstatus == GRB_INF_OR_UNBD) {
+        printf("Model is infeasible or unbounded\n");
+      } else {
+        printf("Optimization was stopped early\n");
+      }
+
+    QUIT:
+
+      /* Error reporting */
+      if (error) {
+        printf("ERROR: %s\n", GRBgeterrormsg(env));
+        exit(1);
+      }
+
+      /* Free model */
+      GRBfreemodel(model);
+
+      /* Free environment */
+      GRBfreeenv(env);
+
+      return 0;
+        }
+};
+
+int main(int   argc,
      char *argv[])
 {
-  GRBenv   *env   = NULL;
-  GRBmodel *model = NULL;
-  int       error = 0;
-  double    sol[3];
-  int       ind[3];
-  double    val[3];
-  double    obj[3];
-  char      vtype[3];
-  int       optimstatus;
-  double    objval;
+    C3 Alg;
+    Alg.SolveSingleProjection();
 
-  /* Create environment */
-  error = GRBemptyenv(&env);
-  if (error) goto QUIT;
 
-  error = GRBsetstrparam(env, "LogFile", "mip1.log");
-  if (error) goto QUIT;
-
-  error = GRBstartenv(env);
-  if (error) goto QUIT;
-
-  /* Create an empty model */
-  error = GRBnewmodel(env, &model, "mip1", 0, NULL, NULL, NULL, NULL, NULL);
-  if (error) goto QUIT;
-
-  /* Add variables */
-  obj[0] = 1; obj[1] = 1; obj[2] = 2;
-  vtype[0] = GRB_BINARY; vtype[1] = GRB_BINARY; vtype[2] = GRB_BINARY;
-  error = GRBaddvars(model, 3, 0, NULL, NULL, NULL, obj, NULL, NULL, vtype,
-                     NULL);
-  if (error) goto QUIT;
-
-  /* Change objective sense to maximization */
-  error = GRBsetintattr(model, GRB_INT_ATTR_MODELSENSE, GRB_MAXIMIZE);
-  if (error) goto QUIT;
-
-  /* First constraint: x + 2 y + 3 z <= 4 */
-  ind[0] = 0; ind[1] = 1; ind[2] = 2;
-  val[0] = 1; val[1] = 2; val[2] = 3;
-
-  error = GRBaddconstr(model, 3, ind, val, GRB_LESS_EQUAL, 4.0, "c0");
-  if (error) goto QUIT;
-
-  /* Second constraint: x + y >= 1 */
-  ind[0] = 0; ind[1] = 1;
-  val[0] = 1; val[1] = 1;
-
-  error = GRBaddconstr(model, 2, ind, val, GRB_GREATER_EQUAL, 1.0, "c1");
-  if (error) goto QUIT;
-
-  /* Optimize model */
-  error = GRBoptimize(model);
-  if (error) goto QUIT;
-
-  /* Write model to 'mip1.lp' */
-  error = GRBwrite(model, "mip1.lp");
-  if (error) goto QUIT;
-
-  /* Capture solution information */
-  error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
-  if (error) goto QUIT;
-
-  error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);
-  if (error) goto QUIT;
-
-  error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, 3, sol);
-  if (error) goto QUIT;
-
-  printf("\nOptimization complete\n");
-  if (optimstatus == GRB_OPTIMAL) {
-    printf("Optimal objective: %.4e\n", objval);
-
-    printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
-  } else if (optimstatus == GRB_INF_OR_UNBD) {
-    printf("Model is infeasible or unbounded\n");
-  } else {
-    printf("Optimization was stopped early\n");
-  }
-
-QUIT:
-
-  /* Error reporting */
-  if (error) {
-    printf("ERROR: %s\n", GRBgeterrormsg(env));
-    exit(1);
-  }
-
-  /* Free model */
-  GRBfreemodel(model);
-
-  /* Free environment */
-  GRBfreeenv(env);
-
-  return 0;
 }

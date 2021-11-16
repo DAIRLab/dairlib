9b4d6232be57d03e5acacffb25f12e35e544a0a7
Alp <alpaydinogl@gmail.com>
2021-11-16 12:53:56 -0500

    one_step_projection

--- examples/tmp/tmp.cc
+++ examples/tmp/tmp.cc
@@ -1,127 +1,94 @@
-/* Copyright 2021, Gurobi Optimization, LLC */
-
-/* This example formulates and solves the following simple MIP model:
-
-     maximize    x +   y + 2 z
-     subject to  x + 2 y + 3 z <= 4
-                 x +   y       >= 1
-                 x, y, z binary
-*/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include "gurobi_c.h"
-
-//For using cout
-#include<iostream>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include "gurobi_c.h"
+#include "gurobi_c++.h"
+//#include "cassie_utils.h"
+#include <Eigen/Dense>
+//#include <iostream>
 using namespace std;
+using Eigen::MatrixXd;
 //
 
-class C3 {        // The class
-  public:              // Access specifier
-    int SolveSingleProjection() {  // Method/function defined inside the class
-      GRBenv   *env   = NULL;
-      GRBmodel *model = NULL;
-      int       error = 0;
-      double    sol[3];
-      int       ind[3];
-      double    val[3];
-      double    obj[3];
-      char      vtype[3];
-      int       optimstatus;
-      double    objval;
+class C3 {
+  public:
+    virtual int SolveSingleProjection(const MatrixXd *m) {  // Method/function defined inside the class
 
-      /* Create environment */
-      error = GRBemptyenv(&env);
-      if (error) goto QUIT;
+        std::cout << *m << std::endl;
 
-      error = GRBsetstrparam(env, "LogFile", "mip1.log");
-      if (error) goto QUIT;
+        // Create an environment
+        GRBEnv env = GRBEnv(true);
+        env.set("LogToConsole", "0");
+        env.set("OutputFlag", "0");
+        env.set("Threads", "1");
+        env.start();
 
-      error = GRBstartenv(env);
-      if (error) goto QUIT;
+        // Create an empty model
+        GRBModel model = GRBModel(env);
 
-      /* Create an empty model */
-      error = GRBnewmodel(env, &model, "mip1", 0, NULL, NULL, NULL, NULL, NULL);
-      if (error) goto QUIT;
+        // Create variables
+        const int n_delta_k = 3;
+        const int n_binary = 3;
+        //double minVal[] = {0, 0};
+        //double maxVal[] = {100, 100};
 
-      /* Add variables */
-      obj[0] = 1; obj[1] = 1; obj[2] = 2;
-      vtype[0] = GRB_BINARY; vtype[1] = GRB_BINARY; vtype[2] = GRB_BINARY;
-      error = GRBaddvars(model, 3, 0, NULL, NULL, NULL, obj, NULL, NULL, vtype,
-                         NULL);
-      if (error) goto QUIT;
+        GRBVar* delta_k = 0;
+        GRBVar* binary = 0;
 
-      /* Change objective sense to maximization */
-      error = GRBsetintattr(model, GRB_INT_ATTR_MODELSENSE, GRB_MAXIMIZE);
-      if (error) goto QUIT;
+        delta_k = model.addVars(n_delta_k, GRB_CONTINUOUS);
+        for (int i = 0; i < n_delta_k; i++) {
+        delta_k[i].set(GRB_DoubleAttr_LB, -GRB_INFINITY);
+            }
 
-      /* First constraint: x + 2 y + 3 z <= 4 */
-      ind[0] = 0; ind[1] = 1; ind[2] = 2;
-      val[0] = 1; val[1] = 2; val[2] = 3;
+        binary = model.addVars(n_binary, GRB_BINARY);
 
-      error = GRBaddconstr(model, 3, ind, val, GRB_LESS_EQUAL, 4.0, "c0");
-      if (error) goto QUIT;
 
-      /* Second constraint: x + y >= 1 */
-      ind[0] = 0; ind[1] = 1;
-      val[0] = 1; val[1] = 1;
 
-      error = GRBaddconstr(model, 2, ind, val, GRB_GREATER_EQUAL, 1.0, "c1");
-      if (error) goto QUIT;
+        //GRBVar x = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x");
+        //GRBVar y = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "y");
+        //GRBVar z = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "z");
+        GRBVar x = binary[0];
+        GRBVar y = binary[1];
+        GRBVar z = binary[2];
 
-      /* Optimize model */
-      error = GRBoptimize(model);
-      if (error) goto QUIT;
+        GRBQuadExpr obj = x * x;
 
-      /* Write model to 'mip1.lp' */
-      error = GRBwrite(model, "mip1.lp");
-      if (error) goto QUIT;
+        // Set objective: maximize x + y + 2 z
+        model.setObjective(x * x, GRB_MAXIMIZE);
 
-      /* Capture solution information */
-      error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
-      if (error) goto QUIT;
+        // Add constraint: x + 2 y + 3 z <= 4
+        //model.addConstr(x + 2 * y + 3 * z <= 4, "c0");
 
-      error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);
-      if (error) goto QUIT;
+        // Add constraint: x + y >= 1
+        //model.addConstr(x + y >= 1, "c1");
 
-      error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, 3, sol);
-      if (error) goto QUIT;
+        // Optimize model
+        model.optimize();
 
-      printf("\nOptimization complete\n");
-      if (optimstatus == GRB_OPTIMAL) {
-        printf("Optimal objective: %.4e\n", objval);
+        cout << x.get(GRB_StringAttr_VarName) << " "
+         << x.get(GRB_DoubleAttr_X) << endl;
+        cout << y.get(GRB_StringAttr_VarName) << " "
+         << y.get(GRB_DoubleAttr_X) << endl;
+        cout << z.get(GRB_StringAttr_VarName) << " "
+         << z.get(GRB_DoubleAttr_X) << endl;
 
-        printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
-      } else if (optimstatus == GRB_INF_OR_UNBD) {
-        printf("Model is infeasible or unbounded\n");
-      } else {
-        printf("Optimization was stopped early\n");
+        cout << "New: "<< delta_k[0].get(GRB_DoubleAttr_X) << endl;
+
+        cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+
       }
-
-    QUIT:
-
-      /* Error reporting */
-      if (error) {
-        printf("ERROR: %s\n", GRBgeterrormsg(env));
-        exit(1);
-      }
-
-      /* Free model */
-      GRBfreemodel(model);
-
-      /* Free environment */
-      GRBfreeenv(env);
-
-      return 0;
-        }
 };
 
 int main(int   argc,
      char *argv[])
 {
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+
     C3 Alg;
-    Alg.SolveSingleProjection();
+    Alg.SolveSingleProjection(&m);
 
 
 }

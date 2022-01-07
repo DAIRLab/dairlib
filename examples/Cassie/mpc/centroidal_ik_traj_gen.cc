c5c15d0bfb1cbbd3da378cd126b44a9ad07f9f19
Brian Acosta <bjacosta@seas.upenn.edu>
2022-01-07 16:02:37 -0500

    IK progress

--- examples/Cassie/mpc/centroidal_ik_traj_gen.cc
+++ examples/Cassie/mpc/centroidal_ik_traj_gen.cc
@@ -1,2 +1,162 @@
-
+#include "systems/framework/output_vector.h"
+#include "multibody/multibody_utils.h"
+#include "lcm/lcm_trajectory.h"
 #include "centroidal_ik_traj_gen.h"
+#include "dairlib/lcmt_saved_traj.hpp"
+
+#include "drake/multibody/optimization/centroidal_momentum_constraint.h"
+#include "drake/multibody/inverse_kinematics/unit_quaternion_constraint.h"
+#include "drake/multibody/inverse_kinematics/position_constraint.h"
+#include "drake/multibody/inverse_kinematics/com_position_constraint.h"
+#include "drake/math/saturate.h"
+
+using dairlib::multibody::SingleRigidBodyPlant;
+using dairlib::multibody::SetPositionsAndVelocitiesIfNew;
+
+using Eigen::VectorXd;
+using Eigen::Vector3d;
+using Eigen::Matrix3d;
+
+using drake::AutoDiffXd;
+using drake::math::ExtractValue;
+using drake::multibody::MultibodyPlant;
+using drake::solvers::MathematicalProgram;
+using drake::solvers::VectorXDecisionVariable;
+using drake::multibody::CentroidalMomentumConstraint;
+using drake::multibody::UnitQuaternionConstraint;
+using drake::multibody::PositionConstraint;
+using drake::multibody::ComPositionConstraint;
+using drake::systems::Context;
+using drake::systems::BasicVector;
+using dairlib::systems::OutputVector;
+using drake::TypeSafeIndex;
+
+namespace dairlib::mpc{
+
+CentroidalIKTrajGen::CentroidalIKTrajGen(
+    const MultibodyPlant<AutoDiffXd>& plant_ad,
+    const MultibodyPlant<double>& plant,
+    const SingleRigidBodyPlant& srb_plant,
+    const Matrix3d& I, double dt, double stance_duration) :
+    plant_ad_(plant_ad),
+    plant_(plant),
+    srb_plant_(srb_plant),
+    I_b_(I),
+    dt_(dt),
+    stance_duration_(stance_duration) {
+
+  fsm_port_ = this->DeclareVectorInputPort("fsm", 1).get_index();
+  touchdown_time_port_ = this->DeclareVectorInputPort(
+      "touchdown_time", 1).get_index();
+  state_port_ = this->DeclareVectorInputPort(
+      "x, u, t",
+      OutputVector<double>(plant.num_positions(),
+                                       plant.num_velocities(),
+                                       plant.num_actuators())).get_index();
+  mpc_traj_port_ = this->DeclareAbstractInputPort(
+          "lcmt_saved_trajectory",
+          drake::Value<dairlib::lcmt_saved_traj>{}).get_index();
+
+  BasicVector<double> model_solution_vec(
+      2 * plant.num_positions() + 2* plant.num_velocities());
+  ik_sol_cache_entry_ = &DeclareCacheEntry(
+      "ik_sol", model_solution_vec,
+      &CentroidalIKTrajGen::CalcTraj,
+      {input_port_ticket(TypeSafeIndex(mpc_traj_port_))});
+}
+
+void CentroidalIKTrajGen::CalcTraj(
+    const drake::systems::Context<double> &context,
+    drake::systems::BasicVector<double> *solvec) const {
+
+  /* Read input ports */
+  const drake::AbstractValue* input =
+      this->EvalAbstractInput(context, mpc_traj_port_);
+  DRAKE_ASSERT(input != nullptr);
+  const OutputVector<double>* robot_output =
+      (OutputVector<double>*)this->EvalVectorInput(context, state_port_);
+  double fsm_state =
+      this->EvalVectorInput(context, fsm_port_)->get_value()(0);
+  double prev_event_time =
+      this->EvalVectorInput(context, touchdown_time_port_)->get_value()(0);
+  VectorXd x = robot_output->GetState();
+
+  // Only solve IK again if there is a new MPC solution
+  const auto& input_msg = input->get_value<lcmt_saved_traj>();
+  double mpc_timestamp = input_msg.trajectories.front().time_vec.front();
+  if (mpc_timestamp == mpc_timestamp_) {
+    solvec->get_mutable_value() = prev_ik_sol_;
+    std::cout << "got new mpc message" << std::endl;
+    return;
+  }
+  mpc_timestamp_ = mpc_timestamp;
+  LcmTrajectory mpc_traj(input_msg);
+  Eigen::MatrixXd angular_momentum = I_b_ *
+      mpc_traj.GetTrajectory("orientation").datapoints.bottomRows(3);
+  Eigen::MatrixXd linear_momentum = srb_plant_.mass() *
+      mpc_traj.GetTrajectory("com_traj").datapoints.bottomRows(3);
+
+
+  // Solve the IK problem
+  double timestamp = robot_output->get_timestamp();
+  double start_time = timestamp;
+  double end_time = prev_event_time + stance_duration_;
+  double mode_duration = end_time - start_time;
+
+  start_time = drake::math::saturate(start_time, -1,end_time - 0.001);
+  MathematicalProgram prog;
+  std::vector<VectorXDecisionVariable> qq, vv, hh, rr;
+
+  int N = round(mode_duration / dt_) + 1;
+
+  // Make decision variables
+  for (int i = 0; i < N; i++) {
+    qq.push_back(prog.NewContinuousVariables(
+      plant_.num_positions(), "q" + std::to_string(i)));
+    vv.push_back(prog.NewContinuousVariables(
+        plant_.num_velocities(), "v" + std::to_string(i)));
+    hh.push_back(prog.NewContinuousVariables(6, "h" + std::to_string(i)));
+    rr.push_back(prog.NewContinuousVariables(3));
+  }
+
+  // Initial state constraint
+  SetPositionsAndVelocitiesIfNew<double>(plant_, robot_output->GetState(), context_);
+  auto CoM = plant_.CalcCenterOfMassPositionInWorld(*context_);
+  auto spatial_momentum = plant_.CalcSpatialMomentumInWorldAboutPoint(*context_, CoM);
+  VectorXd h_i = VectorXd::Zero(6);
+  h_i.head(3) = spatial_momentum.rotational();
+  h_i.tail(3) = spatial_momentum.translational();
+  prog.AddLinearEqualityConstraint(qq.front() == robot_output->GetPositions());
+  prog.AddLinearEqualityConstraint(vv.front() == robot_output->GetVelocities());
+  prog.AddLinearEqualityConstraint(hh.front() == h_i);
+
+  // knot point constraints
+  Vector3d foot_pos;
+  plant_.CalcPointsPositions(
+      *context_,
+      plant_.GetBodyByName(toe_frames_.at((int)fsm_state)).body_frame(),
+      toe_mid_, plant_.world_frame(), &foot_pos);
+  auto centroidal_constraint_ptr =
+      std::make_shared<CentroidalMomentumConstraint>(
+          &plant_ad_,std::vector<drake::multibody::ModelInstanceIndex>(),
+          context_ad_, false);
+  auto quat_norm_constraint = std::make_shared<UnitQuaternionConstraint>();
+  auto stance_foot_pos_contraint = std::make_shared<PositionConstraint>(
+      &plant_ad_, plant_ad_.world_frame(),
+      foot_pos, foot_pos,
+      plant_ad_.GetBodyByName(toe_frames_.at((int)fsm_state)).body_frame(),
+      toe_mid_, context_ad_);
+  auto com_pos_constraint = std::make_shared<ComPositionConstraint>(
+      &plant_, std::vector<drake::multibody::ModelInstanceIndex>(),
+          plant_.world_frame(), context_);
+
+  for (int i = 1; i < N; i++) {
+    prog.AddConstraint(centroidal_constraint_ptr, {qq.at(i), vv.at(i), hh.at(i)});
+    prog.AddConstraint(stance_foot_pos_contraint, qq.at(i));
+    prog.AddConstraint(quat_norm_constraint, qq.at(i).head(4));
+    prog.AddConstraint(com_pos_constraint, {qq.at(i), rr.at(i)});
+    prog.AddLinearEqualityConstraint(hh.at(i).head(3) == angular_momentum.col(i));
+    prog.AddLinearEqualityConstraint(hh.at(i).tail(3) == linear_momentum.col(i));
+  }
+}
+}
--- examples/Cassie/mpc/centroidal_ik_traj_gen.h
+++ examples/Cassie/mpc/centroidal_ik_traj_gen.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "multibody/single_rigid_body_plant.h"
 #include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/framework/basic_vector.h"
 #include "drake/solvers/mathematical_program.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/common/trajectories/trajectory.h"
@@ -10,7 +12,10 @@ namespace dairlib::mpc {
 class CentroidalIKTrajGen : public drake::systems::LeafSystem<double> {
  public:
   CentroidalIKTrajGen(
-      const drake::multibody::MultibodyPlant<drake::AutoDiffXd> plant);
+      const drake::multibody::MultibodyPlant<drake::AutoDiffXd>& plant_ad,
+      const drake::multibody::MultibodyPlant<double>& plant,
+      const multibody::SingleRigidBodyPlant& srb_plant,
+      const Eigen::Matrix3d& I, double dt, double stance_duration);
 
   // Input port getters
   const drake::systems::InputPort<double> &get_input_port_fsm() const {
@@ -22,26 +27,51 @@ class CentroidalIKTrajGen : public drake::systems::LeafSystem<double> {
   const drake::systems::InputPort<double> &get_input_port_mpc_traj() const {
     return this->get_input_port(mpc_traj_port_);
   }
-
-
+  const drake::systems::InputPort<double>& get_input_port_state() const {
+    return this->get_input_port(state_port_);
+  }
+  const drake::systems::OutputPort<double>& get_output_port_pelvis_orientation_traj() const {
+    return this->get_output_port(pelvis_traj_port_);
+  }
+  const drake::systems::OutputPort<double>& get_output_port_swing_foot_traj() const {
+    return this->get_output_port(swing_foot_traj_port_);
+  }
  private:
 
   void CalcTraj(const drake::systems::Context<double> &context,
-                drake::trajectories::Trajectory<double> *traj) const;
+                drake::systems::BasicVector<double> *solvec) const;
+
+  void AssignPelvisTraj(const drake::systems::Context<double> & context,
+                        drake::trajectories::Trajectory<double> *output_traj);
+
+  void AssignSwingFootTraj(const drake::systems::Context<double> & context,
+                        drake::trajectories::Trajectory<double> *output_traj);
 
   const Eigen::Matrix3d& I_b_;
+  const std::vector<std::string> toe_frames_ = {"toe_left", "toe_right"};
+  const Eigen::Vector3d toe_mid_ = Eigen::Vector3d(0.023715, 0.056, 0);
+  const drake::Vector3<drake::AutoDiffXd> toe_mid_ad_ =
+      drake::math::InitializeAutoDiff(toe_mid_);
 
+  const drake::systems::CacheEntry* ik_sol_cache_entry_;
 
   int state_port_;
-  int mpc_traj_port_;
+  drake::TypeSafeIndex<drake::systems::InputPortTag> mpc_traj_port_;
   int fsm_port_;
   int touchdown_time_port_;
-  double swing_foot_traj_port_;
-  double pelvis_traj_port_;
-  double dt_;
+  int swing_foot_traj_port_;
+  int pelvis_traj_port_;
 
-  const drake::multibody::MultibodyPlant<drake::AutoDiffXd>& plant_;
-  const drake::systems::Context<drake::AutoDiffXd>* context_;
+  mutable double mpc_timestamp_ = -1.0;
+  mutable Eigen::VectorXd prev_ik_sol_;
+
+  double dt_;
+  double stance_duration_;
+  const drake::multibody::MultibodyPlant<drake::AutoDiffXd>& plant_ad_;
+  const drake::multibody::MultibodyPlant<double>& plant_;
+  const multibody::SingleRigidBodyPlant& srb_plant_;
+  drake::systems::Context<drake::AutoDiffXd>* context_ad_;
+  drake::systems::Context<double>* context_;
 
 };
 }

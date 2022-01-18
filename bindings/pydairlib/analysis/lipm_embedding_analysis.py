93e333791439b2880e738b4c0c7ebf74b9013c33
Brian Acosta <bjacosta@seas.upenn.edu>
2022-01-18 17:09:11 -0500

    plotting changes

--- bindings/pydairlib/analysis/lipm_embedding_analysis.py
+++ bindings/pydairlib/analysis/lipm_embedding_analysis.py
@@ -4,6 +4,7 @@ import os
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from pydairlib.lcm import lcm_trajectory
 from pydairlib.common import FindResourceOrThrow, plot_styler
@@ -35,7 +36,7 @@ def main():
         floating_base=True, springs=False, loop_closure=False)
 
     frames, pts = cassie_plots.get_toe_frames_and_points(plant)
-    swing_foot = {'frame': frames['left'], 'pt': pts['mid']}
+    swing_foot = {'frame': frames['right'], 'pt': pts['mid']}
 
     n_knot = 100
 
@@ -54,11 +55,22 @@ def main():
     swing_y = np.zeros((n_knot, n_tasks))
     swing_z = np.zeros((n_knot, n_tasks))
 
+    posmap, velmap, _ = mbp_plots.make_name_to_mbp_maps(plant)
+
     t = []
     for i, trajopt_file in enumerate(lip_trajopts):
         dircon_traj = lcm_trajectory.DirconTrajectory(trajopt_file)
         cubic_spline = dircon_traj.ReconstructStateTrajectory()
         t, x = sample_traj(cubic_spline, n_knot)
+
+        plant.SetPositionsAndVelocities(context, x[0])
+        com = plant.CalcCenterOfMassPositionInWorld(context)
+        x_com_i = com[0]
+        z_com[i] = com[-1]
+        plant.SetPositionsAndVelocities(context, x[-1])
+        x_com_f = plant.CalcCenterOfMassPositionInWorld(context)[0]
+        vx[i] = (x_com_f - x_com_i) / (t[-1] - t[0])
+
         rpy = get_rpy_traj(t, x, plant, context)
         swing_pos = mbp_plots.make_point_positions_from_q(
                 x[:, :plant.num_positions()], plant, context,
@@ -70,6 +82,15 @@ def main():
         swing_x[:, i] = swing_pos[:, 0]
         swing_y[:, i] = swing_pos[:, 1]
         swing_z[:, i] = swing_pos[:, 2]
+
+    plt.scatter(z_com, np.max(pitch, axis=0))
+    plt.figure()
+    plt.plot(t, pitch)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(z_com, vx, np.max(pitch, axis=0))
+    plt.show()
 
 
 if __name__ == '__main__':

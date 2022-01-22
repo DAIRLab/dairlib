c47f0216614ad8c700665862353bb76d4949b9e6
Yu-Ming Chen <yminchen@umich.edu>
2022-01-22 18:50:08 -0500

    Add a test for MBP joint order

---
+++ examples/goldilocks_models/test/some_test.cc
@@ -0,0 +1,93 @@
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "examples/Cassie/cassie_utils.h"
+#include "examples/goldilocks_models/reduced_order_models.h"
+#include "multibody/multibody_utils.h"
+
+#include "drake/multibody/plant/multibody_plant.h"
+
+using drake::MatrixX;
+using drake::VectorX;
+using drake::systems::BasicVector;
+using drake::systems::Context;
+using drake::systems::System;
+using drake::systems::SystemOutput;
+using std::cout;
+using std::endl;
+using std::make_unique;
+using std::string;
+using std::unique_ptr;
+
+namespace dairlib {
+namespace goldilocks_models {
+namespace {
+
+class MultibodyPlantJointOrderTest : public ::testing::Test {
+ protected:
+  MultibodyPlantJointOrderTest(){};
+};
+
+TEST_F(MultibodyPlantJointOrderTest, FixedSpringCassie) {
+  // Because in some of my code I hard-coded the joint index.
+  // Putting a test here to give myself a headsup.
+
+  drake::multibody::MultibodyPlant<double> plant(1e-3);
+  addCassieMultibody(&plant, nullptr, true /*floating base*/,
+                     "examples/Cassie/urdf/cassie_fixed_springs.urdf",
+                     false /*spring model*/, false /*loop closure*/);
+  plant.Finalize();
+
+  std::map<string, int> pos_map = multibody::makeNameToPositionsMap(plant);
+
+  EXPECT_TRUE(pos_map.at("base_qw") == 0);
+  EXPECT_TRUE(pos_map.at("base_qx") == 1);
+  EXPECT_TRUE(pos_map.at("base_qy") == 2);
+  EXPECT_TRUE(pos_map.at("base_qz") == 3);
+  EXPECT_TRUE(pos_map.at("base_x") == 4);
+  EXPECT_TRUE(pos_map.at("base_y") == 5);
+  EXPECT_TRUE(pos_map.at("base_z") == 6);
+  EXPECT_TRUE(pos_map.at("hip_roll_left") == 7);
+  EXPECT_TRUE(pos_map.at("hip_roll_right") == 8);
+  EXPECT_TRUE(pos_map.at("hip_yaw_left") == 9);
+  EXPECT_TRUE(pos_map.at("hip_yaw_right") == 10);
+  EXPECT_TRUE(pos_map.at("hip_pitch_left") == 11);
+  EXPECT_TRUE(pos_map.at("hip_pitch_right") == 12);
+  EXPECT_TRUE(pos_map.at("knee_left") == 13);
+  EXPECT_TRUE(pos_map.at("knee_right") == 14);
+  EXPECT_TRUE(pos_map.at("ankle_joint_left") == 15);
+  EXPECT_TRUE(pos_map.at("ankle_joint_right") == 16);
+  EXPECT_TRUE(pos_map.at("toe_left") == 17);
+  EXPECT_TRUE(pos_map.at("toe_right") == 18);
+
+  std::map<string, int> vel_map = multibody::makeNameToVelocitiesMap(plant);
+
+  EXPECT_TRUE(vel_map.at("base_wx") == 0);
+  EXPECT_TRUE(vel_map.at("base_wy") == 1);
+  EXPECT_TRUE(vel_map.at("base_wz") == 2);
+  EXPECT_TRUE(vel_map.at("base_vx") == 3);
+  EXPECT_TRUE(vel_map.at("base_vy") == 4);
+  EXPECT_TRUE(vel_map.at("base_vz") == 5);
+  EXPECT_TRUE(vel_map.at("hip_roll_leftdot") == 6);
+  EXPECT_TRUE(vel_map.at("hip_roll_rightdot") == 7);
+  EXPECT_TRUE(vel_map.at("hip_yaw_leftdot") == 8);
+  EXPECT_TRUE(vel_map.at("hip_yaw_rightdot") == 9);
+  EXPECT_TRUE(vel_map.at("hip_pitch_leftdot") == 10);
+  EXPECT_TRUE(vel_map.at("hip_pitch_rightdot") == 11);
+  EXPECT_TRUE(vel_map.at("knee_leftdot") == 12);
+  EXPECT_TRUE(vel_map.at("knee_rightdot") == 13);
+  EXPECT_TRUE(vel_map.at("ankle_joint_leftdot") == 14);
+  EXPECT_TRUE(vel_map.at("ankle_joint_rightdot") == 15);
+  EXPECT_TRUE(vel_map.at("toe_leftdot") == 16);
+  EXPECT_TRUE(vel_map.at("toe_rightdot") == 17);
+}
+
+}  // namespace
+}  // namespace goldilocks_models
+}  // namespace dairlib
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

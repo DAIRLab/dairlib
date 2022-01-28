d8ce02e9456c346110b81cf93ffca51861fafad3
Brian Acosta <bjacosta@seas.upenn.edu>
2022-01-28 14:53:43 -0500

    terrain generation

---
+++ multibody/terrain_generator.cc
@@ -0,0 +1,10 @@
+#include "multibody/terrain_generator.h"
+
+using drake::VectorX;
+using drake::geometry::HalfSpace;
+using drake::geometry::SceneGraph;
+using drake::multibody::MultibodyPlant;
+using Eigen::VectorXd;
+using Eigen::Vector3d;
+using Eigen::Vector2d;
+using std::vector;
---
+++ multibody/terrain_generator.h
@@ -0,0 +1,6 @@
+/// This file contains utilities to procedurally generate terrains for
+/// simulating difficult environments such as hiking and trail running
+
+#pragma once
+#include "drake/multibody/plant/multibody_plant.h"
+

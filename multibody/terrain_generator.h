c7da9f8ade9903cf6e7b36325375a40803029721
Brian Acosta <bjacosta@seas.upenn.edu>
2022-01-28 14:56:33 -0500

    Revert "terrain generation"
    This reverts commit d8ce02e9456c346110b81cf93ffca51861fafad3.

--- multibody/terrain_generator.h
+++
@@ -1,6 +0,0 @@
-/// This file contains utilities to procedurally generate terrains for
-/// simulating difficult environments such as hiking and trail running
-
-#pragma once
-#include "drake/multibody/plant/multibody_plant.h"
-

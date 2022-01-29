ce3c399b4e37c2638ba9e86b98a74a20fd60e589
Michael Posa <posa@seas.upenn.edu>
2022-01-28 21:59:05 -0500

    -Add logging to example -Add minimal collision URDF

--- examples/trifinger/simulate_trifinger.py
+++ examples/trifinger/simulate_trifinger.py
@@ -2,7 +2,7 @@ from pydairlib.multibody import (addFlatTerrain, makeNameToPositionsMap)
 from pydrake.all import *
 from pydairlib.common import FindResourceOrThrow
 
-# Define the system.
+# A demo controller system
 class TrifingerDemoController(LeafSystem):
   def __init__(self, plant):
     LeafSystem.__init__(self)
@@ -21,26 +21,26 @@ class TrifingerDemoController(LeafSystem):
     # cube position is [quat; xyz] and velocity [ang_vel; xyz]
     q = x[0:self.plant.num_positions()]
     v = x[self.plant.num_positions():]
-    u = -.03*np.ones(self.plant.num_actuators())
+    # u = -.03*np.ones(self.plant.num_actuators())
 
     # use a simple PD controller with constant setpoint
     kp = 10
     kd = 2
     q_des = np.array([.1, 0, -1, .1, -.5, -1, .1, -.5, -1])
     u = kp*(q_des - q[0:9]) - kd * v[0:9]
-    # import pdb; pdb.set_trace()
     output.SetFromVector(u)
 
 # Load the URDF and the cube
 builder = DiagramBuilder()
-plant, scene_graph = AddMultibodyPlantSceneGraph(builder, 1e-4)
+sim_dt = 2e-4
+plant, scene_graph = AddMultibodyPlantSceneGraph(builder, sim_dt)
 addFlatTerrain(plant=plant, scene_graph=scene_graph, mu_static=1.0, mu_kinetic=1.0)
 
 # The package addition here seems necessary due to how the URDF is defined
 parser = Parser(plant)
 parser.package_map().Add("robot_properties_fingers", "examples/trifinger/robot_properties_fingers")
 parser.AddModelFromFile(FindResourceOrThrow(
-    "examples/trifinger/robot_properties_fingers/urdf/trifinger.urdf"))
+    "examples/trifinger/robot_properties_fingers/urdf/trifinger_minimal_collision.urdf"))
 parser.AddModelFromFile(FindResourceOrThrow(
     "examples/trifinger/robot_properties_fingers/cube/cube_v2.urdf"))
 
@@ -56,17 +56,35 @@ builder.Connect(controller.get_output_port(0), plant.get_actuation_input_port())
 
 # Constuct the simulator and visualizer
 DrakeVisualizer.AddToBuilder(builder=builder, scene_graph=scene_graph)
+
+# Data logging [x;u]
+output_dt = 1e-3
+nq = plant.num_positions()
+nv = plant.num_velocities()
+nu = plant.num_actuators()
+logger = builder.AddSystem(VectorLogSink(nq + nv + nu, output_dt))
+
+# Multiplex state and input for logger
+mux = builder.AddSystem(Multiplexer([nq + nv, nu]))
+
+builder.Connect(plant.get_state_output_port(), mux.get_input_port(0))
+builder.Connect(controller.get_output_port(0), mux.get_input_port(1))
+builder.Connect(mux.get_output_port(0), logger.get_input_port(0))
+
+
 diagram = builder.Build()
 
 simulator = Simulator(diagram)
 simulator.Initialize()
+
+# Change the real-time rate to above 1 to simulate faster
 simulator.set_target_realtime_rate(1)
 
 plant_context = diagram.GetMutableSubsystemContext(
     plant, simulator.get_mutable_context())
 
 # Set the initial state
-q = np.zeros(plant.num_positions())
+q = np.zeros(nq)
 q_map = makeNameToPositionsMap(plant)
 q[q_map['finger_base_to_upper_joint_0']] = 0
 q[q_map['finger_upper_to_middle_joint_0']] = -1
@@ -85,5 +103,8 @@ q[q_map['base_y']] = 0
 q[q_map['base_z']] = .05
 plant.SetPositions(plant_context, q)
 
-# Simulate for 1 second
-simulator.AdvanceTo(1)
+# Simulate for 3 seconds
+simulator.AdvanceTo(3)
+
+# numpy array of data (nq+nv+nu) x n_time
+data = logger.FindLog(simulator.get_context()).data()

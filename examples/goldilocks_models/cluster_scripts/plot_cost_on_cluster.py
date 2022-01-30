3dc74f51dc4704c9040f0266aca8f1b1d047e7ab
Yu-Ming Chen <yminchen@umich.edu>
2022-01-29 22:20:56 -0500

    Add a script to call cost plotting script on cluster

---
+++ examples/goldilocks_models/cluster_scripts/plot_cost_on_cluster.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+
+# cmd should be a list if shell=False. Otherwise, a string.
+def RunCommand(cmd, use_shell=False):
+  process = subprocess.Popen(cmd, shell=use_shell)
+  while process.poll() is None:  # while subprocess is alive
+    time.sleep(0.1)
+
+while True:
+  time.sleep(600)
+  # Call plotting script with srun
+  RunCommand("srun --qos=low --nodes=1 --ntasks=1 --cpus-per-task=1 --mem-per-cpu=2G --pty python3 examples/goldilocks_models/find_models/plot_cost.py", True)
+
+

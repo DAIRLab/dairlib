b3dd8da11b18f2880ec88ba384f8aa5664296b3c
William Yang <yangwill@seas.upenn.edu>
2022-03-09 14:57:36 -0500

    small gain tuning

--- examples/Cassie/run_osc_running_controller.cc
+++ examples/Cassie/run_osc_running_controller.cc
@@ -192,7 +192,7 @@ int DoMain(int argc, char* argv[]) {
   auto controller_failure_pub = builder.AddSystem(
       LcmPublisherSystem::Make<dairlib::lcmt_controller_failure>(
           "CONTROLLER_ERROR", &lcm, TriggerTypeSet({TriggerType::kForced})));
-  std::vector<double> tau = {.001, .03, .001};
+  std::vector<double> tau = {.001, .01, .001};
   auto ekf_filter =
       builder.AddSystem<systems::FloatingBaseVelocityFilter>(plant, tau);
 

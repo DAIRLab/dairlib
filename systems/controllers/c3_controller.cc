98097e548010a7c06d6f899cf6bf61c6b02117e5
alp <alpaydinogl@gmail.com>
2022-03-17 14:48:05 -0400

    adding new files

---
+++ systems/controllers/c3_controller.cc
@@ -0,0 +1,116 @@
+#include "c3_controller.h"
+#include "solvers/c3.h"
+#include "solvers/lcs_factory.h"
+#include "external/drake/tools/install/libdrake/_virtual_includes/drake_shared_library/drake/multibody/plant/multibody_plant.h"
+#include "external/drake/tools/install/libdrake/_virtual_includes/drake_shared_library/drake/common/sorted_pair.h"
+#include "solvers/c3_miqp.h"
+//#include "external/drake/common/_virtual_includes/autodiff/drake/common/eigen_autodiff_types.h"
+
+using std::vector;
+
+using drake::AutoDiffVecXd;
+using drake::AutoDiffXd;
+using drake::MatrixX;
+using drake::SortedPair;
+using drake::geometry::GeometryId;
+using drake::math::ExtractGradient;
+using drake::math::ExtractValue;
+using drake::multibody::MultibodyPlant;
+using drake::systems::Context;
+using Eigen::MatrixXd;
+using Eigen::RowVectorXd;
+using Eigen::VectorXd;
+using std::vector;
+
+namespace dairlib {
+namespace systems {
+
+C3Controller::C3Controller(const drake::multibody::MultibodyPlant<double> &plant,
+                           const drake::systems::Context<double> &context,
+                           const drake::multibody::MultibodyPlant<drake::AutoDiffXd> &plant_ad,
+                           const drake::systems::Context<drake::AutoDiffXd> &context_ad,
+                           const std::vector<drake::SortedPair<drake::geometry::GeometryId>>& contact_geoms,
+                           int num_friction_directions,
+                           double mu, const vector<MatrixXd>& Q, const vector<MatrixXd>& R,
+                           const vector<MatrixXd>& G, const vector<MatrixXd>& U,
+                           const vector<VectorXd>& xdesired)
+                           : plant_(plant),
+                           context_(context),
+                           plant_ad_(plant_ad),
+                           context_ad_(context_ad),
+                           contact_geoms_(contact_geoms),
+                           num_friction_directions_(num_friction_directions),
+                           mu_(mu),
+                           Q_(Q),
+                           R_(R),
+                           G_(G),
+                           U_(U),
+                           xdesired_(xdesired){
+
+  int num_positions = plant_.num_positions();
+  int num_velocities = plant_.num_velocities();
+  int num_inputs = plant_.num_actuators();
+
+
+  state_input_port_ = this->DeclareVectorInputPort("x, u, t", OutputVector<double>(num_positions, num_velocities, num_inputs)).get_index();
+
+  control_output_port_ = this->DeclareVectorOutputPort("u, t", TimestampedVector<double>(num_inputs),
+                                                       &C3Controller::CalcControl).get_index();
+
+
+}
+
+void C3Controller::CalcControl(const Context<double> &context,
+                               TimestampedVector<double> *control) const {
+
+  const OutputVector<double> *x =
+      (OutputVector<double> *) this->EvalVectorInput(context, state_input_port_);
+
+
+  solvers::LCS system_ = solvers::LCSFactory::LinearizePlantToLCS(plant_,
+                                                                                 context_,
+                                                                                 plant_ad_,
+                                                                                 context_ad_,
+                                                                                 contact_geoms_,
+                                                                                 num_friction_directions_,
+                                                                                 mu_);
+
+
+  C3Options options;
+  int N = (system_.A_).size();
+  int n = ((system_.A_)[0].cols());
+  int m = ((system_.D_)[0].cols());
+  int k = ((system_.B_)[0].cols());
+
+  /// initialize ADMM variables (delta, w)
+  std::vector<VectorXd> delta(N, VectorXd::Zero(n + m + k));
+  std::vector<VectorXd> w(N, VectorXd::Zero(n + m + k));
+
+  /// initialize ADMM reset variables (delta, w are reseted to these values)
+  std::vector<VectorXd> delta_reset(N, VectorXd::Zero(n + m + k));
+  std::vector<VectorXd> w_reset(N, VectorXd::Zero(n + m + k));
+
+  double total_time = 0;
+  if (options.delta_option == 1) {
+    /// reset delta and w (option 1)
+    delta = delta_reset;
+    w = w_reset;
+    for (int j = 0; j < N; j++) {
+      delta[j].head(n) = x;
+    }
+  } else {
+    /// reset delta and w (default option)
+    delta = delta_reset;
+    w = w_reset;
+  }
+
+  solvers::C3MIQP;
+
+  ///opt(system_, Q_, R_, G_, U_, xdesired_, options);
+
+  /// calculate the input given x[i]
+  VectorXd input = opt.Solve(x, delta, w);
+  control->SetDataVector(input);
+}
+}  // namespace systems
+}  // namespace dairlib
---
+++ systems/controllers/c3_controller.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/Core>
+
+
+#include "drake/systems/framework/leaf_system.h"
+#include "systems/framework/output_vector.h"
+//#include "external/drake/common/_virtual_includes/autodiff/drake/common/eigen_autodiff_types.h"
+//#include "external/drake/tools/install/libdrake/_virtual_includes/drake_shared_library/drake/systems/framework/context.h"
+#include "drake/multibody/plant/multibody_plant.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/geometry/scene_graph.h"
+#include "drake/systems/framework/context.h"
+#include "drake/common/autodiff.h"
+#include "drake/multibody/optimization/manipulator_equation_constraint.h"
+#include <gflags/gflags.h>
+#include <drake/multibody/parsing/parser.h>
+#include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/lcm/lcm_subscriber_system.h"
+#include "common/find_resource.h"
+#include "solvers/lcs_factory.h"
+#include "multibody/geom_geom_collider.h"
+#include "multibody/kinematic/kinematic_evaluator_set.h"
+#include "drake/math/autodiff_gradient.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using drake::systems::LeafSystem;
+using drake::systems::Context;
+using drake::multibody::MultibodyPlant;
+
+namespace dairlib {
+namespace systems {
+
+class C3Controller : public LeafSystem<double> {
+ public:
+  C3Controller(const drake::multibody::MultibodyPlant<double> &plant,
+               const drake::systems::Context<double> &context,
+               const drake::multibody::MultibodyPlant<drake::AutoDiffXd> &plant_ad,
+               const drake::systems::Context<drake::AutoDiffXd> &context_ad,
+               const std::vector<drake::SortedPair<drake::geometry::GeometryId>>& contact_geoms,
+               int num_friction_directions,
+               double mu, const std::vector<Eigen::MatrixXd>& Q,
+               const std::vector<Eigen::MatrixXd>& R,
+               const std::vector<Eigen::MatrixXd>& G,
+               const std::vector<Eigen::MatrixXd>& U,
+               const std::vector<Eigen::VectorXd>& xdesired);
+
+  const drake::systems::InputPort<double> &get_input_port_config()
+  const {
+    return this->get_input_port(state_input_port_);
+  }
+
+  const drake::systems::InputPort<double> &get_input_port_output()
+  const {
+    return this->get_input_port(control_output_port_);
+  }
+
+  //void AddConstraint() const;
+
+ private:
+  void CalcControl(const drake::systems::Context<double> &context,
+                   TimestampedVector<double> *output) const;
+
+  int state_input_port_;
+  int control_output_port_;
+  const MultibodyPlant<double>& plant_;
+  const MultibodyPlant<drake::AutoDiffXd>& plant_ad_;
+  const drake::systems::Context<double>& context_;
+  const drake::systems::Context<drake::AutoDiffXd>& context_ad_;
+  const std::vector<drake::SortedPair<drake::geometry::GeometryId>>& contact_geoms_;
+  int num_friction_directions_;
+  double mu_;
+  const std::vector<Eigen::MatrixXd> Q_;
+  const std::vector<Eigen::MatrixXd> R_;
+  const std::vector<Eigen::MatrixXd> U_;
+  const std::vector<Eigen::MatrixXd> G_;
+  const std::vector<Eigen::VectorXd> xdesired_;
+};
+
+}
+}

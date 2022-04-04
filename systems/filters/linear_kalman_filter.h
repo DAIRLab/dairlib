ec71c1de1333f66517ab5bed32c79effed030937
Brian Acosta <bjacosta@seas.upenn.edu>
2022-04-04 17:14:20 -0400

    kalman progress

--- systems/filters/linear_kalman_filter.h
+++ systems/filters/linear_kalman_filter.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <Eigen/Dense>
 
 /// Implements a Kalman Filter for a Linear Time Invariant (LTI) system with
 /// a variable rate controller by re-discretizing the continuous dynamics at
@@ -7,7 +8,15 @@
 namespace dairlib::systems {
 
 class LinearKalmanFilter {
+ public:
+  LinearKalmanFilter(Eigen::MatrixXd A, Eigen::MatrixXd B, Eigen::MatrixXd C,
+                     Eigen::MatrixXd Q, Eigen::MatrixXd R);
+  Eigen::VectorXd getState();
 
+ private:
+  double t;
+  Eigen::MatrixXd P;
+  Eigen::VectorXd xhat;
 };
 
 }

384bff41a5662dd932321ee5e6549f4058f42cfd
William Yang <yangwill@seas.upenn.edu>
2022-04-05 16:26:31 -0400

    more changes

--- bindings/pydairlib/cassie/gym_envs/cassie_gym_test.py
+++ bindings/pydairlib/cassie/gym_envs/cassie_gym_test.py
@@ -26,14 +26,14 @@ def main():
         controller_plant = MultibodyPlant(8e-5)
         addCassieMultibody(controller_plant, None, True, urdf, False, False)
         controller_plant.Finalize()
-        controller = OSCRunningControllerFactory(controller_plant, osc_running_gains_filename, osqp_settings)
-        # controller = OSCWalkingControllerFactory(controller_plant, True, osc_walking_gains_filename, osqp_settings)
+        # controller = OSCRunningControllerFactory(controller_plant, osc_running_gains_filename, osqp_settings)
+        controller = OSCWalkingControllerFactory(controller_plant, True, osc_walking_gains_filename, osqp_settings)
 
         # reward_func = RewardOSUDRL(reward_function_weights)
         reward_func = RewardOSUDRL()
 
-        gym_env = DrakeCassieGym(reward_func, visualize=True)
-        # gym_env = MuJoCoCassieGym(reward_func, visualize=True)
+        # gym_env = DrakeCassieGym(reward_func, visualize=True)
+        gym_env = MuJoCoCassieGym(reward_func, visualize=True)
         gym_env.make(controller)
         cumulative_reward = gym_env.advance_to(7.5)
         print(cumulative_reward)

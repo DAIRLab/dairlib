1181d488128ba7483f2842deba0a50e28154e8c6
Hersh Sanghvi <hersh500@gmail.com>
2022-04-12 17:23:51 -0400

    add some comments to python file

---
+++ examples/Cassie/swing_ft_learner.py
@@ -0,0 +1,14 @@
+import lcm
+
+# File has three main components:
+# First, the LCM interface
+# Second, the actual optimizer
+# There is an ambiguity. Should we be learning how to actually optimize?
+# Or just have some fixed optimization routine? where does learning get
+# injected into this procedure?
+# Meta-learning? Learn a swing foot that is good after only 1 gradient step?
+# Learn a value function in simulation, try to apply to real? What would the
+# mismatches look like?
+
+
+# Learn what is unsafe in sim, then learn what is optimal in real?

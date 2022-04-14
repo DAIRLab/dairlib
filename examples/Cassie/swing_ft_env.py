71f1fc7dd9522748fb6f51f1283bbead80886574
Hersh Sanghvi <hersh500@gmail.com>
2022-04-14 18:00:35 -0400

    add swing foot env

---
+++ examples/Cassie/swing_ft_env.py
@@ -0,0 +1,8 @@
+from dairlib import lcmt_swing_foot_spline_params
+
+# Class to interface Sim/Robot w/ swing foot learner
+class DrakeSwingFootEnv:
+    def __init__(self, lcm_address = None):
+        return
+
+    def step(self, action):

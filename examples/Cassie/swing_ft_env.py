436bd25fe6488158c197d57c6208eeeebf8075bb
hersh500 <hersh500@gmail.com>
2022-04-14 20:01:25 -0400

    add initial swing ft environment

--- examples/Cassie/swing_ft_env.py
+++ examples/Cassie/swing_ft_env.py
@@ -1,8 +1,14 @@
 from dairlib import lcmt_swing_foot_spline_params
 
 # Class to interface Sim/Robot w/ swing foot learner
-class DrakeSwingFootEnv:
+class DrakeSwingFootEnv(gym.Env):
     def __init__(self, lcm_address = None):
+        self.   
         return
 
     def step(self, action):
+        return    
+
+    # better way to implement a reset?
+    def reset(self):
+        return state

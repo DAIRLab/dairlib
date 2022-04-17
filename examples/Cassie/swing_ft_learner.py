3c813117ffaeba303b6b7357afb19dd0504c1182
hersh500 <hersh500@gmail.com>
2022-04-16 23:19:44 -0400

    random exploration for data gathering code (untested)

--- examples/Cassie/swing_ft_learner.py
+++ examples/Cassie/swing_ft_learner.py
@@ -1,3 +1,23 @@
+import numpy as np
+import yaml
+from swing_ft_env import CassieSwingFootEnv
+
+def get_default_params(gains_file = "examples/Cassie/osc/osc_walking_gains_alip.yaml"):
+    with open(gains_file, 'r') as f:
+        gains = yaml.safe_load(f)
+    
+    n_knot = 5
+    knots = []
+    for i in range(n_knot):
+        t = i/(n_knot - 1)
+        knots.append([0.5 * (np.sin(np.pi * (t - 0.5)) + 1),
+                      0.5 * (np.sin(np.pi * (t - 0.5)) + 1),
+                      (gains["mid_foot_height"] / 0.58) * np.cos(np.pi * (t - 0.5))*t])
+    vel_initial = [0, 0, 0]
+    vel_final = [0, 0, gains["final_foot_velocity_z"]
+    return [n_knot] + knots + vel_initial + vel_final
+
+
 class RandomExplorer:
     def __init__(self, nominal_swing, sigmas):
         """
@@ -5,18 +25,43 @@ class RandomExplorer:
         param sigmas: 3 * n_knots + 3 + 3 array
         """
         self.nominal_swing = nominal_swing
-        self.sigmas = sigmas
+        if len(sigmas) == 3 * nominal_swing[0] + 6:
+            self.sigmas = [0] + sigmas
+        elif len(sigmas) == 3 * nominal_swing[0] + 7:
+            self.sigmas = sigmas
+        else:
+            print("sigmas must be of the correct length!")
+            self.sigmas = [0] * (3 * nominal_swing[0] + 7)
         
     def select_action(self):
         """ Outputs a random deviation from the nominal
         (excluding the n_knots).
         """
-
+        deviation = np.random.normal(np.zeros(1 + 3 * self.nominal_swing[0] + 6), self.sigmas)
+        return self.nominal_swing + deviation
+         
     def collect_data(self, n_steps, cassie_env):
         """ Runs the random explorer for n_steps steps,
         gathering n_steps (s, a, r, s') tuples 
         """
+        data = []
+        done = False
+        s = cassie_env.reset()
+        for _ in range(n_steps):
+            a = self.select_action()
+            s_prime, r, d = cassie_env.step(a)
+            data.append([s, a, r, s_prime])
+            s = s_prime
+            done = d
+        return data
+        
 
+def main():
+    nominal_swing = get_default_params()
+    explorer = RandomExplorer(nominal_swing, np.zeros(len(nominal_swing)))
+    cassie_env = CassieSwingFootEnv()
+    data = explorer.collect_data(10, cassie_env)
+    
 
 if __name__ == "__main__":
     main()

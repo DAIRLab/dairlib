684473da43dbb9bd91101b47cac47be1126ebb66
Yu-Ming Chen <yminchen@umich.edu>
2022-05-07 18:38:00 -0400

    Update hardware cost eval script

--- examples/goldilocks_models/eval_hardware_cost.sh
+++ examples/goldilocks_models/eval_hardware_cost.sh
@@ -1,7 +1,16 @@
-# for i in 00 01 02 03 04 06 07 08 09 10
-for i in 00 01 02 03 04 06 07 08 09 10 16 17 18 19 20 21 22 23
-do
-	echo
-	echo $i
-	bazel-bin/examples/goldilocks_models/eval_single_closedloop_performance /home/yuming/Desktop/temp/20210708_hardware_rom_controller_log/07_08_21/lcmlog-$i ROM_WALKING True
-done
+#for i in 00 01 02 03 04 06 07 08 09 10 16 17 18 19 20 21 22 23
+#do
+#	echo
+#	echo $i
+#	bazel-bin/examples/goldilocks_models/eval_single_closedloop_performance /home/yuming/Desktop/temp/20210708_hardware_rom_controller_log/07_08_21/lcmlog-$i ROM_WALKING True
+#done
+
+################################################################################
+
+eval_dir=../dairlib_data/goldilocks_models/hardware_cost_eval/
+rm -rf eval_dir
+
+bazel-bin/examples/goldilocks_models/eval_single_closedloop_performance /home/yuming/Desktop/temp/0507/lcmlog-02 ROM_WALKING true $eval_dir 1
+bazel-bin/examples/goldilocks_models/eval_single_closedloop_performance /home/yuming/Desktop/temp/0507/lcmlog-03 ROM_WALKING true $eval_dir 20
+bazel-bin/examples/goldilocks_models/eval_single_closedloop_performance /home/yuming/Desktop/temp/0507/lcmlog-04 ROM_WALKING true $eval_dir 40
+bazel-bin/examples/goldilocks_models/eval_single_closedloop_performance /home/yuming/Desktop/temp/0507/lcmlog-05 ROM_WALKING true $eval_dir 60

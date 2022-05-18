a66daf5833bd2c333d3bfc9e3486a19621633412
Brian Acosta <bjacosta@seas.upenn.edu>
2022-05-18 17:03:14 -0400

    tuning adjustments

---
+++ examples/Cassie/osc/hip_yaw_traj_gen.cc
@@ -0,0 +1,5 @@
+//
+// Created by brian on 5/18/22.
+//
+
+#include "hip_yaw_traj_gen.h"
---
+++ examples/Cassie/osc/hip_yaw_traj_gen.h
@@ -0,0 +1,12 @@
+//
+// Created by brian on 5/18/22.
+//
+
+#ifndef DAIRLIB_EXAMPLES_CASSIE_OSC_HIP_YAW_TRAJ_GEN_H_
+#define DAIRLIB_EXAMPLES_CASSIE_OSC_HIP_YAW_TRAJ_GEN_H_
+
+class hip_yaw_traj_gen {
+
+};
+
+#endif //DAIRLIB_EXAMPLES_CASSIE_OSC_HIP_YAW_TRAJ_GEN_H_

c9d6efc931751fbeb18f71c27f8df2078988d930
Brian Acosta <bjacosta@seas.upenn.edu>
2022-05-18 17:08:51 -0400

    finish yaw traj system

--- examples/Cassie/osc/hip_yaw_traj_gen.h
+++ examples/Cassie/osc/hip_yaw_traj_gen.h
@@ -1,12 +1,34 @@
-//
-// Created by brian on 5/18/22.
-//
+#pragma once
 
-#ifndef DAIRLIB_EXAMPLES_CASSIE_OSC_HIP_YAW_TRAJ_GEN_H_
-#define DAIRLIB_EXAMPLES_CASSIE_OSC_HIP_YAW_TRAJ_GEN_H_
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/common/trajectories/piecewise_polynomial.h"
 
-class hip_yaw_traj_gen {
+namespace dairlib::cassie {
 
+class HipYawTrajGen : drake::systems::LeafSystem<double>{
+ public:
+  HipYawTrajGen(int left_stance_state);
+
+  const drake::systems::InputPort<double>& get_fsm_input_port() {
+    return this->get_input_port(fsm_port_);
+  }
+
+  const drake::systems::InputPort<double>& get_radio_input_port() {
+    return this->get_input_port(radio_port_);
+  }
+
+  const drake::systems::OutputPort<double>& get_hip_yaw_output_port() {
+    return this->get_output_port(hip_yaw_traj_port_);
+  }
+
+ private:
+  void CalcYawTraj(const drake::systems::Context<double>& context,
+                       drake::trajectories::Trajectory<double>* traj) const;
+
+  drake::systems::InputPortIndex radio_port_;
+  drake::systems::InputPortIndex fsm_port_;
+  drake::systems::OutputPortIndex hip_yaw_traj_port_;
+  const int left_stance_state_;
 };
+}
 
-#endif //DAIRLIB_EXAMPLES_CASSIE_OSC_HIP_YAW_TRAJ_GEN_H_

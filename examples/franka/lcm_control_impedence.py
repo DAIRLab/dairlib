52af93a703d96dad58a6b44543f2fddfef6887b1
Adam Wei <adam.wei@mail.utoronto.ca>
2022-06-01 10:23:29 -0400

    fixed the actuator port issue

--- examples/franka/lcm_control_impedence.py
+++ examples/franka/lcm_control_impedence.py
@@ -26,19 +26,17 @@ import math
 
 
 lcm = DrakeLcm()
+builder = DiagramBuilder()
 
+# initialize plant for controller
 plant = MultibodyPlant(0.0)
 
-#The package addition here seems necessary due to how the URDF is defined
 parser = Parser(plant)
-
 parser.AddModelFromFile(FindResourceOrThrow(
     "drake/manipulation/models/franka_description/urdf/panda_arm.urdf"))
 parser.AddModelFromFile(pydairlib.common.FindResourceOrThrow(
     "examples/franka/robot_properties_fingers/urdf/sphere.urdf"))
 
-
-# Fix the base of the finger to the world
 X_WI = RigidTransform.Identity()
 plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("panda_link0"), X_WI)
 plant.Finalize()
@@ -46,36 +44,21 @@ plant.Finalize()
 builder = DiagramBuilder()
 state_receiver = builder.AddSystem(RobotOutputReceiver(plant))
 
+# intialize plant for contact_geoms
+unused_builder = DiagramBuilder()
+unused_plant, scene_graph = AddMultibodyPlantSceneGraph(unused_builder, 0.0)
 
+parser = Parser(unused_plant)
+parser.AddModelFromFile(FindResourceOrThrow(
+    "drake/manipulation/models/franka_description/urdf/panda_arm.urdf"))
+parser.AddModelFromFile(pydairlib.common.FindResourceOrThrow(
+    "examples/franka/robot_properties_fingers/urdf/sphere.urdf"))
 
-#############################################################################################
+X_WI = RigidTransform.Identity()
+unused_plant.WeldFrames(unused_plant.world_frame(), unused_plant.GetFrameByName("panda_link0"), X_WI)
+unused_plant.Finalize()
 
-# TODO: figure out why this version doesn't work
-# builder = DiagramBuilder()
-# sim_dt = 2e-4
-
-# plant, scene_graph = AddMultibodyPlantSceneGraph(builder, 0.0)
-# # addFlatTerrain(plant=plant_f, scene_graph=scene_graph, mu_static=1.0,
-# #                mu_kinetic=1.0)
-
-# # The package addition here seems necessary due to how the URDF is defined
-# parser = Parser(plant)
-# parser.package_map().Add("robot_properties_fingers",
-#                          "examples/franka/robot_properties_fingers")                         
-# parser.AddModelFromFile(FindResourceOrThrow(
-#     "drake/manipulation/models/franka_description/urdf/panda_arm.urdf"))
-# parser.AddModelFromFile(pydairlib.common.FindResourceOrThrow(
-#     "examples/franka/robot_properties_fingers/urdf/sphere.urdf"))
-
-
-# # Fix the base of the finger to the world
-# X_WI_f = RigidTransform.Identity()
-# plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("panda_link0"), X_WI_f)
-# plant.Finalize()
-
-# state_receiver = builder.AddSystem(RobotOutputReceiver(plant))
-
-#############################################################################################
+################################################################################################33
 
 nq = plant.num_positions()
 nv = plant.num_velocities()
@@ -99,13 +82,12 @@ B[0:3, 0:3] = coeff * math.sqrt(rotational_stiffness) * np.identity(3)
 B[3:6, 3:6] = coeff * math.sqrt(translational_stiffness) * np.identity(3)
 
 # TODO: confirm that this is right
-# sphere_geoms = plant.GetCollisionGeometriesForBody(plant.GetBodyByName("sphere"))[0]
-# EE_geoms = plant.GetCollisionGeometriesForBody(plant.GetBodyByName("panda_link8"))[0]
 # ground_geoms = plant.GetCollisionGeometriesForBody(plant.GetBodyByName("box"))[0]
+sphere_geoms = unused_plant.GetCollisionGeometriesForBody(plant.GetBodyByName("sphere"))[0]
+EE_geoms = unused_plant.GetCollisionGeometriesForBody(plant.GetBodyByName("panda_link8"))[0]
 
 # contact_geoms = [EE_geoms, sphere_geoms, ground_geoms] #finger_lower_link_120_geoms, finger_lower_link_240_geoms,
-# contact_geoms = [EE_geoms, sphere_geoms]
-contact_geoms = []
+contact_geoms = [EE_geoms, sphere_geoms]
 num_friction_directions = 2
 
 controller = builder.AddSystem(

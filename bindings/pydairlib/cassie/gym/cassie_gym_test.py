61ea128235762c87d415c9bc923b84cdd4f38b4b
Brian Acosta <bjacosta@seas.upenn.edu>
2022-06-07 17:09:05 -0400

    some gym changes

--- bindings/pydairlib/cassie/gym/cassie_gym_test.py
+++ bindings/pydairlib/cassie/gym/cassie_gym_test.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 from drake_cassie_gym import DrakeCassieGym
-from pydairlib.cassie.cassie_utils import *
-
+from pydairlib.cassie.cassie_utils import AddCassieMultibody
 from pydairlib.cassie.controllers import AlipWalkingControllerFactory
 from pydairlib.cassie.simulators import CassieSimDiagram
 from pydrake.common.yaml import yaml_load
 
 from pydrake.multibody.plant import MultibodyPlant
+
 
 def main():
     osc_gains = 'examples/Cassie/osc/osc_walking_gains_alip.yaml'
@@ -17,7 +17,7 @@ def main():
     radio = np.zeros(18)
     while 1:
         controller_plant = MultibodyPlant(8e-5)
-        addCassieMultibody(controller_plant, None, True, urdf, False, False)
+        AddCassieMultibody(controller_plant, None, True, urdf, False, False)
         controller_plant.Finalize()
         controller = AlipWalkingControllerFactory(
             controller_plant, True, osc_gains, osqp_settings)
@@ -26,6 +26,7 @@ def main():
         gym_env.advance_to(0.35)
         gym_env.free_sim()
         gym_env.reset()
+
 
 if __name__ == '__main__':
     main()
--- bindings/pydairlib/cassie/gym/drake_cassie_gym.py
+++ bindings/pydairlib/cassie/gym/drake_cassie_gym.py
@@ -12,68 +12,92 @@ from pydairlib.systems.primitives import *
 from pydairlib.systems.robot_lcm_systems import RobotOutputSender
 from dairlib import lcmt_radio_out
 from pydairlib.cassie.simulators import CassieSimDiagram
-from cassie_env_state import CassieEnvState
+from cassie_env_state import CassieEnvState, CASSIE_NU
 
 
 class DrakeCassieGym():
-    def __init__(self, visualize=False):
-        self.sim_dt = 1e-3
+    def __init__(self, reward_func, visualize=False):
         self.visualize = visualize
+        self.reward_func = reward_func
         self.start_time = 0.00
         self.current_time = 0.00
-        self.end_time = 0.05
-        self.hardware_traj = None
-        self.action_dim = 10
-        self.state_dim = 45
+        self.sim_dt = 1e-2
+        self.plant_dt = 8e-5
+        self.default_action = np.zeros(18)
         self.x_init = np.array(
             [1, 0, 0, 0, 0, 0, 0.85, -0.0358636, 0, 0.67432, -1.588, -0.0458742,
              1.90918, -0.0381073, -1.82312, 0.0358636, 0, 0.67432, -1.588,
              -0.0457885, 1.90919, -0.0382424, -1.82321, 0, 0, 0, 0, 0, 0, 0, 0,
              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        self.prev_cassie_state = None
         self.controller = None
         self.terminated = False
         self.initialized = False
+        self.cassie_state = None
+        self.prev_cassie_state = None
+        self.cumulative_reward = 0
+        self.traj = None
+
+        # Simulation objects
+        self.builder = None
+        self.plant = None
+        self.cassie_sim = None
+        self.sim_plant = None
+        self.diagram = None
+        self.drake_simulator = None
+        self.cassie_sim_context = None
+        self.controller_context = None
+        self.controller_output_port = None
 
     def make(self, controller, urdf='examples/Cassie/urdf/cassie_v2.urdf'):
         self.builder = DiagramBuilder()
-        self.dt = 8e-5
-        self.plant = MultibodyPlant(self.dt)
+        # Add Controller
         self.controller = controller
-        self.simulator = CassieSimDiagram(self.plant, urdf, self.visualize, 0.8)
-        self.sim_plant = self.simulator.get_plant()
         self.builder.AddSystem(self.controller)
-        self.builder.AddSystem(self.simulator)
 
+        # Add Cassie Simulation
+        self.plant = MultibodyPlant(self.plant_dt)
+        self.cassie_sim = CassieSimDiagram(self.plant, urdf, self.visualize, 0.8)
+        self.sim_plant = self.cassie_sim.get_plant()
+        self.builder.AddSystem(self.cassie_sim)
+
+        # Connect ports
         self.builder.Connect(self.controller.get_control_output_port(),
-                             self.simulator.get_actuation_input_port())
-        self.builder.Connect(self.simulator.get_state_output_port(),
+                             self.cassie_sim.get_actuation_input_port())
+        self.builder.Connect(self.cassie_sim.get_state_output_port(),
                              self.controller.get_state_input_port())
 
+        # Build Drake block diagram
         self.diagram = self.builder.Build()
-        self.sim = Simulator(self.diagram)
-        self.simulator_context = self.diagram.GetMutableSubsystemContext(
-            self.simulator, self.sim.get_mutable_context())
+        self.drake_simulator = Simulator(self.diagram)
+
+        # grab relevant contexts
+        self.cassie_sim_context = self.diagram.GetMutableSubsystemContext(
+            self.cassie_sim, self.drake_simulator.get_mutable_context())
         self.controller_context = self.diagram.GetMutableSubsystemContext(
-            self.controller, self.sim.get_mutable_context())
+            self.controller, self.drake_simulator.get_mutable_context())
         self.controller_output_port = self.controller.get_torque_output_port()
-        self.sim.get_mutable_context().SetTime(self.start_time)
+        self.drake_simulator.get_mutable_context().SetTime(self.start_time)
         self.reset()
         self.initialized = True
 
     def reset(self):
+        # Reset plant state
         self.sim_plant.SetPositionsAndVelocities(
             self.sim_plant.GetMyMutableContextFromRoot(
-                self.sim.get_mutable_context()), self.x_init)
-        self.sim.get_mutable_context().SetTime(self.start_time)
+                self.drake_simulator.get_mutable_context()), self.x_init)
+        # Rewind simulator to 0
+        self.drake_simulator.get_mutable_context().SetTime(self.start_time)
         x = self.plant.GetPositionsAndVelocities(
             self.plant.GetMyMutableContextFromRoot(
-                self.sim.get_context()))
-        u = np.zeros(10)
-        self.sim.Initialize()
+                self.drake_simulator.get_context()))
+        u = np.zeros(CASSIE_NU)
+        self.drake_simulator.Initialize()
         self.current_time = self.start_time
-        self.prev_cassie_state = CassieEnvState(self.current_time, x, u)
-        self.cassie_state = CassieEnvState(self.current_time, x, u)
+        self.cassie_state = \
+            CassieEnvState(self.current_time, x, u, self.default_action)
+        self.prev_cassie_state = \
+            CassieEnvState(self.current_time, x, u, self.default_action)
+        self.cumulative_reward = 0
         self.terminated = False
         return
 
@@ -85,23 +109,28 @@ class DrakeCassieGym():
     def check_termination(self):
         return self.cassie_state.get_fb_positions()[2] < 0.4
 
-    def step(self, radio=np.zeros(18)):
+    def step(self, action=None):
         if not self.initialized:
             print("Call make() before calling step() or advance()")
-        next_timestep = self.sim.get_context().get_time() + self.sim_dt
-        self.simulator.get_radio_input_port().FixValue(self.simulator_context, radio)
-        self.controller.get_radio_input_port().FixValue(self.controller_context, radio)
-        self.sim.AdvanceTo(next_timestep)
-        self.current_time = self.sim.get_context().get_time()
+        if action is None:
+            action = self.default_action
+            
+        next_timestep = self.drake_simulator.get_context().get_time() + self.sim_dt
+        self.cassie_sim.get_radio_input_port().FixValue(self.cassie_sim_context, action)
+        self.controller.get_radio_input_port().FixValue(self.controller_context, action)
+        self.drake_simulator.AdvanceTo(next_timestep)
+        self.current_time = self.drake_simulator.get_context().get_time()
 
         x = self.plant.GetPositionsAndVelocities(
             self.plant.GetMyMutableContextFromRoot(
-                self.sim.get_context()))
+                self.drake_simulator.get_context()))
         u = self.controller_output_port.Eval(self.controller_context)[:-1] # remove the timestamp
-        self.cassie_state = CassieEnvState(self.current_time, x, u)
+        self.cassie_state = CassieEnvState(self.current_time, x, u, action)
+        reward = self.reward_func.compute_reward(self.sim_dt, self.cassie_state, self.prev_cassie_state)
         self.terminated = self.check_termination()
         self.prev_cassie_state = self.cassie_state
-        return self.cassie_state
+        self.cumulative_reward += reward
+        return self.cassie_state, reward
 
     def get_traj(self):
         return self.traj

df8c613bdf5dccb861daa676a57a82d697ec489c
dair <dair>
2022-07-15 15:02:29 -0400

    updating gains, mostly period

--- examples/Cassie/osc_run/pelvis_trans_traj_generator.cc
+++ examples/Cassie/osc_run/pelvis_trans_traj_generator.cc
@@ -108,10 +108,10 @@ PiecewisePolynomial<double> PelvisTransTrajGenerator::GenerateSLIPTraj(
   double dt = 1e-3;
   Eigen::Vector3d breaks;
   if(t <= 0.3){
-      breaks << 0, 0.15, 0.3;
+      breaks << 0, 0.3, 0.4;
   }
   else{
-      breaks << 0.3, 0.45, 0.6;
+      breaks << 0.4, 0.7, 0.8;
   }
   MatrixXd samples(3, 3);
 //  MatrixXd samples_dot(3, 2);

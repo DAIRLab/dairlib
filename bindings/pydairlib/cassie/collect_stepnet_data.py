441bac3266166a86710158f8245ee17b2455a00c
Brian Acosta <bjacosta@seas.upenn.edu>
2022-07-27 11:25:01 -0400

    fix data directory

--- bindings/pydairlib/cassie/collect_stepnet_data.py
+++ bindings/pydairlib/cassie/collect_stepnet_data.py
@@ -12,7 +12,8 @@ NMAPS = 20000
 NSTEPS = 10
 NTHREADS = 6
 
-DATASET_DIR = '~/workspace/stepnet_learning_data/dataset/'
+HOME = os.getenv("HOME")
+DATASET_DIR = HOME + '/workspace/stepnet_learning_data/dataset/'
 DEPTH_DIR = DATASET_DIR + 'depth/'
 ROBO_DIR = DATASET_DIR + 'robot/'
 

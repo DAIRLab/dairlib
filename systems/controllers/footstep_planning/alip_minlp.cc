5726f7c1004861d62c3a1c4b54650169445a1831
Brian Acosta <bjacosta@seas.upenn.edu>
2022-08-25 16:45:25 -0400

    most of a C++ minlp implementation

--- systems/controllers/footstep_planning/alip_minlp.cc
+++ systems/controllers/footstep_planning/alip_minlp.cc
@@ -1,5 +1,245 @@
-//
-// Created by brian on 8/24/22.
-//
+#include <algorithm>
 
 #include "alip_minlp.h"
+#include "alip_utils.h"
+
+#include "drake/math/autodiff.h"
+#include "drake/math/autodiff_gradient.h"
+#include "drake/solvers/solve.h"
+
+namespace dairlib::systems::controllers{
+
+using std::to_string;
+using std::vector;
+using std::numeric_limits;
+
+using Eigen::Matrix;
+using Eigen::VectorXd;
+using Eigen::Vector3d;
+using Eigen::Vector4d;
+using Eigen::MatrixXd;
+using Eigen::Matrix4d;
+using Eigen::Matrix3d;
+
+using drake::AutoDiffXd;
+using drake::AutoDiffVecXd;
+using drake::math::InitializeAutoDiff;
+
+using drake::solvers::Solve;
+using drake::solvers::Binding;
+using drake::solvers::QuadraticCost;
+using drake::solvers::DecisionVariable;
+using drake::solvers::LinearConstraint;
+using drake::solvers::MathematicalProgram;
+using drake::solvers::BoundingBoxConstraint;
+using drake::solvers::VectorXDecisionVariable;
+using drake::solvers::MathematicalProgramResult;
+using drake::solvers::LinearEqualityConstraint;
+
+AlipDynamicsConstraint::AlipDynamicsConstraint(double m, double H) :
+    NonlinearConstraint<AutoDiffXd>(
+        4, 10, Vector4d::Zero(), Vector4d::Zero(), "dynamics"), m_(m), H_(H) {
+  A_ = alip_utils::CalcA(H_, m_);
+  A_inv_ = A_.inverse();
+  B_ = Vector4d(0, 0, 0, 1);
+}
+
+void AlipDynamicsConstraint::EvaluateConstraint(
+    const Eigen::Ref<const drake::VectorX<AutoDiffXd>> &x,
+          drake::VectorX<AutoDiffXd>* y) const {
+  VectorXd xd = drake::math::ExtractValue(x);
+  Vector4d x0 = xd.head(4);
+  VectorXd u0 = xd.segment(4, 1);
+  Vector4d x1 = xd.segment(5, 4);
+  double t = xd(xd.size() -1);
+
+  Matrix4d Ad = (t * A_).exp();
+  Vector4d Bd = A_inv_ * (Ad - Matrix4d::Identity()) * B_;
+
+  VectorXd y0 = Ad * x0 + Bd * u0 - x1;
+  MatrixXd dy = MatrixXd::Zero(4, 10);
+  dy.block(0, 0, 4, 4) = Ad;
+  dy.col(4) = Bd;
+  dy.block(0, 5, 4, 4) = -Matrix4d::Identity();
+  dy.rightCols(1) = A_ * (Ad * x0 + A_ * Ad * B_ * u0);
+  *y = InitializeAutoDiff(y0, dy);
+}
+
+void AlipMINLP::AddTrackingCost(vector<vector<Eigen::Vector4d>> xd,
+                                Eigen::Matrix4d Q) {
+  for (int i = 0; i < nmodes_; i++){
+    vector<Binding<QuadraticCost>> QQ;
+    for (int k = 0; k < nknots_.at(i); k++){
+      QQ.push_back(prog_.AddQuadraticErrorCost(Q, xd.at(i).at(k), xx_.at(i).at(k)));
+    }
+    tracking_costs_.push_back(QQ);
+  }
+}
+
+void AlipMINLP::AddInputCost(double R) {
+  for (int i = 0; i < nmodes_; i++){
+    vector<Binding<QuadraticCost>> RR;
+    for (int k = 0; k < nknots_.at(i) -1; k++){
+      RR.push_back(
+          prog_.AddQuadraticCost(
+              R*MatrixXd::Identity(nu_, nu_),
+              VectorXd::Zero(nu_), uu_.at(i).at(k)));
+    }
+    tracking_costs_.push_back(RR);
+  }
+}
+
+void AlipMINLP::Build() {
+  mode_sequnces_ = GetPossibleModeSequences();
+  for (int i = 0; i < nmodes_; i++) {
+    tt_.push_back(prog_.NewContinuousVariables(1, "t"));
+  }
+  MakeDynamicsConstraints();
+  MakeResetConstraints();
+  MakeTimingBoundsConstraint();
+  MakeInitialStateConstraint();
+  MakeInitialFootstepConstraint();
+}
+
+void AlipMINLP::AddMode(int nk) {
+  pp_.push_back(prog_.NewContinuousVariables(np_, "pp_" + to_string(nmodes_)));
+  vector<VectorXDecisionVariable> xknots;
+  vector<VectorXDecisionVariable> uknots;
+  for (int i = 0; i < nk; i++) {
+    xknots.push_back(prog_.NewContinuousVariables(
+        nx_, "xx_" + to_string(nmodes_) + "_" + to_string(i) ));
+  }
+  for (int i = 0; i < (nk -1) ; i++) {
+    uknots.push_back(prog_.NewContinuousVariables(
+        nu_, "uu_" + to_string(nmodes_) + "_" + to_string(i)));
+  }
+  nmodes_ += 1;
+  nknots_.push_back(nk);
+}
+
+void AlipMINLP::MakeIndividualFootholdConstraint(int idx_mode,
+                                                 int idx_foothold) {
+  const auto& [A, b] = footholds_.at(idx_foothold).GetConstraintMatrices();
+  const auto& [A_eq, b_eq] = footholds_.at(idx_foothold).GetEqualityConstraintMatrices();
+  footstep_c_.push_back(
+      {prog_.AddLinearConstraint(
+          A, -numeric_limits<double>::infinity()*Vector3d::Ones(),
+          b, pp_.at(idx_mode)),
+       prog_.AddLinearEqualityConstraint(
+           A_eq, b_eq, pp_.at(idx_mode)
+           )}
+      );
+}
+
+void AlipMINLP::MakeFootstepConstraints(vector<int> foothold_idxs) {
+  DRAKE_ASSERT(foothold_idxs.size() == nmodes_);
+  for (int i = 0; i < foothold_idxs.size(); i++) {
+    MakeIndividualFootholdConstraint(i, foothold_idxs.at(i));
+  }
+}
+
+void AlipMINLP::MakeResetConstraints() {
+  Matrix<double, 4, 12> A_eq;
+  A_eq.topLeftCorner<4, 4>() = Matrix4d::Identity();
+  A_eq.block<4, 4>(0, 4) = -Matrix4d::Identity();
+  A_eq.block<2, 2>(0, 8) = Eigen::Matrix2d::Identity();
+  A_eq.block<2, 2>(0, 10) = Eigen::Matrix2d::Identity();
+  for (int i = 0; i < (nmodes_ - 1); i++) {
+    reset_map_c_.push_back(
+        prog_.AddLinearEqualityConstraint(
+            A_eq, Vector4d::Zero(),
+            {xx_.at(i).back(), xx_.at(i+1).front(), pp_.at(i), pp_.at(i+1)}));
+  }
+}
+
+void AlipMINLP::MakeDynamicsConstraints() {
+  auto dynamics_constraint = std::make_shared<AlipDynamicsConstraint>(m_, H_);
+  for (int i = 0; i < nmodes_; i++) {
+    vector<Binding<drake::solvers::Constraint>> dyn_c_this_mode{};
+    for (int k = 0; k < nknots_.at(i)-1; k++) {
+      dyn_c_this_mode.push_back(
+          prog_.AddConstraint(
+              dynamics_constraint,
+              {xx_.at(i).at(k), uu_.at(i).at(k), xx_.at(i).at(k+1), tt_.at(i)}));
+    }
+    dynamics_c_.push_back(dyn_c_this_mode);
+  }
+}
+
+void AlipMINLP::MakeInitialFootstepConstraint() {
+  initial_foot_c_ = prog_.AddLinearEqualityConstraint(
+      Matrix3d::Identity(), Vector3d::Zero(), pp_.at(0)).evaluator().get();
+}
+
+void AlipMINLP::MakeInitialStateConstraint() {
+  initial_state_c_ = prog_.AddLinearEqualityConstraint(
+      Matrix4d::Identity(), Vector4d::Zero(), xx_.at(0).at(0)).evaluator().get();
+}
+
+void AlipMINLP::MakeTimingBoundsConstraint() {
+  for (int i = 0; i < nmodes_; i++) {
+    ts_bounds_c_.push_back(
+      prog_.AddBoundingBoxConstraint(
+          VectorXd::Zero(1), VectorXd::Ones(1), tt_.at(i)));
+  }
+}
+
+void AlipMINLP::ClearFootholdConstraints() {
+  for (auto & i : footstep_c_) {
+    prog_.RemoveConstraint(i.first);
+    prog_.RemoveConstraint(i.second);
+  }
+  footstep_c_.clear();
+}
+
+void AlipMINLP::SolveOCProblemAsIs() {
+  for (auto& seq: mode_sequnces_) {
+    MakeFootstepConstraints(seq);
+    solutions_.push_back(Solve(prog_));
+    ClearFootholdConstraints();
+  }
+  std::sort(
+      solutions_.begin(), solutions_.end(),
+      [](const MathematicalProgramResult& lhs, const MathematicalProgramResult& rhs){
+    return lhs.get_optimal_cost() < rhs.get_optimal_cost();
+  });
+}
+
+void AlipMINLP::CalcOptimalFoostepPlan(const Eigen::Vector4d &x,
+                                       const Eigen::Vector3d &p) {
+  initial_state_c_->UpdateCoefficients(Matrix4d::Identity(), x);
+  initial_foot_c_->UpdateCoefficients(Matrix3d::Identity(), p);
+  SolveOCProblemAsIs();
+}
+
+vector<vector<Vector4d>> AlipMINLP::MakeXdesTrajForVdes(
+    double vdes, double step_width, double Ts, double nk) const {
+  double omega = sqrt(9.81 / H_);
+  double frac = 1 / (m_ * H_ * omega);
+  double Ly = H_ * m_ * vdes;
+  double Lx = m_ * H_ * omega * step_width * atanh(omega * Ts / 2);
+  Matrix4d Ad = ((Ts / (nk-1)) * alip_utils::CalcA(H_, m_)).exp();
+  vector<vector<Vector4d>> xx;
+
+  Eigen::MatrixPower<Matrix4d> APow(Ad);
+  for (int i = 0; i < nmodes_; i++) {
+    double s = (i % 2 == 0) ? 1.0 : -1.0;
+    Vector4d x0(
+        -frac * tanh(omega * Ts /2) * Ly,
+        s / 2 * step_width,
+        s / 2 * Lx,
+        Ly);
+    vector<Vector4d> x;
+    for (int k = 0; k < nk; k++) {
+      x.push_back(APow(k) * x0);
+    }
+    xx.push_back(x);
+  }
+  return xx;
+}
+
+vector<vector<int>> AlipMINLP::GetPossibleModeSequences() {
+  return cartesian_product(footholds_.size(), nmodes_);
+}
+
+}
--- systems/controllers/footstep_planning/alip_minlp.h
+++ systems/controllers/footstep_planning/alip_minlp.h
@@ -1,24 +1,66 @@
 #pragma once
+#include <array>
 
 #include "geometry/convex_foothold.h"
 #include "solvers/nonlinear_constraint.h"
+#include "unsupported/Eigen/MatrixFunctions"
 
-#include "drake/solvers/mathematical_program.h"
 #include "drake/solvers/decision_variable.h"
+#include "drake/solvers/mathematical_program.h"
+#include "drake/solvers/mathematical_program_result.h"
 
 namespace dairlib::systems::controllers {
 
-class AlipDynamicsConstraint : solvers::NonlinearConstraint<drake::AutoDiffXd> {
+std::vector<std::vector<int>> cartesian_product(unsigned long range, int sets) {
+
+  auto products = std::vector<std::vector<int>>();
+  for (int i = 0; i < pow(range, sets); i++) {
+    products.emplace_back(std::vector<int>(range, 0));
+  }
+  auto counter = std::vector<int>(range, 0); // array of zeroes
+  for (auto &product : products) {
+    product = counter;
+
+    // counter increment and wrapping/carry over
+    counter.back()++;
+    for (size_t i = counter.size()-1; i != 0; i--) {
+      if (counter[i] == range) {
+        counter[i] = 0;
+        counter[i-1]++;
+      }
+      else break;
+    }
+  }
+  return products;
+}
+
+using drake::solvers::Binding;
+using solvers::NonlinearConstraint;
+using drake::solvers::QuadraticCost;
+using drake::solvers::DecisionVariable;
+using drake::solvers::LinearConstraint;
+using drake::solvers::MathematicalProgram;
+using drake::solvers::BoundingBoxConstraint;
+using drake::solvers::VectorXDecisionVariable;
+using drake::solvers::LinearEqualityConstraint;
+
+class AlipDynamicsConstraint : public NonlinearConstraint<drake::AutoDiffXd> {
  public:
-  AlipDynamicsConstraint();
-  void EvaluateConstraint(const Eigen::Ref<const drake::AutoDiffVecXd>& x,
-                          drake::AutoDiffVecXd* y) const;
+  AlipDynamicsConstraint(double m, double H);
+  void EvaluateConstraint(const Eigen::Ref<const drake::VectorX<drake::AutoDiffXd>>& x,
+                          drake::VectorX<drake::AutoDiffXd>* y) const;
+ private:
+  double m_;
+  double H_;
+  Eigen::Matrix4d A_;
+  Eigen::Matrix4d A_inv_;
+  Eigen::MatrixXd B_;
 };
 
 class AlipMINLP {
  public:
 
-  AlipMINLP(double m, double H) : m_(m), H_(H){}
+  AlipMINLP(double m, double H) : m_(m), H_(H) {}
 
   // Problem setup
   void AddMode(int nk);
@@ -26,12 +68,12 @@ class AlipMINLP {
                        Eigen::Matrix4d Q);
   void AddInputCost(double R);
   void Build();
-  std::vector<std::vector<Eigen::Vector4d>> MakeXdesTrajeForVdes(
+  std::vector<std::vector<Eigen::Vector4d>> MakeXdesTrajForVdes(
       double vdes, double step_width, double Ts, double nk) const;
 
   // Solving the problem
   void CalcOptimalFoostepPlan(
-      const Eigen::Vector4d& x, const Eigen::Vector3d& p);
+      const Eigen::Vector4d &x, const Eigen::Vector3d &p);
 
   // Getting the solution
   std::vector<Eigen::Vector3d> GetFootstepSolution() const;
@@ -39,41 +81,49 @@ class AlipMINLP {
   std::vector<std::vector<double>> GetInputSolution() const;
   Eigen::VectorXd GetTimingSolution() const;
 
-
-  void set_m(double m) {m_ = m;}
-  void set_H(double H) {H_ = H;}
+  void set_m(double m) { m_ = m; }
+  void set_H(double H) { H_ = H; }
 
  private:
+
+  int np_ = 3;
+  int nx_ = 4;
+  int nu_ = 1;
+  int nmodes_ = 0;
+  std::vector<int> nknots_{};
   double m_;
   double H_;
+
   void SolveOCProblemAsIs();
   void ClearFootholdConstraints();
   void MakeIndividualFootholdConstraint(int idx_mode, int idx_foothold);
-  void MakeFootstepConstraints(std::vector<geometry::ConvexFoothold> footholds);
+  void MakeFootstepConstraints(std::vector<int> foothold_idxs);
   void MakeResetConstraints();
   void MakeDynamicsConstraints();
   void MakeTimingBoundsConstraint();
   void MakeInitialStateConstraint();
   void MakeInitialFootstepConstraint();
+  std::vector<std::vector<int>> GetPossibleModeSequences();
 
   std::vector<geometry::ConvexFoothold> footholds_;
-  drake::solvers::MathematicalProgram prog_ = drake::solvers::MathematicalProgram{};
-  std::vector<drake::solvers::VectorXDecisionVariable> pp_;
-  std::vector<std::vector<drake::solvers::VectorXDecisionVariable>> xx_;
-  std::vector<std::vector<drake::solvers::VectorXDecisionVariable>> uu_;
-  std::vector<drake::solvers::DecisionVariable> tt_;
+  MathematicalProgram prog_ = MathematicalProgram{};
+  std::vector<VectorXDecisionVariable> pp_;
+  std::vector<std::vector<VectorXDecisionVariable>> xx_;
+  std::vector<std::vector<VectorXDecisionVariable>> uu_;
+  std::vector<VectorXDecisionVariable> tt_;
 
-  std::vector<std::vector<
-  drake::solvers::Binding<drake::solvers::LinearEqualityConstraint>>> dynamics_c_;
+  std::vector<std::vector<Binding<drake::solvers::Constraint>>> dynamics_c_;
+  std::vector<std::pair<Binding<LinearConstraint>, Binding<LinearEqualityConstraint>>> footstep_c_;
+  std::vector<Binding<LinearEqualityConstraint>> reset_map_c_;
+  std::vector<Binding<BoundingBoxConstraint>> ts_bounds_c_;
+  LinearEqualityConstraint *initial_state_c_;
+  LinearEqualityConstraint *initial_foot_c_;
 
-  std::vector<std::pair<
-  drake::solvers::Binding<drake::solvers::LinearConstraint>,
-  drake::solvers::Binding<drake::solvers::LinearEqualityConstraint>>> footstep_c_;
+  std::vector<std::vector<Binding<QuadraticCost>>> tracking_costs_;
+  std::vector<std::vector<Binding<QuadraticCost>>> input_costs_;
 
-  std::vector<drake::solvers::Binding<drake::solvers::LinearEqualityConstraint>> reset_map_c;
-  std::vector<drake::solvers::Binding<drake::solvers::BoundingBoxConstraint>> ts_boudns_c_;
-  drake::solvers::Binding<drake::solvers::LinearEqualityConstraint> initial_state_c_;
-  drake::solvers::Binding<drake::solvers::LinearEqualityConstraint> initial_foot_c_;
+  std::vector<drake::solvers::MathematicalProgramResult> solutions_;
+  std::vector<std::vector<int>> mode_sequnces_{};
+
 };
-
 }

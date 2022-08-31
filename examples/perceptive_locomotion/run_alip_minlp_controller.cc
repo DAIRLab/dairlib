7bbaffe922f555baf2c8436c07d7f39768cada43
Brian Acosta <bjacosta@seas.upenn.edu>
2022-08-31 16:45:31 -0400

    run_controller binary

---
+++ examples/perceptive_locomotion/run_alip_minlp_controller.cc
@@ -0,0 +1,14 @@
+#include "systems/controllers/footstep_planning/alip_minlp_footstep_controller.h"
+
+
+namespace dairlib::examples {
+
+using systems::controllers::AlipMINLPFootstepController;
+using systems::controllers::AlipMINLPGains;
+
+int do_main(int argc, char* argv[]) {
+
+}
+}
+
+int main(int argc, char*argv[]){ return dairlib::examples::do_main(argc, argv);}

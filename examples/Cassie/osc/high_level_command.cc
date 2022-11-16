2da656ad07117307312c9382aad428c4ed631616
Yu-Ming Chen <yminchen@umich.edu>
2022-11-16 17:55:16 -0500

    Offset velocity command to help optimal ROM to get into steady state when starting the MPC

--- examples/Cassie/osc/high_level_command.cc
+++ examples/Cassie/osc/high_level_command.cc
@@ -121,6 +121,7 @@ EventStatus HighLevelCommand::DiscreteVariableUpdate(
     des_vel << vel_scale_rot_ * cassie_out->pelvis.radio.channel[3],
         vel_scale_trans_sagital_ * cassie_out->pelvis.radio.channel[0],
         vel_scale_trans_lateral_ * cassie_out->pelvis.radio.channel[1];
+    //des_vel(1) += 0.1;  //hack: help rom_iter=300 walk in place at start
     discrete_state->get_mutable_vector(des_vel_idx_).set_value(des_vel);
   } else {
     discrete_state->get_mutable_vector(des_vel_idx_)

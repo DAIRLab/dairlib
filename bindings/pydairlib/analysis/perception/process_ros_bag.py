a4bc771859b36c9d12574b470dcf98eae4791203
Brian Acosta <bjacosta@seas.upenn.edu>
2022-12-16 18:35:18 -0500

    rosbag splitter

--- bindings/pydairlib/analysis/perception/process_ros_bag.py
+++ bindings/pydairlib/analysis/perception/process_ros_bag.py
@@ -5,11 +5,30 @@ except ImportError as e:
     print("-----")
     raise e
 
+import os
 import sys
+
+
+def split_log(fname, outfolder):
+    topics = {}
+    for topic, msg, t in rosbag.Bag(fname):
+        if topic not in topics.keys():
+            topic_fname = os.path.join(
+                outfolder,
+                f'{topic.replace("/", "_")[1:]}.bag'
+            )
+            topics[topic] = rosbag.Bag(topic_fname, 'w')
+        topics[topic].write(topic, msg, t)
+
+    for topic in topics.items():
+        topic[1].close()
 
 
 def main():
     fname = sys.argv[1]
+    outfolder = sys.argv[2]
+
+    split_log(fname, outfolder)
 
     topics = {}
     for topic, msg, t in rosbag.Bag(fname):

230a3e69ac3866a296c0b4b67720b44ef6a0708b
Brian Acosta <bjacosta@seas.upenn.edu>
2023-01-10 16:41:32 -0500

    plane receiver

--- geometry/convex_foothold.h
+++ geometry/convex_foothold.h
@@ -2,28 +2,27 @@
 #include "Eigen/Dense"
 
 namespace dairlib::geometry {
-  constexpr int kMaxFootholdFaces = 10;
 
- class ConvexFoothold {
-  public:
-   ConvexFoothold()= default;
-   void SetContactPlane(Eigen::Vector3d normal, Eigen::Vector3d pt);
-   void AddHalfspace(Eigen::Vector3d a, Eigen::VectorXd b);
-   void AddFace(Eigen::Vector3d normal, Eigen::Vector3d pt);
-   std::pair<Eigen::MatrixXd, Eigen::VectorXd>GetConstraintMatrices() const;
-   std::pair<Eigen::MatrixXd, Eigen::VectorXd>GetEqualityConstraintMatrices() const;
-   std::vector<Eigen::Vector3d> GetVertices();
-   void ReExpressInNewFrame(const Eigen::Matrix3d& R_WF);
+constexpr int kMaxFootholdFaces = 10;
 
-  private:
-   Eigen::Vector3d SolveForVertexSharedByFaces(int i, int j);
-   void SortFacesByYawAngle();
+class ConvexFoothold {
+ public:
+  ConvexFoothold()= default;
+  void SetContactPlane(Eigen::Vector3d normal, Eigen::Vector3d pt);
+  void AddHalfspace(Eigen::Vector3d a, Eigen::VectorXd b);
+  void AddFace(const Eigen::Vector3d& normal, const Eigen::Vector3d& pt);
+  void AddVertices(const Eigen::Vector3d& v1, const Eigen::Vector3d& v2);
+  std::pair<Eigen::MatrixXd, Eigen::VectorXd>GetConstraintMatrices() const;
+  std::pair<Eigen::MatrixXd, Eigen::VectorXd>GetEqualityConstraintMatrices() const;
+  std::vector<Eigen::Vector3d> GetVertices();
+  void ReExpressInNewFrame(const Eigen::Matrix3d& R_WF);
 
-   Eigen::RowVector3d A_eq_;
-   Eigen::VectorXd b_eq_;
-   Eigen::MatrixXd A_ = Eigen::MatrixXd::Zero(0,0);
-   Eigen::VectorXd b_ = Eigen::VectorXd::Zero(0);
-
- };
-
+ private:
+  Eigen::Vector3d SolveForVertexSharedByFaces(int i, int j);
+  void SortFacesByYawAngle();
+  Eigen::RowVector3d A_eq_;
+  Eigen::VectorXd b_eq_;
+  Eigen::MatrixXd A_ = Eigen::MatrixXd::Zero(0,0);
+  Eigen::VectorXd b_ = Eigen::VectorXd::Zero(0);
+};
 }
---
+++ geometry/convex_foothold_receiver.cc
@@ -0,0 +1,74 @@
+#include "convex_foothold_receiver.h"
+#include "drake/geometry/optimization/vpolytope.h"
+#include "tf2_eigen/tf2_eigen.h"
+
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::Vector3d;
+using Eigen::RowVector3d;
+using drake::systems::Context;
+
+using convex_plane_decomposition_msgs::PlanarRegion;
+using convex_plane_decomposition_msgs::PlanarTerrain;
+using convex_plane_decomposition_msgs::Point2d;
+
+namespace dairlib::geometry {
+
+namespace {
+// For now, ignore any non-convexity and take the convex hull of each region,
+// ignoring holes
+ConvexFoothold MakeFootholdFromPlanarRegion(
+    const convex_plane_decomposition_msgs::PlanarRegion &foothold) {
+  int n = foothold.boundary.outer_boundary.points.size();
+  MatrixXd verts = MatrixXd::Zero(2, n);
+  for (int i = 0; i < n; i++) {
+    const auto &pt = foothold.boundary.outer_boundary.points.at(i);
+    verts.col(i)(0) = pt.x;
+    verts.col(i)(1) = pt.y;
+  }
+
+  MatrixXd convex_hull = drake::geometry::optimization::VPolytope(verts)
+      .GetMinimalRepresentation().vertices();
+
+  // Append the first vertex to the end of the list to make the H representation
+  // closed
+  convex_hull.conservativeResize(2, convex_hull.cols() + 1);
+  convex_hull.col(convex_hull.cols() - 1) = convex_hull.col(0);
+
+  Eigen::Isometry3d X_WP;
+  tf2::fromMsg(foothold.plane_parameters, X_WP);
+  auto converted_foothold = ConvexFoothold();
+  converted_foothold.SetContactPlane(X_WP.linear().col(2), X_WP.translation());
+
+  for (int i = 0; i < convex_hull.cols() - 1; i++) {
+    Vector3d p1 = X_WP.translation() +
+        X_WP.linear().leftCols(2) * convex_hull.col(i);
+    Vector3d p2 = X_WP.translation() +
+        X_WP.linear().leftCols(2) * convex_hull.col(i + 1);
+    converted_foothold.AddVertices(p1, p2);
+  }
+  return converted_foothold;
+}
+}
+
+ConvexFootholdReceiver::ConvexFootholdReceiver() {
+  PlanarTerrain terrain_msg;
+  DeclareAbstractInputPort("PlanarTerrainRosMsg", drake::Value<PlanarTerrain>(terrain_msg));
+  DeclareAbstractOutputPort("foolholds", &ConvexFootholdReceiver::CopyTerrain);
+}
+
+void ConvexFootholdReceiver::CopyTerrain(
+    const drake::systems::Context<double> &context,
+    std::vector<ConvexFoothold> *footholds) const {
+  footholds->clear();
+
+  const auto& planes =
+      EvalAbstractInput(context, 0)->get_value<PlanarTerrain>();
+
+  for (const auto& planar_region : planes.planarRegions) {
+    footholds->push_back(MakeFootholdFromPlanarRegion(planar_region));
+  }
+}
+
+}

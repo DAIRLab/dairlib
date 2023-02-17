cde69d5d22481c6f36da832edeb2749c839c0e6b
William Yang <yangwill@seas.upenn.edu>
2023-02-17 18:56:45 -0500

    updating parameter study to run multiple experiments per set of parameters

--- bindings/pydairlib/analysis/impact_invariant_control/jumping_parameter_study.py
+++ bindings/pydairlib/analysis/impact_invariant_control/jumping_parameter_study.py
@@ -32,7 +32,7 @@ def main():
   trajectory_name = ''
   results_folder = ''
   delay_time = 2.0
-  traj_time = 5.0
+  traj_time = 4.0
   sim_time = 0
   start_time = 0
   if controller_type == 'jumping':
@@ -42,7 +42,7 @@ def main():
 
   landing_times = np.arange(0.000, 0.055, 0.005)
   # nominal_delay = 0.040 # box
-  nominal_delay = 0.025 # long
+  nominal_delay = 0.040 # long
   # nominal_delay = 0.065 # down
   # nominal_delay = 0.000 # jump
   # nominal_threshold = 0.000 # box
@@ -66,120 +66,121 @@ def main():
   save_gains_cmd = ''
 
   # for i in range(0, disturbances.shape[0]):
-  for traj in trajectory_names:
-    for impact_threshold in impact_thresholds:
-      for landing_time in landing_times:
-        log_suffix = ''
-        if parameter == 'landing_delay':
-          log_suffix = 't_%.3f' % (landing_time) + '_duration_%.3f' % (
-            impact_threshold)
-        log_path = results_folder + traj + '/lcmlog-' + log_suffix
-        print(log_path)
-        gain_filename = ''
-        if traj == 'jump' or traj == 'long_jump':
-          if traj == 'jump':
-            gain_filename = 'osc_jumping_gains.yaml'
-          elif traj == 'long_jump':
-            gain_filename = 'osc_jumping_gains_long.yaml'
-          controller_cmd = [
-            'bazel-bin/examples/Cassie/run_osc_jumping_controller',
-            '--delay_time=%.1f' % delay_time,
-            '--channel_u=OSC_JUMPING',
-            '--gains_filename=examples/Cassie/osc_jump/osc_jumping_gains_param.yaml',
-            '--traj_name=%s' % traj,
-          ]
-          simulator_cmd = ['bazel-bin/examples/Cassie/multibody_sim',
-                           '--init_height=%.1f' % 0.9,
-                           '--toe_spread=0.12',
-                           '--target_realtime_rate=%.2f' % realtime_rate,
-                           '--dt=%.5f' % 1e-3,
-                           '--publish_rate=%d' % publish_rate,
-                           '--end_time=%.3f' % sim_time,
-                           '--channel_u=OSC_JUMPING',
-                           ]
-        elif traj == 'box_jump':
-          controller_cmd = [
-            'bazel-bin/examples/Cassie/run_osc_jumping_controller',
-            '--delay_time=%.1f' % delay_time,
-            '--channel_u=OSC_JUMPING',
-            '--traj_name=%s' % traj,
-            '--gains_filename=examples/Cassie/osc_jump/osc_jumping_gains_param.yaml',
-          ]
-          simulator_cmd = ['bazel-bin/examples/Cassie/multibody_sim_w_platform',
-                           '--init_height=%.1f' % 0.9,
-                           '--target_realtime_rate=%.2f' % realtime_rate,
-                           '--dt=%.5f' % 1e-3,
-                           '--publish_rate=%d' % publish_rate,
-                           '--end_time=%.3f' % sim_time,
-                           '--channel_u=OSC_JUMPING',
-                           '--traj_name=box_jump',
-                           '--platform_height=0.5',
-                           '--platform_x=0.25',
-                           '--visualize=1',
-                           ]
-          gain_filename = 'osc_jumping_gains_box.yaml'
-        elif traj == 'down_jump':
-          controller_cmd = [
-            'bazel-bin/examples/Cassie/run_osc_jumping_controller',
-            '--delay_time=%.1f' % delay_time,
-            '--channel_u=OSC_JUMPING',
-            '--traj_name=%s' % traj,
-            '--gains_filename=examples/Cassie/osc_jump/osc_jumping_gains_param.yaml',
-          ]
-          simulator_cmd = ['bazel-bin/examples/Cassie/multibody_sim_w_platform',
-                           '--init_height=%.1f' % 0.9,
-                           '--target_realtime_rate=%.2f' % realtime_rate,
-                           '--dt=%.5f' % 1e-3,
-                           '--publish_rate=%d' % publish_rate,
-                           '--end_time=%.3f' % sim_time,
-                           '--channel_u=OSC_JUMPING',
-                           '--traj_name=down_jump',
-                           '--platform_height=0.5',
-                           '--platform_x=-0.99',
-                           '--visualize=1',
-                           ]
-          gain_filename = 'osc_jumping_gains_down.yaml'
+  for i in range(5):
+    for traj in trajectory_names:
+      for impact_threshold in impact_thresholds:
+        for landing_time in landing_times:
+          log_suffix = ''
+          if parameter == 'landing_delay':
+            log_suffix = 't_%.3f' % (landing_time) + '_duration_%.3f' % (
+              impact_threshold)
+          log_path = results_folder + traj + '_' + str(i) + '/lcmlog-' + log_suffix
+          print(log_path)
+          gain_filename = ''
+          if traj == 'jump' or traj == 'long_jump':
+            if traj == 'jump':
+              gain_filename = 'osc_jumping_gains.yaml'
+            elif traj == 'long_jump':
+              gain_filename = 'osc_jumping_gains_long.yaml'
+            controller_cmd = [
+              'bazel-bin/examples/Cassie/run_osc_jumping_controller',
+              '--delay_time=%.1f' % delay_time,
+              '--channel_u=OSC_JUMPING',
+              '--gains_filename=examples/Cassie/osc_jump/osc_jumping_gains_param.yaml',
+              '--traj_name=%s' % traj,
+            ]
+            simulator_cmd = ['bazel-bin/examples/Cassie/multibody_sim',
+                             '--init_height=%.1f' % 0.9,
+                             '--toe_spread=0.12',
+                             '--target_realtime_rate=%.2f' % realtime_rate,
+                             '--dt=%.5f' % 1e-3,
+                             '--publish_rate=%d' % publish_rate,
+                             '--end_time=%.3f' % sim_time,
+                             '--channel_u=OSC_JUMPING',
+                             ]
+          elif traj == 'box_jump':
+            controller_cmd = [
+              'bazel-bin/examples/Cassie/run_osc_jumping_controller',
+              '--delay_time=%.1f' % delay_time,
+              '--channel_u=OSC_JUMPING',
+              '--traj_name=%s' % traj,
+              '--gains_filename=examples/Cassie/osc_jump/osc_jumping_gains_param.yaml',
+            ]
+            simulator_cmd = ['bazel-bin/examples/Cassie/multibody_sim_w_platform',
+                             '--init_height=%.1f' % 0.9,
+                             '--target_realtime_rate=%.2f' % realtime_rate,
+                             '--dt=%.5f' % 1e-3,
+                             '--publish_rate=%d' % publish_rate,
+                             '--end_time=%.3f' % sim_time,
+                             '--channel_u=OSC_JUMPING',
+                             '--traj_name=box_jump',
+                             '--platform_height=0.5',
+                             '--platform_x=0.25',
+                             '--visualize=1',
+                             ]
+            gain_filename = 'osc_jumping_gains_box.yaml'
+          elif traj == 'down_jump':
+            controller_cmd = [
+              'bazel-bin/examples/Cassie/run_osc_jumping_controller',
+              '--delay_time=%.1f' % delay_time,
+              '--channel_u=OSC_JUMPING',
+              '--traj_name=%s' % traj,
+              '--gains_filename=examples/Cassie/osc_jump/osc_jumping_gains_param.yaml',
+            ]
+            simulator_cmd = ['bazel-bin/examples/Cassie/multibody_sim_w_platform',
+                             '--init_height=%.1f' % 0.9,
+                             '--target_realtime_rate=%.2f' % realtime_rate,
+                             '--dt=%.5f' % 1e-3,
+                             '--publish_rate=%d' % publish_rate,
+                             '--end_time=%.3f' % sim_time,
+                             '--channel_u=OSC_JUMPING',
+                             '--traj_name=down_jump',
+                             '--platform_height=0.5',
+                             '--platform_x=-0.99',
+                             '--visualize=1',
+                             ]
+            gain_filename = 'osc_jumping_gains_down.yaml'
 
-        f = open(gains_path + gain_filename, 'r')
-        filedata = f.read()
-        f.close()
-        newdata = filedata.replace('impact_threshold: %.3f' % nominal_threshold,
-                                   'impact_threshold: %.3f' % impact_threshold)
-        newdata = newdata.replace('landing_delay: %.3f' % nominal_delay,
-                                  'landing_delay: %.3f' % landing_time)
-        f = open(gains_path + 'osc_jumping_gains_param.yaml', 'w')
-        f.write(newdata)
-        f.close()
+          f = open(gains_path + gain_filename, 'r')
+          filedata = f.read()
+          f.close()
+          newdata = filedata.replace('impact_threshold: %.3f' % nominal_threshold,
+                                     'impact_threshold: %.3f' % impact_threshold)
+          newdata = newdata.replace('landing_delay: %.3f' % nominal_delay,
+                                    'landing_delay: %.3f' % landing_time)
+          f = open(gains_path + 'osc_jumping_gains_param.yaml', 'w')
+          f.write(newdata)
+          f.close()
 
-        lcm_logger_cmd = ['lcm-logger',
-                          '-f',
-                          '%s' % log_path,
-                          ]
+          lcm_logger_cmd = ['lcm-logger',
+                            '-f',
+                            '%s' % log_path,
+                            ]
 
-        parameter_file.write(log_path + '\n')
-        parameter_file.write(' '.join(controller_cmd) + '\n')
-        parameter_file.write(' '.join(simulator_cmd) + '\n')
-        parameter_file.write('**********\n')
-        controller_process = subprocess.Popen(controller_cmd)
-        logger_process = subprocess.Popen(lcm_logger_cmd)
-        time.sleep(controller_startup_time)
-        simulator_process = subprocess.Popen(simulator_cmd)
-        time.sleep(sim_run_time)
-        simulator_process.kill()
-        controller_process.kill()
-        logger_process.kill()
+          parameter_file.write(log_path + '\n')
+          parameter_file.write(' '.join(controller_cmd) + '\n')
+          parameter_file.write(' '.join(simulator_cmd) + '\n')
+          parameter_file.write('**********\n')
+          controller_process = subprocess.Popen(controller_cmd)
+          logger_process = subprocess.Popen(lcm_logger_cmd)
+          time.sleep(controller_startup_time)
+          simulator_process = subprocess.Popen(simulator_cmd)
+          time.sleep(sim_run_time)
+          simulator_process.kill()
+          controller_process.kill()
+          logger_process.kill()
 
   parameter_file.close()
 
 
 def construct_success_plot():
   results_folder = "/media/yangwill/backups/home/yangwill/Documents/research/projects/cassie/sim/jumping/logs/2023/param_study/"
-  all_logs = sorted(glob.glob(results_folder + 'down_jump/' + 'lcmlog-*'))
+  all_logs = sorted(glob.glob(results_folder + 'long_jump_0/' + 'lcmlog-*'))
 
   landing_times = np.arange(0.000, 0.055, 0.005)
   # nominal_delay = 0.040 # box
-  # nominal_delay = 0.025 # long
-  nominal_delay = 0.065 # down
+  nominal_delay = 0.025 # long
+  # nominal_delay = 0.065 # down
   # nominal_delay = 0.000 # jump
   landing_times += nominal_delay - 0.5 * 0.05
   impact_thresholds = np.arange(0.000, 0.125, 0.025)
@@ -207,7 +208,7 @@ def construct_success_plot():
     print(land_idx)
     impact_idx = int(np.round(float(impact_threshold)/0.02500))
     success[land_idx, impact_idx] = not np.any(robot_output['q'][:, 6] < 0.4)
-  np.save('down_jump_success', success)
+  np.save('long_jump_0_success', success)
   plt.imshow(success)
   plt.show()
 
@@ -215,14 +216,21 @@ def plot_success():
   landing_times = np.linspace(0.000, 0.050, 11)
   landing_times -= 0.025
   impact_thresholds = np.linspace(0.000, 0.100, 5)
+  success = np.load('long_jump_0_success.npy')
+  total_success = np.zeros(success.shape)
+  total_success += success
+  for i in range(1, 5):
+    success = np.load('long_jump_' + str(i) + '_success.npy')
+    total_success += success
 
-  success = np.load('long_jump_success.npy')
   # plt.imshow(success, cmap='tab20')
   ps = plot_styler.PlotStyler()
   plot_styler.PlotStyler.set_default_styling()
   cmap = matplotlib.colors.ListedColormap([ps.grey, ps.blue])
-  plt.figure()
-  plt.imshow(success, cmap=cmap)
+  fig = plt.figure()
+  # plt.imshow(total_success, cmap=cmap)
+  im = plt.imshow(total_success, cmap='Reds')
+  plt.colorbar(im)
   plt.xlabel('Projection Window Duration (s)')
   plt.ylabel('Deviation from Nominal Transition Time (s)')
   ax = plt.gca()
@@ -233,8 +241,8 @@ def plot_success():
   ax.set_yticklabels(np.around(landing_times, 3).tolist())
   ax.grid(which="minor", color='k', linestyle='-', linewidth=2)
 
-  legend_elements = [Patch(facecolor=ps.grey, alpha=0.7, label='Fail'), Patch(facecolor=ps.blue, alpha=0.3, label='Success')]
-  legend = ax.legend(handles=legend_elements, loc=1)
+  # legend_elements = [Patch(facecolor=ps.grey, alpha=0.7, label='Fail'), Patch(facecolor=ps.blue, alpha=0.7, label='Success')]
+  # legend = ax.legend(handles=legend_elements, loc=1)
   plt.savefig('long_jump_success.png', dpi=240)
   plt.show()
 

ee33ca6e5642a805ae602ec69436ec4193b2c747
Brian Acosta <bjacosta@seas.upenn.edu>
2023-02-22 11:13:43 -0500

    create alip_mpc base class to try different controllers more easily

--- systems/controllers/footstep_planning/alip_minlp.h
+++ systems/controllers/footstep_planning/alip_minlp.h
@@ -1,17 +1,7 @@
 #pragma once
 #include <array>
 #include <chrono>
-
-#include "alip_utils.h"
-#include "geometry/convex_foothold.h"
-#include "solvers/nonlinear_constraint.h"
-#include "unsupported/Eigen/MatrixFunctions"
-
-#include "drake/solvers/decision_variable.h"
-#include "drake/solvers/mathematical_program.h"
-#include "drake/solvers/mathematical_program_result.h"
-#include "drake/solvers/osqp_solver.h"
-#include "drake/solvers/mosek_solver.h"
+#include "alip_mpc.h"
 
 namespace dairlib::systems::controllers {
 
@@ -30,271 +20,39 @@ using drake::solvers::LinearEqualityConstraint;
 
 using std::vector;
 
-
-struct profile_data {
-  std::chrono::time_point<std::chrono::steady_clock> start_;
-  std::chrono::duration<double> pre_solve_;
-  std::vector<std::chrono::duration<double>> solves_;
-  std::chrono::duration<double> post_solves_;
-  std::chrono::duration<double> finish_;
-  friend std::ostream& operator<<(std::ostream& os, const profile_data& data);
-};
-
-inline std::ostream& operator<<(std::ostream& os, const profile_data& data) {
-  os << data.pre_solve_.count() << ", ";
-  for (const auto& t : data.solves_) {
-    os << t.count() << ", ";
-  }
-  os << data.post_solves_.count() << ", " << data.finish_.count() << "\n";
-  return os;
-}
-
-/*
- * Nonlinear constraint representing the linear ALIP dynamics with ankle torque,
- * discretized with stance duration t as a decision variable,
- * with n evenly-spaced knot points
- */
-class AlipDynamicsConstraint : public NonlinearConstraint<drake::AutoDiffXd> {
+class AlipMINLP final : public AlipMPC {
  public:
-  AlipDynamicsConstraint(double m, double H, double n);
-  void EvaluateConstraint(
-      const Eigen::Ref<const drake::VectorX<drake::AutoDiffXd>> &x,
-      drake::VectorX<drake::AutoDiffXd> *y) const override;
-  Eigen::Matrix4d Ad(double t);
-
-  void set_m(double m) {
-    m_ = m;
-    A_ = alip_utils::CalcA(H_, m_);
-    A_inv_ = A_.inverse();
-  }
-  void set_H(double H) {
-    H_ = H;
-    A_ = alip_utils::CalcA(H_, m_);
-    A_inv_ = A_.inverse();
-  }
-
- private:
-  double m_;
-  double H_;
-  double n_;
-  Eigen::Matrix4d A_;
-  Eigen::Matrix4d A_inv_;
-  Eigen::MatrixXd B_;
-};
-
-class AlipMINLP {
- public:
-
-  template<typename Derived>
-  static Eigen::VectorBlock<Derived, 4>
-      GetStateAtKnot(Eigen::MatrixBase<Derived>& xx, int knot) {
-    return Eigen::VectorBlock<Derived, 4>(xx.derived(), 4 * knot);
-  }
-  template<typename Derived>
-  static Eigen::VectorBlock<const Derived, 4>
-  GetStateAtKnot(const Eigen::MatrixBase<Derived>& xx, int knot) {
-    return Eigen::VectorBlock<const Derived, 4>(xx.derived(), 4 * knot);
-  }
-  template<typename Derived>
-  static Eigen::VectorBlock<Derived, 1>
-      GetInputAtKnot(Eigen::MatrixBase<Derived>& uu, int knot) {
-    return Eigen::VectorBlock<Derived, 1>(uu.derived(), knot);
-  }
-  template<typename Derived>
-  static Eigen::VectorBlock<const Derived, 1>
-  GetInputAtKnot(const Eigen::MatrixBase<Derived>& uu, int knot) {
-    return Eigen::VectorBlock<const Derived, 1>(uu.derived(), knot);
-  }
 
   /// Constructor takes a nominal robot mass and walking height
   AlipMINLP(double m,
             double H,
             int nknots,
             alip_utils::ResetDiscretization reset_discretization)
-      : m_(m), H_(H), nknots_(nknots),
-        reset_discretization_(reset_discretization){};
+      : AlipMPC(m, H, nknots, reset_discretization) {};
 
   AlipMINLP(double m, double H, int nk,
             alip_utils::ResetDiscretization reset_discretization, int nmodes)
             : AlipMINLP(m, H, nk, reset_discretization) {
-    for (int i = 0; i < nmodes; i ++) {
-      AddMode();
+    for (int i = 0; i < nmodes; i++) {
+      this->AddMode();
     }
   }
 
-  /* -- Problem setup - these must be called before Build() -- */
-
-  void AddMode();
-  void AddInputCost(double R);
-  void SetMinimumStanceTime(double tmin) {
-    tmin_ = vector<double>(nmodes_, tmin);
-  };
-  void SetMaximumStanceTime(double tmax) {
-    tmax_ = vector<double>(nmodes_, tmax);
-  };
-  void SetDoubleSupportTime(double t_ds) { Tds_ = t_ds; }
-  void SetInputLimit(double umax) { umax_ = umax; };
-  void AddTrackingCost(
-      const vector<Eigen::VectorXd> &xd,
-      const Eigen::Matrix4d &Q, const Eigen::MatrixXd &Qf);
-  void AddFootholds(const vector<geometry::ConvexFoothold> &footholds) {
-    footholds_.insert(footholds_.end(), footholds.begin(), footholds.end());
-  }
-
-  void Build(const drake::solvers::SolverOptions &solver_options);
-  void Build();
-
-  vector<Eigen::VectorXd> MakeXdesTrajForVdes(
-      const Eigen::Vector2d &vdes, double step_width, double Ts, int nk,
-      alip_utils::Stance stance = alip_utils::Stance::kRight) const;
-
-  // per solve updates
-  void UpdateMaximumCurrentStanceTime(double tmax);
-  void ActivateInitialTimeEqualityConstraint(double t);
-  void UpdateNextFootstepReachabilityConstraint(
-      const geometry::ConvexFoothold &workspace);
-  void UpdateTrackingCost(const vector<Eigen::VectorXd>& xd);
-  void UpdateFootholds(const vector<geometry::ConvexFoothold> &footholds) {
-    ClearFootholds();
-    AddFootholds(footholds);
-  }
-  void UpdateNominalStanceTime(double t0, double Ts) {
-    vector<double> T(nmodes_, Ts);
-    T.front() = t0;
-    td_ = T;
-  }
-
-  // Update the dynamics constraint to reflect the current mode timings
-  // after maybe updating the mode timing by one gradient step
-  void UpdateModeTiming(bool take_sqp_step);
-  void UpdateNoCrossoverConstraint();
-  void UpdateModeTimingsOnTouchdown();
-
-  // Solving the problem
-  void CalcOptimalFootstepPlan(
-      const Eigen::Vector4d &x,
-      const Eigen::Vector3d &p,
-      bool warmstart = false);
-
-  // Getting the solution
-  Eigen::VectorXd GetTimingSolution() const;
-  vector<Eigen::Vector3d> GetFootstepSolution() const;
-  vector<Eigen::VectorXd> GetStateSolution() const;
-  vector<Eigen::VectorXd> GetInputSolution() const;
-
-  // getting the initial guess
-  Eigen::VectorXd GetTimingGuess() const;
-  vector<Eigen::Vector3d> GetFootstepGuess() const;
-  vector<Eigen::VectorXd> GetStateGuess() const;
-  vector<Eigen::VectorXd> GetInputGuess() const;
-
-  // getting the desired solution
-  Eigen::VectorXd GetTimingDesired() const;
-  vector<Eigen::VectorXd> GetStateDesired() const { return xd_; }
-  vector<Eigen::Vector3d> GetFootstepDesired() const {
-    return vector<Eigen::Vector3d>(nmodes_, Eigen::Vector3d::Zero()); // NOLINT(modernize-return-braced-init-list)
-  };
-  vector<Eigen::VectorXd> GetInputDesired() const {
-    return vector<Eigen::VectorXd>( // NOLINT(modernize-return-braced-init-list)
-        nmodes_, Eigen::VectorXd::Zero(nu_ * (nknots_ - 1)));
-  };
-
-  // misc getters and setters
-  double solve_time() const;
-  void set_m(double m) { m_ = m; }
-  void set_H(double H) { H_ = H; }
-  double H() const {return H_;}
-  double m() const {return m_;}
-  int nmodes() const { return nmodes_; }
-  int nknots() const { return nknots_; }
-
-  drake::solvers::MathematicalProgram *get_prog() { return prog_.get_mutable(); }
-  drake::solvers::MathematicalProgramResult &get_solution() { return solution_.first; }
+  void AddMode() final;
+  void MakeFootholdConstraints() final;
+  void Build(const drake::solvers::SolverOptions &solver_options) final;
+  void Build() final;
 
  private:
 
-  // problem data:
-  // TODO(@Brian-Acosta): Make these gains/parameters as appropriate
-  double m_;
-  double H_;
-  const int nknots_;  // knots per mode
-  const alip_utils::ResetDiscretization reset_discretization_;
-  const int np_ = 3;
-  const int nx_ = 4;
-  const int nu_ = 1;
-  int nmodes_ = 0;
-  double Tds_ = 0;
-  double R_ = 0;
-  double umax_ = -1;
-  vector<double> tmin_{};
-  vector<double> tmax_{};
-  Eigen::MatrixXd Q_;
-  Eigen::MatrixXd Qf_;
-  vector<double> td_;
-  vector<Eigen::VectorXd> xd_;
-  vector<vector<int>> mode_sequnces_{};
-  vector<geometry::ConvexFoothold> footholds_;
-
-  // problem setup methods
-  void MakeTerminalCost();
-  void MakeResetConstraints();
-  void MakeDynamicsConstraints();
-  void MakeFootholdConstraints();
-  void MakeWorkspaceConstraints();
-  void MakeInputBoundConstaints();
-  void MakeNoCrossoverConstraint();
-  void MakeInitialStateConstraint();
-  void MakeInitialFootstepConstraint();
-  void MakeNextFootstepReachabilityConstraint();
-  void MakeCapturePointConstraint(int foothold_idx);
-
-  void CalcResetMap(Eigen::Matrix<double,4,12>* Aeq) const;
-  vector<vector<int>> GetPossibleModeSequences();
-
   // per solve updates and solve steps
-  void SolveOCProblemAsIs();
-  void UpdateInitialGuess();
-  void UpdateInitialGuess(const Eigen::Vector3d &p0, const Eigen::Vector4d &x0);
-  void UpdateTimingGradientStep();
-  void UpdateDynamicsConstraints();
-  void ClearFootholds() { footholds_.clear(); }
+  void SolveOCProblemAsIs() final;
+  void UpdateInitialGuess() final;
+  void UpdateInitialGuess(const Eigen::Vector3d &p0, const Eigen::Vector4d &x0) final;
 
   // Per QP updates
   void UpdateFootholdConstraints(vector<int> foothold_idxs);
   void UpdateIndividualFootholdConstraint(int idx_mode, int idx_foothold);
-
-  // program and decision variables
-  drake::copyable_unique_ptr<MathematicalProgram>
-      prog_{std::make_unique<MathematicalProgram>()};
-  vector<VectorXDecisionVariable> pp_{};
-  vector<VectorXDecisionVariable> xx_{};
-  vector<VectorXDecisionVariable> uu_{};
-  vector<VectorXDecisionVariable> ee_{};
-
-  // store the mode timing as doubles and take gradient steps external to the QP
-  Eigen::VectorXd tt_;
-
-  // constraints
-  vector<Binding<BoundingBoxConstraint>> input_bounds_c_{};
-  vector<Binding<LinearEqualityConstraint>> reset_map_c_{};
-  vector<Binding<LinearConstraint>> no_crossover_constraint_{};
-  std::shared_ptr<LinearEqualityConstraint> initial_foot_c_ = nullptr;
-  std::shared_ptr<LinearEqualityConstraint> initial_state_c_ = nullptr;
-  std::shared_ptr<LinearConstraint> next_step_reach_c_fixed_ = nullptr;
-  std::shared_ptr<Binding<LinearConstraint>> capture_point_contstraint_ = nullptr;
-  vector<vector<Binding<LinearEqualityConstraint>>> dynamics_c_{};
-  vector<std::pair<Binding<LinearConstraint>,
-                   Binding<LinearEqualityConstraint>>> footstep_c_{};
-
-  // costs
-  std::shared_ptr<QuadraticCost> terminal_cost_;
-  vector<Binding<QuadraticCost>> input_costs_{};
-  vector<vector<Binding<QuadraticCost>>> tracking_costs_{};
-
-  // Bookkeeping
-  bool built_ = false;
-  std::pair<drake::solvers::MathematicalProgramResult,
-            vector<Eigen::VectorXd>> solution_;
   drake::solvers::OsqpSolver solver_;
 
 };
---
+++ systems/controllers/footstep_planning/alip_mpc.cc
@@ -0,0 +1,506 @@
+#include <algorithm>
+#include <iostream>
+
+#include "alip_mpc.h"
+#include "solvers/optimization_utils.h"
+
+#include "drake/math/autodiff.h"
+#include "drake/math/autodiff_gradient.h"
+#include "drake/solvers/solve.h"
+#include "drake/solvers/osqp_solver.h"
+
+namespace dairlib::systems::controllers{
+
+using std::pair;
+using std::vector;
+using std::to_string;
+using std::numeric_limits;
+
+using Eigen::Matrix;
+using Eigen::VectorXd;
+using Eigen::Vector2d;
+using Eigen::Vector3d;
+using Eigen::Vector4d;
+using Eigen::MatrixXd;
+using Eigen::Matrix4d;
+using Eigen::Matrix3d;
+
+using drake::VectorX;
+using drake::Vector4;
+using drake::AutoDiffXd;
+using drake::AutoDiffVecXd;
+using drake::math::InitializeAutoDiff;
+
+using drake::solvers::Solve;
+using drake::solvers::Binding;
+using drake::solvers::OsqpSolver;
+using drake::solvers::QuadraticCost;
+using drake::solvers::DecisionVariable;
+using drake::solvers::LinearConstraint;
+using drake::solvers::MathematicalProgram;
+using drake::solvers::BoundingBoxConstraint;
+using drake::solvers::VectorXDecisionVariable;
+using drake::solvers::MathematicalProgramResult;
+using drake::solvers::LinearEqualityConstraint;
+using drake::solvers::kIterationLimit;
+
+using geometry::kMaxFootholdFaces;
+
+AlipDynamicsConstraint::AlipDynamicsConstraint(double m, double H, double n) :
+    NonlinearConstraint<AutoDiffXd>(
+        4, 10, Vector4d::Zero(), Vector4d::Zero(), "dynamics"),
+    m_(m), H_(H), n_(n-1) {
+
+  A_ = alip_utils::CalcA(H_, m_);
+  A_inv_ = A_.inverse();
+  B_ = Vector4d::UnitW();
+
+}
+
+void AlipDynamicsConstraint::EvaluateConstraint(
+    const Eigen::Ref<const drake::VectorX<AutoDiffXd>> &x,
+    drake::VectorX<AutoDiffXd>* y) const {
+  VectorXd xd = drake::math::ExtractValue(x);
+  Vector4d x0 = xd.head(4);
+  VectorXd u0 = xd.segment(4, 1);
+  Vector4d x1 = xd.segment(5, 4);
+  double t = xd(xd.size() -1) / n_;
+
+  Matrix4d Ad = alip_utils::CalcAd(H_, m_, t);
+  Vector4d Bd = A_inv_ * (Ad - Matrix4d::Identity()) * B_;
+
+  VectorXd y0 = Ad * x0 + Bd * u0 - x1;
+  MatrixXd dy = MatrixXd::Zero(4, 10);
+  dy.block(0, 0, 4, 4) = Ad;
+  dy.col(4) = Bd;
+  dy.block(0, 5, 4, 4) = -Matrix4d::Identity();
+  dy.rightCols(1) = (1 / n_) *  (A_ * Ad * x0 + Ad * B_ * u0);
+  *y = InitializeAutoDiff(y0, dy);
+}
+
+Matrix4d AlipDynamicsConstraint::Ad(double t) {
+  return alip_utils::CalcAd(H_, m_, t / n_);
+}
+
+void AlipMPC::CalcOptimalFootstepPlan(const Eigen::Vector4d &x,
+                                      const Eigen::Vector3d &p,
+                                      bool warmstart) {
+  DRAKE_DEMAND(built_);
+  if (warmstart &&
+     (solution_.first.is_success() ||
+      solution_.first.get_solution_result() == kIterationLimit)){
+    UpdateInitialGuess();
+  } else {
+    UpdateInitialGuess(p, x);
+  }
+  initial_state_c_->UpdateCoefficients(Matrix4d::Identity(), x);
+  initial_foot_c_->UpdateCoefficients(Matrix3d::Identity(), p);
+  SolveOCProblemAsIs();
+}
+
+void AlipMPC::Build(const drake::solvers::SolverOptions& options) {
+  prog_->SetSolverOptions(options);
+  Build();
+}
+
+void AlipMPC::AddTrackingCost(const vector<Eigen::VectorXd> &xd,
+                                const Matrix4d &Q, const Eigen::MatrixXd& Qf) {
+  DRAKE_DEMAND(xd.size() == nmodes_);
+  for (int i = 1; i < nmodes_; i++){
+    DRAKE_DEMAND(xd.at(i).size() == nx_ * nknots_);
+    vector<Binding<QuadraticCost>> QQ;
+    for (int k = 0; k < nknots_ ; k++){
+      QQ.push_back(
+          prog_->AddQuadraticErrorCost(
+              Q,
+              GetStateAtKnot(xd.at(i), k),
+              GetStateAtKnot(xx_.at(i), k)));
+    }
+    tracking_costs_.push_back(QQ);
+  }
+  xd_ = xd;
+  Q_ = Q;
+  Qf_ = Qf;
+  MakeTerminalCost();
+}
+
+void AlipMPC::AddInputCost(double R) {
+  for (int i = 0; i < nmodes_; i++){
+    int n = nknots_ - 1;
+    input_costs_.push_back(
+        prog_->AddQuadraticCost(
+            R * MatrixXd::Identity(n, n),
+            VectorXd::Zero(n),
+            uu_.at(i))
+    );
+  }
+  R_ = R;
+}
+
+
+void AlipMPC::MakeTerminalCost(){
+  terminal_cost_ = prog_->AddQuadraticCost(
+      2 * Qf_, -2 * Qf_ * xd_.back().tail<4>(),
+      xx_.back().tail<4>()).evaluator();
+}
+
+void AlipMPC::MakeCapturePointConstraint(int foothold_idx) {
+  const auto& [A, b] = footholds_.at(foothold_idx).GetConstraintMatrices();
+  Matrix<double, 2, 4> Acp = Matrix<double, 2, 4>::Zero();
+  Acp.leftCols(2) = Eigen::Matrix2d::Identity();
+  Acp(0,3) = -1.0 / (m_ * sqrt(9.81 * H_));
+  Acp(1,2) = -Acp(0,3);
+  MatrixXd A_constraint = A.leftCols<2>() * Acp;
+
+  capture_point_contstraint_ =
+      std::make_shared<Binding<LinearConstraint>>(
+      prog_->AddLinearConstraint(
+          A_constraint,
+          -numeric_limits<double>::infinity() * VectorXd::Ones(A_constraint.rows()),
+          b, {pp_.back().head(2), xx_.back().tail(2)}
+      ));
+}
+
+void AlipMPC::MakeResetConstraints() {
+  Matrix<double, 4, 12> A_eq = Matrix<double, 4, 12>::Zero();
+  CalcResetMap(&A_eq);
+  for (int i = 0; i < (nmodes_ - 1); i++) {
+    reset_map_c_.push_back(
+        prog_->AddLinearEqualityConstraint(
+            A_eq, Vector4d::Zero(),
+            {xx_.at(i).tail<4>(), xx_.at(i+1).head<4>(),
+             pp_.at(i).head<2>(), pp_.at(i+1).head<2>()}));
+    reset_map_c_.back().evaluator()->set_description("reset map constraint");
+  }
+}
+
+void AlipMPC::MakeDynamicsConstraints() {
+  for (int i = 0; i < nmodes_; i++) {
+    vector<Binding<LinearEqualityConstraint>> dyn_c_this_mode;
+    double t = tt_(i) / (nknots_ - 1);
+    Matrix4d Ad = alip_utils::CalcAd(H_, m_, t);
+    Vector4d Bd = alip_utils::CalcA(H_, m_).inverse() * (Ad - Matrix4d::Identity()) * Vector4d::UnitW();
+
+    Matrix<double, 4, 9> Adyn = Matrix<double, 4, 9>::Zero();
+    Adyn.leftCols<4>() = Ad;
+    Adyn.col(4) = Bd;
+    Adyn.rightCols<4>() = -Matrix4d::Identity();
+
+    for (int k = 0; k < nknots_ - 1; k++) {
+      dyn_c_this_mode.push_back(
+          prog_->AddLinearEqualityConstraint(
+              Adyn, Vector4d::Zero(),
+              {GetStateAtKnot(xx_.at(i), k),
+               GetInputAtKnot(uu_.at(i), k),
+               GetStateAtKnot(xx_.at(i), k+1)}));
+      dyn_c_this_mode.back().evaluator()->
+          set_description("dynamics" + std::to_string(i) + "_" + std::to_string(k));
+    }
+    dynamics_c_.push_back(dyn_c_this_mode);
+  }
+}
+
+void AlipMPC::MakeInitialFootstepConstraint() {
+  initial_foot_c_ = prog_->AddLinearEqualityConstraint(
+      Matrix3d::Identity(), Vector3d::Zero(), pp_.at(0)).evaluator();
+  initial_foot_c_->set_description("initial_footstep");
+}
+
+void AlipMPC::MakeInitialStateConstraint() {
+  initial_state_c_ = prog_->AddLinearEqualityConstraint(
+      Matrix4d::Identity(),
+      Vector4d::Zero(),
+      xx_.front().head<4>()
+  ).evaluator();
+
+  initial_state_c_->set_description("initial_state");
+}
+
+void AlipMPC::MakeInputBoundConstaints() {
+  DRAKE_DEMAND(umax_ >= 0 && !uu_.empty());
+  int i = 0;
+  for (const auto& vec : uu_) {
+    input_bounds_c_.push_back(
+        prog_->AddBoundingBoxConstraint(-umax_, umax_, vec)
+    );
+    i++;
+    input_bounds_c_.back().evaluator()->set_description(
+        "input bounds" + std::to_string(i));
+  }
+}
+
+void AlipMPC::MakeWorkspaceConstraints() {
+  Vector2d bb(0.6, 0.35);
+
+  MatrixXd I1 = MatrixXd::Zero(2, 4);
+  MatrixXd I2 = MatrixXd::Zero(2, 4);
+  I1.leftCols(2) = MatrixXd::Identity(2, 2);
+  I1.rightCols(2) = -MatrixXd::Identity(2, 2);
+  I2.leftCols(2) = -MatrixXd::Identity(2, 2);
+  I2.rightCols(2) = -MatrixXd::Identity(2, 2);
+
+  for (int n = 1; n < nmodes_; n++) {
+    ee_.push_back(prog_->NewContinuousVariables(2 * nknots_));
+    for (int k = 0; k < nknots_; k++) {
+      const auto& ee = ee_.at(n-1).segment(2*k, 2);
+      prog_->AddQuadraticErrorCost(1000 * MatrixXd::Identity(2,2), bb, ee);
+      prog_->AddLinearConstraint(
+          I1,
+          -numeric_limits<double>::infinity() * Vector2d::Ones(),
+          Vector2d::Zero(),
+          {GetStateAtKnot(xx_.at(n), k).head<2>(), ee});
+      prog_->AddLinearConstraint(
+          I2,
+          -numeric_limits<double>::infinity() * Vector2d::Ones(),
+          Vector2d::Zero(),
+          {GetStateAtKnot(xx_.at(n), k).head<2>(), ee});
+    }
+  }
+}
+
+void AlipMPC::MakeNextFootstepReachabilityConstraint() {
+  next_step_reach_c_fixed_ = prog_->AddLinearConstraint(
+          MatrixXd::Zero(1,3),
+          -numeric_limits<double>::infinity()*VectorXd::Ones(1),
+          VectorXd::Zero(1), pp_.at(1))
+      .evaluator();
+  next_step_reach_c_fixed_->set_description("next footstep workspace constraint");
+}
+
+void AlipMPC::MakeNoCrossoverConstraint() {
+  for (int i = 0; i < nmodes_ - 1 ; i++) {
+    int stance = i % 2 == 0 ? -1 : 1;
+    Eigen::RowVector2d A = {stance, -stance};
+    no_crossover_constraint_.push_back(
+        prog_->AddLinearConstraint(
+            A, -numeric_limits<double>::infinity(), -0.05,
+            {pp_.at(i).segment(1,1), pp_.at(i+1).segment(1,1)})
+    );
+    no_crossover_constraint_.back().evaluator()->set_description(
+        "no crossover constraint" + std::to_string(i));
+  }
+}
+
+vector<VectorXd> AlipMPC::MakeXdesTrajForVdes(
+    const Vector2d& vdes, double step_width, double Ts, int nk,
+    alip_utils::Stance stance) const {
+  alip_utils::AlipGaitParams params;
+  params.height = H_;
+  params.mass = m_;
+  params.single_stance_duration = Ts;
+  params.double_stance_duration = Tds_;
+  params.stance_width = step_width;
+  params.desired_velocity = vdes;
+  params.reset_discretization_method = reset_discretization_;
+  params.intial_stance_foot = stance;
+  return alip_utils::MakePeriodicAlipGaitTrajectory(params, nmodes(), nk);
+}
+
+void AlipMPC::ActivateInitialTimeEqualityConstraint(double t) {
+  DRAKE_DEMAND(built_);
+  tt_(0) = t;
+}
+
+void AlipMPC::UpdateMaximumCurrentStanceTime(double tmax) {
+  tmax_.at(0) = tmax;
+}
+
+void AlipMPC::UpdateTrackingCost(const vector<VectorXd>& xd) {
+  for(int n = 0; n < nmodes_ - 1 ; n++) {
+    for (int k = 0; k < nknots_; k++) {
+      tracking_costs_.at(n).at(k).evaluator()->
+          UpdateCoefficients( 2.0*Q_,
+                              -2.0*Q_ * GetStateAtKnot(xd.at(n+1), k));
+    }
+  }
+  terminal_cost_->UpdateCoefficients(2.0 * Qf_,
+                                     -2.0 * Qf_ * xd.back().tail<4>());
+  xd_ = xd;
+}
+
+void AlipMPC::CalcResetMap(Eigen::Matrix<double, 4, 12> *Aeq) const {
+  Matrix<double, 4, 8> A =
+      alip_utils::CalcResetMap(H_, m_, Tds_, reset_discretization_);
+  Aeq->topLeftCorner<4, 4>() = A.topLeftCorner<4, 4>();
+  Aeq->topRightCorner<4, 4>() = A.topRightCorner<4, 4>();
+  Aeq->block<4, 4>(0, 4) = -Matrix4d::Identity();
+}
+
+void AlipMPC::UpdateInitialGuess(const Eigen::Vector3d &p0,
+                                   const Eigen::Vector4d &x0) {
+  // Update state initial guess
+  vector<VectorXd> xg = xd_;
+
+  // Set the initial guess for the current mode based on limited time
+  VectorXd xx = VectorXd (nx_ * nknots_);
+  xx.head<4>() = x0;
+  Matrix4d Ad = alip_utils::CalcAd(H_, m_, tt_(0) / (nknots_ - 1));
+  for (int i = 1; i < nknots_; i++) {
+    GetStateAtKnot(xx, i) = Ad * GetStateAtKnot(xx, i-1);
+  }
+  xg.front() = xx;
+
+  for (int n = 0; n < nmodes_; n++) {
+    for (int k = 0; k < nknots_ ; k++) {
+      prog_->SetInitialGuess(
+          GetStateAtKnot(xx_.at(n), k),
+          GetStateAtKnot(xg.at(n), k));
+    }
+  }
+  Vector3d ptemp = p0;
+  prog_->SetInitialGuess(pp_.front(), p0);
+  for(int n = 1; n < nmodes_; n++) {
+    Vector2d p1 = (xd_.at(n-1).tail<4>() - xd_.at(n).head<4>()).head<2>() + ptemp.head<2>();
+    prog_->SetInitialGuess(pp_.at(n).head<2>(), p1);
+    ptemp.head<2>() = p1;
+  }
+}
+
+void AlipMPC::UpdateInitialGuess() {
+  DRAKE_DEMAND(
+      solution_.first.is_success() ||
+          solution_.first.get_solution_result() ==
+              drake::solvers::kIterationLimit);
+  prog_->SetInitialGuessForAllVariables(solution_.first.GetSolution());
+}
+
+void AlipMPC::UpdateNextFootstepReachabilityConstraint(const geometry::ConvexFoothold &workspace) {
+
+  const auto& [Af, bf] = workspace.GetConstraintMatrices();
+  double neg_inf  = -numeric_limits<double>::infinity();
+  next_step_reach_c_fixed_->UpdateCoefficients(
+      Af, neg_inf * VectorXd::Ones(bf.rows()), bf);
+}
+
+void AlipMPC::UpdateNoCrossoverConstraint() {
+  for (auto& constraint: no_crossover_constraint_) {
+    auto A = constraint.evaluator()->GetDenseA();
+    A *= -1;
+    constraint.evaluator()->UpdateCoefficients(
+        A,
+        -numeric_limits<double>::infinity()*VectorXd::Ones(1),
+        VectorXd::Zero(1));
+  }
+}
+
+void AlipMPC::UpdateModeTiming(bool take_sqp_step) {
+  tt_(0) = td_.front();
+  if (take_sqp_step) {
+    UpdateTimingGradientStep();
+  }
+  UpdateDynamicsConstraints();
+}
+
+void AlipMPC::UpdateModeTimingsOnTouchdown() {
+  for (int i = 0; i < nmodes_ - 1; i++){
+    tt_(i) = tt_(i+1);
+  }
+  tt_.tail(1)(0) = td_.back();
+}
+
+void AlipMPC::UpdateTimingGradientStep() {
+  Matrix4d A = alip_utils::CalcA(H_, m_);
+  Vector4d B = Vector4d::UnitW();
+
+  for (int n = 0; n < nmodes_; n++) {
+    double dLdt_n = 0;
+    Matrix4d Ad = alip_utils::CalcAd(H_, m_, tt_(n) / (nknots_ - 1));
+    for (int k = 0; k < nknots_ - 1; k++) {
+      Vector4d nu = GetStateAtKnot(solution_.second.at(n), k);
+      Vector4d x = solution_.first.GetSolution(GetStateAtKnot(xx_.at(n), k));
+      VectorXd u = solution_.first.GetSolution(GetInputAtKnot(uu_.at(n), k));
+      dLdt_n += (1.0 / (nknots_ - 1)) * nu.dot(A * Ad * x + Ad * B * u);
+    }
+    double dt = std::clamp(1e-4 * dLdt_n, -0.02, 0.02);
+    double tnew = tt_(n) - dt;
+    tt_(n) =  std::isnan(tnew) ? tt_(n)  : std::clamp(tnew, tmin_.at(n), tmax_.at(n));
+  }
+}
+
+void AlipMPC::UpdateDynamicsConstraints() {
+  for (int n = 0; n < nmodes_; n++) {
+    int nk =  nknots_ - 1;
+    double t = tt_(n) / nk;
+    Matrix4d Ad = alip_utils::CalcAd(H_, m_, t);
+    Vector4d Bd = alip_utils::CalcBd(H_, m_, t);
+    Matrix<double, 4, 9> Adyn = Matrix<double, 4, 9>::Zero();
+    Adyn.leftCols<4>() = Ad;
+    Adyn.col(4) = Bd;
+    Adyn.rightCols<4>() = -Matrix4d::Identity();
+    Vector4d bdyn = Vector4d::Zero();
+    for (int k = 0; k < nk; k++) {
+      dynamics_c_.at(n).at(k).evaluator()->UpdateCoefficients(Adyn, bdyn);
+    }
+  }
+}
+
+vector<vector<int>> AlipMPC::GetPossibleModeSequences() {
+  return alip_utils::cartesian_product(footholds_.size(), nmodes_ - 1);
+}
+
+double AlipMPC::solve_time() const {
+  return solution_.first.get_solver_details<OsqpSolver>().run_time;
+};
+
+vector<Vector3d> AlipMPC::GetFootstepSolution() const {
+  vector<Vector3d> pp;
+  for (auto& p : pp_){
+    pp.emplace_back(solution_.first.GetSolution(p));
+  }
+  return pp;
+}
+
+vector<Vector3d> AlipMPC::GetFootstepGuess() const {
+  vector<Vector3d> pp;
+  for (auto& p : pp_){
+    pp.emplace_back(prog_->GetInitialGuess(p));
+  }
+  return pp;
+}
+
+vector<VectorXd> AlipMPC::GetStateSolution() const {
+  vector<VectorXd> xx;
+  for(auto& x : xx_) {
+    xx.push_back(solution_.first.GetSolution(x));
+  }
+  return xx;
+}
+
+vector<VectorXd> AlipMPC::GetStateGuess() const {
+  vector<VectorXd> xx;
+  for(auto& x : xx_) {
+    xx.push_back(prog_->GetInitialGuess(x));
+  }
+  return xx;
+}
+
+vector<VectorXd> AlipMPC::GetInputSolution() const {
+  vector<VectorXd> uu;
+  for(auto& u : uu_) {
+    uu.push_back(solution_.first.GetSolution(u));
+  }
+  return uu;
+}
+
+vector<VectorXd> AlipMPC::GetInputGuess() const {
+  vector<VectorXd> uu;
+  for(auto& u : uu_) {
+    uu.push_back(prog_->GetInitialGuess(u));
+  }
+  return uu;
+}
+
+VectorXd AlipMPC::GetTimingSolution() const {
+  VectorXd tt = VectorXd::Zero(nmodes_);
+  return tt_;
+}
+
+VectorXd AlipMPC::GetTimingGuess() const {
+  return tt_;
+}
+
+VectorXd AlipMPC::GetTimingDesired() const {
+  const VectorXd tt = Eigen::Map<const VectorXd>(td_.data(), td_.size());
+  return tt; // NOLINT(performance-no-automatic-move)
+}
+}

303a675e6c8ad07e2439617b6fc6561f16d59a11
Brian Acosta <bjacosta@seas.upenn.edu>
2023-02-28 20:29:22 -0500

    file for foothold vizualization

---
+++ examples/perceptive_locomotion/meshcat_foothold_visualizer.cc
@@ -0,0 +1,5 @@
+//
+// Created by brian on 2/28/23.
+//
+
+#include "meshcat_foothold_visualizer.h"
---
+++ examples/perceptive_locomotion/meshcat_foothold_visualizer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "drake/systems/framework/leaf_system.h"
+
+namespace dairlib {
+namespace perceptive_locomotion {
+
+class MeshcatFootholdVisualizer : public drake::systems::LeafSystem<double>{
+
+};
+
+}
+}
+

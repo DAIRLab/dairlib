d286e354f9d6c7384f226982571d85fb55bcc78f
William Yang <yangwill@seas.upenn.edu>
2023-04-13 12:35:23 -0400

    adding meshcat visualizer to five link biped

--- examples/impact_invariant_control/five_link_biped_visualizer.cc
+++ examples/impact_invariant_control/five_link_biped_visualizer.cc
@@ -14,6 +14,8 @@
 #include "drake/systems/lcm/lcm_interface_system.h"
 #include "drake/systems/lcm/lcm_subscriber_system.h"
 #include "drake/systems/rendering/multibody_position_to_geometry_pose.h"
+#include "drake/geometry/meshcat_visualizer.h"
+#include "drake/geometry/meshcat_visualizer_params.h"
 
 namespace dairlib {
 
@@ -118,6 +120,21 @@ int do_main(int argc, char* argv[]) {
         ball_to_pose->get_output_port(),
         scene_graph->get_source_pose_port(ball_plant->get_source_id().value()));
   }
+  drake::geometry::MeshcatVisualizerParams params;
+  params.publish_period = 1.0 / 60.0;
+  auto meshcat = std::make_shared<drake::geometry::Meshcat>();
+  auto visualizer = &drake::geometry::MeshcatVisualizer<double>::AddToBuilder(
+      &builder, *scene_graph, meshcat, std::move(params));
+
+  auto ortho_camera = drake::geometry::Meshcat::OrthographicCamera();
+  ortho_camera.top = 2;
+  ortho_camera.bottom = -0.1;
+  ortho_camera.left = -1;
+  ortho_camera.right = 4;
+  ortho_camera.near = 0;
+  ortho_camera.far = 500;
+  ortho_camera.zoom = 1;
+  meshcat->SetCamera(ortho_camera);
 
   DrakeVisualizer<double>::AddToBuilder(&builder, *scene_graph);
 

0667f44eb67161bfc8dcd9710cbbec327cb6d2a0
William Yang <yangwill@seas.upenn.edu>
2023-05-16 16:48:10 -0400

    adding foce tracking

--- systems/ros/robot_lcm_ros_translator.cc
+++ systems/ros/robot_lcm_ros_translator.cc
@@ -1,5 +1,11 @@
-//
-// Created by yangwill on 5/11/23.
-//
-
 #include "robot_lcm_ros_translator.h"
+
+
+using drake::multibody::MultibodyPlant;
+
+namespace dairlib {
+namespace systems {
+
+RobotInputToROS::RobotInputToROS(MultibodyPlant<double>& plant){
+
+}

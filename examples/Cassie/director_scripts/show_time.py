11e97a1f362a3548ce9de6b5014df1b4942c8529
Yu-Ming Chen <yminchen@umich.edu>
2023-05-27 17:45:20 -0400

    Improve way point generation using start and end points alongside the block center position

--- examples/Cassie/director_scripts/show_time.py
+++ examples/Cassie/director_scripts/show_time.py
@@ -37,7 +37,7 @@ class TimeVisualizer(object):
         # self.exit_conditions['start'] = [1, 1, 0, np.inf]
         # self.exit_conditions['U-turn'] = [-np.inf, 1, 0, 1]
         ######
-        # 2023-05-27 15:58:38.245735Straight 2.5 meters -> Straight 2.5 meters #2 -> Turn 180 degrees -> Straight 2.5 meters #3 -> Straight 2.5 meters #4
+        # 2023-05-27 17h34m44s: Straight 2.5 meters -> Straight 2.5 meters #2 -> Turn 180 degrees -> Straight 2.5 meters #3 -> Straight 2.5 meters #4
         self.terrain_state_list = ['start', 'Straight 2.5 meters', 'Straight 2.5 meters #2', 'Turn 180 degrees', 'Straight 2.5 meters #3', 'Straight 2.5 meters #4', 'end']
         self.exit_conditions = {}
         self.exit_conditions['start'] = [1.000, 1.000, 0.000, np.inf]
--- examples/goldilocks_models/terrains/generate_road_stones.py
+++ examples/goldilocks_models/terrains/generate_road_stones.py
@@ -22,7 +22,7 @@ def CreateExitConditionGivenYawAndEndPoint(yaw, exit_pos):
 
 
 # CreateBlocksForTurning computes the blocks configurations to create an arc
-def CreateBlocksForTurning(init_x, init_y, init_yaw, radius, delta_yaw, n_segment, speed, exit_conditions, stones):
+def CreateBlocksForTurning(init_x, init_y, init_yaw, radius, delta_yaw, n_segment, speed, both_start_and_end_at_block_center, exit_conditions, stones):
   terrain_name = "Turn %d degrees" % int(delta_yaw * 180 / np.pi)
   idx = np.sum([(terrain_name in name) for name, _ in exit_conditions])
   terrain_name += " #%d" % (idx+1) if idx > 0 else ""
@@ -42,19 +42,30 @@ def CreateBlocksForTurning(init_x, init_y, init_yaw, radius, delta_yaw, n_segmen
   block_dimension = [length_per_segment + width * math.tan(delta_yaw_per_box/2), width, thinkness]
 
   # Compute x y and yaw
-  stones.append({"xyz": [init_x, init_y, height], "normal": normal, "dim": block_dimension, "yaw": [init_yaw], "speed": speed})
+  traj_start_x = init_x
+  traj_start_y = init_y
+  yaw = init_yaw
+  x = traj_start_x if both_start_and_end_at_block_center else traj_start_x + (length_per_segment / 2) * math.cos(yaw)
+  y = traj_start_y if both_start_and_end_at_block_center else traj_start_y + (length_per_segment / 2) * math.sin(yaw)
+  traj_end_x = x + (length_per_segment / 2) * math.cos(yaw)
+  traj_end_y = y + (length_per_segment / 2) * math.sin(yaw)
+  stones.append({"xyz": [x, y, height], "normal": normal, "dim": block_dimension, "yaw": [yaw],
+                 "traj_data": {"start_pos": [traj_start_x, traj_start_y], "end_pos": [traj_end_x, traj_end_y], "speed": speed}})
   for i in range(n_segment - 1):
-    x = stones[-1]["xyz"][0] + (length_per_segment / 2) * math.cos(stones[-1]["yaw"][0])
-    y = stones[-1]["xyz"][1] + (length_per_segment / 2) * math.sin(stones[-1]["yaw"][0])
-    yaw = stones[-1]["yaw"][0] + delta_yaw_per_box
-    x += (length_per_segment / 2) * math.cos(yaw)
-    y += (length_per_segment / 2) * math.sin(yaw)
-    stones.append({"xyz": [x, y, height], "normal": normal, "dim": block_dimension, "yaw": [yaw], "speed": speed})
+    traj_start_x = traj_end_x
+    traj_start_y = traj_end_y
+    yaw += delta_yaw_per_box
+    x = traj_end_x + (length_per_segment / 2) * math.cos(yaw)
+    y = traj_end_y + (length_per_segment / 2) * math.sin(yaw)
+    traj_end_x = x if (both_start_and_end_at_block_center and i == n_segment - 2) else x + (length_per_segment / 2) * math.cos(yaw)
+    traj_end_y = y if (both_start_and_end_at_block_center and i == n_segment - 2) else y + (length_per_segment / 2) * math.sin(yaw)
+    stones.append({"xyz": [x, y, height], "normal": normal, "dim": block_dimension, "yaw": [yaw],
+                   "traj_data": {"start_pos": [traj_start_x, traj_start_y], "end_pos": [traj_end_x, traj_end_y], "speed": speed}})
 
   # Update exit condition
-  exit_conditions.append([terrain_name, CreateExitConditionGivenYawAndEndPoint(stones[-1]["yaw"][0], stones[-1]["xyz"][:2])])
+  exit_conditions.append([terrain_name, CreateExitConditionGivenYawAndEndPoint(yaw, np.array([traj_end_x, traj_end_y]))])
 
-  return stones[-1]["xyz"][0], stones[-1]["xyz"][1], stones[-1]["yaw"][0]
+  return traj_end_x, traj_end_y, yaw
 
 
 def CreateOneBlock(init_x, init_y, init_yaw, length, speed, exit_conditions, stones):
@@ -73,7 +84,8 @@ def CreateOneBlock(init_x, init_y, init_yaw, length, speed, exit_conditions, sto
   final_x = init_x + length * math.cos(init_yaw)
   final_y = init_y + length * math.sin(init_yaw)
   final_yaw = init_yaw
-  stones.append({"xyz": [(init_x + final_x)/2, (init_y + final_y)/2, height], "normal": normal, "dim": block_dimension, "yaw": [final_yaw], "speed": speed})
+  stones.append({"xyz": [(init_x + final_x)/2, (init_y + final_y)/2, height], "normal": normal, "dim": block_dimension, "yaw": [final_yaw],
+                 "traj_data": {"start_pos": [init_x, init_y], "end_pos": [final_x, final_y], "speed": speed}})
 
   # Update exit condition
   exit_conditions.append([terrain_name, CreateExitConditionGivenYawAndEndPoint(final_yaw, np.array([final_x, final_y]))])
@@ -81,7 +93,7 @@ def CreateOneBlock(init_x, init_y, init_yaw, length, speed, exit_conditions, sto
   return final_x, final_y, final_yaw
 
 
-def CreateEndpointBlock(init_x, init_y, init_yaw, length, width, center_at_current_pos, final_at_center_pos, exit_conditions, stones):
+def CreateEndpointBlock(init_x, init_y, init_yaw, length, width, speed, center_at_current_pos, final_at_center_pos, exit_conditions, stones):
   terrain_name = "Endpoint"
   idx = np.sum([(terrain_name in name) for name, _ in exit_conditions])
   terrain_name += " #%d" % (idx+1) if idx > 0 else ""
@@ -95,16 +107,16 @@ def CreateEndpointBlock(init_x, init_y, init_yaw, length, width, center_at_curre
   # Compute x y and yaw
   center_x = init_x if center_at_current_pos else init_x + (length/2) * math.cos(init_yaw)
   center_y = init_y if center_at_current_pos else init_y + (length/2) * math.sin(init_yaw)
-  stones.append({"xyz": [center_x, center_y, height], "normal": normal, "dim": block_dimension, "yaw": [init_yaw], "speed": 0})
-
-  # Update exit condition
   final_x = center_x if final_at_center_pos else center_x + (length/2) * math.cos(init_yaw)
   final_y = center_y if final_at_center_pos else center_y + (length/2) * math.sin(init_yaw)
-  final_yaw = init_yaw
+  stones.append({"xyz": [center_x, center_y, height], "normal": normal, "dim": block_dimension, "yaw": [init_yaw],
+                 "traj_data": {"start_pos": [init_x, init_y], "end_pos": [final_x, final_y], "speed": speed}})
+
+  # Update exit condition
   if not final_at_center_pos:
     exit_conditions.append([terrain_name, CreateExitConditionGivenYawAndEndPoint(init_yaw, np.array([final_x, final_y]))])
 
-  return final_x, final_y, final_yaw
+  return final_x, final_y, init_yaw
 
 
 
@@ -116,79 +128,79 @@ current_y = 0
 current_yaw = 0
 
 # name = "turn -> long stretch -> turn"
-# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 1, 1, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
-# current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi/2, 10, speed=1, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 1, 1, speed=0.0, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi/2, 10, speed=1, both_start_and_end_at_block_center=True, exit_conditions=exit_conditions, stones=stones)
 # current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 5, speed=1, exit_conditions=exit_conditions, stones=stones)
-# current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi/2, 10, speed=1, exit_conditions=exit_conditions, stones=stones)
-# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 1, 1, center_at_current_pos=False, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi/2, 10, speed=1, both_start_and_end_at_block_center=True, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 1, 1, speed=0.5, center_at_current_pos=False, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
 
 # name = "long stretch -> 180 turn -> long stretch"
-# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
-# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, center_at_current_pos=True, final_at_center_pos=False, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, speed=0.0, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, speed=0.5, center_at_current_pos=True, final_at_center_pos=False, exit_conditions=exit_conditions, stones=stones)
 # current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=1, exit_conditions=exit_conditions, stones=stones)
 # current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=1, exit_conditions=exit_conditions, stones=stones)
-# current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi, n_segment=20, speed=1, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi, n_segment=20, speed=1, both_start_and_end_at_block_center=True, exit_conditions=exit_conditions, stones=stones)
 # current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=1, exit_conditions=exit_conditions, stones=stones)
 # current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=1, exit_conditions=exit_conditions, stones=stones)
-# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, center_at_current_pos=False, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
+# current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, speed=0.5, center_at_current_pos=False, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
 
 name = "20230526 long stretch -> 180 turn -> long stretch "
-current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
-current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, center_at_current_pos=True, final_at_center_pos=False, exit_conditions=exit_conditions, stones=stones)
-current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=0.5, exit_conditions=exit_conditions, stones=stones)
+current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, speed=0.0, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
+current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, speed=0.5, center_at_current_pos=True, final_at_center_pos=False, exit_conditions=exit_conditions, stones=stones)
+current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=0.75, exit_conditions=exit_conditions, stones=stones)
 current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=1, exit_conditions=exit_conditions, stones=stones)
-current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi, n_segment=20, speed=1, exit_conditions=exit_conditions, stones=stones)
+current_x, current_y, current_yaw = CreateBlocksForTurning(current_x, current_y, current_yaw, 2, np.pi, n_segment=20, speed=1, both_start_and_end_at_block_center=True, exit_conditions=exit_conditions, stones=stones)
 current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=1, exit_conditions=exit_conditions, stones=stones)
-current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=0.5, exit_conditions=exit_conditions, stones=stones)
-current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, center_at_current_pos=False, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
+current_x, current_y, current_yaw = CreateOneBlock(current_x, current_y, current_yaw, 2.5, speed=0.75, exit_conditions=exit_conditions, stones=stones)
+current_x, current_y, current_yaw = CreateEndpointBlock(current_x, current_y, current_yaw, 2, 2, speed=0.5, center_at_current_pos=False, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
 
 
 # Create name automatically
 name = " -> ".join([name for name, _ in exit_conditions if "Endpoint" not in name])
-name = str(datetime.datetime.now()) + name
+name = datetime.datetime.now().strftime("%Y-%m-%d %Hh%Mm%Ss") + ": " + name
 
 # Code gen stone.yaml
 print("# ", name)
 for i in range(len(stones)):
   print(" - [", stones[i]["xyz"], ", ", stones[i]["normal"], ", ", stones[i]["dim"], ", ", stones[i]["yaw"], "]")
 
-print("\n\n\n")
+print("\n\n")
 
 # Print to create trajectory to track
-t = 0
 t_breaks = []
-# Method 1
-# for i in range(len(stones)):
-#   t_breaks.append(t)
-#   t += (stones[i]["dim"][0] / 2) / stones[i]["speed"]  # second half of the current block
-#   if i < len(stones) - 1:
-#     t += (stones[i+1]["dim"][0] / 2) / stones[i]["speed"]  # first half of the next block
-# Method 2
+t_breaks.append(0.0)
 for i in range(len(stones)):
-  t_breaks.append(t)
+  speed = stones[i]["traj_data"]["speed"]
+  distance = np.linalg.norm(np.array(stones[i]["traj_data"]["end_pos"]) - np.array(stones[i]["traj_data"]["start_pos"]))
+  dt = distance / speed if speed > 0 else 2  # if speed=0, it means we want to stay there for a bit. Here we give it 2 seconds
+  t_breaks.append(t_breaks[-1] + dt)
+assert len(t_breaks) == len(stones) + 1
+
+# unit test dx/dt, start pos and end pos
+for i in range(len(stones)):
+  assert np.isclose(stones[i]["traj_data"]["speed"], (np.linalg.norm(np.array(stones[i]["traj_data"]["end_pos"]) - np.array(stones[i]["traj_data"]["start_pos"]))) / (t_breaks[i+1]-t_breaks[i]))
   if i < len(stones) - 1:
-    distance = np.linalg.norm(np.array(stones[i+1]["xyz"]) - np.array(stones[i]["xyz"]))
-    t += (distance) / ((stones[i+1]["speed"]+stones[i]["speed"])/2) if distance > 0 else 2  # if the way point is the same, it means we want to stay there for a bit. Here we give it 2 seconds
+    assert stones[i]["traj_data"]["end_pos"] == stones[i+1]["traj_data"]["start_pos"]
 
 # Code gen C++
 print("// Traj: ", name)
 for i in range(len(stones)):
-  # print("%.3f: {%.3f, %.3f}%s" % (t_breaks[i], stones[i]["xyz"][0], stones[i]["xyz"][1], ", " if i < len(stones) - 1 else ""))
-  print("// %.3f, dt=%.3f, dx/dt=%.1f: {%.3f, %.3f}%s" % (t_breaks[i],
-                                                       t_breaks[i+1]-t_breaks[i] if i < len(stones) - 1 else -1,
-                                                       np.linalg.norm(np.array(stones[i+1]["xyz"]) - np.array(stones[i]["xyz"]))/(t_breaks[i+1]-t_breaks[i]) if i < len(stones) - 1 else -1,
-                                                       stones[i]["xyz"][0], stones[i]["xyz"][1], ", " if i < len(stones) - 1 else ""))
-print("std::vector<double> breaks = {")
-for i in range(len(stones)):
-  print("%.3f%s" % (t_breaks[i], ", " if i < len(stones) - 1 else ""))
+  print("// %.3f: {%.3f, %.3f}" % (t_breaks[i], stones[i]["traj_data"]["start_pos"][0], stones[i]["traj_data"]["start_pos"][1]), end="")
+  print(";   dt=%.3f, dx/dt=%.1f" % (t_breaks[i+1]-t_breaks[i], stones[i]["traj_data"]["speed"]))
+  if i == len(stones) - 1:
+    print("// %.3f: {%.3f, %.3f}" % (t_breaks[i], stones[i]["traj_data"]["end_pos"][0], stones[i]["traj_data"]["end_pos"][1]))
+print("std::vector<double> breaks = {", end="")
+for i in range(len(t_breaks)):
+  print("%.3f%s" % (t_breaks[i], ", " if i < len(t_breaks) - 1 else ""), end="")
 print("};")
-print("std::vector<std::vector<double>> knots_vec = {")
+print("std::vector<std::vector<double>> knots_vec = {", end="")
+print("{%.3f, %.3f}%s" % (stones[0]["traj_data"]["start_pos"][0], stones[0]["traj_data"]["start_pos"][1], ", "), end="")
 for i in range(len(stones)):
-  print("{%.3f, %.3f}%s" % (stones[i]["xyz"][0], stones[i]["xyz"][1], ", " if i < len(stones) - 1 else ""))
+  print("{%.3f, %.3f}%s" % (stones[i]["traj_data"]["end_pos"][0], stones[i]["traj_data"]["end_pos"][1], ", " if i < len(stones) - 1 else ""), end="")
 print("};")
 
 # Code gen (python) for exit conditions
-print("\n\n\n")
+print("\n\n")
 exit_conditions[0][0] = "start"
 print("# " + name)
 code = "self.terrain_state_list = ["
@@ -206,4 +218,5 @@ print(code)
 
 # print("// Traj: ", name)
 # print(exit_conditions)
+
 

cdc00e6488f3b6323ce4910f762bd527731d0fde
Brian Acosta <bjacosta@seas.upenn.edu>
2023-06-04 23:30:53 -0700

    finish logplayer, still need to test

--- bindings/pydairlib/analysis/log_browser.py
+++ bindings/pydairlib/analysis/log_browser.py
@@ -2,83 +2,132 @@
     PyGame utility to support playing back LCM logs in a more streamlined
     fashion than LcmLogPlayer by using the arrow keys to navigate between logs.
 """
-
+import os
+import lcm
+import glob
 import pygame
 import argparse
 
-# Initialize Pygame
-pygame.init()
+from typing import List
 
-# Set the screen size
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Set the caption
-pygame.display.set_caption("Lcm Log Browser")
+class LcmLogPlayer:
+    def __init__(self, filename: str, channels: List[str]):
+        self.lcm_log = lcm.EventLog(filename, "r")
+        self.channels = channels
+        self.lc = lcm.LCM()
 
-# Set the initial state
-is_playing = False
+    def get_next_message(self):
+        assert self.channels
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--log_folder", nargs=1, default="",
-                    help="Folder containing lcm logs", type=str, required=True)
-parser.add_argument("--log_name_template", nargs=1, default="lcmlog-",
-                    help="Template for lcm log name - all lcmlogs should be "
-                         "named <template>-##, where ## is the log number",
-                    type=str)
-parser.add_argument("--channels", nargs="+", default=[""],
-                    help="List of lcm channels to play back")
+        event = self.lcm_log.read_next_event()
+        while event.channel not in self.channels:
+            event = self.lcm_log.read_next_event()
+        return event.channel, event.data
 
-args = parser.parse_args()
+    def publish_next(self):
+        channel, data = self.get_next_message()
+        # TODO (@Brian-Acosta) does this need to be decoded?
+        self.lc.publish(channel, data)
 
-# Load the font
-font = pygame.font.Font(None, 36)
+    def reset(self):
+        self.lcm_log.seek(0)
 
-# Helper function to play channels from the current stream
-def play_log(logpath):
-    # Add code to play the channels here
 
-# Game loop
-running = True
-current_stream_name = "Stream 1"  # Default current stream
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def get_log_names(logfolder, logtemplate):
+    logpath_template = os.path.join(logfolder, logtemplate)
+    logs = glob.glob(f'{logpath_template}[0-9][0-9]')
+    print("Found logs:")
+    for log in logs:
+        print(log)
+    return logs
 
-        # Check for keyboard events
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                is_playing = not is_playing  # Toggle play/pause state
-                if is_playing:
-                    print("Stream started")
-                    # Add code to start the stream here
-                    play_channels(current_stream_name)
-                else:
-                    print("Stream paused")
-                    # Add code to pause the stream here
 
-            elif event.key == pygame.K_RIGHT:
-                print("Next")
-                # Add code to go to the next stream here
-                # Update current_stream_name to the next stream
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log_folder",
+        nargs=1,
+        default="",
+        help="Folder containing lcm logs",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        "--log_name_template",
+        nargs=1,
+        default="lcmlog-",
+        help="Template for lcm log name - all lcmlogs should be named "
+             "<template>-##, where ## is the log number",
+        type=str
+    )
+    parser.add_argument(
+        "--channels",
+        nargs="+",
+        default=[""],
+        help="List of lcm channels to play back"
+    )
+    args = parser.parse_args()
 
-            elif event.key == pygame.K_LEFT:
-                print("Back")
-                # Add code to go back to the previous stream here
-                # Update current_stream_name to the previous stream
+    # PyGame Setup
+    pygame.init()
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Lcm Log Browser")
+    font = pygame.font.Font(None, 36)
 
-    # Clear the screen
-    screen.fill((0, 0, 0))
+    running = True
+    playing = False
 
-    # Render the current stream name
-    text_surface = font.render(current_stream_name, True, (255, 255, 255))
-    text_rect = text_surface.get_rect(center=(screen_width // 2, screen_height // 2))
-    screen.blit(text_surface, text_rect)
+    assert(os.path.isdir(args.log_folder))
 
-    # Update the display
-    pygame.display.flip()
+    logpaths = get_log_names(args.log_folder, args.log_name_template)
+    active_log_idx = 0
+    nlogs = len(logpaths)
+    logplayers = {}
 
-# Quit Pygame
-pygame.quit()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # Check for keyboard events
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    playing = not playing  # Toggle play/pause state
+
+                elif event.key == pygame.K_RIGHT:
+                    active_log_idx = (active_log_idx + 1) % nlogs
+
+                elif event.key == pygame.K_LEFT:
+                    active_log_idx -= 1
+                    active_log_idx = active_log_idx if active_log_idx >= 0 \
+                        else nlogs - 1
+
+        if active_log_idx not in logplayers:
+            logplayers[active_log_idx] = LcmLogPlayer(
+                logpaths[active_log_idx],
+                args.channels
+            )
+
+        if playing:
+            logplayers[active_log_idx].publish_next()
+
+        # Clear the screen
+        screen.fill((0, 0, 0))
+
+        # Render the current stream name
+        text_surface = font.render(logpaths[active_log_idx], True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(screen_width // 2, screen_height // 2))
+        screen.blit(text_surface, text_rect)
+
+        # Update the display
+        pygame.display.flip()
+
+    # Quit Pygame
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()

984ba9de827ad7e620c0b737b3dfd35d4a316e76
Yu-Ming Chen <yminchen@umich.edu>
2023-06-13 13:03:24 -0400

    update visualization for timing; improve course

--- examples/goldilocks_models/terrains/generate_road_stones.py
+++ examples/goldilocks_models/terrains/generate_road_stones.py
@@ -402,7 +402,8 @@ previous_high_speed = 2 #0.5 / 0.35
 course_name = "Complete course for TRO resubmission"
 current_pose = CreateEndpointBlock(current_pose, 2, 1.2, speed=0.0, center_at_current_pos=True, final_at_center_pos=True, exit_conditions=exit_conditions, stones=stones)
 current_pose = CreateEndpointBlock(current_pose, 2, 1.2, speed=0.5, center_at_current_pos=True, final_at_center_pos=False, exit_conditions=exit_conditions, stones=stones)
-current_pose = CreateOneBlock(current_pose, 6, speed=2, exit_conditions=exit_conditions, stones=stones)
+current_pose = CreateOneBlock(current_pose, 1, speed=2, exit_conditions=exit_conditions, stones=stones)
+current_pose = CreateOneBlock(current_pose, 5, speed=2, exit_conditions=exit_conditions, stones=stones)
 current_pose = CreateOneBlock(current_pose, 2.5, speed=previous_high_speed, exit_conditions=exit_conditions, stones=stones)
 ### fast 90 turn
 current_pose = CreateBlocksForTurning(current_pose, 1, np.pi/2, n_segment=10, speed=previous_high_speed, both_start_and_end_at_block_center=True, exit_conditions=exit_conditions, stones=stones)
@@ -411,8 +412,8 @@ current_pose = CreateOneBlock(current_pose, 2.5, speed=previous_high_speed, exit
 # current_pose = CreateOneBlock(current_pose, 2.5, speed=previous_high_speed, exit_conditions=exit_conditions, stones=stones)
 # current_pose = CreateOneBlock(current_pose, 2.5, speed=previous_high_speed, exit_conditions=exit_conditions, stones=stones)
 current_pose = CreateBlocksForTurning(current_pose, 2, np.pi, n_segment=20, speed=previous_high_speed, both_start_and_end_at_block_center=True, exit_conditions=exit_conditions, stones=stones)
-current_pose = CreateOneBlock(current_pose, 2, speed=1.0, exit_conditions=exit_conditions, stones=stones)
-current_pose = CreateOneBlock(current_pose, 2, speed=1.0, exit_conditions=exit_conditions, stones=stones)
+current_pose = CreateOneBlock(current_pose, 2, speed=previous_high_speed, exit_conditions=exit_conditions, stones=stones)
+current_pose = CreateOneBlock(current_pose, 2, speed=previous_high_speed, exit_conditions=exit_conditions, stones=stones)
 ### 20% ramp down
 # current_pose = CreateRamp(current_pose, -0.2, 2.5, speed=0.5, exit_conditions=exit_conditions, stones=stones)
 current_pose = CreateRamp(current_pose, -0.2, 10, speed=previous_high_speed, exit_conditions=exit_conditions, stones=stones)

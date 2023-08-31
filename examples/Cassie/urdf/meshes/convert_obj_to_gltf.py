da89cf014c35f52b87e7e3eff544d9963e47d489
Brian Acosta <bjacosta@seas.upenn.edu>
2023-08-31 10:40:37 -0400

    add normals to objs and remove gltf

--- examples/Cassie/urdf/meshes/convert_obj_to_gltf.py
+++ examples/Cassie/urdf/meshes/convert_obj_to_gltf.py
@@ -7,16 +7,14 @@
 import bpy
 import glob
 
-fnames = glob.glob('agility/*.obj')
+fnames = glob.glob('*.obj') + glob.glob('agility/*.obj')
 
-objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH')]
+objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH', 'LIGHT')]
 bpy.ops.object.delete({"selected_objects": objs})
 
 for fname in fnames:
-    savename = fname.replace('obj', 'glb')
     bpy.ops.import_scene.obj(filepath=fname)
-    bpy.ops.export_scene.gltf(filepath=savename,
-                              export_format='GLTF_EMBEDDED')
+    bpy.ops.export_scene.obj(filepath=fname, use_normals=True)
 
     objs = [ob for ob in bpy.context.scene.objects if ob.type in ('MESH')]
     bpy.ops.object.delete({"selected_objects": objs})

4d6d0109f879d836d3349843b41e5026ab852a5e
Brian Acosta <bjacosta@seas.upenn.edu>
2023-09-05 14:09:49 -0400

    parse snopt output

---
+++ solvers/parse_snopt_outfile.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def main():
+    filename = sys.argv[1]
+    with open(filename, 'r') as fp:
+        lines = fp.readlines()
+    lines_exit_status = [line.strip() for line in lines if
+                         "SNOPTA EXIT" in line]
+    lines_exit_info = [line.strip() for line in lines if "SNOPTA INFO" in line]
+
+    for i, line in enumerate(lines_exit_status):
+        if int(line.split()[2]) != 0:
+            print(f'solution {i} failed, {line}, {lines_exit_info[i]}')
+
+
+if __name__ == "__main__":
+    main()

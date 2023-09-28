5ecd3dbe41a0c88155eb8f6b75de81e148ef83d1
Brian Acosta <bjacosta@seas.upenn.edu>
2023-09-28 10:40:32 -0400

    bind simulator

---
+++ bindings/pydairlib/perceptive_locomotion/simulators_py.cc
@@ -0,0 +1,41 @@
+#include <pybind11/eigen.h>
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "examples/perceptive_locomotion/diagrams/hiking_sim_diagram"
+
+namespace dairlib{
+namespace pydairlib {
+
+using perceptive_locomotion::HikingSimDiagram;
+
+PYBIND11_MODULE(simulators, m) {
+  m.doc() = "Bindings for perceptive locomotion simulators";
+  using py_rvp = py::return_value_policy;
+
+  py::class_<HikingSimDiagram>(
+      m, "HikingSimDiagram")
+      .def(py::init<const std::string&, const std::string&>(),
+           py::arg("tarrain_yaml"), py::arg("camera_pose_yaml"))
+    .def("get_input_port_actuation",
+         &HikingSimDiagram::get_input_port_actuation,
+         py_rvp::reference_internal)
+    .def("get_input_port_radio",
+         &HikingSimDiagram::get_input_port_radio,
+         py_rvp::reference_internal)
+    .def("get_output_port_state",
+         &HikingSimDiagram::get_output_port_state,
+         py_rvp::reference_internal)
+    .def("get_output_port_cassie_out",
+         &HikingSimDiagram::get_output_port_cassie_out,
+         py_rvp::reference_internal)
+    .def("get_output_port_lcm_radio",
+         &HikingSimDiagram::get_output_port_lcm_radio,
+         py_rvp::reference_internal)
+    .def("get_plant", &HikingSimDiagram::get_plant(), py_rvp__Reference_internal);
+}
+
+
+}
+}

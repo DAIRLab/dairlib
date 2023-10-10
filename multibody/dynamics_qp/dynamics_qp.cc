e3df8c6decd83f6c6bbfd469314524cc004981e5
Brian Acosta <bjacosta@seas.upenn.edu>
2023-10-10 16:40:52 -0400

    decent progress on dynamics qp

--- multibody/dynamics_qp/dynamics_qp.cc
+++ multibody/dynamics_qp/dynamics_qp.cc
@@ -1,5 +1,104 @@
-//
-// Created by brian on 10/6/23.
-//
-
 #include "dynamics_qp.h"
+#include "solvers/constraint_factory.h"
+
+namespace dairlib {
+namespace multibody {
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+
+using drake::multibody::JointActuatorIndex;
+using drake::multibody::MultibodyPlant;
+using drake::systems::Context;
+
+// solver
+using drake::solvers::MathematicalProgram;
+using drake::solvers::LinearConstraint;
+using drake::solvers::LinearEqualityConstraint;
+
+DynamicsQP::DynamicsQP(const MultibodyPlant<double> &plant) :
+    nq_(plant.num_positions()), nv_(plant.num_velocities()),
+    nu_(plant.num_velocities()), plant_(plant) {}
+
+void DynamicsQP::Build() {
+  prog_ = std::make_unique<MathematicalProgram>();
+  u_ = prog_->NewContinuousVariables(nu_);
+  dv_ = prog_->NewContinuousVariables(nv_);
+  lambda_c_ = prog_->NewContinuousVariables(nc_);
+  lambda_h_ = prog_->NewContinuousVariables(nh_);
+  epsilon_c_ = prog_->NewContinuousVariables(nc_soft_);
+  MakeDynamicsConstraint();
+  MakeContactConstraints();
+  MakeInputBoundsConstraint();
+}
+
+void DynamicsQP::MakeDynamicsConstraint() {
+  dynamics_constraint_ = prog_->AddLinearEqualityConstraint(
+      MatrixXd::Zero(nv_, nv_ + nc_ + nh_ + nu_),
+      VectorXd::Zero(nv_),
+      {dv_, lambda_c_, lambda_h_, u_}
+  ).evaluator();
+}
+
+void DynamicsQP::MakeContactConstraints() {
+  // Make Contact and Friction Cone Constraints
+  for (auto& entry: contacts_) {
+    // get the constraint struct
+    auto& constraint = entry.second;
+    DRAKE_DEMAND(constraint.evaluator_ != nullptr);
+
+    // dimensions of this constraint
+    int nc_active = constraint.nc_active_;
+    int n_eps = constraint.n_eps_;
+
+    // get the decision variables
+    const auto& lambda_c = lambda_c_.segment<3>(constraint.idx_lambda_start_);
+    const auto& eps_c = epsilon_c_.segment(constraint.idx_eps_start_, n_eps);
+
+    // contact constraint
+    constraint.contact_constraint_ = prog_->AddLinearEqualityConstraint(
+        MatrixXd::Zero(nc_active, nv_ + n_eps),
+        VectorXd::Zero(nc_active),
+        {dv_, eps_c}
+    ).evaluator();
+
+    // friction cone constraint
+    auto friction_cone = solvers::CreateLinearFrictionConstraint(
+        constraint.mu_, 8, 2, true
+    );
+    constraint.friction_cone_ = prog_->AddLinearConstraint(
+        friction_cone->GetDenseA(),
+        friction_cone->lower_bound(),
+        friction_cone->upper_bound(),
+        lambda_c
+    ).evaluator();
+
+    // soft constraint cost
+    if (constraint.soft_constraint_) {
+      constraint.soft_constraint_cost_ = prog_->AddQuadraticCost(
+          constraint.W_soft_constraint_, VectorXd::Zero(n_eps), eps_c, true
+      ).evaluator();
+    }
+  }
+}
+
+void DynamicsQP::MakeKinematicConstraints() {
+  holonomic_constraint_ = prog_->AddLinearEqualityConstraint(
+      MatrixXd::Zero(nh_, nv_), VectorXd::Zero(nv_), dv_
+  ).evaluator();
+}
+
+void DynamicsQP::MakeInputBoundsConstraint() {
+  DRAKE_DEMAND(prog_ != nullptr);
+
+  VectorXd u_min(nu_);
+  VectorXd u_max(nu_);
+  for (JointActuatorIndex i(0); i < nu_; ++i) {
+    u_min[i] = -plant_.get_joint_actuator(i).effort_limit();
+    u_max[i] = plant_.get_joint_actuator(i).effort_limit();
+  }
+  input_bounds_ = prog_->AddBoundingBoxConstraint(u_min, u_max, u_).evaluator();
+}
+
+}
+}
--- multibody/dynamics_qp/dynamics_qp.h
+++ multibody/dynamics_qp/dynamics_qp.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <limits>
 #include "multibody/kinematic/kinematic_evaluator_set.h"
 #include "multibody/kinematic/world_point_evaluator.h"
 #include "solvers/fast_osqp_solver.h"
@@ -15,10 +15,29 @@
 
 namespace dairlib {
 namespace multibody {
+
+using std::map;
+using std::vector;
+using std::unique_ptr;
+using std::shared_ptr;
+
+using drake::systems::Context;
+using drake::solvers::QuadraticCost;
+using drake::solvers::LinearConstraint;
+using drake::solvers::MathematicalProgram;
+using drake::solvers::BoundingBoxConstraint;
+using drake::solvers::VectorXDecisionVariable;
+using drake::solvers::LinearEqualityConstraint;
+
 class DynamicsQP {
  public:
-  DynamicsQP(const drake::multibody::MultibodyPlant<double> &plant,
-             drake::systems::Context<double> *context);
+  DynamicsQP(const drake::multibody::MultibodyPlant<double> &plant);
+
+  /*!
+   * Build the QP by adding all of the costs and constraints to the
+   * MathematicalProgram
+   */
+  void Build();
 
   /*!
    * @brief Add the permanent kinematic constraints defined by evaluators
@@ -26,17 +45,129 @@ class DynamicsQP {
   void AddKinematicConstraint(
       std::unique_ptr<const KinematicEvaluatorSet<double>> evaluators) {
     DRAKE_DEMAND(&evaluators->plant() == &plant_);
-    kinematic_evaluators_ = std::move(evaluators);
+    kinematic_evals_ = std::move(evaluators);
+    nh_ = kinematic_evals_->count_full();
   }
 
+  /*!
+   * @brief Update the lower and upper bounds of the input constraint.
+   *        Set from the URDF by default.
+   * @param lb: input lower bound
+   * @param ub: input upper bound
+   */
+  void UpdateInputLimits(const Eigen::VectorXd &lb, const Eigen::VectorXd ub) {
+    DRAKE_DEMAND(lb.rows() == nu_);
+    DRAKE_DEMAND(ub.rows() == nu_);
+    input_bounds_->set_bounds(lb, ub);
+  }
 
+  /*!
+   * @brief Update all constraints which depend on q and v (kinematic, contact,
+   * and dynamics)
+   */
+  void UpdateStateDependentConstraints(const Context<double>& context,
+                                       vector<std::string>& active_contacts);
+
+  /*!
+   * @brief Update the manipulator equations given the current robot state
+   * (inn context) and active contacts
+   * @param context robot context
+   * @param active_contacts names of the active contexts
+   */
+  void UpdateDynamicsConstraint(const Context<double>& context,
+                                vector<std::string>& active_contacts);
+
+  /*!
+   * @brief Add a contact point to the QP
+   * @param name name of the contact
+   * @param W_soft_constraint wieght of the soft contraint version of this
+   * contact constraint. To enforce a hard constraint, make this a 0x0 matrix
+   */
+  void AddContactPoint(
+      unique_ptr<const WorldPointEvaluator<double>>,
+      const std::string& name,
+      Eigen::MatrixXd W_soft_constraint, double mu);
 
  private:
 
-  const drake::multibody::MultibodyPlant<double> &plant_;
-  drake::systems::Context<double> *context_;
-  std::unique_ptr<const KinematicEvaluatorSet<double>> kinematic_evaluators_;
+  // Specialized struct to make it easier to deal with contact constraints
+  struct ContactConstraint {
+    // Initialized in AddContactPoint
+    unique_ptr<const WorldPointEvaluator<double>> evaluator_ = nullptr;
+    Eigen::MatrixXd W_soft_constraint_;
+    const int idx_lambda_start_;
+    const int idx_eps_start_;
+    double mu_;
+    bool soft_constraint_;
+    int nc_active_;
+    int n_eps_;
 
+    // Initialized in MakeContactConstraints
+    shared_ptr<LinearEqualityConstraint> contact_constraint_ = nullptr;
+    shared_ptr<LinearConstraint> friction_cone_ = nullptr;
+    shared_ptr<QuadraticCost> soft_constraint_cost_ = nullptr;
+
+    void UpdateActive(const Context<double>& context) {
+      int nv = evaluator_->plant().num_velocities();
+      friction_cone_->UpdateLowerBound(
+          Eigen::VectorXd::Zero(friction_cone_->num_constraints())
+      );
+      Eigen::MatrixXd A_c = Eigen::MatrixXd::Zero(nc_active_, nv + n_eps_);
+      A_c.leftCols(nv) = evaluator_->EvalActiveJacobian(context);
+      A_c.rightCols(n_eps_) = Eigen::MatrixXd::Identity(n_eps_, n_eps_);
+      contact_constraint_->UpdateCoefficients(
+          A_c, -evaluator_->EvalActiveJacobianDotTimesV(context)
+      );
+    }
+    void UpdateInactive(const Context<double>& context) {
+      friction_cone_->UpdateLowerBound(Eigen::VectorXd::Constant(
+          friction_cone_->num_constraints(),
+          -std::numeric_limits<double>::infinity()
+      ));
+      contact_constraint_->UpdateCoefficients(
+          Eigen::MatrixXd::Zero(
+              contact_constraint_->num_constraints(),
+              contact_constraint_->num_vars()
+          ),
+          Eigen::VectorXd::Zero(contact_constraint_->num_constraints())
+      );
+    }
+  };
+
+  // Helper functions for Build
+  void MakeDynamicsConstraint();
+  void MakeContactConstraints();
+  void MakeKinematicConstraints();
+  void MakeInputBoundsConstraint();
+
+  // variable counts
+  const int nq_;
+  const int nv_;
+  const int nu_;
+  int nh_ = 0;
+  int nc_ = 0;
+  int nc_active_ = 0;
+  int nc_soft_ = 0;
+
+  // multibody
+  const drake::multibody::MultibodyPlant<double> &plant_;
+  unique_ptr<const KinematicEvaluatorSet<double>> kinematic_evals_ = nullptr;
+  map<std::string, ContactConstraint> contacts_;
+
+  // MathematicalProgram
+  unique_ptr<MathematicalProgram> prog_ = nullptr;
+
+  // Decision variables
+  VectorXDecisionVariable u_;
+  VectorXDecisionVariable dv_;
+  VectorXDecisionVariable lambda_h_;
+  VectorXDecisionVariable lambda_c_;
+  VectorXDecisionVariable epsilon_c_;
+
+  // Constraints
+  shared_ptr<LinearEqualityConstraint> dynamics_constraint_ = nullptr;
+  shared_ptr<LinearEqualityConstraint> holonomic_constraint_ = nullptr;
+  shared_ptr<BoundingBoxConstraint> input_bounds_ = nullptr;
 
 };
 }

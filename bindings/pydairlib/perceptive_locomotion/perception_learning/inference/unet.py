d831848eb43f8b36916212e2d774522ff6c7a4fd
Brian-Acosta <bjacosta@seas.upenn.edu>
2023-10-29 14:56:14 -0400

    add unet and reorganize

---
+++ bindings/pydairlib/perceptive_locomotion/perception_learning/inference/unet.py
@@ -0,0 +1,88 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+import torchinfo
+from torch_utils import get_device
+
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3)
+
+    def forward(self, x):
+        return self.conv2(self.relu(self.conv1(x)))
+
+
+class Encoder(nn.Module):
+    def __init__(self, chs=(3, 64, 128, 256, 512, 1024)):
+        super().__init__()
+        self.enc_blocks = nn.ModuleList([Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)])
+        self.pool = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        ftrs = []
+        for block in self.enc_blocks:
+            x = block(x)
+            ftrs.append(x)
+            x = self.pool(x)
+        return ftrs
+
+
+class Decoder(nn.Module):
+    def __init__(self, chs=(1024, 512, 256, 128, 64)):
+        super().__init__()
+        self.chs = chs
+        self.upconvs = nn.ModuleList([nn.ConvTranspose2d(chs[i], chs[i + 1], 2, 2) for i in range(len(chs) - 1)])
+        self.dec_blocks = nn.ModuleList([Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)])
+
+    def forward(self, x, encoder_features):
+        for i in range(len(self.chs) - 1):
+            x = self.upconvs[i](x)
+            enc_ftrs = self.crop(encoder_features[i], x)
+            x = torch.cat([x, enc_ftrs], dim=1)
+            x = self.dec_blocks[i](x)
+        return x
+
+    def crop(self, enc_ftrs, x):
+        _, _, H, W = x.shape
+        enc_ftrs = torchvision.transforms.CenterCrop([H, W])(enc_ftrs)
+        return enc_ftrs
+
+
+class UNet(nn.Module):
+    def __init__(self, enc_chs=(3, 64, 128, 256, 512), dec_chs=(512, 256, 128, 64), num_class=1,
+                 retain_dim=False, out_sz=(572, 572)):
+        super().__init__()
+        self.encoder = Encoder(enc_chs)
+        self.decoder = Decoder(dec_chs)
+        self.head = nn.Conv2d(dec_chs[-1], num_class, 1)
+        self.retain_dim = retain_dim
+        self.out_sz = out_sz
+
+    def forward(self, x):
+        enc_ftrs = self.encoder(x)
+        out = self.decoder(enc_ftrs[::-1][0], enc_ftrs[::-1][1:])
+        out = self.head(out)
+        if self.retain_dim:
+            out = F.interpolate(out, self.out_sz)
+        return out
+
+
+def main():
+    model = UNet()
+    input_data = torch.randn(1, 3, 256, 256, device = get_device())
+
+    # Use device=torch.device('cuda') for GPU details
+    device = get_device()
+    model = model.to(device)
+    x = model.forward(input_data)
+
+    summary_str = torchinfo.summary(model, input_size=(1, 3, 256, 256))
+    print(summary_str)
+
+if __name__ == '__main__':
+    main()

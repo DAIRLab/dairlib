5af3d43964ee6082aad6091ecb4bc542736fa179
Wei-Cheng Huang Y9000P (Ubuntu 20.04) <rory0306@gmail.com>
2023-11-01 08:43:23 -0400

    wrap data processing as a pytorch dataloader class

--- bindings/pydairlib/perceptive_locomotion/perception_learning/inference/torch_utils.py
+++ bindings/pydairlib/perceptive_locomotion/perception_learning/inference/torch_utils.py
@@ -1,6 +1,8 @@
 import os
+import pdb
 import numpy as np
 import torch
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 from tqdm import tqdm
 
@@ -57,67 +59,49 @@ def data_reader():
     )
     return data
 
+# try writing it as a DataLoader
+class CassieDataset(Dataset):
+    def __init__(self, data_list):
+        self.data_list = data_list
 
-def data_process(data):
-    """
-        :param data: The data from data_loader()
-        :return: Returns the processed data in the form of a tensor of shape (number of data points, 7, 20, 20) and the target tensor of shape (number of data points, 1, 20, 20)
-    """
-    
-    # data is a list of dictionaries by the name arr_0.
-    # Getting list
-    data_list = data['arr_0']
-    
-    # Create tensors of length = number of datapoints in data_list for input data and target data
-    concatenated_data = []
-    ground_truth_residual = []
+    def __len__(self):
+        return len(self.data_list)
 
-    # Iterating over each datapoint in list while displaying a progress bar
+    def __getitem__(self, idx):
+        data_point = self.data_list[idx]
+        input_data, target_data = Cassie_data_process(data_point)
+        return input_data, target_data
 
-    import pdb; pdb.set_trace()
+def Cassie_data_process(data_point):
+    hmap = data_point['hmap']                                                               # Getting the heightmap of size (20,20) for the data point.
+    # Get hmap shape and reshape to (1,hmap.shape[0],hmap.shape[1])
+    hmap = hmap.reshape(1, hmap.shape[0],hmap.shape[1])
+    # print("shape of hmap is: ", hmap.shape)
 
-    for data_point in tqdm(data_list):
-        hmap = data_point['hmap']                                                               # Getting the heightmap of size (20,20) for the data point.
-        # Get hmap shape and reshape to (1,hmap.shape[0],hmap.shape[1])
-        hmap = hmap.reshape(1, hmap.shape[0],hmap.shape[1])
-        # print("shape of hmap is: ", hmap.shape)
+    U = data_point['U']                                                                     # Getting all possible control inputs for the data point.
+    # print("shape of U is: ", U.shape)
 
-        U = data_point['U']                                                                     # Getting all possible control inputs for the data point.
-        # print("shape of U is: ", U.shape)
+    initial_state = data_point['x_k']                                                       # Getting the initial state of the data point.
+    # print("shape of initial_state is: ", initial_state.shape)
 
-        initial_state = data_point['x_k']                                                       # Getting the initial state of the data point.
-        # print("shape of initial_state is: ", initial_state.shape)
+    # Repeat the initial state for all points in hmap to get an array of shape (4,20,20)
+    initial_state = np.broadcast_to(initial_state[:, np.newaxis, np.newaxis], (initial_state.shape[0], hmap.shape[1], hmap.shape[2]))
+    # print("shape of initial_state after tiling is: ", initial_state.shape)
 
-        # Repeat the initial state for all points in hmap to get an array of shape (4,20,20)
-        initial_state = np.broadcast_to(initial_state[:, np.newaxis, np.newaxis], (initial_state.shape[0], hmap.shape[1], hmap.shape[2]))
-        # print("shape of initial_state after tiling is: ", initial_state.shape)                
+    # Converting the data to tensors
+    hmap_tensor = torch.tensor(hmap, dtype=torch.float32)
+    initial_state_tensor = torch.tensor(initial_state, dtype=torch.float32)
+    U_tensor = torch.tensor(U, dtype=torch.float32)
 
-        # Converting the data to tensors
-        hmap_tensor = torch.tensor(hmap, dtype=torch.float32)
-        initial_state_tensor = torch.tensor(initial_state, dtype=torch.float32)
-        U_tensor = torch.tensor(U, dtype=torch.float32)
+    # concatenate the tiled data for a single data point
+    combined_input = torch.cat([hmap_tensor, initial_state_tensor, U_tensor], dim=0)        # Confirm axis. Final shape should be 7,20,20
 
-        # concatenate the tiled data for a single data point
-        combined_input = torch.cat([hmap_tensor, initial_state_tensor, U_tensor], dim=0)        # Confirm axis. Final shape should be 7,20,20
-        
-        # Append the combined input to the list
-        concatenated_data.append(combined_input)
-
-        # Stack the list of concatenated tensors into a single tensor
-        input_data_tensor = torch.stack(concatenated_data, dim=0)
-
-        # ground truth residual data
-        residual = data_point['residual']
-        target = np.zeros((1, hmap.shape[1], hmap.shape[2]), dtype=float)
-        target[:, :, :] = residual
-        target_tensor = torch.tensor(target, dtype=torch.float32)
-        # Append the target tensor to the list
-        ground_truth_residual.append(target_tensor)
-        # Stack the list of target tensors into a single tensor
-        ground_truth_residual_tensor = torch.stack(ground_truth_residual, dim=0)
-
-    return input_data_tensor, ground_truth_residual_tensor
-
+    # ground truth residual data
+    residual = data_point['residual']
+    target = np.zeros((1, hmap.shape[1], hmap.shape[2]), dtype=float)
+    target[:, :, :] = residual
+    target_tensor = torch.tensor(target, dtype=torch.float32)
+    return combined_input, target_tensor
 
 def main() -> None:
     """
@@ -131,9 +115,23 @@ def main() -> None:
     x = torch.ones(1, device=device)
     print(x)
     
+    # data = data_reader()
+    # input_data_tensor, ground_truth_residual = data_process(data)
+    # print("input shape ", input_data_tensor.shape, "target shape ", ground_truth_residual.shape)   # Expected shape is (number of data points, 7, 20, 20)
+
+    # test pytorch Dataloader
     data = data_reader()
-    input_data_tensor, ground_truth_residual = data_process(data)
-    print("input shape ", input_data_tensor.shape, "target shape ", ground_truth_residual.shape)   # Expected shape is (number of data points, 7, 20, 20)
+    data_list = data['arr_0']
+    custom_dataset = CassieDataset(data_list)
+    batch_size = 32  # Set your desired batch size
+    shuffle = True  # Set to True if you want to shuffle the data
+    num_workers = 4  # Number of parallel data loading processes
+    data_loader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    for input_data, target_data in tqdm(data_loader):
+        print(input_data.shape)
+        print(target_data.shape) # target_data is the ground truth residual
+        pdb.set_trace()
 
 
 if __name__ == '__main__':

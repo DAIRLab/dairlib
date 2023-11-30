afe06d4768968728f72fb700e4bee3b649453ff1
Wei-Cheng Huang Y9000P (Ubuntu 20.04) <rory0306@gmail.com>
2023-11-30 18:10:09 -0500

    fix ALIP NN controller bugs, now adding precomputing feature, need more polishing on codes

--- bindings/pydairlib/perceptive_locomotion/perception_learning/alip_nn_lqr.py
+++ bindings/pydairlib/perceptive_locomotion/perception_learning/alip_nn_lqr.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import numpy as np
+import time
 
 from pydairlib.perceptive_locomotion.perception_learning.alip_lqr import (
     AlipFootstepLQR,
@@ -9,7 +10,8 @@ from pydairlib.perceptive_locomotion.perception_learning.alip_lqr import (
 
 from pydairlib.perceptive_locomotion.perception_learning.height_map_server \
 import (
-    HeightMapQueryObject
+    HeightMapQueryObject,
+    HeightMapOptions
 )
 
 from pydrake.common.value import Value
@@ -27,7 +29,7 @@ from pydairlib.perceptive_locomotion.perception_learning.inference.torch_utils \
 perception_learning_base_folder = \
     "bindings/pydairlib/perceptive_locomotion/perception_learning"
 checkpoint_path = os.path.join(
-    perception_learning_base_folder, 'tmp/still-cosmos-93.pth')
+    perception_learning_base_folder, 'tmp/decent-cosmos-105.pth')
 
 
 class AlipFootstepNNLQR(AlipFootstepLQR):
@@ -38,15 +40,36 @@ class AlipFootstepNNLQR(AlipFootstepLQR):
         # controller now has internal network module
         # maybe it is not worth putting things on GPU
         self.device = get_device()
-        # self.residual_unet = UNet()
-        # self.residual_unet.to(self.device)
-        # self.residual_unet.load_state_dict(torch.load(checkpoint_path))
-        # self.residual_unet.eval()
+        self.residual_unet = UNet()
+        self.residual_unet.to(self.device)
+        self.residual_unet.load_state_dict(torch.load(checkpoint_path))
+        self.residual_unet.eval()
 
         self.input_port_indices['height_map'] = self.DeclareAbstractInputPort(
             "height_map_query",
             model_value=Value(HeightMapQueryObject())
         ).get_index()
+
+        # input error cooridinate ue grid precomputation
+        self.map_opts = HeightMapOptions()
+        H = self.map_opts.nx
+        W = self.map_opts.ny
+        uxgrid = np.linspace(-self.map_opts.resolution * H / 2,
+            self.map_opts.resolution * H / 2, H)
+        uygrid = np.linspace(-self.map_opts.resolution * W / 2,
+            self.map_opts.resolution * W / 2,W)
+        UX, UY = np.meshgrid(uxgrid, uygrid)
+        self.ue_grid = np.stack([UX, UY])
+
+        # use double for loop to calculate grid qudratic form
+        self.u_cost_grid = np.zeros((H, W))
+        self.u_next_value_grid = np.zeros((H, W))
+        self.linear_coeff_grid = np.zeros((self.A.shape[0], H, W))
+        for i in range(H):
+            for j in range(W):
+                self.u_cost_grid[i, j] = (self.ue_grid[:,i,j]).T @ self.params.R @ (self.ue_grid[:,i,j])
+                self.u_next_value_grid[i, j] = (self.ue_grid[:,i,j]).T @ self.B.T @ self.S @ self.B @ (self.ue_grid[:,i,j])
+                self.linear_coeff_grid[:, i, j] = 2 * self.A.T @ self.S @ self.B @ (self.ue_grid[:,i,j])
 
     def calculate_optimal_footstep(
             self, context: Context, footstep: BasicVector) -> None:
@@ -76,18 +99,18 @@ class AlipFootstepNNLQR(AlipFootstepLQR):
         )
         _, H, W = hmap.shape
 
-        # put the input space in error coordinates
+        # put the input space in error coordinates, equivalent to self.ue_grid
         hmap[:2] -= np.expand_dims(np.expand_dims(ud, 1), 1)
 
         # use utils to tile the state, input and heightmap
         # combined_input = torch.cat([hmap_input, state, input_space_grid]
-        # here hmap_input = hmap[-1,:,:], input_space_grid = hmap[:2,:,:]
+        # here hmap_input = hmap[-1, :, :], input_space_grid = hmap[:2, :, :]
         # combined_input size: (1, 7, 20, 20) after unsqueeze, additional dimension
         # needed for current implementation
 
         with torch.inference_mode():
             combined_input = tile_and_concatenate_inputs(
-                hmap[-1, :, :], x, hmap[:2, :, :]
+                hmap[-1, :, :], x - xd, hmap[:2, :, :]
             ).to(self.device)
             combined_input = combined_input.unsqueeze(0)
 
@@ -95,24 +118,29 @@ class AlipFootstepNNLQR(AlipFootstepLQR):
             # residual_grid size: (1, 20, 20) after squeeze
             # transform to numpy and reduce the additional dimension
             # still, maybe not put things onto GPU
-            residual_grid = np.zeros((30,30))
-            #
-            # residual_grid = self.residual_unet(combined_input).squeeze(0)
-            # residual_grid = residual_grid.detach().cpu().numpy().reshape(H,W)
+
+            residual_grid = self.residual_unet(combined_input).squeeze(0)
+            residual_grid = residual_grid.detach().cpu().numpy().reshape(H,W)
 
         # calculate the LQR Q function grid
         # Q value = current cost + next_value_function
         # instead of calling current function and use for loop, might be better to do in batch?
-        cost_grid = np.zeros((H, W))
-        next_value_grid = np.zeros((H, W))
-        for i in range(H):
-            for j in range(W):
-                cost_grid[i, j] = (xd - x).T @ self.params.Q @ (xd - x) + \
-                                 (hmap[:2,i,j] - ud).T @ self.params.R @ (hmap[:2,i,j] - ud)
-                next_value_grid[i,j] = self.get_next_value_estimate(x,hmap[:2,i,j], xd, ud)
+        # note that now hmap[:2, i, j] is error corrdinate ue since we subtract ud before
 
+        linear_term_grid_loop = np.zeros((H,W))
+        linear_term_grid_batch = np.zeros((H,W))
+
+        # # double for loop method for calculating linear term grid
+        # for i in range(H):
+        #     for j in range(W):
+        #         linear_term_grid_loop[i, j] = (x-xd).T @ self.linear_coeff_grid[:, i, j]
+
+        # tensor algebra (batch operation) for calculating linear term grid
+        linear_term_grid_batch = np.einsum('n,nhw->hw', (x - xd), self.linear_coeff_grid)
+
+        linear_term_grid = linear_term_grid_batch
         # sum up the grid values, add select the minimum value index
-        final_grid = cost_grid + next_value_grid + residual_grid
+        final_grid = self.u_cost_grid + self.u_next_value_grid + linear_term_grid + residual_grid
         footstep_i, footstep_j = np.unravel_index(
             np.argmin(final_grid), final_grid.shape
         )

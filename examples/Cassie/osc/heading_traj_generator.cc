e5f4f11aaa222de19577d85a3152bfa1b3572187
William Yang <yangwill@seas.upenn.edu>
2023-12-13 14:03:58 -0500

    fixing cassie stuff with rot space changes

--- examples/Cassie/osc/heading_traj_generator.cc
+++ examples/Cassie/osc/heading_traj_generator.cc
@@ -3,6 +3,7 @@
 #include <math.h>
 
 #include <string>
+#include <drake/common/trajectories/piecewise_quaternion.h>
 
 #include "multibody/multibody_utils.h"
 
@@ -21,6 +22,7 @@ using drake::systems::Context;
 using drake::systems::LeafSystem;
 
 using drake::trajectories::PiecewisePolynomial;
+using drake::trajectories::PiecewiseQuaternionSlerp;
 
 namespace dairlib {
 namespace cassie {
@@ -43,8 +45,8 @@ HeadingTrajGenerator::HeadingTrajGenerator(
       this->DeclareVectorInputPort("pelvis_yaw", BasicVector<double>(1))
           .get_index();
   // Provide an instance to allocate the memory first (for the output)
-  PiecewisePolynomial<double> pp(VectorXd(0));
-  drake::trajectories::Trajectory<double>& traj_inst = pp;
+  PiecewiseQuaternionSlerp<double> empty_slerp_traj;
+  drake::trajectories::Trajectory<double>& traj_inst = empty_slerp_traj;
   this->DeclareAbstractOutputPort("pelvis_quat", traj_inst,
                                   &HeadingTrajGenerator::CalcHeadingTraj);
 }
@@ -64,33 +66,13 @@ void HeadingTrajGenerator::CalcHeadingTraj(
 
   multibody::SetPositionsIfNew<double>(plant_, q, context_);
 
-  // Get approximated heading angle of pelvis
-  Vector3d pelvis_heading_vec =
-      plant_.EvalBodyPoseInWorld(*context_, pelvis_).rotation().col(0);
-  double approx_pelvis_yaw_i =
-      atan2(pelvis_heading_vec(1), pelvis_heading_vec(0));
-
-  // Get quaternion from body rotation matrix instead of state directly, becasue
-  // this is how osc tracking data get the quaternion.
   Quaterniond quat(
       plant_.EvalBodyPoseInWorld(*context_, pelvis_).rotation().matrix());
   quat.normalize();
 
-  // Construct the PiecewisePolynomial.
-  /// Given yaw position p_i and velocity v_i, we want to generate affine
-  /// functions, p_i + v_i*t, for the desired trajectory. We use
-  /// FirstOrderHold() to approximately generate the function, so we need to
-  /// have the endpoint of the trajectory. We generate the endpoint by
-  /// looking ahead what the position is in dt second with fixed velocity v_i.
-  /// Note that we construct trajectories in R^4 (quaternion space), so we need
-  /// to transform the yaw trajectory into quaternion representation.
-  /// The value of dt changes the trajectory time horizon. As long as it's
-  /// larger than the recompute time, the value doesn't affect the control
-  /// outcome.
+
   double dt = 0.1;
   double des_delta_yaw = des_yaw_vel(0) * dt;
-  // We set pitch and roll = 0, because we also use this traj for balance in
-  // some controller
   Eigen::Vector4d pelvis_rotation_i;
   pelvis_rotation_i << quat.w(), 0, 0, quat.z();
   pelvis_rotation_i.normalize();
@@ -100,19 +82,15 @@ void HeadingTrajGenerator::CalcHeadingTraj(
   Quaterniond relative_quat(cos(des_delta_yaw / 2), 0, 0,
                             sin(des_delta_yaw / 2));
   Quaterniond final_quat = relative_quat * init_quat;
-  Eigen::Vector4d pelvis_rotation_f;
-  pelvis_rotation_f << final_quat.w(), final_quat.vec();
 
   const std::vector<double> breaks = {context.get_time(),
                                       context.get_time() + dt};
-  std::vector<MatrixXd> knots(breaks.size(), MatrixXd::Zero(4, 1));
-  knots[0] = pelvis_rotation_i;
-  knots[1] = pelvis_rotation_f;
-  const auto pp = PiecewisePolynomial<double>::FirstOrderHold(breaks, knots);
+  const auto pp = drake::trajectories::PiecewiseQuaternionSlerp<double>(
+      breaks, {quat, final_quat});
 
   // Assign traj
   auto* pp_traj =
-      (PiecewisePolynomial<double>*)dynamic_cast<PiecewisePolynomial<double>*>(
+      (PiecewiseQuaternionSlerp<double>*)dynamic_cast<PiecewiseQuaternionSlerp<double>*>(
           traj);
   *pp_traj = pp;
 }
--- systems/controllers/osc/operational_space_control.cc
+++ systems/controllers/osc/operational_space_control.cc
@@ -1086,9 +1086,10 @@ void OperationalSpaceControl::AssignOscLcmOutput(
   qp_output.v_dim = n_v_;
   qp_output.epsilon_dim = n_c_active_;
   qp_output.u_sol = CopyVectorXdToStdVector(*u_sol_);
-  //  qp_output.lambda_c_sol = CopyVectorXdToStdVector(*lambda_c_sol_);
-  qp_output.lambda_c_sol = CopyVectorXdToStdVector(*lambda_ext_sol_);
-  qp_output.lambda_h_sol = CopyVectorXdToStdVector(force_tracking_data_vec_->at(0)->GetLambdaDes());
+  qp_output.lambda_c_sol = CopyVectorXdToStdVector(*lambda_c_sol_);
+  qp_output.lambda_h_sol = CopyVectorXdToStdVector(*lambda_h_sol_);
+//  qp_output.lambda_c_sol = CopyVectorXdToStdVector(*lambda_ext_sol_);
+//  qp_output.lambda_h_sol = CopyVectorXdToStdVector(force_tracking_data_vec_->at(0)->GetLambdaDes());
   qp_output.dv_sol = CopyVectorXdToStdVector(*dv_sol_);
   qp_output.epsilon_sol = CopyVectorXdToStdVector(*epsilon_sol_);
   output->qp_output = qp_output;

de15c07edea26ec433dd73070d121ee750ed1dfd
Brian-Acosta <bjacosta@seas.upenn.edu>
2023-12-15 15:09:46 -0600

    add bazelisk and grid map bindings

---
+++ bindings/grid_map/__init__.py
@@ -0,0 +1,15 @@
+from .grid_map import *
+
+class GridMap(GridMapBinding):
+    def __setitem__(self, layer, val):
+        self[layer][:] = val
+
+    def __repr__(self):
+        return f'<{self.getLength()[0]:.2f}x{self.getLength()[1]:.2f}x{self.getResolution():.2f} grid on {self.getLayers()} at {self.getPosition()}>'
+
+    @classmethod
+    def from_msg(cls, msg):
+        return from_msg(msg, cls)
+
+    def to_msg(self):
+        return to_msg(self)

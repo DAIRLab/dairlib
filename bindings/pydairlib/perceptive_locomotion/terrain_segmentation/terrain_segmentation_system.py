00c715a16a19ea1014e518ea8c24c760debe7a07
Brian-Acosta <bjacosta@seas.upenn.edu>
2023-12-26 18:13:00 -0500

    animation works

--- bindings/pydairlib/perceptive_locomotion/terrain_segmentation/terrain_segmentation_system.py
+++ bindings/pydairlib/perceptive_locomotion/terrain_segmentation/terrain_segmentation_system.py
@@ -7,6 +7,7 @@ from pydrake.systems.all import (
 )
 
 from grid_map import GridMap
+from scipy.ndimage import sobel
 
 
 class TerrainSegmentationSystem(LeafSystem):
@@ -19,16 +20,25 @@ class TerrainSegmentationSystem(LeafSystem):
         ).get_index()
 
         self.segmentation_state_idx = self.DeclareAbstractState(
-            Value(GridMap())
+            Value(GridMap(["safe_probabilities", "segmentation"]))
         )
+        self.DeclareStateOutputPort("segmentation", self.segmentation_state_idx)
 
         self.DeclareForcedUnrestrictedUpdateEvent(self.UpdateTerrainSegmentation)
 
     def UpdateTerrainSegmentation(self, context: Context, state: State):
         elevation_map = self.get_input_port(self.input_port_grid_map).Eval(context)
+        elevation_map.convertToDefaultStartIndex()
         hmap = elevation_map['elevation']
-        import cv2
-        import numpy as np
-        from scipy.ndimage import sobel, gaussian_filter
-        import pdb; pdb.set_trace()
+
+        segmented = state.get_mutable_abstract_state(self.segmentation_state_idx).get_value()
+
+        if segmented.getSize()[0] == 0:
+            segmented.setGeometry(
+                elevation_map.getLength(),
+                elevation_map.getResolution(),
+                elevation_map.getPosition()
+            )
+
+        segmented['safe_probabilities'][:] = sobel(hmap, axis=0) / elevation_map.getResolution()
 
--- bindings/pydairlib/perceptive_locomotion/terrain_segmentation/terrain_segmentation_test.py
+++ bindings/pydairlib/perceptive_locomotion/terrain_segmentation/terrain_segmentation_test.py
@@ -1,4 +1,7 @@
 import numpy as np
+from copy import deepcopy
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from pydairlib.perceptive_locomotion.terrain_segmentation.\
     terrain_segmentation_system import TerrainSegmentationSystem
@@ -6,13 +9,33 @@ from pydairlib.perceptive_locomotion.terrain_segmentation.\
 
 def main():
     data = np.load('../elevation_maps.npz', allow_pickle=True)
-    terrain = data['elevation_maps']
+    terrain = data['arr_0']
     terrain_segmentation = TerrainSegmentationSystem()
     context = terrain_segmentation.CreateDefaultContext()
 
-    for map in terrain:
-        terrain_segmentation.get_input_port().FixValue(context, map)
+    imdata = np.zeros((terrain[0].getSize()))
+
+    # Create a figure and axis
+    fig, ax = plt.subplots()
+
+    # Display the initial frame using imshow
+    img = ax.imshow(imdata, interpolation='none', aspect='auto', vmin=-1, vmax=1)
+
+    def anim_callback(i):
+        terrain_segmentation.get_input_port().FixValue(context, terrain[i])
         terrain_segmentation.CalcForcedUnrestrictedUpdate(context, context.get_mutable_state())
+        segment = terrain_segmentation.get_output_port().Eval(context)
+        segment = segment['safe_probabilities']
+        img.set_array(segment)
+        return [img]
+
+    animation = FuncAnimation(
+        fig, anim_callback, frames=range(len(terrain)), blit=True, interval=100
+    )
+
+    # animation.save('../test_anim.gif', fps=30)
+    # Show the animation
+    plt.show()
 
 
 if __name__ == '__main__':

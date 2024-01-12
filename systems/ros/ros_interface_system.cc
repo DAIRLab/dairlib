5cf3c4dc1eeb380cf7411cf42b0d84d2deffcb49
Brian Acosta <bjacosta@seas.upenn.edu>
2024-01-12 10:22:42 -0500

    start ros interface system

---
+++ systems/ros/ros_interface_system.cc
@@ -0,0 +1,5 @@
+//
+// Created by brian on 1/12/24.
+//
+
+#include "ros_interface_system.h"
---
+++ systems/ros/ros_interface_system.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "drake/systems/framework/leaf_system.h"
+#include "ros/ros.h"
+
+namespace dairlib {
+namespace systems {
+
+class RosInterfaceSystem : public drake::systems::LeafSystem {
+
+};
+
+}
+}

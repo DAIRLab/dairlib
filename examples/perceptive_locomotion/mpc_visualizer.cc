17990847751e04b52fb1db2dca6e0d7e908b7857
Brian Acosta <bjacosta@seas.upenn.edu>
2024-04-10 10:58:40 -0400

    update mpc debug channel in mpc_visulaizer

--- examples/perceptive_locomotion/mpc_visualizer.cc
+++ examples/perceptive_locomotion/mpc_visualizer.cc
@@ -13,7 +13,7 @@
 namespace dairlib {
 
 DEFINE_string(channel_x, "NETWORK_CASSIE_STATE_DISPATCHER", "State channel");
-DEFINE_string(channel_mpc, "ALIP__S2S_MPFC_DEBUG", "mpc_debug_channel");
+DEFINE_string(channel_mpc, "ALIP_S2S_MPFC_DEBUG", "mpc_debug_channel");
 DEFINE_string(channel_terrain, "", "lcm channel with processed footholds from "
                                    "lcm translator");
 

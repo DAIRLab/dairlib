5d0e9e4163a87264f1e3d9258a23a9044a8ecb15
sharanyashastry <sshastry@seas.upenn.edu>
2024-04-17 15:50:59 -0400

    Made changes to logging script to add the lcm-logger directory explicitly and to make a new folder for every log and store the yaml files associated with each experiment in the same folder.

--- start_logging.py
+++ start_logging.py
@@ -14,11 +14,17 @@ def main(log_type):
     if not os.path.isdir(logdir):
         os.mkdir(logdir)
 
+    osc_gains = dair + "examples/jacktoy/parameters/franka_osc_controller_params.yaml"
+    sim_params = dair + "examples/jacktoy/parameters/franka_sim_params.yaml"
+    c3_gains = dair + "examples/jacktoy/parameters/franka_c3_options_floating.yaml"
+    sampling_params = dair + "examples/jacktoy/parameters/sampling_params.yaml"
+    trajectory_params = dair + "examples/jacktoy/parameters/trajectory_params.yaml"
+
     git_diff = subprocess.check_output(['git', 'diff'], cwd=dair)
     commit_tag = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=dair)
 
     os.chdir(logdir)
-    current_logs = sorted(glob.glob(log_type + 'log-*'))
+    current_logs = sorted(glob.glob('*'))
     try:
         last_log = int(current_logs[-1].split('-')[-1])
         log_num = f'{last_log+1:02}'
@@ -30,8 +36,16 @@ def main(log_type):
             f.write(str(commit_tag))
             f.write("\n\ngit diff:\n\n")
             f.write(codecs.getdecoder("unicode_escape")(git_diff)[0])
+    if not os.path.isdir(log_num):
+        os.mkdir(log_num)
     
+    os.chdir(log_num)
     logname = f'{log_type}log-{log_num}'
+    subprocess.run(['cp', osc_gains, f'osc_gains_{log_num}.yaml'])
+    subprocess.run(['cp', sim_params, f'sim_params_{log_num}.yaml'])
+    subprocess.run(['cp', c3_gains, f'c3_gains_{log_num}.yaml'])
+    subprocess.run(['cp', sampling_params, f'sampling_params_{log_num}.yaml'])
+    subprocess.run(['cp', trajectory_params, f'trajectory_params_{log_num}.yaml'])
     subprocess.run(['/opt/lcm/1.4.0/bin/lcm-logger', '-f', logname])
 
 

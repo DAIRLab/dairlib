66c1bda581d0fd563a91a78335fbd14da9bd449d
Brian-Acosta <bjacosta@seas.upenn.edu>
2024-09-19 13:46:53 -0400

    cleanupn swing foot straj solver

--- systems/controllers/footstep_planning/swing_foot_traj_solver.cc
+++ systems/controllers/footstep_planning/swing_foot_traj_solver.cc
@@ -15,18 +15,10 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using Eigen::RowVectorXd;
 
-
-namespace {
-
-long factorial(long i) {
-  long f = 1;
-  for (int j = 1; j <= i; ++j) {
-    f *= j;
-  }
-  return f;
-}
-
-}
+using polynomials::EvalLegendreBasis;
+using polynomials::EvalLegendreBasisDerivative;
+using polynomials::MakeChangeOfBasisOperatorFromLegendreToMonomials;
+using polynomials::MakeCostMatrixForMinimizingPathDerivativeSquaredWithLegendreBasis;
 
 SwingFootTrajSolver::SwingFootTrajSolver() {
 
@@ -35,7 +27,6 @@ SwingFootTrajSolver::SwingFootTrajSolver() {
   cz_ = prog_.NewContinuousVariables(kPolyDegZ, "cz");
 
   dim_var_map_ = {cx_, cy_, cz_};
-  dim_degree_map_ = {kPolyDegXY, kPolyDegXY, kPolyDegZ};
 
   knot_deriv_multipliers_ = std::vector<std::vector<RowVectorXd>>(
       3, std::vector<RowVectorXd>(3, RowVectorXd::Zero(kPolyDegZ))
@@ -67,8 +58,9 @@ SwingFootTrajSolver::SwingFootTrajSolver() {
 
   }
 
-  min_accel_Q_ = polynomials::MakeCostMatrixForMinimizingPathDerivativeSquaredWithLegendreBasis(
-      kPolyDegZ - 1 , 2);
+  min_acc_Q_ =
+      MakeCostMatrixForMinimizingPathDerivativeSquaredWithLegendreBasis(
+          kPolyDegZ - 1 , 2);
 
   int n = 2 * kPolyDegXY + kPolyDegZ;
   midpoint_target_cost_ = prog_.AddQuadraticCost(
@@ -76,11 +68,25 @@ SwingFootTrajSolver::SwingFootTrajSolver() {
   ).evaluator();
 
   x_min_accel_cost = prog_.AddQuadraticCost(
-      min_accel_Q_.topLeftCorner<kPolyDegXY, kPolyDegXY>(), VectorXd::Zero(kPolyDegXY), cx_, true).evaluator();
+      min_acc_Q_.topLeftCorner<kPolyDegXY, kPolyDegXY>(),
+      VectorXd::Zero(kPolyDegXY),
+      cx_,
+      true
+  ).evaluator();
+
   y_min_accel_cost = prog_.AddQuadraticCost(
-      min_accel_Q_.topLeftCorner<kPolyDegXY, kPolyDegXY>(), VectorXd::Zero(kPolyDegXY),  cy_, true).evaluator();
+      min_acc_Q_.topLeftCorner<kPolyDegXY, kPolyDegXY>(),
+      VectorXd::Zero(kPolyDegXY),
+      cy_,
+      true
+  ).evaluator();
+
   z_min_accel_cost = prog_.AddQuadraticCost(
-      0.01 * min_accel_Q_, VectorXd::Zero(kPolyDegZ), cz_, true).evaluator();
+      0.01 * min_acc_Q_,
+      VectorXd::Zero(kPolyDegZ),
+      cz_,
+      true
+  ).evaluator();
 
   solver_.InitializeSolver(prog_, drake::solvers::SolverOptions(), 0, 0, {});
 
@@ -92,26 +98,24 @@ SwingFootTrajSolver::AdaptSwingFootTraj(
     double prev_time,
     double t_start,
     double t_end,
-    double swing_foot_clearance,
+    double clearance,
     double z_vel_final,
     double z_pos_final_offset,
     const Eigen::Vector3d &initial_pos,
-    const Eigen::Vector3d &footstep_target) {
+    const Eigen::Vector3d &target) {
 
   // we are using the polynomial from the kth control cycle to
   // derive coefficients for the k+1th control cycle
   double tk = -1 + 2 * (prev_time - t_start) / (t_end - t_start);
-
   std::vector<double> knots = {-1, tk, 1};
 
-  double tmid = 0;
-
-  RowVectorXd mid_mult = polynomials::EvalLegendreBasis(kPolyDegZ - 1, tmid).transpose();
-
   for (int k = 0; k < 3; ++k) {
-    knot_deriv_multipliers_.at(k).at(0) = polynomials::EvalLegendreBasis(kPolyDegZ - 1, knots.at(k)).transpose();
-    knot_deriv_multipliers_.at(k).at(1) = polynomials::EvalLegendreBasisDerivative(kPolyDegZ - 1, 1, knots.at(k)).transpose();
-    knot_deriv_multipliers_.at(k).at(2) = polynomials::EvalLegendreBasisDerivative(kPolyDegZ - 1, 2, knots.at(k)).transpose();
+    knot_deriv_multipliers_.at(k).at(0) =
+        EvalLegendreBasis(kPolyDegZ - 1, knots.at(k)).transpose();
+    knot_deriv_multipliers_.at(k).at(1) =
+        EvalLegendreBasisDerivative(kPolyDegZ - 1, 1, knots.at(k)).transpose();
+    knot_deriv_multipliers_.at(k).at(2) =
+        EvalLegendreBasisDerivative(kPolyDegZ - 1, 2, knots.at(k)).transpose();
   }
 
   for (int i = 0; i < 3; ++i) {
@@ -122,14 +126,10 @@ SwingFootTrajSolver::AdaptSwingFootTraj(
   knot_deriv_rhs_.front().front() = initial_pos;
 
   double z_vel_final_scaled = (t_end - t_start) * (t_end - t_start) * z_vel_final / 4.0;
-  knot_deriv_rhs_.at(2).at(0) = footstep_target + Vector3d(0, 0, z_pos_final_offset);
+  knot_deriv_rhs_.at(2).at(0) = target + Vector3d(0, 0, z_pos_final_offset);
   knot_deriv_rhs_.at(2).at(1) = Vector3d(0, 0, z_vel_final_scaled);
 
-  Vector3d pos_T = footstep_target - initial_pos;
-  double disp_yaw = atan2(pos_T(1), pos_T(0));
-  Vector3d n_planar(cos(disp_yaw - M_PI_2), sin(disp_yaw - M_PI_2), 0);
-  Vector3d n = n_planar.cross(pos_T).normalized();
-  Vector3d des_mid_point = initial_pos + 0.5 * pos_T + swing_foot_clearance * n;
+  Vector3d des_mid_point = CalcDesiredMidpoint(initial_pos, target, clearance);
 
   for (int knot = 1; knot < 3; ++knot) {
     for (int deriv = 0; deriv < 3; ++ deriv) {
@@ -145,6 +145,7 @@ SwingFootTrajSolver::AdaptSwingFootTraj(
     }
   }
 
+  RowVectorXd mid_mult = EvalLegendreBasis(kPolyDegZ - 1, 0).transpose();
   MatrixXd mid_mult_broad = MatrixXd::Zero(3, 2 * kPolyDegXY + kPolyDegZ);
   mid_mult_broad.topLeftCorner<2, 2*kPolyDegXY>() =
       BlockDiagonalRepeat<double>(mid_mult.leftCols<kPolyDegXY>(), 2);
@@ -158,8 +159,16 @@ SwingFootTrajSolver::AdaptSwingFootTraj(
 
   auto result = solver_.Solve(prog_);
 
+  return ConvertSolutionToTrajectory(result, t_start, t_end);
+}
+
+drake::trajectories::PathParameterizedTrajectory<double>
+SwingFootTrajSolver::ConvertSolutionToTrajectory(
+    const drake::solvers::MathematicalProgramResult& result,
+    double t_start, double t_end) const {
+
   drake::Vector3<Polynomial<double>> polymat;
-  const MatrixXd bchange = polynomials::MakeChangeOfBasisOperatorFromLegendreToMonomials(kPolyDegZ - 1);
+  const MatrixXd bchange = MakeChangeOfBasisOperatorFromLegendreToMonomials(kPolyDegZ - 1);
 
   Polynomial<double> t_shift(Eigen::Vector2d(-1, 1));
 
@@ -171,8 +180,8 @@ SwingFootTrajSolver::AdaptSwingFootTraj(
 
     for (double t = -1; t < 1; t+=.05) {
       double sum = 0;
-      for (int i = 0; i < coeffs.rows(); ++i) {
-        sum += coeffs(i) * pow(t, i);
+      for (int j = 0; j < coeffs.rows(); ++j) {
+        sum += coeffs(j) * pow(t, j);
       }
     }
 
@@ -186,12 +195,20 @@ SwingFootTrajSolver::AdaptSwingFootTraj(
 
   auto time_scaling = PiecewisePolynomial<double>::FirstOrderHold(
       Eigen::Vector2d(t_start, t_end),
-      Eigen::RowVector2d(0, 2) // hack for pp offset
+      Eigen::RowVector2d(0, 2)
   );
 
-  PathParameterizedTrajectory<double> path(pp, time_scaling);
+  return PathParameterizedTrajectory<double>(pp, time_scaling);
+}
 
-  return path;
+Eigen::Vector3d SwingFootTrajSolver::CalcDesiredMidpoint(
+    const Vector3d &initial_pos, const Vector3d &target, double clearance) {
+  Vector3d pos_T = target - initial_pos;
+  double disp_yaw = atan2(pos_T(1), pos_T(0));
+  Vector3d n_planar(cos(disp_yaw - M_PI_2), sin(disp_yaw - M_PI_2), 0);
+  Vector3d n = n_planar.cross(pos_T).normalized();
+  Vector3d des_mid_point = initial_pos + 0.5 * pos_T + clearance * n;
+  return des_mid_point;
 }
 
 }
--- systems/controllers/footstep_planning/swing_foot_traj_solver.h
+++ systems/controllers/footstep_planning/swing_foot_traj_solver.h
@@ -24,16 +24,28 @@ class SwingFootTrajSolver {
   // position to accommodate stepping up/down
   drake::trajectories::PathParameterizedTrajectory<double> AdaptSwingFootTraj(
       const drake::trajectories::PathParameterizedTrajectory<double>& prev_traj,
-      double prev_time, double t_start, double t_end,
-      double swing_foot_clearance, double z_vel_final, double z_pos_final_offset,
-      const Eigen::Vector3d& initial_pos, const Eigen::Vector3d& footstep_target);
+      double prev_time,
+      double t_start,
+      double t_end,
+      double clearance,
+      double z_vel_final,
+      double z_pos_final_offset,
+      const Eigen::Vector3d& initial_pos,
+      const Eigen::Vector3d& target);
 
  private:
-  const double conditioning_offset_ = 0.4;
+
+  drake::trajectories::PathParameterizedTrajectory<double> ConvertSolutionToTrajectory(
+      const drake::solvers::MathematicalProgramResult& result,
+      double t_start, double t_end) const;
+
+  static Eigen::Vector3d CalcDesiredMidpoint(const Eigen::Vector3d& initial_pos,
+                                      const Eigen::Vector3d& target,
+                                      double clearance);
 
   drake::solvers::MathematicalProgram prog_{};
 
-  Eigen::MatrixXd min_accel_Q_;
+  Eigen::MatrixXd min_acc_Q_;
 
   drake::solvers::VectorXDecisionVariable cx_;
   drake::solvers::VectorXDecisionVariable cy_;
@@ -41,23 +53,21 @@ class SwingFootTrajSolver {
 
   solvers::FCCQPSolver solver_;
 
-  std::vector<int> dim_degree_map_;
   std::vector<drake::solvers::VectorXDecisionVariable> dim_var_map_;
   std::vector<std::vector<Eigen::RowVectorXd>> knot_deriv_multipliers_;
   std::vector<std::vector<Eigen::Vector3d>> knot_deriv_rhs_;
 
   std::vector<std::vector<
-  drake::solvers::LinearEqualityConstraint*>> x_knot_constraints_;
+      drake::solvers::LinearEqualityConstraint*>> x_knot_constraints_;
   std::vector<std::vector<
       drake::solvers::LinearEqualityConstraint*>> y_knot_constraints_;
   std::vector<std::vector<
       drake::solvers::LinearEqualityConstraint*>> z_knot_constraints_;
 
-  std::shared_ptr<drake::solvers::QuadraticCost> midpoint_target_cost_;
   std::shared_ptr<drake::solvers::QuadraticCost> x_min_accel_cost;
   std::shared_ptr<drake::solvers::QuadraticCost> y_min_accel_cost;
   std::shared_ptr<drake::solvers::QuadraticCost> z_min_accel_cost;
-
+  std::shared_ptr<drake::solvers::QuadraticCost> midpoint_target_cost_;
 
 };
 

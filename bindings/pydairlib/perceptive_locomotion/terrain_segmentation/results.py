8d1c8aa6938f6227359900e16f31736e90fb0e9f
Brian-Acosta <bjacosta@seas.upenn.edu>
2024-11-14 22:15:18 -0500

    try some things with histogram - needs work

--- bindings/pydairlib/perceptive_locomotion/terrain_segmentation/results.py
+++ bindings/pydairlib/perceptive_locomotion/terrain_segmentation/results.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.colors import to_rgba
 from dairlib import lcmt_grid_map, lcmt_foothold_set, lcmt_robot_output
 
 from pydrake.systems.all import (
@@ -289,12 +290,18 @@ def plot_segmentation_run_time_results(results, title, savefile):
 
 def plot_iou_results(results, title, savefile):
     fig = plt.figure()
-    plt.title(title)
-    for r in results:
-        plt.plot(r['iou'])
-    plt.legend([r['name'] for r in results])
-    plt.xlabel('Frame Number')
-    plt.ylabel('IOU  with Next Frame')
+    #results = [results[0], results[1], results[0]]
+    linestyles = ['-', '-.', '--']
+    colors = ['blue', 'red', 'green']
+    facecolrs = ['blue', 'orange', 'green']
+    bins = np.histogram(np.hstack([r['iou'] for r in results]), bins=20)[1]
+    plot_ordered_histograms(
+        [r['iou'] for r in results],
+        [r['name'] for r in results],
+        'IoU With Previous Frame',
+        'Number of Frames',
+        title
+    )
     fig.tight_layout()
     if savefile:
         plt.savefig(savefile)
@@ -303,10 +310,10 @@ def plot_iou_results(results, title, savefile):
 def make_plane_segmentation_system_info(params_folder):
     yamls = [
         os.path.join(params_folder, f'{y}.yaml') for y in
-        ['no_ransac_no_preprocessing', 'no_ransac_with_preprocessing', 'ransac_no_preprocessing', 'ransac_and_preprocessing']
+        ['ransac_and_preprocessing', 'no_ransac_with_preprocessing']
     ]
-    configs = ['XX', 'PX', 'XR', 'PR']
-    system_names = ['EM_cupy_' + config for config in configs]
+    configs = ['_PR', '']
+    system_names = ['EM_cupy' + config for config in configs]
     return zip(yamls, system_names)
 
 
@@ -320,7 +327,7 @@ def profile_worker_wrapper(args):
 
 
 def run_segmentation_profiling(logfile):
-    grid_maps, robot_outputs = get_grid_maps_from_log(logfile, duration=80)
+    grid_maps, robot_outputs = get_grid_maps_from_log(logfile, duration=40)
     params_folder = 'bindings/pydairlib/perceptive_locomotion/terrain_segmentation/plane_segmentation_results_params/'
     s3 = TerrainSegmentationSystem(
         {
@@ -339,6 +346,7 @@ def run_segmentation_profiling(logfile):
 
     utils.setup_plots()
 
+    np.savez('../cached_results_for_plot_testing_2.npz', results=results)
     plot_segmentation_run_time_results(results, 'Run Time', None)
     plot_iou_results(results, 'Frame-to-Frame IoU', None)
 
@@ -353,6 +361,70 @@ def run_pipeline_figure_script(logfile):
         grid_maps[example_idx], q, '../terrain_seg_figures')
 
 
+def plot_ordered_histograms(data_list, labels, xlabel, ylabel, title, bins=20, alpha=1.0, colors=None):
+    """
+    Plot multiple histograms with bars ordered by height within each bin.
+
+    Parameters:
+    data_list: List of arrays containing the data for each histogram
+    labels: List of labels for each dataset
+    bins: Number of bins or array of bin edges
+    alpha: Transparency of bars
+    colors: List of colors for each histogram (optional)
+    """
+    # Set default colors if none provided
+    if colors is None:
+        colors = plt.cm.viridis(np.linspace(0, 1, len(data_list)))
+
+    # Calculate bin edges using the combined range of all datasets
+    all_data = np.concatenate(data_list)
+    bin_edges = np.histogram_bin_edges(all_data, bins=bins)
+
+    # Calculate histograms for all datasets
+    hists = []
+    for data in data_list:
+        hist, _ = np.histogram(data, bins=bin_edges)
+        hists.append(hist)
+
+    # Create figure and axis
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Width of bars
+    width = (bin_edges[1] - bin_edges[0])
+
+    # Plot histograms bin by bin
+    for bin_idx in range(len(bin_edges) - 1):
+        # Get heights for this bin from all histograms
+        heights = [hist[bin_idx] for hist in hists]
+        # Sort indices by height
+        sorted_indices = np.argsort(heights)
+        sorted_indices = reversed(sorted_indices)
+
+        # Plot bars in order of height (Highest to lowest)
+        x = bin_edges[bin_idx]
+        for idx in sorted_indices:
+            if heights[idx] > 0:  # Only plot if there's data
+                ax.bar(x, heights[idx], width,
+                       alpha=alpha,
+                       color=colors[idx],
+                       label=labels[idx] if bin_idx == 0 else "")
+
+    # Customize plot
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.legend()
+
+    return fig, ax
+
+def test_iou_plot():
+    utils.setup_plots()
+    data = np.load('../cached_results_for_plot_testing.npz', allow_pickle=True)
+    results = data['results']
+    plot_iou_results(results, 'Frame-to-Frame IoU', None)
+    plt.show()
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('--logfile', type=str)
@@ -360,7 +432,8 @@ def main():
     # run_pipeline_figure_script(args.logfile)
     # run_segmentation_profiling(args.logfile)
     # profile_full_pipeline(args.logfile)
-    write_perception_video(args.logfile)
+    # write_perception_video(args.logfile)
+    test_iou_plot()
 
 
 if __name__ == '__main__':

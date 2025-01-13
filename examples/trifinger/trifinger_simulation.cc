8154812a4a19ace25920f4590b5d898e01e37c47
bbaraki <bbaraki@seas.upenn.edu>
2025-01-13 16:29:28 -0500

    densetact

--- examples/trifinger/trifinger_simulation.cc
+++ examples/trifinger/trifinger_simulation.cc
@@ -19,6 +19,8 @@
 #include "drake/systems/lcm/lcm_interface_system.h"
 #include "drake/systems/lcm/lcm_publisher_system.h"
 
+#include "dairlib/lcmt_contact_force.hpp"
+
 DEFINE_string(sim_parameters,
               "examples/trifinger/parameters/trifinger_sim_params.yaml",
               "Filepath to simulation configs");
@@ -50,11 +52,10 @@ int SimulateTrifinger(int argc, char* argv[]) {
   drake::yaml::LoadYamlOptions yaml_options;
   yaml_options.allow_yaml_with_no_cpp = true;
 
-  auto sim_params =
-      drake::yaml::LoadYamlFile<TrifingerSimParams>(FLAGS_sim_parameters);
+  auto sim_params = drake::yaml::LoadYamlFile<TrifingerSimParams>(FLAGS_sim_parameters);
 
-  auto lcm_channels =
-      drake::yaml::LoadYamlFile<TrifingerLcmChannels>(FLAGS_lcm_channels);
+  auto lcm_channels = drake::yaml::LoadYamlFile<TrifingerLcmChannels>(FLAGS_lcm_channels);
+  
   DiagramBuilder<double> builder;
 
   // Creates default plant and scene graph.
@@ -66,23 +67,21 @@ int SimulateTrifinger(int argc, char* argv[]) {
   multibody::AddFlatTerrain(&plant, &scene_graph, .8, .8, {0, 0, 1}, true);
   Parser parser(&plant, &scene_graph);
 
-  drake::multibody::ModelInstanceIndex trifinger_index =
-      parser.AddModels(FindResourceOrThrow(sim_params.trifinger_model))[0];
-  drake::multibody::ModelInstanceIndex cube_index =
-      parser.AddModels(FindResourceOrThrow(sim_params.cube_model))[0];
+  drake::multibody::ModelInstanceIndex trifinger_index = parser.AddModels(FindResourceOrThrow(sim_params.trifinger_model))[0];
+  
+  drake::multibody::ModelInstanceIndex cube_index = parser.AddModels(FindResourceOrThrow(sim_params.cube_model))[0];
 
   // Fixes the trifinger base to the world frame.
-  plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base_link"),
-                   drake::math::RigidTransform<double>::Identity());
+  plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base_link"), drake::math::RigidTransform<double>::Identity());
 
   // Sets up contact solver.
   if (sim_params.contact_solver == "SAP") {
-    plant.set_discrete_contact_approximation(
-        drake::multibody::DiscreteContactApproximation::kSap);
-  } else if (sim_params.contact_solver == "TAMSI") {
-    plant.set_discrete_contact_approximation(
-        drake::multibody::DiscreteContactApproximation::kTamsi);
-  } else {
+    plant.set_discrete_contact_approximation(drake::multibody::DiscreteContactApproximation::kSap);
+  } 
+  else if (sim_params.contact_solver == "TAMSI") {
+    plant.set_discrete_contact_approximation(drake::multibody::DiscreteContactApproximation::kTamsi);
+  } 
+  else {
     std::cerr << "Unknown contact solver setting." << std::endl;
   }
 
@@ -95,17 +94,31 @@ int SimulateTrifinger(int argc, char* argv[]) {
       lcm_channels.trifinger_state_channel, sim_params.trifinger_publish_rate,
       trifinger_index, sim_params.publish_efforts, sim_params.actuator_delay);
 
-  auto cube_state_sender =
-      builder.AddSystem<systems::ObjectStateSender>(plant, cube_index);
-  auto cube_state_pub =
-      builder.AddSystem(LcmPublisherSystem::Make<dairlib ::lcmt_object_state>(
-          lcm_channels.cube_state_channel, lcm,
-          1.0 / sim_params.cube_publish_rate));
+  auto cube_state_sender = builder.AddSystem<systems::ObjectStateSender>(plant, cube_index);
+  auto cube_state_pub = builder.AddSystem(
+    LcmPublisherSystem::Make<dairlib::lcmt_object_state>(
+      lcm_channels.cube_state_channel, 
+      lcm,
+      1.0 / sim_params.cube_publish_rate));
 
+  // Connect 
   builder.Connect(plant.get_state_output_port(cube_index),
                   cube_state_sender->get_input_port_state());
   builder.Connect(cube_state_sender->get_output_port(),
                   cube_state_pub->get_input_port());
+
+  // publisher block
+  auto tri_reaction_pub = builder.AddSystem(
+    LcmPublisherSystem::Make<dairlib::lcmt_contact_force>(
+      lcm_channels.contact_force_channel, 
+      lcm,
+      1.0 / sim_params.trifinger_publish_rate));
+          
+
+  // Connect plant reaction to lcm
+  builder.Connect(plant.get_reaction_forces_output_port(), 
+    tri_reaction_pub->get_input_port());
+
 
   // Builds diagram.
   auto diagram = builder.Build();
@@ -113,12 +126,12 @@ int SimulateTrifinger(int argc, char* argv[]) {
   DrawAndSaveDiagramGraph(*diagram);
 
   // Create a context for this system:
-  std::unique_ptr<Context<double>> diagram_context =
-      diagram->CreateDefaultContext();
+  std::unique_ptr<Context<double>> diagram_context = diagram->CreateDefaultContext();
+
   diagram_context->EnableCaching();
   diagram->SetDefaultContext(diagram_context.get());
-  Context<double>& plant_context =
-      diagram->GetMutableSubsystemContext(plant, diagram_context.get());
+
+  Context<double>& plant_context = diagram->GetMutableSubsystemContext(plant, diagram_context.get());
 
   // Set initial conditions of the simulation
   VectorXd q_init(plant.num_positions());
@@ -147,8 +160,7 @@ Eigen::VectorXd GenerateInitialCubePose() {
   cube_pos << dis(gen) * 0.005, dis(gen) * 0.005, 0.0325;
   auto cube_quaternion = Eigen::Quaternion<double>(
       Eigen::AngleAxis<double>(dis(gen) * 0.05, Vector3d::UnitZ()));
-  cube_pose << cube_quaternion.w(), cube_quaternion.x(), cube_quaternion.y(),
-      cube_quaternion.z(), cube_pos;
+      cube_pose << cube_quaternion.w(), cube_quaternion.x(), cube_quaternion.y(), cube_quaternion.z(), cube_pos;
   return cube_pose;
 }
 

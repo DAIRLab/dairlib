d19b37632036423fd71dfd9c1998e3a5df918a9f
Brian-Acosta <bjacosta@seas.upenn.edu>
2025-02-03 14:30:39 -0500

    add relative position cost

--- systems/controllers/id_mpc/costs/relative_position_cost.h
+++ systems/controllers/id_mpc/costs/relative_position_cost.h
@@ -1,12 +1,44 @@
-//
-// Created by brian on 2/3/25.
-//
+#pragma once
+#include "nonlinear_least_squares_cost.h"
+#include "drake/multibody/plant/multibody_plant.h"
 
-#ifndef DAIRLIB_SYSTEMS_CONTROLLERS_ID_MPC_COSTS_RELATIVE_POSITION_COST_H_
-#define DAIRLIB_SYSTEMS_CONTROLLERS_ID_MPC_COSTS_RELATIVE_POSITION_COST_H_
+namespace dairlib::systems::controllers::id_mpc {
 
-class relative_position_cost {
+class RelativePositionCost : public NonlinearLeastSquaresCost<double> {
+ public:
+  RelativePositionCost(const Eigen::MatrixXd& Q, const Eigen::Vector3d& yref,
+                       const drake::multibody::MultibodyPlant<double>& plant_,
+                       const std::string& from_frame,
+                       const std::string& to_frame,
+                       const Eigen::Vector3d& from_point,
+                       const Eigen::Vector3d& to_point,
+                       const std::string& description="");
+
+
+  void EvaluateInnerTerm(const Eigen::Ref<const drake::AutoDiffVecXd> &x,
+                         drake::AutoDiffVecXd *y) const override;
+
+  void EvaluateInnerTerm(const Eigen::Ref<const Eigen::VectorXd> &x,
+                         Eigen::VectorXd *y) const override;
+
+  void UpdateReference(const Eigen::VectorXd& y) override {
+    DRAKE_ASSERT(y.rows() == 3);
+    y_ = y;
+  }
+
+ private:
+
+  const drake::multibody::MultibodyPlant<double>& plant_;
+  std::unique_ptr<drake::systems::Context<double>> context_;
+
+  const drake::multibody::Frame<double>& frame_from_;
+  const drake::multibody::Frame<double>& frame_to_;
+  const Eigen::Vector3d& point_from_;
+  const Eigen::Vector3d& point_to_;
+
+  Eigen::Vector3d y_;
+
 
 };
 
-#endif //DAIRLIB_SYSTEMS_CONTROLLERS_ID_MPC_COSTS_RELATIVE_POSITION_COST_H_
+}

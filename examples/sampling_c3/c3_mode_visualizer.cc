5bf59cb310156a8a1e05dfab0c3de1bc98780706
sharanyashastry <sshastry@seas.upenn.edu>
2025-05-28 13:26:57 -0400

    Cleaning up mode visualizer system - removing duplicated lines of code.

--- examples/sampling_c3/c3_mode_visualizer.cc
+++ examples/sampling_c3/c3_mode_visualizer.cc
@@ -1,8 +1,10 @@
-/** This system is used in the visualizer diagram. It subscribes to the lcm message containing information about whether
- * we are in c3 mode or not. it also subscribes to the current lcs state and outputs a trajectory for visualization.
- * This trajectory is a single 3d point at the current location of the end effector if we are in C3 mode. It is a single
- * point at the origin if we are not in C3 mode. The trajectory is output as a lcmt_timestamped_saved_traj object.
- */
+/** This system is used in the visualizer diagram. It subscribes to the 
+ * lcm message containing information about whether we are in c3 mode or not. 
+ * It also subscribes to the current lcs state and outputs a trajectory for 
+ * visualization. This trajectory is a single 3d point at the current location 
+ * of the end effector if we are in C3 mode. It is a single point at the origin 
+ * if we are not in C3 mode. The trajectory is output as a 
+ * lcmt_timestamped_saved_traj object. */
 
 #include "dairlib/lcmt_saved_traj.hpp"
 #include "dairlib/lcmt_timestamped_saved_traj.hpp"
@@ -24,17 +26,14 @@ namespace systems {
 C3ModeVisualizer::C3ModeVisualizer() {
   this->set_name("C3ModeVisualizer");
 
-  is_c3_mode_input_port_ = 
-      this->DeclareAbstractInputPort(
-              "lcmt_timestamped_saved_traj: is_c3_mode_input", 
-              drake::Value<dairlib::lcmt_timestamped_saved_traj>{})
-            .get_index();
+  is_c3_mode_input_port_ = this->DeclareAbstractInputPort(
+    "lcmt_timestamped_saved_traj: is_c3_mode_input", 
+    drake::Value<dairlib::lcmt_timestamped_saved_traj>{}).get_index();
   
-  // TODO: Figure out how to read state vector size here. Might have to be an 
-  // input like in C3StateSender.
-  curr_lcs_state_ = this->DeclareVectorInputPort("curr_lcs_state",
-                                              TimestampedVector<double>(19))
-                    .get_index();
+  // 19 is the hardcoded size of the current lcs state vector. Alternatively,
+  // pass in the plant and read the size from there.
+  curr_lcs_state_ = this->DeclareVectorInputPort(
+    "curr_lcs_state",TimestampedVector<double>(19)).get_index();
 
   // Execution trajectory output ports.
   c3_mode_visualization_traj_port_ = this->DeclareAbstractOutputPort(
@@ -44,30 +43,19 @@ C3ModeVisualizer::C3ModeVisualizer() {
 
 void C3ModeVisualizer::OutputC3ModeVisualization(
         const drake::systems::Context<double>& context,
-        dairlib::lcmt_timestamped_saved_traj* c3_mode_visualization_traj) const {
-  // Check if it's too early to output.
+        dairlib::lcmt_timestamped_saved_traj* c3_mode_visualization_traj) 
+        const {
+  
+  // Initialize knots to be a 3x2 matrix of zeros.
+  Eigen::MatrixXd knots = Eigen::MatrixXd::Zero(3, 2);
+
+  // Check if it's too early to output. That is, if the input port has not
+  // received any data yet for when the visualizer is started before the
+  // controller is running.
   if (this->EvalInputValue<dairlib::lcmt_timestamped_saved_traj>(
           context, is_c3_mode_input_port_) ->utime < 1e-3) {
-    // If it's too early, output a single point at 0,0,3.
-    Eigen::MatrixXd knots = Eigen::MatrixXd::Zero(3, 2);
-    knots.col(0) << 0, 0, 3;
-    knots.col(1) << 0, 0, 3;
-    Eigen::VectorXd timestamp = Eigen::VectorXd::Zero(2);
-    timestamp(0) = context.get_time();
-    timestamp(1) = context.get_time() + 1e-3;
-
-    LcmTrajectory::Trajectory c3_mode;
-    c3_mode.traj_name = "c3_mode_visualization";
-    c3_mode.datatypes = std::vector<std::string>(3, "double");
-    c3_mode.datapoints = knots;
-    c3_mode.time_vector = timestamp.cast<double>();
-
-    LcmTrajectory c3_mode_traj({c3_mode}, {"c3_mode_visualization"},
-                            "c3_mode_visualization",
-                            "c3_mode_visualization", false);
-
-    c3_mode_visualization_traj->saved_traj = c3_mode_traj.GenerateLcmObject();
-    c3_mode_visualization_traj->utime = context.get_time() * 1e6;
+    // Timestamp too early - skipping trajectory update.
+    return;
   }
   else{
     // Evaluate input port to get the sample locations as an 
@@ -87,33 +75,31 @@ void C3ModeVisualizer::OutputC3ModeVisualization(
     // This should be the size of the state vector.
     int n_x = curr_lcs_state->get_data().size();
 
-    Eigen::MatrixXd knots = Eigen::MatrixXd::Zero(3, 2);
-    Eigen::VectorXd timestamp = Eigen::VectorXd::Zero(2);
-
-    // This timestamp corresponds to the trajectory object.
-    timestamp(0) = context.get_time();
-    timestamp(1) = context.get_time() + 1e-3;
-
     if (is_c3_mode(0)) {
       // If we are in C3 mode, we want to visualize the current state.
       knots.col(0) = curr_lcs_state->get_data().head(3);
       knots.col(1) = curr_lcs_state->get_data().head(3);
     }
-
-    LcmTrajectory::Trajectory c3_mode;
-    c3_mode.traj_name = "c3_mode_visualization";
-    c3_mode.datatypes = std::vector<std::string>(3, "double");
-    c3_mode.datapoints = knots;
-    c3_mode.time_vector = timestamp.cast<double>();
-
-    LcmTrajectory c3_mode_traj({c3_mode}, {"c3_mode_visualization"},
-                            "c3_mode_visualization",
-                            "c3_mode_visualization", false);
-
-    // Output as lcmt_timestamped_saved_traj
-    c3_mode_visualization_traj->saved_traj = c3_mode_traj.GenerateLcmObject();
-    c3_mode_visualization_traj->utime = context.get_time() * 1e6;
   }
+
+  Eigen::VectorXd timestamp = Eigen::VectorXd::Zero(2);
+  // This timestamp corresponds to the trajectory object.
+  timestamp(0) = context.get_time();
+  timestamp(1) = context.get_time() + 1e-3;
+  
+  LcmTrajectory::Trajectory c3_mode;
+  c3_mode.traj_name = "c3_mode_visualization";
+  c3_mode.datatypes = std::vector<std::string>(3, "double");
+  c3_mode.datapoints = knots;
+  c3_mode.time_vector = timestamp.cast<double>();
+
+  LcmTrajectory c3_mode_traj({c3_mode}, {"c3_mode_visualization"},
+    "c3_mode_visualization",
+    "c3_mode_visualization", false);
+
+  // Output as lcmt_timestamped_saved_traj
+  c3_mode_visualization_traj->saved_traj = c3_mode_traj.GenerateLcmObject();
+  c3_mode_visualization_traj->utime = context.get_time() * 1e6;
 }
 
 }  // namespace systems

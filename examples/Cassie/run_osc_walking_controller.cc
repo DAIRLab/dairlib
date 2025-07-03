b1aa79a6881eadf5b6aa4901d53fed71f2610fdc
Michael Posa <posa@seas.upenn.edu>
2025-07-03 09:11:29 -0400

    -Update all uses of LcmDrivenLoop to shared_ptr -Update CassieLcmDrivenLoop to shared_ptr

--- examples/Cassie/run_osc_walking_controller.cc
+++ examples/Cassie/run_osc_walking_controller.cc
@@ -46,6 +46,7 @@ using Eigen::Vector3d;
 using Eigen::VectorXd;
 
 using drake::multibody::Frame;
+using drake::systems::Diagram;
 using drake::systems::DiagramBuilder;
 using drake::systems::TriggerType;
 using drake::systems::TriggerTypeSet;
@@ -620,12 +621,13 @@ int DoMain(int argc, char* argv[]) {
 
   // Create the diagram
   auto owned_diagram = builder.Build();
-  owned_diagram->set_name("osc walking controller");
+  std::shared_ptr<Diagram<double>> shared_diagram = std::move(owned_diagram);
+  shared_diagram->set_name("osc walking controller");
 
   // Run lcm-driven simulation
-  DrawAndSaveDiagramGraph(*owned_diagram);
+  DrawAndSaveDiagramGraph(*shared_diagram);
   systems::LcmDrivenLoop<dairlib::lcmt_robot_output> loop(
-      &lcm_local, std::move(owned_diagram), state_receiver, FLAGS_channel_x,
+      &lcm_local, shared_diagram, state_receiver, FLAGS_channel_x,
       true);
   loop.Simulate();
 

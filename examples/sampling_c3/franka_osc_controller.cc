9274c3336ecc37219d1abf7d2d2a469e5aefd372
xuanhien070594 <xuanhien070594@gmail.com>
2025-07-08 15:16:53 -0400

    Revert code formatting

--- examples/sampling_c3/franka_osc_controller.cc
+++ examples/sampling_c3/franka_osc_controller.cc
@@ -4,16 +4,16 @@
 #include <gflags/gflags.h>
 
 #include "common/eigen_utils.h"
+#include "examples/sampling_c3/sampling_c3_utils.h"
+#include "examples/sampling_c3/parameter_headers/sampling_c3_controller_params.h"
 #include "examples/sampling_c3/parameter_headers/lcm_channels.h"
 #include "examples/sampling_c3/parameter_headers/osc_params.h"
-#include "examples/sampling_c3/parameter_headers/sampling_c3_controller_params.h"
-#include "examples/sampling_c3/sampling_c3_utils.h"
-#include "lcm/lcm_trajectory.h"
-#include "multibody/multibody_utils.h"
-#include "systems/controllers/gravity_compensator.h"
 #include "systems/controllers/osc/end_effector_force.h"
 #include "systems/controllers/osc/end_effector_orientation.h"
 #include "systems/controllers/osc/end_effector_position.h"
+#include "lcm/lcm_trajectory.h"
+#include "multibody/multibody_utils.h"
+#include "systems/controllers/gravity_compensator.h"
 #include "systems/controllers/osc/external_force_tracking_data.h"
 #include "systems/controllers/osc/joint_space_tracking_data.h"
 #include "systems/controllers/osc/operational_space_control.h"
@@ -67,18 +67,17 @@ int DoMain(int argc, char* argv[]) {
   drake::lcm::DrakeLcm lcm(FLAGS_lcm_url);
 
   // Load parameters.
-  std::string controller_params_path =
-      "examples/sampling_c3/" + FLAGS_demo_name +
-      "/parameters/sampling_c3_controller_params.yaml";
+  std::string controller_params_path = "examples/sampling_c3/" +
+    FLAGS_demo_name + "/parameters/sampling_c3_controller_params.yaml";
   SamplingC3ControllerParams controller_params =
       drake::yaml::LoadYamlFile<SamplingC3ControllerParams>(
           controller_params_path);
   SamplingC3OSCParams osc_params =
       drake::yaml::LoadYamlFile<SamplingC3OSCParams>(
           controller_params.osc_params_file);
-  std::string lcm_channels_file =
-      FLAGS_is_simulation ? controller_params.lcm_channels_simulation_file
-                          : controller_params.lcm_channels_hardware_file;
+  std::string lcm_channels_file = FLAGS_is_simulation ?
+      controller_params.lcm_channels_simulation_file :
+      controller_params.lcm_channels_hardware_file;
   SamplingC3LcmChannels lcm_channel_params =
       drake::yaml::LoadYamlFile<SamplingC3LcmChannels>(lcm_channels_file);
   drake::solvers::SolverOptions solver_options =
@@ -125,8 +124,8 @@ int DoMain(int argc, char* argv[]) {
           plant, plant_context.get(), osc_params.neutral_position,
           osc_params.teleop_neutral_position, kEndEffectorName);
   end_effector_trajectory->SetRemoteControlParameters(
-      osc_params.neutral_position, osc_params.x_scale, osc_params.y_scale,
-      osc_params.z_scale);
+      osc_params.neutral_position, osc_params.x_scale,
+      osc_params.y_scale, osc_params.z_scale);
   auto end_effector_orientation_trajectory =
       builder.AddSystem<EndEffectorOrientationTrajectoryGenerator>();
   end_effector_orientation_trajectory->SetTrackOrientation(
@@ -150,7 +149,8 @@ int DoMain(int argc, char* argv[]) {
   auto end_effector_position_tracking_data =
       std::make_unique<TransTaskSpaceTrackingData>(
           "end_effector_target", osc_params.K_p_end_effector,
-          osc_params.K_d_end_effector, osc_params.W_end_effector, plant, plant);
+          osc_params.K_d_end_effector, osc_params.W_end_effector,
+          plant, plant);
   end_effector_position_tracking_data->AddPointToTrack(kEndEffectorName);
   const VectorXd& end_effector_acceleration_limits =
       osc_params.end_effector_acceleration * Vector3d::Ones();
@@ -159,7 +159,8 @@ int DoMain(int argc, char* argv[]) {
   auto mid_link_position_tracking_data_for_rel =
       std::make_unique<JointSpaceTrackingData>(
           "panda_joint2_target", osc_params.K_p_mid_link,
-          osc_params.K_d_mid_link, osc_params.W_mid_link, plant, plant);
+          osc_params.K_d_mid_link, osc_params.W_mid_link, plant,
+          plant);
   mid_link_position_tracking_data_for_rel->AddJointToTrack("panda_joint2",
                                                            "panda_joint2dot");
 
@@ -170,9 +171,10 @@ int DoMain(int argc, char* argv[]) {
 
   auto end_effector_orientation_tracking_data =
       std::make_unique<RotTaskSpaceTrackingData>(
-          "end_effector_orientation_target", osc_params.K_p_end_effector_rot,
-          osc_params.K_d_end_effector_rot, osc_params.W_end_effector_rot, plant,
-          plant);
+          "end_effector_orientation_target",
+          osc_params.K_p_end_effector_rot,
+          osc_params.K_d_end_effector_rot,
+          osc_params.W_end_effector_rot, plant, plant);
   end_effector_orientation_tracking_data->AddFrameToTrack(kEndEffectorName);
   Eigen::VectorXd orientation_target = Eigen::VectorXd::Zero(4);
   orientation_target(0) = 1;
@@ -186,14 +188,12 @@ int DoMain(int argc, char* argv[]) {
   osc->AddForceTrackingData(std::move(end_effector_force_tracking_data));
   osc->SetAccelerationCostWeights(osc_params.W_acceleration);
   osc->SetInputCostWeights(osc_params.W_input_regularization);
-  osc->SetInputSmoothingCostWeights(
-      osc_params.W_input_smoothing_regularization);
+  osc->SetInputSmoothingCostWeights(osc_params.W_input_smoothing_regularization);
   if (osc_params.enforce_acceleration_constraints) {
     osc->EnableAccelerationConstraints();
   } else {
     osc->DisableAccelerationConstraints();
   }
-
   osc->SetContactFriction(osc_params.mu);
   osc->SetOsqpSolverOptions(solver_options);
 
@@ -201,8 +201,7 @@ int DoMain(int argc, char* argv[]) {
 
   if (osc_params.cancel_gravity_compensation) {
     if (FLAGS_is_simulation) {
-      std::cerr << "Sim OSC needs cancel_gravity_compensation: false"
-                << std::endl;
+      std::cerr<<"Sim OSC needs cancel_gravity_compensation: false"<<std::endl;
       return -1;
       return -1;
     }
@@ -215,8 +214,7 @@ int DoMain(int argc, char* argv[]) {
                     franka_command_sender->get_input_port());
   } else {
     if (!FLAGS_is_simulation) {
-      std::cerr << "HW OSC needs cancel_gravity_compensation: true"
-                << std::endl;
+      std::cerr<<"HW OSC needs cancel_gravity_compensation: true"<<std::endl;
       return -1;
     }
     builder.Connect(osc->get_output_port_osc_command(),

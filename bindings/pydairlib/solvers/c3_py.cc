44c7152a5acb5aeb9e1597f6ca2ab3a1b8b17d73
Haoran <75158631+PDKy@users.noreply.github.com>
2025-08-21 16:00:51 -0400

    Update c3_py.cc

--- bindings/pydairlib/solvers/c3_py.cc
+++ bindings/pydairlib/solvers/c3_py.cc
@@ -53,10 +53,10 @@ PYBIND11_MODULE(c3, m) {
         py::arg("mu"),
         py::arg("dt"),
         py::arg("N"),
-        py::arg("contact_model"),
         py::arg("n_lambda_with_tangential"),
-        py::arg("direction_counts_per_contact"),
-        py::arg("contact_matrix_index"));
+        py::arg("num_friction_directions_per_contact"),
+        py::arg("starting_index_per_contact_in_lambda_t_vector"),
+        py::arg("contact_model"));
 
   {
     using Enum = dairlib::solvers::ContactModel;

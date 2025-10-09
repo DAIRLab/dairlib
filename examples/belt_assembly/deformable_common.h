31aa11bd288b17a112d96d58c324603e192a6224
xuanhien070594 <xuanhien070594@gmail.com>
2025-10-09 16:40:22 -0400

    Simulate deformable object with Drake

---
+++ examples/belt_assembly/deformable_common.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+#include "drake/math/rigid_transform.h"
+#include "drake/multibody/fem/deformable_body_config.h"
+#include "drake/multibody/plant/deformable_model.h"
+#include "drake/multibody/plant/multibody_plant.h"
+
+namespace dairlib {
+
+/* Adds collision and visual box geometry serving as the world's floor. The
+ floor is placed just below the XY plane and assigned rigid proximity
+ properties. */
+void RegisterRigidGround(drake::multibody::MultibodyPlant<double>* plant);
+
+/* Adds a deformable torus model with the given properties.
+ @param[in,out] model             The deformable model to which the torus will
+                                  be added.
+ @param[in]     model_name        A unique name assigned to the torus instance.
+ @param[in]     X_WB              The initial pose of the torus expressed in the
+                                  world frame.
+ @param[in]     deformable_config Deformable properties of the torus.
+ @param[in]     scale             Scaling factor applied to the loaded torus
+                                  mesh.
+ @param[in]     contact_damping   Damping value assigned to the torus material.
+ */
+drake::multibody::DeformableBodyId RegisterRoundBelt(
+    drake::multibody::DeformableModel<double>* model, const std::string& model_name,
+    const drake::math::RigidTransformd& X_WB,
+    const drake::multibody::fem::DeformableBodyConfig<double>& deformable_config,
+    double scale, double contact_damping);
+
+}  // namespace dairlib
---
+++ examples/belt_assembly/tetrahedralize_stl.py
@@ -0,0 +1,17 @@
+import pyvista
+import tetgen
+
+name = 'round_belt_300'
+
+filename = f'urdf/{name}.stl'
+reader = pyvista.get_reader(filename)
+mesh = reader.read()
+mesh.save(f'{name}.ply', binary=False)
+
+tgen = tetgen.TetGen(f'{name}.ply')
+# Same as this command-line option: tgen.tetrahedralize(switches='qYMkV')
+tgen.tetrahedralize(quality=True, nobisect=True, nomergefacet=True,
+                    nomergevertex=True, vtksurfview=True, vtkview=True,
+                    verbose=True)
+
+tgen.write(f'{name}.vtk', binary=False)
